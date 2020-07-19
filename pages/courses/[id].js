108a121afadbf8d98bd93cee93b1fcbe739f8123
Joel Hooks <joelhooks@gmail.com>
2020-07-18 18:15:53 -0700

    courses are rendered statically

--- pages/courses/[id].js
+++ pages/courses/[id].js
@@ -3,47 +3,52 @@ import Link from 'next/link'
 import ReactPlayer from '../../components/ReactPlayer'
 import get from 'lodash/get'
 import Markdown from 'react-markdown'
+import fetch from 'unfetch'
 import useSWR from 'swr'
 
 const fetcher = (url) => fetch(url).then((r) => r.json())
 
 export default function Course({courseData}) {
-  const {title, instructor, square_cover_256_url, lessons_url} = courseData
-
-  // the endpoint for a series is too slow when it loads lessons
-  // consider a v3?
-  const {data: lessons} = useSWR(lessons_url, fetcher)
-
+  const {
+    slug,
+    path,
+    title,
+    instructor,
+    square_cover_256_url,
+    lessons,
+  } = courseData
   return (
     <div className="">
       <div>by {instructor.full_name}</div>
-      <h1>{title}</h1>
+      <h1>{courseData.title}</h1>
       <img src={square_cover_256_url} />
       <ul className="list-disc">
-        {lessons &&
-          lessons.map((lesson) => {
-            return (
-              <li key={lesson.slug}>
-                <Link href={`/lessons/[id]`} as={lesson.path}>
-                  <a className="no-underline hover:underline text-blue-500">
-                    {lesson.title}
-                  </a>
-                </Link>
-              </li>
-            )
-          })}
+        {lessons.map((lesson) => {
+          return (
+            <li key={lesson.slug}>
+              <Link href={`/lessons/[id]`} as={lesson.path}>
+                <a className="no-underline hover:underline text-blue-500">
+                  {lesson.title}
+                </a>
+              </Link>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
 }
 
-export async function getServerSideProps({params}) {
-  const res = await fetch(`https://egghead.io/api/v1/series/${params.id}`)
-  let courseData = await res.json()
-  courseData = {
-    ...courseData,
-    id: params.id,
+export async function getStaticPaths() {
+  const paths = getAllCourseIds()
+  return {
+    paths,
+    fallback: false,
   }
+}
+
+export async function getStaticProps({params}) {
+  const courseData = getCourseData(params.id)
   return {
     props: {
       courseData,
