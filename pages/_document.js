112b5444b54fd479276e81762511301506f2bd2d
Joel Hooks <joelhooks@gmail.com>
2020-07-26 15:52:08 -0700

    add a favicon

--- pages/_document.js
+++ pages/_document.js
@@ -22,7 +22,12 @@ export default class MyDocument extends Document {
   render() {
     return (
       <html>
-        <Head />
+        <Head>
+          <link
+            rel="icon"
+            href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/apple/237/fisted-hand-sign_1f44a.png"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
