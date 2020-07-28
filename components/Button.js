25b38fc35cf1bf5fe1be41a481d938ee5ecc7e1c
John Lindquist <johnlindquist@gmail.com>
2020-07-28 13:07:31 -0600

    mdx shortcode components working (#5)

---
+++ components/Button.js
@@ -0,0 +1,10 @@
+function Button({children}) {
+    return <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        {children}
+    </button>
+
+
+}
+
+
+export default Button
