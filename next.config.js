7733b28fceec221301d1b66241e3bf1721d56c23
Joel Hooks <joelhooks@gmail.com>
2020-07-28 15:45:27 -0700

    build can break

--- next.config.js
+++ next.config.js
@@ -2,8 +2,5 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 })
 module.exports = withMDX({
-  typscript: {
-    ignoreBuildErrors: true,
-  },
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'tsx'],
 })
