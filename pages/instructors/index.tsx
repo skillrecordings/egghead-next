a4077551ef57f0ebe099a9fb8a1a2cae0c15486d
johnlindquist <johnlindquist@gmail.com>
2020-07-30 13:55:09 -0600

    Basic InstructorCard Styling

--- pages/instructors/index.tsx
+++ pages/instructors/index.tsx
@@ -1,8 +1,29 @@
-import useSWR from 'swr'
 import {loadInstructors} from '../../lib/instructors'
 
+const InstructorCard = ({instructor}) => {
+  return (
+    <div className="flex flex-col items-center">
+      <img
+        className="rounded-full"
+        src={instructor.avatar_url}
+        alt={`Avatar for ${instructor.full_name}`}
+      />
+
+      <p>{instructor.full_name}</p>
+    </div>
+  )
+}
+
 export default function Instructors({instructorsData}) {
-  return <div>{JSON.stringify(instructorsData)}</div>
+  return (
+    <div className="flex flex-wrap">
+      {instructorsData.map((instructor) => (
+        <div key={instructor.id}>
+          <InstructorCard instructor={instructor}></InstructorCard>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export async function getServerSideProps({res, params}) {
