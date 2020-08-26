7d7118fa216e7c1fe73763ea0de432eed9203e7f
Joel Hooks <joelhooks@gmail.com>
2020-08-26 09:50:38 -0700

    update to next 9.4.2

--- src/layouts/ultimate-guide.tsx
+++ src/layouts/ultimate-guide.tsx
@@ -1,33 +1,32 @@
 import React from 'react'
 import {NextSeo} from 'next-seo'
 
-export default function UltimateGuideLayout({
-  title,
-  description,
-  titleAppendSiteName = true,
-  url,
-  ...frontMatter
-}) {
-  return ({children: content}) => {
-    return (
-      <>
-        <NextSeo
-          title={title}
-          description={description}
-          titleTemplate={titleAppendSiteName ? undefined : '%s'}
-          openGraph={{
-            title,
-            description,
-            url,
-            images: frontMatter.ogImage ? [frontMatter.ogImage] : undefined,
-          }}
-          canonical={url}
-        />
-        <div className="prose max-w-none">
-          <h1>{title}</h1>
-          {content}
-        </div>
-      </>
-    )
-  }
+export default function UltimateGuideLayout({children, frontMatter}) {
+  const {
+    title,
+    description,
+    titleAppendSiteName = false,
+    url,
+    ogImage,
+  } = frontMatter
+  return (
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        titleTemplate={titleAppendSiteName ? undefined : '%s'}
+        openGraph={{
+          title,
+          description,
+          url,
+          images: ogImage ? [ogImage] : undefined,
+        }}
+        canonical={url}
+      />
+      <div className="prose max-w-none">
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </>
+  )
 }
