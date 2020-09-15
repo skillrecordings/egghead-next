08cbe6c5f87f6fb0d1b785e705a1c3a07d4fded0
Joel Hooks <joelhooks@gmail.com>
2020-09-15 15:49:20 -0700

    add more discord detail

--- src/pages/discord/callback.tsx
+++ src/pages/discord/callback.tsx
@@ -15,6 +15,7 @@ type CombinedEggheadDiscordUserData = {
   discordUser?: {
     username: string
     discriminator: string
+    email: string
   }
 }
 
@@ -52,39 +53,54 @@ const CallbackPage: FunctionComponent<LoginRequiredParams> = ({
 
   return (
     <LoginRequired loginRequired={loginRequired}>
-      {syncingAccount ? (
-        <>
-          <h1>
-            Currently syncing your egghead account to the egghead Discord
-            server.
-          </h1>
-        </>
-      ) : (
-        <>
-          {userData && (
-            <div className="flex flex-col space-y-3">
-              <h1>
-                Your Discord account{' '}
-                {userData.discordUser &&
-                  `(${userData.discordUser.username}#${userData.discordUser.discriminator})`}{' '}
-                has been updated.
-              </h1>
-              {userData.discordMember.guildId ===
-                process.env.NEXT_PUBLIC_DISCORD_GUILD_ID && (
-                <div>We added you to the egghead Discord!</div>
-              )}
-              {userData.eggheadUser && (
-                <div>
-                  We found your egghead account!{' '}
-                  {userData.eggheadUser.is_pro
-                    ? 'You have a pro membership 🎉'
-                    : `You are not a pro member on ${userData.eggheadUser.email}`}
-                </div>
-              )}
-            </div>
-          )}
-        </>
-      )}
+      <div>
+        {syncingAccount ? (
+          <>
+            <h1>
+              Currently syncing your egghead account to the egghead Discord
+              server. Please wait! ♻️
+            </h1>
+          </>
+        ) : (
+          <>
+            {userData && (
+              <div className="flex flex-col space-y-3">
+                <h1>
+                  Your Discord account{' '}
+                  {userData.discordUser &&
+                    `(${userData.discordUser.username}#${userData.discordUser.discriminator} - ${userData.discordUser.email})`}{' '}
+                  has been updated.
+                </h1>
+                {userData.discordMember.guildId ===
+                  process.env.NEXT_PUBLIC_DISCORD_GUILD_ID && (
+                  <div>We added you to the egghead Discord!</div>
+                )}
+                {userData.eggheadUser && (
+                  <div>
+                    We found your egghead account!{' '}
+                    {userData.eggheadUser.is_pro
+                      ? 'You have a pro membership 🎉'
+                      : `You are not a pro member on ${userData.eggheadUser.email}`}
+                  </div>
+                )}
+              </div>
+            )}
+          </>
+        )}
+        <div className="pt-16">
+          <h2 className="text-2xl font-bold">FAQ</h2>
+          <div>
+            <h3 className="text-lg font-bold">
+              The egghead Discord doesn't show up in my server list?
+            </h3>
+            <p>
+              The authorization flow uses the Discord account currently logged
+              in to the browser. Sometimes this is different than the account
+              logged into the Discord app.
+            </p>
+          </div>
+        </div>
+      </div>
     </LoginRequired>
   )
 }
