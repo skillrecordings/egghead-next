3c4bf57ef501a75483a07cba85b470ffb4b767c5
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-09-23 17:41:08 +0100

    added card component with header element

---
+++ src/components/mdx/HeaderCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = ({header, children}) => {
+  return (
+    <div class="bg-white shadow-lg px-6 py-5 border-b border-gray-300 sm:px-8">
+      <h3 class="font-medium text-gray-900 border-b border-cool-gray-500 pb-4">
+        {header}
+      </h3>
+      <div>{children}</div>
+    </div>
+  )
+}
+
+export default Card
--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -2,12 +2,14 @@ import Course from './course'
 import Link from 'next/link'
 import ResourceLink from './ResourceLink'
 import Card from './Card'
+import HeaderCard from './HeaderCard'
 
 const mdxComponents = {
   Course,
   Link,
   ResourceLink,
   Card,
+  HeaderCard,
 }
 
 export default mdxComponents
