96cb08e40d3f335cb97ab1a4578831a707defc3a
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-09-23 12:06:49 +0100

    mocked up half-made resource link card

--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -1,9 +1,11 @@
 import Course from './course'
 import Link from 'next/link'
+import ResourceLink from './ResourceLink'
 
 const mdxComponents = {
   Course,
   Link,
+  ResourceLink,
 }
 
 export default mdxComponents
