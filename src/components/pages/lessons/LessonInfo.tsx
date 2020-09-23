db4223cf46e0765974ac7f197da6c672b8fa9678
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2020-09-23 14:03:43 +0300

    tweet button

--- src/components/pages/lessons/LessonInfo.tsx
+++ src/components/pages/lessons/LessonInfo.tsx
@@ -48,9 +48,36 @@ const NextUp: FunctionComponent<NextUpProps> = ({data, currentLessonSlug}) => {
   ) : null
 }
 
-const CopyToClipboard: FunctionComponent<{linkToCopy: string}> = ({
-  linkToCopy,
-}) => {
+const TweetLink: FunctionComponent<{
+  source: {
+    title: string
+    slug: string
+  }
+  className?: string
+}> = ({source, className = ''}) => {
+  if (source) {
+    return (
+      <a
+        className={`flex items-center rounded p-2 bg-gray-200 hover:bg-gray-400 transition-colors ease-in-out duration-150 ${className}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        href={`https://twitter.com/intent/tweet/?text=${encodeURIComponent(
+          source.title + ', lesson by @eggheadio',
+        )}&url=${encodeURIComponent(
+          `https://next.egghead.io/lessons/${source.slug}/`,
+        )}`}
+      >
+        <IconTwitter className="w-5 mr-2" />
+        <span>Tweet</span>
+      </a>
+    )
+  }
+  return null
+}
+const CopyToClipboard: FunctionComponent<{
+  linkToCopy: string
+  className?: string
+}> = ({linkToCopy, className = ''}) => {
   if (linkToCopy) {
     const [copied, setCopied] = useState(false)
     const [state, copyToClipboard] = useCopyToClipboard()
@@ -66,7 +93,7 @@ const CopyToClipboard: FunctionComponent<{linkToCopy: string}> = ({
           type="button"
           disabled={copied}
           onClick={() => copyHandler()}
-          className="rounded p-2 flex justify-center items-center bg-gray-200 hover:bg-gray-400 transition duration-150 ease-in-out"
+          className={`rounded p-2 flex justify-center items-center bg-gray-200 hover:bg-gray-400 transition-colors duration-150 ease-in-out ${className}`}
           style={{minWidth: '210px'}}
         >
           {state.error ? (
@@ -236,7 +263,8 @@ const LessonInfo: FunctionComponent<LessonInfo> = ({
         </h4>
         <div className="flex items-center mt-3">
           <div className="flex items-center">
-            <CopyToClipboard linkToCopy={lesson.http_url} />
+            <TweetLink source={lesson} />
+            <CopyToClipboard linkToCopy={lesson.http_url} className="ml-4" />
           </div>
         </div>
       </div>
@@ -338,5 +366,20 @@ const IconLink: FunctionComponent<{className?: string}> = ({
       strokeWidth={2}
       d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
     />
+  </svg>
+)
+
+const IconTwitter: FunctionComponent<{className?: string}> = ({
+  className = '',
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className={className}
+  >
+    <g fill="currentColor">
+      <path fill="none" d="M0 0h24v24H0z"></path>
+      <path d="M22.162 5.656a8.384 8.384 0 0 1-2.402.658A4.196 4.196 0 0 0 21.6 4c-.82.488-1.719.83-2.656 1.015a4.182 4.182 0 0 0-7.126 3.814 11.874 11.874 0 0 1-8.62-4.37 4.168 4.168 0 0 0-.566 2.103c0 1.45.738 2.731 1.86 3.481a4.168 4.168 0 0 1-1.894-.523v.052a4.185 4.185 0 0 0 3.355 4.101 4.21 4.21 0 0 1-1.89.072A4.185 4.185 0 0 0 7.97 16.65a8.394 8.394 0 0 1-6.191 1.732 11.83 11.83 0 0 0 6.41 1.88c7.693 0 11.9-6.373 11.9-11.9 0-.18-.005-.362-.013-.54a8.496 8.496 0 0 0 2.087-2.165z"></path>
+    </g>
   </svg>
 )
