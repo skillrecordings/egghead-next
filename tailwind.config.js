be8378aaa4dfbb0ed6721668d8c3cc225cba8989
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-10-08 11:09:02 +0100

    moved prose type size adjustments to tailwinds config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,16 @@ module.exports = {
       '2xl': '1440px',
       '3xl': '1920px',
     },
+    fontSize: {
+      // Set in Major Third typescale (1.25)
+      base: '1em',
+      lg: '1.25em',
+      xl: '1.563em',
+      '2xl': '1.953em',
+      '3xl': '2.441em',
+      '4xl': '3.052em',
+      '5xl': '3.815em',
+    },
     typography: (theme) => ({
       default: {
         css: {
@@ -25,12 +35,15 @@ module.exports = {
           },
           h1: {
             lineHeight: theme('lineHeight.tight'),
+            fontSize: theme('fontSize.3xl'),
           },
           h2: {
             lineHeight: theme('lineHeight.snug'),
+            fontSize: theme('fontSize.xl'),
           },
           h3: {
             lineHeight: theme('lineHeight.normal'),
+            fontSize: theme('fontSize.lg'),
           },
         },
       },
@@ -38,12 +51,15 @@ module.exports = {
         css: {
           h1: {
             lineHeight: theme('lineHeight.tight'),
+            fontSize: theme('fontSize.4xl'),
           },
           h2: {
             lineHeight: theme('lineHeight.snug'),
+            fontSize: theme('fontSize.2xl'),
           },
           h3: {
             lineHeight: theme('lineHeight.normal'),
+            fontSize: theme('fontSize.xl'),
           },
         },
       },
