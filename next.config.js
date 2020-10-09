793a5a9d7d00506b6c5cb973a6e59b6d15b003de
Joel Hooks <joelhooks@gmail.com>
2020-10-09 07:47:30 -0700

    create customer

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 const withSvgr = require('next-svgr')
 const withBundleAnalyzer = require('@next/bundle-analyzer')
 const withPlugins = require('next-compose-plugins')
+
 const checkEnv = require('@47ng/check-env').default
 const withImages = require('next-images')
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 })
+
 
 checkEnv({
   required: ['NEXT_PUBLIC_DEPLOYMENT_URL'],
--- src/pages/api/stripe/create-customer.ts
+++ src/pages/api/stripe/create-customer.ts
@@ -1,4 +1,5 @@
 import Stripe from 'stripe'
+import axios from 'axios'
 
 if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error('no Stripe secret key found')
@@ -9,22 +10,37 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 })
 
 import {NextApiRequest, NextApiResponse} from 'next'
+import emailIsValid from 'utils/email-is-valid'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    // Create a new Stripe customer object
-    // This doesn't do anything on the egghead side. We **probably** want to capture
-    // this at the contact level and associate it with an existing contact
-    // if possible
-    const customer = await stripe.customers.create({
-      email: req.body.email,
-    })
+    const {email} = req.body
 
-    // update/create a Contact as required with appropriate StripeID
-    // Should this be a StripeCustomer so a given Contact can have
-    // multiple StripeCustomers based on client_id/site?
+    if (!emailIsValid(email)) {
+      res.status(400).end()
+    } else {
+      // Create a new Stripe customer object
+      // This doesn't do anything on the egghead side. We **probably** want to capture
+      // this at the contact level and associate it with an existing contact
+      // if possible
+      const customer = await stripe.customers.create({
+        email: req.body.email,
+      })
 
-    res.status(200).json(customer)
+      const eggheadContact = axios
+        .get(
+          `${process.env.NEXT_PUBLIC_AUTH_DOMAIN}/api/v1/stripe/customer?email=${email}`,
+        )
+        .then(({data}) => data)
+
+      console.log(eggheadContact)
+
+      // update/create a Contact as required with appropriate StripeID
+      // Should this be a StripeCustomer so a given Contact can have
+      // multiple StripeCustomers based on client_id/site?
+
+      res.status(200).json(customer)
+    }
   } else {
     res.statusCode = 404
     res.end()
