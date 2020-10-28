041d98e8f7efefa34d557f74036c691d6846f2f3
Daniel Miller <dealingwith@gmail.com>
2020-10-28 16:26:12 -0500

    updates to tests includes failing test pushing in order to pass to Lucas

--- cypress/integration/lesson_page_spec.js
+++ cypress/integration/lesson_page_spec.js
@@ -1,16 +1,20 @@
 describe('lesson page', () => {
-  it('/lesson loads', () => {
+  it('video element loads', () => {
     cy.visit(
       '/lessons/react-using-react-error-boundaries-to-handle-errors-in-react-components',
     )
-    cy.expect('#bmpui-id-204 > .bmpui-image').to.exist
-
-    // the code tab is there and clickable
-    cy.expect('#tabs--1--tab--0').to.exist
-    cy.get('#tabs--1--tab--1').click().get('#tabs--1--panel--1 > p')
-
-    // another free video page
-    cy.visit('/lessons/react-what-is-jsx')
-    cy.expect('#bmpui-id-204 > .bmpui-image').to.exist
+    // failing
+    cy.get('#bmpui-id-204').should('be.visible')
+  })
+  it('tabs load', () => {
+    cy.visit(
+      '/lessons/react-using-react-error-boundaries-to-handle-errors-in-react-components',
+    )
+    cy.get('#tabs--1--tab--0').should('be.visible')
+    cy.get('#tabs--1--panel--0').should('be.visible')
+    cy.get('#tabs--1--panel--1').should('not.be.visible')
+    cy.get('#tabs--1--tab--1').click()
+    cy.get('#tabs--1--panel--1').should('be.visible')
+    cy.get('#tabs--1--panel--0').should('not.be.visible')
   })
 })
