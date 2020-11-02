67d6903ceac139e8f0266947821df145860a4132
Joel Hooks <joelhooks@gmail.com>
2020-11-02 14:34:37 -0800

    make search a tad nicer

--- src/components/search/hits.tsx
+++ src/components/search/hits.tsx
@@ -1,6 +1,8 @@
 import React, {FunctionComponent} from 'react'
 import Link from 'next/link'
 import {connectHits} from 'react-instantsearch-dom'
+import formatDuration from 'utils/format-duration'
+import {convertTimeWithTitles} from 'utils/time-utils'
 
 type CustomHitsProps = {
   hits: any[]
@@ -21,21 +23,41 @@ type HitComponentProps = {
 }
 
 const HitComponent: FunctionComponent<HitComponentProps> = ({hit}) => {
-  const {path, type, image, summary, title} = hit
+  const {
+    path,
+    type,
+    image,
+    title,
+    duration,
+    instructor_avatar_url,
+    instructor_name,
+  } = hit
+
   return (
     <Link href={path}>
-      <a className="grid grid-cols-4 gap-4 items-center mb-5">
+      <a className="flex items-center mb-5 w-full">
         <div className="col-span-1 items-center flex justify-center">
           <img
-            className="w-24"
+            className="w-16"
             src={`${image}`}
             alt={`illustration for ${title}`}
           />
         </div>
-        <div className="col-span-3">
-          <h1 className="md:text-2xl text-xl font-semibold">{title}</h1>
-          <div>{type}</div>
-          <p className="prose max-w-none">{summary}</p>
+        <div className="flex items-center pl-4 w-full">
+          <div className="flex flex-col w-2/3">
+            <h2 className="text-lg font-semibold ml-0">{title}</h2>
+            <div className="text-base text-gray-600">
+              {type} • {convertTimeWithTitles(duration)}
+            </div>
+          </div>
+          <div className="flex pl-6">
+            <img
+              className="w-6 h-6 rounded-full"
+              src={`${instructor_avatar_url}`}
+              alt={`illustration for ${title}`}
+            />
+            <div className="pl-2 text-gray-600">{instructor_name}</div>
+          </div>
         </div>
       </a>
     </Link>
---
+++ src/utils/format-duration.ts
@@ -0,0 +1,10 @@
+import {number} from 'yup'
+
+const formatDuration = (duration: number) => {
+  const mins = Math.floor(duration / 60)
+  const secs = duration % 60
+  const formattedSecs = secs < 10 ? `0${secs}` : secs
+  return `${mins}:${formattedSecs}`
+}
+
+export default formatDuration
