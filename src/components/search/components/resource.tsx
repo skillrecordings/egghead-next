7b8e4d8f1692e499d75fbdb39588e8641be9669e
Vojta Holik <vojta@egghead.io>
2020-11-06 08:11:37 +0100

    use li

--- src/components/search/components/resource.tsx
+++ src/components/search/components/resource.tsx
@@ -14,14 +14,16 @@ const Resource: FunctionComponent<ResourceProps> = ({
   imageUrl,
 }) => {
   return (
-    <Link href={path}>
-      <a className="flex items-center font-semibold py-2 hover:underline cursor-pointer leading-tight">
-        <div className="flex-shrink-0 flex items-center">
-          <Image src={imageUrl} width={24} height={24} alt={title} />
-        </div>
-        <span className="ml-2">{title}</span>
-      </a>
-    </Link>
+    <li>
+      <Link href={path}>
+        <a className="flex items-center font-semibold py-2 hover:underline cursor-pointer leading-tight">
+          <div className="flex-shrink-0 flex items-center">
+            <Image src={imageUrl} width={24} height={24} alt={title} />
+          </div>
+          <span className="ml-2">{title}</span>
+        </a>
+      </Link>
+    </li>
   )
 }
 
