2b586a42a261d467fbb079ca2ef729aea110501c
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2020-11-09 16:00:13 +0200

    clean up

--- src/components/app/Main.tsx
+++ src/components/app/Main.tsx
@@ -2,7 +2,10 @@ import {FunctionComponent} from 'react'
 
 const Main: FunctionComponent = ({children}) => {
   return (
-    <div className="w-full px-3 sm:px-4 lg:px-8 flex flex-col flex-grow">
+    <div
+      className="w-full px-3 sm:px-4 lg:px-8 flex flex-col flex-grow m-auto"
+      style={{maxWidth: '1440px'}}
+    >
       {children}
     </div>
   )
