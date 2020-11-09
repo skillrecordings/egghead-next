6d0342e01ba7dbe6f4b4ba5a3b8c6f0ac7c5f9a0
jh3y <jh3y@users.noreply.github.com>
2020-11-09 14:28:49 +0000

    feat: foundation for podcasts page

--- src/pages/podcasts/[slug].tsx
+++ src/pages/podcasts/[slug].tsx
@@ -1,4 +1,5 @@
 import React, {FunctionComponent} from 'react'
+import Markdown from 'react-markdown/with-html'
 import {loadPodcast} from 'lib/podcasts'
 import {GetServerSideProps} from 'next'
 
@@ -6,20 +7,52 @@ type PodcastProps = {
   podcast: any
 }
 
-const Podcast: FunctionComponent<PodcastProps> = ({podcast}) => {
+const MorePodcasts = () => <h1>More Podcasts Here...</h1>
+
+const Podcast: FunctionComponent<PodcastProps> = ({
+  podcast: {
+    description,
+    contributors,
+    episode_number,
+    transcript,
+    title,
+    image_url,
+    simplecast_uid,
+  },
+}) => {
   return (
-    <div>
-      <h1>{podcast.title}</h1>{' '}
-      <img className="max-w-xs" src={podcast.image_url} />
-      <iframe
-        height="52px"
-        width="100%"
-        frameBorder="no"
-        scrolling="no"
-        seamless
-        src={`https://player.simplecast.com/${podcast.simplecast_uid}?dark=false`}
-      ></iframe>
-    </div>
+    <>
+      <div className="prose md:prose-xl max-w-2xl w-full mx-auto leading-6">
+        <img className="max-w-xs mx-auto mb-0" src={image_url} />
+        <div className="text-sm uppercase font-light text-center text-gray-500">{`Episode ${episode_number} ${
+          contributors && contributors.length > 0
+            ? `• ${contributors.join(' && ')}`
+            : ''
+        }`}</div>
+        <h1>{title}</h1>
+        <iframe
+          height="52px"
+          width="100%"
+          frameBorder="no"
+          scrolling="no"
+          seamless
+          className="mb-10 sticky top-0 z-10"
+          src={`https://player.simplecast.com/${simplecast_uid}?dark=false`}
+        ></iframe>
+        {description && (
+          <Markdown allowDangerousHtml className="prose">
+            {description}
+          </Markdown>
+        )}
+        {transcript && <h2>Transcript</h2>}
+        {transcript && (
+          <Markdown allowDangerousHtml className="prose">
+            {transcript}
+          </Markdown>
+        )}
+      </div>
+      <MorePodcasts />
+    </>
   )
 }
 
