625d74449033a680fe3bf4cea1b951b7e0436916
jh3y <jh3y@users.noreply.github.com>
2020-11-10 20:30:26 +0000

    feat: Integrate basic story for Podcast Card

---
+++ .storybook/main.js
@@ -0,0 +1,5 @@
+const path = require('path')
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+}
---
+++ .storybook/preview.js
@@ -0,0 +1,4 @@
+import '../src/styles/index.css'
+export const parameters = {
+  actions: {argTypesRegex: '^on[A-Z].*'},
+}
