5ab2147cf8c23b109f407c593b5ecf2ba3bb10fd
Jhey Tompkins <jh3y@users.noreply.github.com>
2020-11-11 01:02:20 +0000

    feat: Integrate basic story for Podcast Card (#150)
    * feat: Integrate basic story for Podcast Card
    
    * feat: handle next/image in Storybook
    
    * feat: revert prop destructuring && fix aliases
    
    * feat: Use emotion for consistent sizing of images on Card
    
    Co-authored-by: Joel Hooks <joelhooks@gmail.com>

---
+++ src/components/podcasts/card.tsx
@@ -0,0 +1,47 @@
+import React, {FunctionComponent} from 'react'
+import {css} from '@emotion/core'
+import {PodcastResource} from 'types'
+import Image from 'next/image'
+
+type PodcastCardProps = {
+  podcast: PodcastResource
+}
+
+const IMAGE_SIZE = 220
+
+const PodcastCard: FunctionComponent<PodcastCardProps> = ({
+  podcast: {title, path, image_url, contributors},
+}) => (
+  <li className="list-none bg-white p-3 text-center max-w-xs shadow-md rounded-md transform transition-transform duration-300 hover:scale-105">
+    <a
+      title="View podcast"
+      href={path}
+      className="flex flex-col h-full items-center"
+    >
+      <div
+        css={{
+          height: `${IMAGE_SIZE}px`,
+          width: `${IMAGE_SIZE}px`,
+        }}
+        className="mb-0 flex-grow-0"
+      >
+        <Image
+          width={IMAGE_SIZE}
+          height={IMAGE_SIZE}
+          src={image_url}
+          className="object-cover mx-auto"
+        />
+      </div>
+      <h4 className="text-gray-700 flex-grow mb-6 text-lg leading-6">
+        {title}
+      </h4>
+      <div className="text-sm flex-grow-0 font-light text-center text-gray-500">{`${
+        contributors && contributors.length > 0
+          ? `${contributors.join(' && ')}`
+          : ''
+      }`}</div>
+    </a>
+  </li>
+)
+
+export default PodcastCard
--- src/pages/podcasts/[slug].tsx
+++ src/pages/podcasts/[slug].tsx
@@ -4,6 +4,7 @@ import {loadPodcast, loadPodcasts} from 'lib/podcasts'
 import Rss from '../../components/images/rss.svg'
 import Spotify from '../../components/images/spotify.svg'
 import ApplePodcasts from '../../components/images/apple-podcasts.svg'
+import PodcastCard from 'components/podcasts/card'
 import {GetServerSideProps} from 'next'
 import {PodcastResource} from 'types'
 
@@ -15,28 +16,6 @@ type PodcastProps = {
 type MorePodcastProps = {
   podcasts: Array<PodcastResource>
 }
-
-type PodcastCardProps = {
-  podcast: PodcastResource
-}
-
-const PodcastCard: FunctionComponent<PodcastCardProps> = ({
-  podcast: {title, path, image_url, contributors},
-}) => (
-  <li className="bg-white p-3 text-center max-w-xs shadow-md rounded-md transform transition-transform duration-300 hover:scale-105">
-    <a title="View podcast" href={path} className="flex flex-col h-full">
-      <img src={image_url} className="flex-grow-0" />
-      <h4 className="text-gray-700 flex-grow mb-6 text-lg leading-6">
-        {title}
-      </h4>
-      <div className="text-sm flex-grow-0 font-light text-center text-gray-500">{`${
-        contributors && contributors.length > 0
-          ? `${contributors.join(' && ')}`
-          : ''
-      }`}</div>
-    </a>
-  </li>
-)
 
 const MorePodcasts: FunctionComponent<MorePodcastProps> = ({podcasts}) => (
   <div className="w-screen bg-gray-200 -ml-3 sm:-ml-4 lg:-ml-8 sm:p-8 p-3">
