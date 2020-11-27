fec6839856be2be4b346c97ff2ea5a4b73bca045
Vojta Holik <vojta@egghead.io>
2020-11-27 14:15:52 +0100

    move components under c.../pages/home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import React, {FunctionComponent} from 'react'
 import {NextSeo} from 'next-seo'
-import Home from 'components/home'
+import Home from 'components/pages/home'
 
 type HomePageProps = {}
 
