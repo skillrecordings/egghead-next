98f557a98dcf35b586b351a4a4221060fcd0508d
Richard C <inf.darze@gmail.com>
2020-12-04 17:32:40 +0100

    Add loader to process CSS with PostCSS

--- .storybook/main.js
+++ .storybook/main.js
@@ -20,6 +20,19 @@ module.exports = {
         }),
       }),
     )
+
+    // Remove the existing css rule
+    config.module.rules = config.module.rules.filter(
+      (f) => f.test.toString() !== '/\\.css$/',
+    )
+
+    // Add loader to process CSS
+    config.module.rules.push({
+      test: /\.css$/,
+      loaders: ['style-loader', 'css-loader', 'postcss-loader'],
+      include: path.resolve(__dirname, '../'),
+    })
+
     return config
   },
 }
