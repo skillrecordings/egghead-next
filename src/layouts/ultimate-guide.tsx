e2baea86fbcbee11d20486f6407ee85e25be8a65
Vojta Holik <vojta@egghead.io>
2020-12-11 17:19:56 +0100

    article: add state & edit link

--- src/layouts/ultimate-guide.tsx
+++ src/layouts/ultimate-guide.tsx
@@ -3,6 +3,7 @@ import {NextSeo} from 'next-seo'
 import Contributors from 'components/Contributors'
 import Image from 'next/image'
 import Link from 'next/link'
+import {useRouter} from 'next/router'
 
 type LayoutProps = {
   meta?: {
@@ -14,6 +15,7 @@ type LayoutProps = {
     coverImage?: {url: string; alt: string}
     author?: {name: string; image: string; path: string} | undefined
     contributors?: {name: string; type: string; image: string; path: string}[]
+    state?: string
   }
 }
 
@@ -30,6 +32,7 @@ const UltimateGuideLayout: FunctionComponent<LayoutProps> = ({
     coverImage,
     author,
     contributors,
+    state,
   } = meta
 
   const defaultOgImage: string | undefined = title
@@ -37,6 +40,22 @@ const UltimateGuideLayout: FunctionComponent<LayoutProps> = ({
         title,
       )}`
     : undefined
+
+  const router = useRouter()
+
+  const editUrl = `https://github.com/eggheadio/egghead-next/edit/main/src/pages${router.pathname}/index.mdx`
+  const EditLink: FunctionComponent<{className: string}> = ({className}) => (
+    <div>
+      <a
+        href={editUrl}
+        className={`flex items-center space-x-1 font-medium ${className}`}
+      >
+        {/* prettier-ignore */}
+        <svg className="text-blue-500" width={18} height={18} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><g fill="none"><path d="M17.414 2.586a2 2 0 0 0-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 0 0 0-2.828z" fill="currentColor"/><path fillRule="evenodd" clipRule="evenodd" d="M2 6a2 2 0 0 1 2-2h4a1 1 0 0 1 0 2H4v10h10v-4a1 1 0 1 1 2 0v4a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6z" fill="currentColor"/></g></svg>
+        <span>Edit this article on GitHub</span>
+      </a>
+    </div>
+  )
 
   return (
     <>
@@ -62,6 +81,7 @@ const UltimateGuideLayout: FunctionComponent<LayoutProps> = ({
       <div>
         <article className="mx-auto max-w-screen-md lg:mt-14 md:mt-8 mt-3">
           <header>
+            {state && <State state={state} />}
             <h1 className="max-w-screen-md lg:text-6xl md:text-5xl sm:text-4xl text-2xl w-full font-extrabold mb-8 lg:mb-10 leading-tighter">
               {title}
             </h1>
@@ -80,13 +100,28 @@ const UltimateGuideLayout: FunctionComponent<LayoutProps> = ({
           <main className="prose md:prose-lg max-w-none">
             <div>{children}</div>
           </main>
-          <footer>
+          <footer className="mt-8 border-t border-gray-200 py-10 flex sm:flex-row flex-col-reverse justify-between sm:items-start items-center sm:text-left text-center">
             {contributors && <Contributors contributors={contributors} />}
+            <EditLink className="sm:mb-0 mb-16" />
           </footer>
         </article>
       </div>
     </>
   )
+}
+
+const State: FunctionComponent<{state: string}> = ({state}) => {
+  switch (state) {
+    case 'draft':
+      return (
+        <div className="px-3 py-1 rounded-lg bg-yellow-100 inline-block mb-2 text-sm font-semibold">
+          Draft
+        </div>
+      )
+
+    default:
+      return null
+  }
 }
 
 const Author: FunctionComponent<{
