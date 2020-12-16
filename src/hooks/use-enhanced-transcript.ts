154ab508b67373c88e8e82fbf9494adde9cd2310
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2020-12-16 19:54:38 +0200

    use optional chaining

--- src/hooks/use-enhanced-transcript.ts
+++ src/hooks/use-enhanced-transcript.ts
@@ -1,9 +1,8 @@
 import fetcher from 'utils/fetcher'
-import {get} from 'lodash'
 import useSWR from 'swr'
 
 export const useEnhancedTranscript = (url: string) => {
   const {data: transcriptData} = useSWR(url, fetcher)
-  const enhancedTranscript = get(transcriptData, 'text')
+  const enhancedTranscript = transcriptData?.text
   return {enhancedTranscript}
 }
