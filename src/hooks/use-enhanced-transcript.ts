d3905b93c3107d24b08c619187029392c0ad7cac
Joel Hooks <joelhooks@gmail.com>
2020-12-17 09:06:36 -0800

    Update src/hooks/use-enhanced-transcript.ts

--- src/hooks/use-enhanced-transcript.ts
+++ src/hooks/use-enhanced-transcript.ts
@@ -4,5 +4,5 @@ import useSWR from 'swr'
 export const useEnhancedTranscript = (url: string) => {
   const {data: transcriptData} = useSWR(url, fetcher)
   const enhancedTranscript = transcriptData?.text
-  return {enhancedTranscript}
+  return enhancedTranscript
 }
