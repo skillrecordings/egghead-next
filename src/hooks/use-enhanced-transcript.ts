f16ac1c09961e2e717ef16d6ccb0035628288e0a
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2020-12-19 15:44:04 +0200

    clean up

--- src/hooks/use-enhanced-transcript.ts
+++ src/hooks/use-enhanced-transcript.ts
@@ -2,7 +2,6 @@ import fetcher from 'utils/fetcher'
 import useSWR from 'swr'
 
 export const useEnhancedTranscript = (url: string) => {
-  const {data: transcriptData} = useSWR(url, fetcher)
-  const enhancedTranscript = transcriptData?.text
-  return enhancedTranscript
+  const {data} = useSWR(url, fetcher)
+  return data?.text
 }
