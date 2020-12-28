2ef1e59c3963fb457dd8e2c2f2fbebaba118d376
Joel Hooks <joelhooks@gmail.com>
2020-12-28 14:18:07 -0800

    handle legacy accounts

--- src/pages/accounts/[slug].tsx
+++ src/pages/accounts/[slug].tsx
@@ -5,6 +5,7 @@ import {getTokenFromCookieHeaders} from 'utils/auth'
 import LoginRequired, {LoginRequiredParams} from 'components/login-required'
 import axios from 'axios'
 import Link from 'next/link'
+import {useViewer} from '../../context/viewer-context'
 
 export const getServerSideProps: GetServerSideProps = async function ({
   req,
@@ -33,6 +34,7 @@ const Account: React.FunctionComponent<
 > = ({loginRequired, account = {}}) => {
   const [subscriptionData, setSubscriptionData] = React.useState<any>()
   const {stripe_customer_id, slug} = account
+  const {viewer} = useViewer()
 
   const recur = (price: any) => {
     const {
@@ -55,16 +57,17 @@ const Account: React.FunctionComponent<
           },
         })
         .then(({data}) => {
-          if (data.subscription) {
+          console.log(data)
+          if (data) {
             setSubscriptionData(data)
           }
         })
     }
   }, [stripe_customer_id, slug])
 
-  const subscriptionName = subscriptionData && subscriptionData.product.name
+  const subscriptionName = subscriptionData && subscriptionData.product?.name
   const subscriptionPrice =
-    subscriptionData &&
+    subscriptionData?.price &&
     new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: subscriptionData.price.currency,
@@ -73,55 +76,85 @@ const Account: React.FunctionComponent<
 
   return (
     <LoginRequired loginRequired={loginRequired}>
-      <main className="max-w-7xl mx-auto pb-10 lg:py-3 lg:px-8">
+      <main className="pb-10 lg:py-3 lg:px-8">
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-5">
           {/* Payment details */}
           <div className="sm:px-6 lg:px-0 lg:col-span-9">
-            <section className="mb-32">
-              <div className="p-4">
-                <div className="border border-accents-1	max-w-3xl w-full p rounded-md m-auto my-8">
-                  <div className="px-5 py-4">
-                    <h3 className="text-2xl mb-1 font-medium">
-                      You've got a {subscriptionName}
-                    </h3>
-                    <p className="text-accents-5">
-                      You can update your plan and payment information below.
-                    </p>
-                    <div className="text-xl mt-8 mb-4 font-semibold">
-                      {!subscriptionData?.portalUrl ? (
-                        <div className="h-12 mb-6">loading</div>
-                      ) : subscriptionPrice ? (
-                        `${subscriptionPrice}/${recur(subscriptionData.price)}`
-                      ) : (
-                        <Link href="/pricing">
-                          <a>Join today!</a>
-                        </Link>
-                      )}
-                    </div>
-                  </div>
-                  {subscriptionData && (
-                    <div className="border-t border-accents-1 bg-primary-2 p-4 text-accents-3 rounded-b-md">
-                      <div className="flex flex-col items-start justify-between  sm:items-center">
-                        {subscriptionData?.subscription
-                          .cancel_at_period_end && (
-                          <p className="pb-4 sm:pb-0">
-                            Your account is currently cancelled. You'll have
-                            access until the end of your current billing period.
-                            You can also renew at any time.
+            {subscriptionData && (
+              <section className="mb-32">
+                <div className="p-4">
+                  <div className="border border-accents-1	w-full p rounded-md m-auto my-8">
+                    {subscriptionName ? (
+                      <div className="px-5 py-4">
+                        <h3 className="text-2xl mb-1 font-medium">
+                          You've got a {subscriptionName}
+                        </h3>
+                        <p className="text-accents-5">
+                          You can update your plan and payment information below
+                          via Stripe.
+                        </p>
+                        <div className="text-xl mt-8 mb-4 font-semibold">
+                          {!subscriptionData?.portalUrl ? (
+                            <div className="h-12 mb-6">loading</div>
+                          ) : subscriptionPrice ? (
+                            `${subscriptionPrice}/${recur(
+                              subscriptionData.price,
+                            )}`
+                          ) : (
+                            <Link href="/pricing">
+                              <a>Join today!</a>
+                            </Link>
+                          )}
+                        </div>
+                      </div>
+                    ) : (
+                      <div className="px-5 py-4">
+                        <h3 className="text-2xl mb-1 font-medium">
+                          No paid subscription found.
+                        </h3>
+                        {(viewer.is_pro || viewer.is_instructor) && (
+                          <p>
+                            You still have access to a Pro Membership. If you
+                            feel this is in error please email{' '}
+                            <a
+                              className="text-blue-600 hover:text-blue-700 underline"
+                              href="mailto:support@egghead.io"
+                            >
+                              support@egghead.io
+                            </a>
                           </p>
                         )}
-                        <a
-                          className="w-full mt-4 text-center transition-all duration-150 ease-in-out bg-blue-600 hover:bg-blue-700 active:bg-blue-800 hover:scale-105 transform hover:shadow-xl text-white font-semibold py-3 px-5 rounded-md"
-                          href={subscriptionData?.portalUrl}
-                        >
-                          Manage Your Membership
-                        </a>
+                        <p className="py-3">
+                          You can still update your payment information below
+                          via Stripe.
+                        </p>
                       </div>
-                    </div>
-                  )}
+                    )}
+                    {subscriptionData && (
+                      <div className="border-t border-accents-1 bg-primary-2 p-4 text-accents-3 rounded-b-md">
+                        <div className="flex flex-col items-start justify-between  sm:items-center">
+                          {subscriptionData?.subscription
+                            ?.cancel_at_period_end && (
+                            <p className="pb-4 sm:pb-0">
+                              Your account is currently cancelled. You'll have
+                              access until the end of your current billing
+                              period. You can also renew at any time.
+                            </p>
+                          )}
+                          {subscriptionData?.portalUrl && (
+                            <Link href={subscriptionData?.portalUrl}>
+                              <a className="w-full mt-4 text-center transition-all duration-150 ease-in-out bg-blue-600 hover:bg-blue-700 active:bg-blue-800 hover:scale-105 transform hover:shadow-xl text-white font-semibold py-3 px-5 rounded-md">
+                                Manage Your Membership
+                              </a>
+                            </Link>
+                          )}
+                        </div>
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </section>
+              </section>
+            )}
           </div>
         </div>
       </main>
