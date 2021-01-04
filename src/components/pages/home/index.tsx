e4b041f8b9f9bec04611e20b1263b8264983d7eb
Joel Hooks <joelhooks@gmail.com>
2021-01-03 19:20:40 -0800

    Update index.tsx

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -39,7 +39,7 @@ const Home: FunctionComponent = () => {
         path: lastResource.path,
         image: lastResource.image_url,
       },
-      ...take(reject(featured, {path: lastResource.path}), 2),
+      ...take(reject(featured, {path: lastResource.path}), featured.length-1),
     ]
   }
 
