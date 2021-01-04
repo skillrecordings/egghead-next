b1331f1d7b1c9c45c9094f2875738f606b2ed49d
Joel Hooks <joelhooks@gmail.com>
2021-01-03 16:59:57 -0800

    expire the cookie

--- src/hooks/use-last-resource.tsx
+++ src/hooks/use-last-resource.tsx
@@ -18,14 +18,18 @@ function reducer(_: any, action: LastResourceAction) {
   switch (action.type) {
     case 'update':
       const {title, path, image_url, type, description, slug} = action.resource
-      const resource = cookie.set(LAST_RESOURCE_COOKIE_NAME, {
-        title,
-        path,
-        image_url,
-        type,
-        description,
-        slug,
-      })
+      const resource = cookie.set(
+        LAST_RESOURCE_COOKIE_NAME,
+        {
+          title,
+          path,
+          image_url,
+          type,
+          description,
+          slug,
+        },
+        {expires: 7},
+      )
       return {resource}
     case 'clear':
       cookie.remove(LAST_RESOURCE_COOKIE_NAME)
