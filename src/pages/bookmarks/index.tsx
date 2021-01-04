9e9133cda8a87f4de417c8fba55601a857829cc0
Vojta Holik <vojta@egghead.io>
2021-01-04 21:20:25 +0100

    fix bookmarks page (by adding a check for image)

--- src/pages/bookmarks/index.tsx
+++ src/pages/bookmarks/index.tsx
@@ -48,54 +48,58 @@ const Bookmarks: React.FunctionComponent<LoginRequiredParams> = ({
 
   return (
     <LoginRequired loginRequired={loginRequired}>
-      <div className="mb-32 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="max-w-3xl mx-auto">
-          <h1 className="text-3xl font-bold mb-3">Your Bookmarks</h1>
-          {loadingBookmarks || isEmpty(bookmarks) ? (
-            <div>
-              {loadingBookmarks
-                ? 'loading...'
-                : `You don't have any content bookmarked to watch later.`}
-            </div>
-          ) : (
-            <ul className="space-y-5">
-              {bookmarks.map((bookmark: any) => {
-                return (
-                  <li key={bookmark.slug}>
-                    <Link href={bookmark.path}>
-                      <a>
-                        <div className="flex items-center space-x-2">
-                          <Image
-                            width={32}
-                            height={32}
-                            src={bookmark.square_cover_128_url}
-                          />
-                          <div>{bookmark.title}</div>
-                          <button
-                            className="rounded text-xs px-2 py-1 flex justify-center items-center bg-gray-100 hover:bg-gray-200 transition-colors duration-150 ease-in-out "
-                            onClick={(e) => {
-                              e.preventDefault()
-                              axios.post(bookmark.toggle_favorite_url)
-                              const lessBookmarks = reject(bookmarks, {
-                                slug: bookmark.slug,
-                              }) as []
-                              track('removed bookmark', {
-                                resource: bookmark.slug,
-                              })
-                              setBookmarks(lessBookmarks)
-                            }}
-                          >
-                            remove
-                          </button>
-                        </div>
-                      </a>
-                    </Link>
-                  </li>
-                )
-              })}
-            </ul>
-          )}
-        </div>
+      <div className="mb-32 max-w-screen-md w-full mx-auto">
+        <h1 className="sm:text-3xl text-xl font-bold mb-3 leading-tight">
+          Your Bookmarks
+        </h1>
+        {loadingBookmarks || isEmpty(bookmarks) ? (
+          <div>
+            {loadingBookmarks
+              ? 'loading...'
+              : `You don't have any content bookmarked to watch later.`}
+          </div>
+        ) : (
+          <ul className="space-y-5">
+            {bookmarks.map((bookmark: any) => {
+              return (
+                <li className="flex items-center space-x-2" key={bookmark.slug}>
+                  {bookmark.square_cover_128_url && (
+                    <div className="flex items-center flex-shrink-0">
+                      <Image
+                        width={32}
+                        height={32}
+                        src={bookmark.square_cover_128_url}
+                      />
+                    </div>
+                  )}
+                  <Link href={bookmark.path}>
+                    <a className="group inline-flex items-center space-x-2">
+                      <div className="group-hover:underline font-medium md:text-lg text-normal leading-tight">
+                        {bookmark.title}
+                      </div>
+                    </a>
+                  </Link>
+                  <button
+                    className="rounded text-xs px-2 py-1 justify-center items-center bg-gray-100 hover:bg-gray-200 transition-colors duration-150 ease-in-out "
+                    onClick={(e) => {
+                      e.preventDefault()
+                      axios.post(bookmark.toggle_favorite_url)
+                      const lessBookmarks = reject(bookmarks, {
+                        slug: bookmark.slug,
+                      }) as []
+                      track('removed bookmark', {
+                        resource: bookmark.slug,
+                      })
+                      setBookmarks(lessBookmarks)
+                    }}
+                  >
+                    remove
+                  </button>
+                </li>
+              )
+            })}
+          </ul>
+        )}
       </div>
     </LoginRequired>
   )
