6356bf853f8b54e1f32360dd51f923b8f72a8349
Joel Hooks <joelhooks@gmail.com>
2021-01-08 17:22:02 -0800

    track GH clicks

---
+++ src/components/external-tracked-link.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import {isFunction} from 'formik'
+import {track} from 'utils/analytics'
+
+const isModifiedEvent = (event) =>
+  !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey)
+
+const ExternalTrackedLink: React.FunctionComponent<any> = ({
+  eventName,
+  params,
+  label,
+  children,
+  ...props
+}) => {
+  const handleClick = (event: any) => {
+    const {href, eventName, params} = props
+
+    if (isFunction(props.onClick)) props.onClick(event)
+
+    function updateLocation() {
+      window.location.href = href || '#'
+    }
+
+    if (
+      !event.defaultPrevented && // onClick prevented default
+      event.button === 0 && // ignore right clicks
+      !props.target && // let browser handle "target=_blank" etc.
+      !isModifiedEvent(event) // ignore clicks with modifier keys
+    ) {
+      event.preventDefault()
+
+      if (eventName) {
+        track(eventName, params, updateLocation)
+      } else {
+        updateLocation()
+      }
+    }
+  }
+  return (
+    <a {...props} aria-label={label || ''} onClick={handleClick}>
+      {children}
+    </a>
+  )
+}
+
+export default ExternalTrackedLink
