ba5067f01e0c34afb1ac16bb6dd1d86108f7e93a
Joel Hooks <joelhooks@gmail.com>
2021-01-15 10:16:34 -0600

    swap speed select

--- src/pages/lessons/[slug].tsx
+++ src/pages/lessons/[slug].tsx
@@ -561,6 +561,14 @@ const Lesson: FunctionComponent<LessonProps> = ({initialLesson}) => {
               </div>
               <div className="flex justify-between items-center w-full border-t border-gray-800 pl-2 3xl:pl-0 pr-3 3xl:pr-4 py-2">
                 <div className="flex items-center flex-grow space-x-4">
+                  <AutoplayToggle
+                    enabled={true}
+                    onDark={true}
+                    player={actualPlayerRef.current}
+                  />
+                  <LessonDownload lesson={lesson} />
+                </div>
+                <div>
                   {playbackRate && (
                     <PlaybackSpeedSelect
                       playbackRate={playbackRate}
@@ -570,14 +578,6 @@ const Lesson: FunctionComponent<LessonProps> = ({initialLesson}) => {
                       video={slug}
                     />
                   )}
-                  <LessonDownload lesson={lesson} />
-                </div>
-                <div>
-                  <AutoplayToggle
-                    enabled={true}
-                    onDark={true}
-                    player={actualPlayerRef.current}
-                  />
                 </div>
               </div>
             </div>
