d3cd38d1c21a4a6cf6e1453d667ad5fc61af6528
Joel Hooks <joelhooks@gmail.com>
2021-01-15 09:36:35 -0600

    add a custom error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,14 +2,14 @@ import {useRouter} from 'next/router'
 
 export default function Custom404() {
   const router = useRouter()
-  const appVersionOfPath = `https://app.egghead.io${router.asPath}`
+  const appVersionOfPath = `${process.env.NEXT_PUBLIC_AUTH_DOMAIN}${router.asPath}`
 
   return (
     <div>
       <h1>404 - Page Not Found</h1>
       <p>
-        The page you are looking for may now live at <code>app.egghead.io</code>
-        . Try visiting{' '}
+        The page you are looking for may now live at{' '}
+        <code>{process.env.NEXT_PUBLIC_AUTH_DOMAIN}</code>. Try visiting{' '}
         <a style={{color: '#1C64F2'}} href={appVersionOfPath}>
           {appVersionOfPath}
         </a>
