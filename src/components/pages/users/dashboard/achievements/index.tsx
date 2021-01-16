0c79eb5e3e7d82bf7cf75a72441918d9ff58da60
Vojta Holik <vojta@egghead.io>
2021-01-16 15:44:15 +0100

    compose components for stats & activities (#294)

---
+++ src/components/pages/users/dashboard/achievements/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import {FunctionComponent} from 'react'
+
+type AchievementsProps = {
+  viewer: {
+    lessons_completed: number
+    series_completed: number
+  }
+}
+
+const Achievements: FunctionComponent<AchievementsProps> = ({viewer}) => {
+  return (
+    <div className="sm:p-5 p-4 bg-white rounded-lg overflow-hidden text-center flex md:flex-row flex-col items-center md:space-x-5 md:space-y-0 space-y-5">
+      <div>
+        <div className="text-2xl font-semibold leading-tight">
+          {viewer.lessons_completed}
+        </div>
+        <div className="text-xs uppercase">Lessons watched</div>
+      </div>
+      <div>
+        <div className="text-2xl font-semibold leading-tight">
+          {viewer.series_completed}
+        </div>
+        <div className="text-xs uppercase">Courses watched</div>
+      </div>
+    </div>
+  )
+}
+
+export default Achievements
---
+++ src/components/pages/users/dashboard/activity/continue-watching.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import {FunctionComponent} from 'react'
+import ResourceCard from './resource'
+import {Resource} from 'types'
+import {filter, uniq, orderBy} from 'lodash'
+
+type ContinueWatchingProps = {
+  resources: [Resource]
+}
+
+const ContinueWatching: FunctionComponent<ContinueWatchingProps> = ({
+  resources,
+}) => {
+  const coursesInProgress = orderBy(
+    uniq(
+      filter(resources, {
+        collection_progress: {is_completed: false},
+      }),
+    ),
+    'collection_progress.last_lesson_watched_on',
+    'desc',
+  )
+
+  return (
+    <div className="space-y-10">
+      <div>
+        <h2 className="flex text-lg items-center font-semibold space-x-1 pb-6">
+          <span>Continue Watching</span>
+        </h2>
+        <div className="space-y-5">
+          {coursesInProgress.map((resource: any) => {
+            return <ResourceCard {...resource} key={resource.id} />
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ContinueWatching
