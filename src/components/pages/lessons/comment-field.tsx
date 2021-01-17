b09d4cba27d7aeebb8f9baa44f9a23a6aa5ada06
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-01-17 12:10:51 +0200

    styles

--- src/components/pages/lessons/comment-field.tsx
+++ src/components/pages/lessons/comment-field.tsx
@@ -16,7 +16,7 @@ const CommentField: FunctionComponent<CommentFieldProps> = ({url}) => {
 
   return url ? (
     <div className="">
-      {!isSubmitted && !isError && (
+      {!isError && (
         <Formik
           initialValues={{newCommentText: 'eee'}}
           //   validationSchema={loginSchema}
@@ -33,31 +33,38 @@ const CommentField: FunctionComponent<CommentFieldProps> = ({url}) => {
               handleSubmit,
             } = props
             return (
-              <>
-                <form onSubmit={handleSubmit} className="flex">
+              <div className="max-w-2xl">
+                <h4 className="font-semibold mb-3">Your comment:</h4>
+                <form
+                  onSubmit={handleSubmit}
+                  className="w-full flex flex-col md:flex-row items-end"
+                >
                   <textarea
                     id="newCommentText"
                     value={values.newCommentText}
                     onChange={handleChange}
                     onBlur={handleBlur}
                     placeholder="Start to type here..."
-                    className="py-3 text-gray-900 placeholder-gray-400 focus:ring-indigo-500 focus:border-blue-500 block w-full pl-10 border-gray-300 rounded-md"
+                    className="px-4 py-3 md:py-4 text-gray-900 placeholder-gray-400 focus:ring-indigo-500 focus:border-blue-500 block w-full border-gray-300 rounded-md resize-none"
                     required
+                    css={{
+                      minHeight: '120px',
+                    }}
                   />
                   <button
                     type="submit"
                     disabled={isSubmitting}
-                    className="w-full mt-4 transition-all duration-150 ease-in-out bg-blue-600 hover:bg-blue-700 active:bg-blue-800 hover:scale-105 transform hover:shadow-xl text-white font-semibold py-3 px-5 rounded-md"
+                    className="mt-4 md:mt-0 md:ml-6 transition-all duration-150 ease-in-out bg-blue-600 hover:bg-blue-700 active:bg-blue-800 hover:scale-105 transform hover:shadow-xl text-white font-semibold py-3 px-5 rounded-md"
                   >
                     Send
                   </button>
                 </form>
-              </>
+              </div>
             )
           }}
         </Formik>
       )}
-      {isSubmitted && <div className="text-green-500">comment sent</div>}
+      {/* {isSubmitted && <div className="text-green-500">comment sent</div>} */}
       {isError && <div className="text-red-500">error</div>}
     </div>
   ) : null
