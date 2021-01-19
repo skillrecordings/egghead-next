bda36f663c89d823a504d87c26cca8aeddc8909c
Joel Hooks <joelhooks@gmail.com>
2021-01-19 10:36:33 -0600

    change analytics

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -239,7 +239,7 @@ const TopicsList: React.FunctionComponent<{topics: CardResource}> = ({
                 <a
                   onClick={() => {
                     track('clicked home page topic', {
-                      resource: resource.path,
+                      topic: resource.title,
                     })
                   }}
                   className="w-full bg-white border border-gray-100 active:bg-gray-50 hover:shadow-sm transition-all ease-in-out duration-150 rounded-md py-2 px-3 space-x-1 text-base tracking-tight font-bold leading-tight flex items-center hover:text-blue-600"
