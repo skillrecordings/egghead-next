3ca6bb444b701394648619c25439ed1b41802086
Joel Hooks <joelhooks@gmail.com>
2021-01-19 10:44:44 -0600

    trim redux

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -231,7 +231,7 @@ const TopicsList: React.FunctionComponent<{topics: CardResource}> = ({
     <Card className="shadow-none bg-gray-50">
       <div className="w-full">
         <ul
-          className={`grid md:grid-cols-${allTopics.length} sm:grid-cols-4 grid-cols-2 sm:gap-5 gap-3`}
+          className={`grid sm:grid-cols-${allTopics.length} sm:grid-cols-4 grid-cols-2 sm:gap-5 gap-3`}
         >
           {map(allTopics, (resource) => (
             <li className="" key={resource.path}>
