55975f7ee55a9c1734a03693191128381a4f2688
Zac Jones <zacjones93@gmail.com>
2021-01-21 17:24:38 -0500

    Add new content for react landing page

--- src/components/search/curated/react-page-data.ts
+++ src/components/search/curated/react-page-data.ts
@@ -1,84 +1,85 @@
 const reactPageData = [
   {
     id: 'video',
-    name: 'Modern Web Development',
-    title: 'Full Stack Development in the Era of Serverless Computing',
+    name: 'State Management',
+    title: 'XState for State Management in React Apps',
     description:
-      'Learn a workflow that allows front end developers to build full stack applications using JavaScript by taking advantage of managed services, implementing real-world features like authentication, managed GraphQL APIs, serverless Lambda functions, & chatbots using a CLI based workflow.',
-    instructor: 'nader dabit',
-    instructor_path: '/q/resources-by-nader-dabit',
+      'Joel and David chat about the big ideas of State Management, the difference between a State Machine and Statecharts, and how they overlap with music. David also gives a walkthrough of XState, XState Visualizer, and the upcoming dev tools for XState.',
+    instructor: 'David Khourshid',
+    instructor_path: '/q/resources-by-joel-hooks',
     path:
-      '/talks/egghead-full-stack-development-in-the-era-of-serverless-computing',
+      '/talks/react-xstate-for-state-management-in-react-apps-with-david-khourshid?pl=react-state-management-2020-6bec',
     poster:
-      'https://dcv19h61vib2d.cloudfront.net/thumbs/egghead-nader-dabit-full-stack-development-in-the-era-of-serverless-computing-B1UJE7z_L/egghead-nader-dabit-full-stack-development-in-the-era-of-serverless-computing-B1UJE7z_L.jpg',
+      'https://dcv19h61vib2d.cloudfront.net/thumbs/react-xstate-for-state-management-in-react-apps-with-david-khourshid-R0Tbe57sa/react-xstate-for-state-management-in-react-apps-with-david-khourshid-R0Tbe57sa.jpg',
     hls_url:
-      'https://d2c5owlt6rorc3.cloudfront.net/egghead-nader-dabit-full-stack-development-in-the-era-of-serverless-computing-B1UJE7z_L/hls/egghead-nader-dabit-full-stack-development-in-the-era-of-serverless-computing-B1UJE7z_L.m3u8',
+      'https://d2c5owlt6rorc3.cloudfront.net/react-xstate-for-state-management-in-react-apps-with-david-khourshid-R0Tbe57sa/hls/react-xstate-for-state-management-in-react-apps-with-david-khourshid-R0Tbe57sa.m3u8',
 
     dash_url:
-      'https://d2c5owlt6rorc3.cloudfront.net/egghead-nader-dabit-full-stack-development-in-the-era-of-serverless-computing-B1UJE7z_L/dash/egghead-nader-dabit-full-stack-development-in-the-era-of-serverless-computing-B1UJE7z_L.mpd',
+      'https://d2c5owlt6rorc3.cloudfront.net/react-xstate-for-state-management-in-react-apps-with-david-khourshid-R0Tbe57sa/dash/react-xstate-for-state-management-in-react-apps-with-david-khourshid-R0Tbe57sa.mpd',
 
     subtitlesUrl:
-      'https://app.egghead.io/api/v1/lessons/egghead-full-stack-development-in-the-era-of-serverless-computing/subtitles',
+      'https://app.egghead.io/api/v1/lessons/react-xstate-for-state-management-in-react-apps-with-david-khourshid/subtitles',
   },
   {
     id: 'featured',
     title: 'Featured',
     resources: [
       {
-        name: 'Featured Course',
-        title: 'Scale React Development with NX',
-        byline: 'Juri Strumpflohner',
+        name: 'Get Started',
+        title: 'Introduction to State Machines Using XState',
+        byline: 'Kyle Shevlin・52m・Course',
         image:
-          'https://d2eip9sf3oo6c2.cloudfront.net/playlists/square_covers/000/405/344/full/EGH_ScalingReactNx.png',
-        path: '/playlists/scale-react-development-with-nx-4038',
+          'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/403/full/IntroxState_1000.png',
+        path: '/courses/introduction-to-state-machines-using-xstate',
       },
       {
-        name: 'Cutting Edge',
-        title: 'Introduction to Cloudflare Workers',
-        byline: 'Free course by Kristian Freeman',
+        name: 'Level up your UI',
+        title: 'Construct Sturdy UIs with XState',
+        byline: 'Isaac Mann・34m・Course',
         image:
-          'https://d2eip9sf3oo6c2.cloudfront.net/playlists/square_covers/000/418/892/square_480/EGH_IntroCloudFlareWorkers_Final.png',
-        path: '/playlists/introduction-to-cloudflare-workers-5aa3',
+          'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/402/full/State_Machine.png',
+        path: '/courses/construct-sturdy-uis-with-xstate',
       },
       {
-        name: 'Something Different',
-        title: 'Professor Frisby Introduces Composable Functional JavaScript',
-        byline: 'Brian Lonsdorf・1h 50m・Course',
+        name: 'Pure React',
+        title: 'React Context for State Management',
+        byline: 'Dave Ceddia・35m・Course',
         image:
-          'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/072/thumb/EGH_ComposableJS_Final.png',
-        path:
-          '/courses/professor-frisby-introduces-composable-functional-javascript',
-        slug: 'professor-frisby-introduces-composable-functional-javascript',
-        description:
-          "This course teaches the ubiquitous abstractions for modeling pure functional programs. Functional languages have adopted these algebraic constructs across the board as a way to compose applications in a principled way. \r\n\r\nWe can do the same in JavaScript. While the subject matter will move beyond the functional programming basics, no previous knowledge of functional programming is required. You'll start composing functionality before you know it.",
+          'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/262/full/EGH_ReactContext_Final.png',
+        path: '/courses/react-context-for-state-management',
       },
     ],
   },
   {
-    id: 'mdxConf',
-    name: 'Future of Markdown',
-    title: 'MDX Conf 2020',
-    path: '/playlists/mdx-conf-3fc2',
+    id: 'reactArticles',
+    name: 'Written Resources',
+    title: 'Mental Models for React',
+    path: '/learn/react',
     byline: 'Chris Biscardi',
     image:
       'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/289/full/mdx.png',
     description:
-      'MDX has grown rapidly since the first commit two and a half years ago. Learn how MDX increases developer productivity, improves educational content authoring, and even peek behind the curtains to see how MDX works.',
+      "Never written a line of React? We've got a curated guide designed to teach you all the fundamentals skills and mental models you'll need to build in React.",
     resources: [
       {
-        title: 'Demystifying MDX',
-        byline: 'Cole Bremis',
-        path: '/talks/mdx-demystifying-mdx',
+        title: 'Is React.js a framework or a toolkit?',
+        byline: 'Joel Hooks',
+        path: '/learn/react/toolkit-or-framework',
       },
       {
-        title: 'MDX v2 Syntax',
-        byline: 'Laurie Barth',
-        path: '/talks/egghead-mdx-v2-syntax',
+        title: 'WTF is React?',
+        byline: 'Hiro Nishimura',
+        path: '/learn/react/beginners/wtf-is-react',
       },
       {
-        title: 'MDX and VueJS/NuxtJS',
-        byline: 'Cole Bremis',
-        path: '/talks/mdx-mdx-and-vuejs-nuxtjs',
+        title: 'How to React ⚛️',
+        byline: 'Kent C. Dodds',
+        path: 'https://kentcdodds.com/blog/how-to-react',
+      },
+      {
+        title: 'WTF is JSX?',
+        byline: 'Hiro Nishimura',
+        path: '/learn/react/beginners/wtf-is-jsx',
       },
       {
         title: 'Migrating to MDX',
@@ -86,19 +87,14 @@ const reactPageData = [
         path: '/talks/mdx-migrating-to-mdx',
       },
       {
-        title: 'Personal Site Playground with MDX',
-        byline: 'Prince Wilson',
-        path: '/talks/mdx-personal-site-playgrounds-with-mdx',
+        title: 'Super Simple Start to React',
+        byline: 'Kent C. Dodds',
+        path: 'https://kentcdodds.com/blog/super-simple-start-to-react',
       },
       {
-        title: 'The X in MDX',
-        byline: 'Rodrigo Pombo',
-        path: '/talks/mdx-the-x-in-mdx',
-      },
-      {
-        title: 'Digital Gardening with MDX Magic',
-        byline: 'Kathleen McMahon',
-        path: '/talks/mdx-digital-gardening-with-mdx-magic',
+        title: 'JavaScript Prerequisites before React',
+        byline: 'Hiro Nishimura',
+        path: '/learn/react/beginners/js-before-react',
       },
     ],
   },
--- src/components/search/curated/react.tsx
+++ src/components/search/curated/react.tsx
@@ -19,7 +19,7 @@ const SearchReact = () => {
   const title = `Advanced React Tutorials for ${new Date().getFullYear()}`
 
   const video: any = find(reactPageData, {id: 'video'})
-  const mdxConf: any = find(reactPageData, {id: 'mdxConf'})
+  const reactArticles: any = find(reactPageData, {id: 'reactArticles'})
   const featured: any = find(reactPageData, {id: 'featured'})
 
   console.log(featured)
@@ -52,12 +52,12 @@ const SearchReact = () => {
           imageUrl="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1604411002/next.egghead.io/react/space_2x.png"
         >
           {`
-The one of the web’s most popular frameworks for building
-JavaScript applications. If you know what you’re doing, React
-can drastically simplify how you build, use, and maintain code.
+One of the web’s most popular frameworks for building JavaScript applications. If you know what you’re doing, React can drastically simplify how you build, use, and maintain code.
 
-Whether you’re a React newbie or you’re ready for advanced
-techniques, you can level-up with egghead.
+Whether you’re a React newbie or you’re ready for advanced techniques, you can level-up with egghead.
+
+You can find courses below curated just for you whether you're looking for a particular topic or want to take your React skills to the next level.
+
 `}
         </Topic>
 
@@ -74,71 +74,56 @@ techniques, you can level-up with egghead.
         <Card title="Beginner" description="Just Starting Out">
           <ul className="-mb-2">
             <Resource
-              title="Up and running with Recoil - a new state management library for React"
-              path="/playlists/up-and-running-with-recoil-a-new-state-management-library-for-react-78b8"
-              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
+              title="The Beginner's Guide to React"
+              path="/courses/the-beginner-s-guide-to-react"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/160/full/EGH_BeginnersReact2.png"
             />
             <Resource
-              title="Manage React Form State with redux-form"
-              path="/courses/manage-react-form-state-with-redux-form"
-              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/197/full/Manage_React_Form.png"
+              title="Develop Accessible Web Apps with React"
+              path="/courses/develop-accessible-web-apps-with-react"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/412/full/AccessibleReact_1000.png"
             />
             <Resource
-              title="React Context for State Management"
-              path="/courses/react-context-for-state-management"
-              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/262/full/EGH_ReactContext_Final.png"
-            />
-            <Resource
-              title="Update state asynchronously in React using Promise and `setState()`"
-              path="/lessons/react-update-state-asynchronously-in-react-using-promise-and-setstate"
-              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
+              title="Build Maps with React Leaflet"
+              path="/courses/build-maps-with-react-leaflet"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/490/full/React_Leaflet_Final.png"
             />
           </ul>
         </Card>
         <Card title="Intermediate" description="Hitting Your Stride">
           <ul className="-mb-2">
             <Resource
-              title="Up and running with Recoil - a new state management library for React"
-              path="/playlists/up-and-running-with-recoil-a-new-state-management-library-for-react-78b8"
-              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
+              title="VR Applications using React 360"
+              path="/courses/vr-applications-using-react-360"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/272/full/React360-final.png"
             />
             <Resource
-              title="Manage React Form State with redux-form"
-              path="/courses/manage-react-form-state-with-redux-form"
-              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/197/full/Manage_React_Form.png"
+              title="Shareable Custom Hooks in React"
+              path="/courses/shareable-custom-hooks-in-react"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/313/full/EGH_CustomReactHooks_Final.png"
             />
             <Resource
-              title="React Context for State Management"
-              path="/courses/react-context-for-state-management"
-              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/262/full/EGH_ReactContext_Final.png"
-            />
-            <Resource
-              title="Update state asynchronously in React using Promise and `setState()`"
-              path="/lessons/react-update-state-asynchronously-in-react-using-promise-and-setstate"
-              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
+              title="Simplify React Apps with React Hooks"
+              path="/courses/simplify-react-apps-with-react-hooks"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/263/full/EGH_SimplifyHooks_Final.png"
             />
           </ul>
         </Card>
         <Card title="Advanced" description="Above and Beyond">
           <ul className="-mb-2">
             <Resource
-              title="Up and running with Recoil - a new state management library for React"
+              title="Redux with React Hooks"
+              path="/playlists/redux-with-react-hooks-8a37"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/full/react.png"
+            />
+            <Resource
+              title="Build an App with React Suspense"
+              path="/courses/build-an-app-with-react-suspense"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/399/full/React_Suspense_Final.png"
+            />
+            <Resource
+              title="Up and running with Recoil"
               path="/playlists/up-and-running-with-recoil-a-new-state-management-library-for-react-78b8"
-              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
-            />
-            <Resource
-              title="Manage React Form State with redux-form"
-              path="/courses/manage-react-form-state-with-redux-form"
-              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/197/full/Manage_React_Form.png"
-            />
-            <Resource
-              title="React Context for State Management"
-              path="/courses/react-context-for-state-management"
-              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/262/full/EGH_ReactContext_Final.png"
-            />
-            <Resource
-              title="Update state asynchronously in React using Promise and `setState()`"
-              path="/lessons/react-update-state-asynchronously-in-react-using-promise-and-setstate"
               imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
             />
           </ul>
@@ -191,7 +176,7 @@ techniques, you can level-up with egghead.
             })}
           </div>
         </div>
-        <CardVerticalWithStack data={mdxConf} />
+        <CardVerticalWithStack data={reactArticles} />
       </div>
     </div>
   )
@@ -247,8 +232,8 @@ const CardVerticalWithStack: FunctionComponent<CardProps> = ({
   const {viewer} = useViewer()
   const {name, title, description, path} = data
   return (
-    <div className="md:col-span-4 grid grid-flow-row gap-5 md:p-6 p-4 rounded-md overflow-hidden border border-gray-100">
-      <h2 className="uppercase font-semibold text-xs -mb-6 text-gray-700">
+    <div className="md:col-span-4 flex flex-col gap-5 md:p-6 p-4 rounded-md overflow-hidden border border-gray-100 ">
+      <h2 className="uppercase font-semibold text-xs text-gray-700">
         {(viewer?.is_pro || viewer?.is_instructor) && memberTitle
           ? memberTitle
           : name}
@@ -256,13 +241,13 @@ const CardVerticalWithStack: FunctionComponent<CardProps> = ({
       {path ? (
         <Link href={path}>
           <a className="hover:text-blue-600">
-            <h3 className="text-xl font-bold tracking-tight leading-tight -mb-6">
+            <h3 className="align-top text-xl font-bold tracking-tight leading-tight mb-2 -mt-5">
               {title}
             </h3>
           </a>
         </Link>
       ) : (
-        <h3 className="text-xl font-bold tracking-tight leading-tight -mb-6">
+        <h3 className="align-top text-xl font-bold tracking-tight leading-tight -mb-6">
           {title}
         </h3>
       )}
