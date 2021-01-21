418f67c5e6171f69d7780f6c95d7f35c6a977f9b
Lauro <hey@laurosilva.com>
2021-01-21 13:18:19 -0800

    Adding layout for react landing page

--- src/components/search/curated/react.tsx
+++ src/components/search/curated/react.tsx
@@ -1,15 +1,31 @@
-import * as React from 'react'
+import React, {FunctionComponent} from 'react'
 import {NextSeo} from 'next-seo'
 import Card from '../components/card'
-import Callout from '../components/callout'
 import Topic from '../components/topic'
 import Resource from '../components/resource'
+import reactPageData from './react-page-data'
+import Link from 'next/link'
+import {map, get, find} from 'lodash'
+import EggheadPlayer from 'components/EggheadPlayer'
+import Markdown from 'react-markdown'
+import CardVideo, {CardResource} from '../../pages/home/card/index'
+import {useViewer} from 'context/viewer-context'
+import Collection from '../../pages/home/collection/index'
+import Image from 'next/image'
+import Textfit from 'react-textfit'
 
 const SearchReact = () => {
   const description = `Life is too short for lonnnnnng boring videos. Learn React using the best screencast tutorial videos online.`
   const title = `Advanced React Tutorials for ${new Date().getFullYear()}`
+
+  const video: any = find(reactPageData, {id: 'video'})
+  const mdxConf: any = find(reactPageData, {id: 'mdxConf'})
+  const featured: any = find(reactPageData, {id: 'featured'})
+
+  console.log(featured)
+
   return (
-    <>
+    <div className="mb-12">
       <NextSeo
         description={description}
         title={title}
@@ -31,7 +47,7 @@ const SearchReact = () => {
       />
       <div className="grid md:grid-cols-12 grid-cols-1 gap-5 items-start sm:mt-5 mt-3">
         <Topic
-          className="col-span-6"
+          className="col-span-8"
           title="React"
           imageUrl="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1604411002/next.egghead.io/react/space_2x.png"
         >
@@ -45,97 +61,6 @@ techniques, you can level-up with egghead.
 `}
         </Topic>
 
-        <Callout
-          title="React is just JavaScript"
-          imageUrl="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1604409609/next.egghead.io/icons/bulp.svg"
-          className="col-span-6"
-        >
-          {`
-One of the nice aspects of React is that it is just JavaScript.
-When you are creating react applications, you are able to use all
-of your core knowledge about JavaScript and apply it to the
-creation of components in React.
-
-Of course, if your JavaScript fundamentals aren't quite there yet,
-this can be an entirely different challenge!
-
-We highly recommend [Kent C. Dodds's article on JavaScript for React](https://kentcdodds.com/blog/javascript-to-know-for-react) to get an idea of the **essential JavaScript** you'll need to
-be successful on your React journey.
-
-`}
-        </Callout>
-      </div>
-      <div className="grid md:grid-cols-12 grid-cols-1 gap-5 mt-5">
-        <div className="md:col-span-8 grid grid-flow-row gap-5">
-          <Card
-            title="State Management in React"
-            description="Learn how to store state in React, the right way"
-          >
-            <ul className="-mb-2">
-              <Resource
-                title="Up and running with Recoil - a new state management library for React"
-                path="/playlists/up-and-running-with-recoil-a-new-state-management-library-for-react-78b8"
-                imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
-              />
-              <Resource
-                title="Manage React Form State with redux-form"
-                path="/courses/manage-react-form-state-with-redux-form"
-                imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/197/full/Manage_React_Form.png"
-              />
-              <Resource
-                title="React Context for State Management"
-                path="/courses/react-context-for-state-management"
-                imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/262/full/EGH_ReactContext_Final.png"
-              />
-              <Resource
-                title="Update state asynchronously in React using Promise and `setState()`"
-                path="/lessons/react-update-state-asynchronously-in-react-using-promise-and-setstate"
-                imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
-              />
-            </ul>
-          </Card>
-          <Card title="React Hooks" description="Simplify your code base">
-            <ul className="-mb-2">
-              <Resource
-                title="Build Advanced Component with React Hooks"
-                path="/playlists/build-advanced-component-with-react-hooks-cd6a"
-                imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
-              />
-              <Resource
-                title="Learn Apollo Client Hooks"
-                path="/playlists/learn-apollo-client-hooks-9226f672"
-                imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/034/thumb/graphqllogo.png"
-              />
-              <Resource
-                title="React Crash Course (with hooks)"
-                path="/playlists/react-crash-course-58eb"
-                imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
-              />
-            </ul>
-          </Card>
-          <Card
-            title=" Build Professional Real World Apps"
-            description="Level up your React development career"
-          >
-            <ul className="-mb-2">
-              <Resource
-                title="Progressive Web Apps in React with create-react-app"
-                path="/courses/progressive-web-apps-in-react-with-create-react-app"
-                imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/266/full/EGH_PWAReact_Final.png"
-              />
-              <Resource
-                title="Build A React App With Redux"
-                path="/courses/build-a-react-app-with-redux"
-                imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/135/full/EGH_ReactRedux_Final.png"
-              />
-              <Resource
-                title={`Build a "Name Picker" app - Intro to React, Hooks & Context API`}
-                path="/playlists/build-a-name-picker-app-intro-to-react-hooks-context-api-1ded"
-                imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/262/full/EGH_ReactContext_Final.png"
-              />
-            </ul>
-          </Card>
-        </div>
         <aside className="md:col-span-4 rounded-md overflow-hidden border-0 border-gray-100">
           <a href="https://epicreact.dev">
             <img
@@ -145,7 +70,211 @@ be successful on your React journey.
           </a>
         </aside>
       </div>
-    </>
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-5 items-start sm:mt-5 mt-3">
+        <Card title="Beginner" description="Just Starting Out">
+          <ul className="-mb-2">
+            <Resource
+              title="Up and running with Recoil - a new state management library for React"
+              path="/playlists/up-and-running-with-recoil-a-new-state-management-library-for-react-78b8"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
+            />
+            <Resource
+              title="Manage React Form State with redux-form"
+              path="/courses/manage-react-form-state-with-redux-form"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/197/full/Manage_React_Form.png"
+            />
+            <Resource
+              title="React Context for State Management"
+              path="/courses/react-context-for-state-management"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/262/full/EGH_ReactContext_Final.png"
+            />
+            <Resource
+              title="Update state asynchronously in React using Promise and `setState()`"
+              path="/lessons/react-update-state-asynchronously-in-react-using-promise-and-setstate"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
+            />
+          </ul>
+        </Card>
+        <Card title="Intermediate" description="Hitting Your Stride">
+          <ul className="-mb-2">
+            <Resource
+              title="Up and running with Recoil - a new state management library for React"
+              path="/playlists/up-and-running-with-recoil-a-new-state-management-library-for-react-78b8"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
+            />
+            <Resource
+              title="Manage React Form State with redux-form"
+              path="/courses/manage-react-form-state-with-redux-form"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/197/full/Manage_React_Form.png"
+            />
+            <Resource
+              title="React Context for State Management"
+              path="/courses/react-context-for-state-management"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/262/full/EGH_ReactContext_Final.png"
+            />
+            <Resource
+              title="Update state asynchronously in React using Promise and `setState()`"
+              path="/lessons/react-update-state-asynchronously-in-react-using-promise-and-setstate"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
+            />
+          </ul>
+        </Card>
+        <Card title="Advanced" description="Above and Beyond">
+          <ul className="-mb-2">
+            <Resource
+              title="Up and running with Recoil - a new state management library for React"
+              path="/playlists/up-and-running-with-recoil-a-new-state-management-library-for-react-78b8"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
+            />
+            <Resource
+              title="Manage React Form State with redux-form"
+              path="/courses/manage-react-form-state-with-redux-form"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/197/full/Manage_React_Form.png"
+            />
+            <Resource
+              title="React Context for State Management"
+              path="/courses/react-context-for-state-management"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/262/full/EGH_ReactContext_Final.png"
+            />
+            <Resource
+              title="Update state asynchronously in React using Promise and `setState()`"
+              path="/lessons/react-update-state-asynchronously-in-react-using-promise-and-setstate"
+              imageUrl="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/thumb/react.png"
+            />
+          </ul>
+        </Card>
+      </div>
+
+      <div className="grid lg:grid-cols-12 grid-cols-1 gap-5 mt-12">
+        <div className="lg:col-span-8">
+          <CardVideo className="rounded border border-gray-100">
+            <div className="flex sm:flex-row flex-col justify-center">
+              <div className="flex flex-col justify-between items-start sm:pr-16 sm:pb-0 pb-10">
+                <div>
+                  <h2 className="uppercase font-semibold text-xs text-gray-700">
+                    {video.name}
+                  </h2>
+                  <Link href={video.path}>
+                    <a className="hover:text-blue-600">
+                      <h3 className="text-2xl font-bold tracking-tight leading-tighter mt-2">
+                        {video.title}
+                      </h3>
+                    </a>
+                  </Link>
+                  <div className="text-sm text-gray-600 hover:text-gray-900 transition-colors duration-150 ease-in-out mt-1">
+                    <Link href={video.instructor_path || ''}>
+                      <a className="hover:text-blue-600">{video.instructor}</a>
+                    </Link>
+                  </div>
+                  <Markdown className="prose prose-sm mt-4">
+                    {video.description}
+                  </Markdown>
+                </div>
+              </div>
+              <div className="sm:w-full -m-5 flex items-center flex-grow bg-black">
+                <EggheadPlayer
+                  preload={false}
+                  autoplay={false}
+                  poster={video.poster}
+                  hls_url={video.hls_url}
+                  dash_url={video.dash_url}
+                  subtitlesUrl={video.subtitlesUrl}
+                  width="100%"
+                  height="auto"
+                />
+              </div>
+            </div>
+          </CardVideo>
+          <div className="grid lg:grid-cols-12 grid-cols-1 gap-5 mt-12">
+            {featured.resources.map((resource: any) => {
+              return <CardVerticalLarge key={resource.path} data={resource} />
+            })}
+          </div>
+        </div>
+        <CardVerticalWithStack data={mdxConf} />
+      </div>
+    </div>
   )
 }
+
+type CardProps = {
+  data: CardResource
+  className?: string
+  memberTitle?: string
+}
+
+const CardVerticalLarge: FunctionComponent<CardProps> = ({data}) => {
+  const {path, image, title, name, byline} = data
+  return (
+    <div className="lg:col-span-4 rounded border border-gray-100">
+      <div className="border-none flex flex-col items-center justify-center text-center sm:py-8 py-6">
+        <>
+          {image && (
+            <Link href={path}>
+              <a className="mb-2 mx-auto w-24" tabIndex={-1}>
+                <Image
+                  width={140}
+                  height={140}
+                  src={get(image, 'src', image)}
+                  alt={`illustration for ${title}`}
+                />
+              </a>
+            </Link>
+          )}
+          <h2 className="uppercase font-semibold text-xs mb-1 text-gray-700">
+            {name}
+          </h2>
+          <Link href={path}>
+            <a className="hover:text-blue-600">
+              <h3 className="md:text-lg text-base sm:font-semibold font-bold leading-tight">
+                <Textfit mode="multi" min={14} max={20}>
+                  {title}
+                </Textfit>
+              </h3>
+            </a>
+          </Link>
+          <div className="text-xs text-gray-600 mt-1">{byline}</div>
+        </>
+      </div>
+    </div>
+  )
+}
+
+const CardVerticalWithStack: FunctionComponent<CardProps> = ({
+  data,
+  memberTitle,
+}) => {
+  const {viewer} = useViewer()
+  const {name, title, description, path} = data
+  return (
+    <div className="md:col-span-4 grid grid-flow-row gap-5 md:p-6 p-4 rounded-md overflow-hidden border border-gray-100">
+      <h2 className="uppercase font-semibold text-xs -mb-6 text-gray-700">
+        {(viewer?.is_pro || viewer?.is_instructor) && memberTitle
+          ? memberTitle
+          : name}
+      </h2>
+      {path ? (
+        <Link href={path}>
+          <a className="hover:text-blue-600">
+            <h3 className="text-xl font-bold tracking-tight leading-tight -mb-6">
+              {title}
+            </h3>
+          </a>
+        </Link>
+      ) : (
+        <h3 className="text-xl font-bold tracking-tight leading-tight -mb-6">
+          {title}
+        </h3>
+      )}
+      <div>
+        <Markdown
+          source={description || ''}
+          className="prose prose-sm max-w-none mb-3 "
+        />
+        <Collection resource={data} />
+      </div>
+    </div>
+  )
+}
+
 export default SearchReact
