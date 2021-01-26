58862121bcd18f5b2fb7f546d415803a3f4d46eb
Vojta Holik <vojta@egghead.io>
2021-01-26 19:17:45 +0100

    use coolGray instead of trueGray

--- src/components/pages/courses/learner-ratings.tsx
+++ src/components/pages/courses/learner-ratings.tsx
@@ -41,7 +41,7 @@ const LearnerRatings: React.FunctionComponent<{collection: any}> = ({
           return (
             <li
               key={`rating-${rating.id}`}
-              className={`space-y-2 dark:border-trueGray-800 border rounded-md p-4 ${
+              className={`space-y-2 dark:border-gray-800 border rounded-md p-4 ${
                 rating.hidden && 'hidden'
               }`}
             >
@@ -59,12 +59,12 @@ const LearnerRatings: React.FunctionComponent<{collection: any}> = ({
                   />
                 </div>
                 <div className="font-bold">{user.full_name || 'Learner'}</div>
-                <div className="text-sm text-gray-500 dark:text-trueGray-400">
+                <div className="text-sm text-gray-500 dark:text-gray-400">
                   {friendlyTime(new Date(created_at))}
                 </div>
               </div>
               <FiveStars rating={rating_out_of_5} />
-              <div className="text-xs text-gray-500 dark:text-trueGray-500">
+              <div className="text-xs text-gray-500 dark:text-gray-500">
                 {comment.prompt}
               </div>
               <div className="prose dark:prose-dark overflow-hidden">
@@ -72,7 +72,7 @@ const LearnerRatings: React.FunctionComponent<{collection: any}> = ({
               </div>
               {displayAdminContent && (
                 <button
-                  className="dark:text-trueGray-900 rounded text-xs px-2 py-1 flex justify-center items-center bg-gray-100 hover:bg-gray-200 transition-colors duration-150 ease-in-out"
+                  className="dark:text-gray-900 rounded text-xs px-2 py-1 flex justify-center items-center bg-gray-100 hover:bg-gray-200 transition-colors duration-150 ease-in-out"
                   onClick={() => {
                     rating.hidden = true
                     axios.post(comment.hide_url).then(() => {
