15e88d36d92059d45f26ee5eecb5998aad796671
Joel Hooks <joelhooks@gmail.com>
2021-01-25 16:16:49 -0800

    remove dark mode setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  darkMode: 'class',
   purge: {
     layers: ['components', 'utilities'],
     content: [
