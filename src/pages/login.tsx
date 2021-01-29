c82cd9e4cae17ef60129ab9c19e2394a783402bd
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-01-29 16:41:30 +0200

    fix github button

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -152,7 +152,7 @@ const LoginForm: FunctionComponent<LoginFormProps> = ({
                       <ExternalTrackedLink
                         href={`${process.env.NEXT_PUBLIC_AUTH_DOMAIN}/users/github_passthrough?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}`}
                         eventName="clicked github login"
-                        className="flex justify-center mt-4 py-3 px-5 bg-gray-300 hover:opacity-100 hover:bg-gray-100 active:bg-gray-100 rounded-md transition-all ease-in-out duration-300"
+                        className="flex justify-center mt-4 py-3 px-5 text-white bg-black hover:bg-gray-800 active:bg-gray-700 rounded-md transition-all ease-in-out duration-300"
                       >
                         <div className="flex items-center dark:text-gray-900">
                           <span className="mr-2 flex items-center justify-center">
