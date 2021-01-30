a785ad2822e00b72d8df999cf67deeb08da443da
Lauro <hey@laurosilva.com>
2021-01-30 00:32:46 -0800

    Leveling up graphql landing page

--- src/components/search/curated/graphql/graphql-page-data.ts
+++ src/components/search/curated/graphql/graphql-page-data.ts
@@ -1,43 +1,47 @@
-let graphqlPageData = [
+const graphqlPageData = [
   {
     id: 'get-started',
-    title: 'Getting Started with GraphQL',
-    name: 'LEARN THE BASICS',
+    title: 'Up and Running',
+    name: 'Get Started',
     resources: [
       {
-        title: 'WTF is GraphQL',
-        path: '/lessons/graphql-wtf-is-graphql',
+        title:
+          'Build a full-stack JAMstack application with Gatsby, Hasura, and Auth0',
+        path:
+          '/playlists/build-a-full-stack-jamstack-application-with-gatsby-c6f8',
         image:
-          'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1592859008/maggieappleton.com/egghead-course-notes/graphql/thumb_2x.png',
-        byline: 'Eve Porcello',
+          'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/034/full/graphqllogo.png',
+        byline: 'Kyle Gill',
       },
       {
-        title: 'GraphQL Query Language',
-        path: '/courses/graphql-query-language',
+        title: 'Getting Started With Hasura and GraphQL',
+        path: '/playlists/getting-started-with-hasura-cloud-872e',
         image:
-          'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/236/full/EGH_GraphQLQuery_Final.png',
-        byline: 'Eve Porcello',
+          'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/034/full/graphqllogo.png',
+        byline: 'Ali Spittel',
       },
       {
-        title: 'Transforming REST APIs to GraphQL with AWS AppSync',
-        path: '/lessons/aws-transforming-rest-apis-to-graphql-with-aws-appsync',
+        title: 'Learn Apollo Client Hooks',
+        path: '/playlists/learn-apollo-client-hooks-9226f672',
         image:
-          'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1592859008/maggieappleton.com/egghead-course-notes/graphql/thumb_2x.png',
-        byline: 'Nader Dabit',
+          'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/full/react.png',
+        byline: 'Tomasz Łakomy',
       },
     ],
   },
   {
-    id: 'pro-graphql',
-    title: 'Pro GraphQL skills',
-    name: 'LEARN MORE SKILLS',
+    id: 'project-based',
+    title: 'For Your Porfolio',
+    name: 'Project-Based',
     resources: [
       {
-        title: 'Designing GraphQL Schemas',
-        path: '/courses/designing-graphql-schemas-99db',
+        title:
+          'Building a Serverless JAMStack Todo app with Netlify, Gatsby, GraphQL, and FaunaDB',
+        path:
+          '/playlists/building-a-serverless-jamstack-todo-app-with-netlify-gatsby-graphql-and-faunadb-53bb',
         image:
-          'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/405/full/DesignGraphQLSchemas.png',
-        byline: 'Nik Graf',
+          'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/281/full/Fauna_Logo_blue.png',
+        byline: 'Chris Biscardi',
       },
       {
         title: 'React Real-Time Messaging with GraphQL using urql and OneGraph',
@@ -48,41 +52,68 @@ let graphqlPageData = [
         byline: 'Ian Jones',
       },
       {
-        title: 'GraphQL Data in React with Apollo Client',
-        path: '/courses/graphql-data-in-react-with-apollo-client',
+        title: 'Build a CRUD-ready GraphQL API + Admin UI with KeystoneJS',
+        path:
+          '/playlists/build-a-crud-ready-graphql-api-admin-ui-with-keystonejs-86fa4eea',
         image:
-          'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/231/full/EGH_Apollo-GraphQL-React_Final.png',
-        byline: 'Nik Graf',
+          'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/034/full/graphqllogo.png',
+        byline: 'Simon Vrachliotis',
       },
     ],
   },
   {
-    id: 'graphql-apollo',
-    title: 'GraphQL with Apollo',
-    name: 'PRODUCTION READY GRAPHQL',
+    id: 'graphql-featured',
+    title: 'Featured',
     resources: [
       {
-        title: 'Learn Apollo Client Hooks',
-        path: '/playlists/learn-apollo-client-hooks-9226f672',
-        byline: 'Tomasz Łakomy',
-      },
-      {
-        title: 'Create Fullstack Applications with GraphQL and Apollo',
-        path:
-          '/playlists/create-fullstack-applications-with-graphql-and-apollo-794dc9c7',
+        name: 'Beginner',
+        title: 'GraphQL Query Language',
+        path: '/courses/graphql-query-language',
+        image:
+          'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/236/full/EGH_GraphQLQuery_Final.png',
         byline: 'Eve Porcello',
       },
       {
-        title: 'Getting Started with Apollo Federation',
-        path: '/playlists/getting-started-with-apollo-federation-60ad0165',
-        byline: 'Alex Banks',
+        name: 'Intermediate',
+        title:
+          'WordPress as a Headless Content Management System (CMS) and GraphQL API',
+        path: '/playlists/headless-wordpress-4a14',
+        image:
+          'https://d2eip9sf3oo6c2.cloudfront.net/playlists/square_covers/000/410/100/full/Headless-Wordpress.png',
+        byline: 'Kevin Cunningham',
+      },
+      {
+        name: 'Advanced',
+        title:
+          'Scalable Offline-Ready GraphQL Applications with AWS AppSync & React',
+        path:
+          '/courses/scalable-offline-ready-graphql-applications-with-aws-appsync-react',
+        image:
+          'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/211/full/EGH_GraphQL-AWs_Final.png',
+        byline: 'Nader Dabit',
       },
     ],
+  },
+  {
+    id: 'graphql-video',
+    name: 'Introduce GraphQL',
+    title: 'WTF is GraphQL?',
+    description: `GraphQL is a query language for your API, which means that you can query or ask for the data that you need in one request, and you'll get one response as JSON. In this lesson, we'll introduce the key features of GraphQL.`,
+    byline: 'Eve Porcello',
+    path: '/lessons/graphql-wtf-is-graphql',
+    poster:
+      'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1611990262/egghead-next-pages/graphql/video-cover-image_2x.png',
+    hls_url:
+      'https://d2c5owlt6rorc3.cloudfront.net/egghead-wtf-is-graphql-83e388b9cb/egghead-wtf-is-graphql-83e388b9cb.m3u8',
+    dash_url:
+      'https://d2c5owlt6rorc3.cloudfront.net/egghead-wtf-is-graphql-83e388b9cb/egghead-wtf-is-graphql-83e388b9cb.mpd',
+    subtitlesUrl:
+      'https://app.egghead.io/api/v1/lessons/graphql-wtf-is-graphql/subtitles',
   },
   {
     id: 'graphql-serverless',
     title: 'Going Serverless with GraphQL',
-    name: 'WHAT SHOULD THIS BE',
+    name: 'Serverless Architecture',
     resources: [
       {
         title:
@@ -94,11 +125,11 @@ let graphqlPageData = [
         byline: 'Nader Dabit',
       },
       {
-        title: 'Getting Started With Hasura',
-        path: '/playlists/getting-started-with-hasura-cloud-872e',
+        title: 'GraphQL Data in React with Apollo Client',
+        path: '/courses/graphql-data-in-react-with-apollo-client',
         image:
-          'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/034/full/graphqllogo.png',
-        byline: 'Ali Spittel',
+          'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/231/full/EGH_Apollo-GraphQL-React_Final.png',
+        byline: 'Nik Graf',
       },
       {
         title: 'Building Serverless Web Applications with React & AWS Amplify',
@@ -107,43 +138,6 @@ let graphqlPageData = [
         image:
           'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/217/full/EGH_AmplifyAWS_React_Final.png',
         byline: 'Nader Dabit',
-      },
-    ],
-  },
-  {
-    id: 'graphql-cool-things',
-    title: 'Cool Things You Can Do with GraphQL',
-    name: 'Portfolio Projects',
-    resources: [
-      {
-        title:
-          'WordPress as a Headless Content Management System (CMS) and GraphQL API',
-        path: '/playlists/headless-wordpress-4a14',
-        image:
-          'https://d2eip9sf3oo6c2.cloudfront.net/playlists/square_covers/000/410/100/full/Headless-Wordpress.png',
-        byline: 'Kevin Cunningham',
-      },
-      {
-        title: 'React Real-Time Messaging with GraphQL using urql and OneGraph',
-        path:
-          '/playlists/react-real-time-messaging-with-graphql-using-urql-and-onegraph-be5a',
-        image:
-          'https://d2eip9sf3oo6c2.cloudfront.net/playlists/square_covers/000/409/013/full/EGH_RealtimeGraphqL.png',
-        byline: 'Ian Jones',
-      },
-    ],
-  },
-  {
-    id: 'podcasts',
-    title: 'egghead podcasts',
-    name: 'Conversations with GraphQL Experts',
-    resources: [
-      {
-        title: 'Eve Porcello on GraphQL',
-        path: '/podcasts/eve-porcello-on-graphql',
-        image:
-          'https://image.simplecastcdn.com/images/2ac34c/2ac34cab-4949-40aa-bac7-d7e3a70f0a39/0e28b303-5ae5-4c93-8f1a-95ccd885e8b7/1537472482artwork.jpg',
-        byline: 'Joel Hooks and Eve Porcello',
       },
     ],
   },
