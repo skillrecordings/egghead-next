95e6b0ae33ee2ee7f619e228697dbc421a659173
Zac Jones <zacjones93@gmail.com>
2021-02-01 17:16:55 -0500

    add start of cloudflare project page

---
+++ src/pages/projects/introduction-to-cloudflare-workers/index.tsx
@@ -0,0 +1,126 @@
+import React, {FunctionComponent} from 'react'
+import {NextSeo} from 'next-seo'
+import Markdown from 'react-markdown'
+import Topic from 'components/search/components/topic'
+import {find} from 'lodash'
+
+const landingPage = () => {
+  return (
+    <>
+      <div className="">
+        <ProjectBrief
+          topic={{
+            name: 'cloudflare',
+            label: 'Project Brief',
+          }}
+        >
+          <p>
+            <Markdown
+              className="prose dark:prose-dark pt-2 sm:text-base text-sm leading-normal text-gray-800 dark:text-gray-200 mt-0"
+              source={`You are a developer for a national concert promoter.`}
+            />
+          </p>
+          <p>
+            <Markdown
+              className="prose dark:prose-dark pt-2 sm:text-base text-sm leading-normal text-gray-800 dark:text-gray-200 mt-0"
+              source={`Music fans from all over the world visit your site to see when and where the next show is going to be. To save them time when they visit the page, we want to show them upcoming events at a venue close to their location.`}
+            />
+          </p>
+          <p>
+            <Markdown
+              className="prose dark:prose-dark pt-2 sm:text-base text-sm leading-normal text-gray-800 dark:text-gray-200 mt-0"
+              source={`Your task is to create and deploy a Cloudflare Worker that will examine the request for location data, and render HTML featuring information for the closest concert taking place based on the nearest [regional Cloudflare Location](https://www.cloudflare.com/network/). Choose locations for testing where you have friends (or a VPN 😅) so you can prove this functionality works as expected.`}
+            />
+          </p>
+        </ProjectBrief>
+      </div>
+    </>
+  )
+}
+
+export enum CARD_TYPES {
+  SUMMARY = 'summary',
+  SUMMARY_LARGE_IMAGE = 'summary_large_image',
+}
+
+export type Topic = {
+  name: string
+  label: string
+  title?: string
+}
+
+type ProjectBriefProps = {
+  topic: Topic
+  className: any
+  pageData?: any
+  CTAComponent?: React.FC
+  ogImage?: string
+  verticalImage?: string
+  cardType?: CARD_TYPES
+}
+
+const ProjectBrief: React.FC<ProjectBriefProps> = ({
+  topic,
+  children,
+  ogImage,
+  className,
+  cardType = CARD_TYPES.SUMMARY_LARGE_IMAGE,
+}) => {
+  const description = `Life is too short for long boring videos. Learn ${topic.label} using the best screencast tutorial videos online.`
+  const location = `${topic} landing`
+  const title =
+    topic.title ||
+    `In-Depth ${topic.label} Tutorials for ${new Date().getFullYear()}`
+
+  return (
+    <div
+      className={`mb-10 pb-10 xl:px-0 px-5 bg-blue-600 dark:bg-gray-900 ${
+        className ? className : ''
+      }`}
+    >
+      <NextSeo
+        description={description}
+        title={title}
+        titleTemplate={'%s | egghead.io'}
+        twitter={{
+          site: `@eggheadio`,
+          cardType,
+        }}
+        openGraph={{
+          title,
+          description: description,
+          site_name: 'egghead',
+          images: [
+            {
+              url:
+                ogImage ||
+                `https://og-image-react-egghead.now.sh/topic/${topic.name}?orientation=landscape&v=20201104`,
+            },
+          ],
+        }}
+      />
+      <div className="md:grid md:grid-cols-12 grid-cols-1 gap-5 justify-self-center space-y-5 md:space-y-0 dark:bg-gray-900">
+        <div
+          className={`bg-white dark:bg-gray-800 dark:text-gray-200 shadow-sm grid grid-cols-8 h-full relative items-center overflow-hidden rounded-md border border-gray-100 dark:border-gray-800 col-span-8`}
+        >
+          <div
+            className="overflow-hidden sm:col-span-2 col-span-2 w-full h-full"
+            style={{
+              background: `url(https://og-image-react-egghead.now.sh/topic/${topic.name}?orientation=portrait&v=20201104)`,
+              backgroundSize: 'cover',
+              backgroundPosition: '38%',
+            }}
+          />
+          <div className="sm:col-span-6 col-span-6 flex flex-col justify-start h-full p-8">
+            <h1 className="sm:text-2xl text-xl font-bold mb-2">
+              {topic.label}
+            </h1>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default landingPage
