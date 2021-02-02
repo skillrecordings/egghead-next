1699f5a05c63ce805910f7f9fed735024d294857
Joel Hooks <joelhooks@gmail.com>
2021-02-01 21:19:13 -0800

    svg proper

--- src/components/search/instructors/instructor-essential.tsx
+++ src/components/search/instructors/instructor-essential.tsx
@@ -110,9 +110,9 @@ const SearchInstructorEssential: FunctionComponent<InstructorProps> = ({
                         aria-hidden="true"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
                         />
                       </svg>
