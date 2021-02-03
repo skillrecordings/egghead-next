d7dcb918491b5ab389776fcd2408594b02a9fd54
Zac Jones <zacjones93@gmail.com>
2021-02-03 17:29:25 -0500

    Add docker banner to docker search page (#385)
    * Add docker banner to docker search page
    
    * remove console.log
    
    * remove docker story
    
    * Update docker-page-data.ts
    
    Co-authored-by: Joel Hooks <joelhooks@gmail.com>

--- src/components/search/curated/index.tsx
+++ src/components/search/curated/index.tsx
@@ -2,12 +2,14 @@ import SearchJavaScript from './javascript'
 import SearchReact from './react'
 import SearchGraphql from './graphql'
 import SearchTypeScript from './typescript'
+import SearchDocker from './docker'
 
 const CuratedTopicsIndex: any = {
   javascript: SearchJavaScript,
   react: SearchReact,
   graphql: SearchGraphql,
   typescript: SearchTypeScript,
+  docker: SearchDocker,
 }
 
 export default CuratedTopicsIndex
