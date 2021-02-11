99c8da7d84efff87d1deba4e757903ace2167c0b
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-02-11 19:20:13 +0200

    fix size

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -4,7 +4,7 @@ const Spinner: React.FunctionComponent<{className?: string}> = ({
   className = '',
 }) => (
   <svg
-    className={`animate-spin h-5 w-5 text-white ${className}`}
+    className={`animate-spin h-6 w-6 text-white ${className}`}
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
