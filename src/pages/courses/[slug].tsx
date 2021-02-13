9c7a9cf1b859c91bd6d3c3fef3d7a098ec827236
ijones16 <jones58ian@gmail.com>
2021-02-13 14:24:43 -0500

    load playlist when the switch is made

--- src/pages/courses/[slug].tsx
+++ src/pages/courses/[slug].tsx
@@ -1,10 +1,14 @@
 import * as React from 'react'
 import useSWR from 'swr'
 import {loadCourse} from 'lib/courses'
+import {loadPlaylist} from 'lib/playlists'
 import {FunctionComponent} from 'react'
 import {GetServerSideProps} from 'next'
 import fetcher from 'utils/fetcher'
 import CollectionPageLayout from 'components/layouts/collection-page-layout'
+import filter from 'lodash/filter'
+import isEmpty from 'lodash/isEmpty'
+import get from 'lodash/get'
 
 type CourseProps = {
   course: any
@@ -18,10 +22,17 @@ const Course: FunctionComponent<CourseProps> = ({course: initialCourse}) => {
   console.debug(`course loaded`, course)
 
   const {slug, lessons} = course
+  const items = get(course, 'items', [])
+
+  const courseLessons = isEmpty(lessons)
+    ? filter(items, (item) => {
+        return ['lesson', 'talk'].includes(item.type)
+      })
+    : lessons
 
   return (
     <CollectionPageLayout
-      lessons={lessons}
+      lessons={courseLessons}
       course={course}
       ogImageUrl={`https://og-image-egghead-course.now.sh/${slug}?v=20201027`}
     />
@@ -31,7 +42,11 @@ const Course: FunctionComponent<CourseProps> = ({course: initialCourse}) => {
 export default Course
 
 export const getServerSideProps: GetServerSideProps = async ({res, params}) => {
-  const course = params && (await loadCourse(params.slug as string))
+  const course =
+    params &&
+    (process.env.NEXT_PUBLIC_PLAYLISTS_ARE_COURSES === 'true'
+      ? await loadPlaylist(params.slug as string)
+      : await loadCourse(params.slug as string))
 
   if (course && course?.slug !== params?.slug) {
     res.setHeader('Location', course.path)
