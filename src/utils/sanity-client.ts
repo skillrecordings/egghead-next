a972da6f8b9a6bd7c074d39df8dda26f95e02c24
Joel Hooks <joelhooks@gmail.com>
2021-02-21 14:30:07 -0800

    double gql

--- src/utils/sanity-client.ts
+++ src/utils/sanity-client.ts
@@ -7,12 +7,12 @@ const config =
         dataset: 'tester',
         token: process.env.NEXT_PUBLIC_SANITY_PUBLIC_KEY,
         withCredentials: true,
-        useCdn: false, // `false` if you want to ensure fresh data
+        useCdn: false,
       }
     : {
         projectId: 'sb1i5dlc',
         dataset: 'tester',
-        useCdn: true, // `false` if you want to ensure fresh data
+        useCdn: true,
       }
 
 export const sanityClient = client(config)
