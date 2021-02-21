8a6a89ed545781fb56ae28b0df5d5394a4202017
Joel Hooks <joelhooks@gmail.com>
2021-02-21 14:26:26 -0800

    sanity config for prod

--- src/utils/sanity-client.ts
+++ src/utils/sanity-client.ts
@@ -1,9 +1,18 @@
 import client from '@sanity/client'
 
-export const sanityClient = client({
-  projectId: 'sb1i5dlc',
-  dataset: 'tester',
-  token: process.env.NEXT_PUBLIC_SANITY_PUBLIC_KEY,
-  withCredentials: true,
-  useCdn: true, // `false` if you want to ensure fresh data
-})
+const config =
+  process.env.NODE_ENV === 'development'
+    ? {
+        projectId: 'sb1i5dlc',
+        dataset: 'tester',
+        token: process.env.NEXT_PUBLIC_SANITY_PUBLIC_KEY,
+        withCredentials: true,
+        useCdn: false, // `false` if you want to ensure fresh data
+      }
+    : {
+        projectId: 'sb1i5dlc',
+        dataset: 'tester',
+        useCdn: true, // `false` if you want to ensure fresh data
+      }
+
+export const sanityClient = client(config)
