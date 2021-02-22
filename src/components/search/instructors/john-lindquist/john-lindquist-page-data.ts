4cec9d33e851cb417a4e62518d59bb2a0fbbc431
Vojta Holik <vojta@egghead.io>
2021-02-22 12:28:48 +0100

    add Simple Scripts cta

---
+++ src/components/search/instructors/john-lindquist/john-lindquist-page-data.ts
@@ -0,0 +1,20 @@
+const JohnLindquistPageData = [
+  {
+    id: 'instructor-data',
+    name: 'John Lindquist',
+    company: 'egghead.io',
+    twitterHandle: 'johnlindquist',
+    websiteURL: 'https://github.com/johnlindquist',
+    slug: 'john-lindquist',
+    location: 'John Lindquist Instructor Page',
+    ctaTitle: 'Simple Scripts',
+    imageUrl:
+      'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1613983225/egghead-next-pages/instructors/john-lindquist/john-lindquist.jpg',
+    ctaImage:
+      'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1613993286/egghead-next-pages/instructors/john-lindquist/simplescripts_2x.png',
+    ctaLink: 'https://simplescripts.dev',
+    bio: `John Lindquist is a co-founder of egghead.io John has spent the majority of his career developing Rich Web Applications. Before starting egghead.io, John worked at JetBrains as a WebStorm evangelist, and before that he was a Technical Architect at Isobar leading large teams in building the latest and greatest in web apps for the Air Force, HBO, and Bloomberg. [Learn more about John's story with egghead here.](https://howtoegghead.com/instructor/case-studies/john-lindquist)`,
+  },
+]
+
+export default JohnLindquistPageData
