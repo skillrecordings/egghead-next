047f874b9b1ed62448a91a6abac3efed7404508d
Joel Hooks <joelhooks@gmail.com>
2021-02-22 16:08:27 -0800

    use Sanity with cdn true

--- src/utils/sanity-client.ts
+++ src/utils/sanity-client.ts
@@ -2,8 +2,6 @@ import client from '@sanity/client'
 
 export const sanityClient = client({
   projectId: 'sb1i5dlc',
-  dataset: 'tester',
-  token: process.env.NEXT_PUBLIC_SANITY_PUBLIC_KEY,
-  withCredentials: true,
-  useCdn: false, // `false` if you want to ensure fresh data
+  dataset: 'production',
+  useCdn: true, // `false` if you want to ensure fresh data
 })
