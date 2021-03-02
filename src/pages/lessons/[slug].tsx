3d0414320358178f67247e4dc371039e87e1d672
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-03-02 20:41:12 +0200

    cleanup

--- src/pages/lessons/[slug].tsx
+++ src/pages/lessons/[slug].tsx
@@ -375,10 +375,7 @@ const Lesson: FunctionComponent<LessonProps> = ({initialLesson}) => {
           src="https://cdn.bitmovin.com/player/web/8/bitmovinplayer.js"
         />
       </Head>
-      <div
-        className="sm:space-y-8 space-y-6 w-full sm:pb-16 pb-8 dark:text-gray-100"
-        css={{background: 'magenta'}}
-      >
+      <div className="sm:space-y-8 space-y-6 w-full sm:pb-16 pb-8 dark:text-gray-100">
         <div className="bg-black -mt-3 sm:-mt-5 -mx-5 sm:border-b border-gray-100  dark:border-gray-800">
           <div className="w-full flex flex-col lg:flex-row justify-center items-center">
             <div
