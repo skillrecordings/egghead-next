1f46be06f16a0ea7841fbb47f3229d606ba58a09
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-03-17 17:39:26 +0200

    initial set up

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    'postcss-preset-env': {
-      autoprefixer: {
-        flexbox: 'no-2009',
-      },
+    '@tailwindcss/jit': {},
+    autoprefixer: {
+      flexbox: 'no-2009',
     },
     'postcss-nested': {},
   },
