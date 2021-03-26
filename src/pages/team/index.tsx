fa2772b6d72294f184ee096b4c27d7712095d912
Josh Branchaud <jbranchaud@gmail.com>
2021-03-26 14:10:41 -0500

    Add team capacity indications on team page (#499)
    * Add team capacity indications on team page
    
    - List # of members / # of seats
    - Show warning when seats are full and prompt user to upgrade
    subscription
    
    * Link to Stripe Billing Portal when at capacity
    
    * Add missing keys per type definition

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -1,37 +1,146 @@
 import * as React from 'react'
+import Link from 'next/link'
 import {useViewer} from '../../context/viewer-context'
 import LoginRequired from '../../components/login-required'
 import {useRouter} from 'next/router'
 import {FunctionComponent} from 'react'
 import useClipboard from 'react-use-clipboard'
 import isEmpty from 'lodash/isEmpty'
+import axios from 'axios'
+import {track} from 'utils/analytics'
 
 interface TeamData {
   inviteUrl: string
   members: Array<any>
+  numberOfMembers: number
+  capacity: number
   isFull: boolean
+  accountSlug: string | undefined
+  stripeCustomerId: string | undefined
 }
 
 const normalizeTeamData = (viewer: any): TeamData | undefined => {
   if (!!viewer?.team) {
+    const members = viewer.team.members || []
+
     // Managed Subscription
     return {
       inviteUrl: viewer.team.invite_url,
-      members: viewer.team.members || [],
-      isFull: viewer.team.user_limit <= viewer.team.members.length,
+      members: members,
+      numberOfMembers: members.length,
+      capacity: viewer.team.user_limit,
+      isFull: viewer.team.user_limit <= members.length,
+      accountSlug: undefined,
+      stripeCustomerId: undefined,
     }
   } else if (!isEmpty(viewer?.team_accounts)) {
     // Team Account that this user is an Owner of
     // *grab the first, assuming just one team account for now
     const team = viewer.team_accounts[0]
+    const members = team.members || []
 
     return {
       inviteUrl: `${process.env.NEXT_PUBLIC_DEPLOYMENT_URL}/team-invite/${team.invite_token}`,
-      members: team.members || [],
-      isFull: team.user_limit <= team.members.length,
+      members,
+      numberOfMembers: members.length,
+      capacity: team.user_limit,
+      isFull: team.user_limit <= members.length,
+      accountSlug: team.slug,
+      stripeCustomerId: team.stripe_customer_id,
     }
   }
   // implicitly return undefined if the user doesn't have a team
+}
+
+const TeamComposition = ({teamData}: {teamData: TeamData}) => {
+  const {numberOfMembers, capacity} = teamData
+
+  const valid =
+    typeof numberOfMembers === 'number' &&
+    numberOfMembers > 0 &&
+    typeof capacity === 'number' &&
+    capacity > 0
+
+  if (valid) {
+    return <span>{`(${numberOfMembers}/${capacity})`}</span>
+  } else {
+    return null
+  }
+}
+
+interface SubscriptionData {
+  portalUrl: string | undefined
+}
+
+const AtCapacityNotice = ({teamData}: {teamData: TeamData}) => {
+  const {accountSlug, stripeCustomerId} = teamData
+  const [
+    subscriptionData,
+    setSubscriptionData,
+  ] = React.useState<SubscriptionData>({} as SubscriptionData)
+
+  React.useEffect(() => {
+    if (stripeCustomerId) {
+      axios
+        .get(`/api/stripe/billing/session`, {
+          params: {
+            customer_id: stripeCustomerId,
+            account_slug: accountSlug,
+          },
+        })
+        .then(({data}) => {
+          if (data) {
+            setSubscriptionData(data)
+          }
+        })
+    }
+  }, [stripeCustomerId, accountSlug])
+
+  if (!teamData.isFull) {
+    return null
+  }
+
+  return (
+    <div
+      className="relative px-4 py-1 mt-4 mb-4 leading-normal text-orange-700 bg-orange-100 dark:text-orange-100 dark:bg-orange-800 rounded"
+      role="alert"
+    >
+      <span className="absolute inset-y-0 left-0 flex items-center ml-4">
+        <svg
+          className="w-6 h-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+          />
+        </svg>
+      </span>
+      <div className="ml-8 flex flex-col space-y-2 p-2">
+        <span>
+          Your team account is full. You can add more seats to your account
+          through the Stripe Billing Portal.
+        </span>
+        {subscriptionData?.portalUrl && (
+          <Link href={subscriptionData.portalUrl}>
+            <a
+              onClick={() => {
+                track(`clicked manage membership`)
+              }}
+              className="transition-all duration-150 ease-in-out text-white font-semibold rounded-md"
+            >
+              Visit Stripe Billing Portal
+            </a>
+          </Link>
+        )}
+      </div>
+    </div>
+  )
 }
 
 const Team = () => {
@@ -68,7 +177,10 @@ const Team = () => {
               label={true}
             />
           </div>
-          <h3>Current Team Members:</h3>
+          <AtCapacityNotice teamData={teamData} />
+          <h3>
+            Current Team Members <TeamComposition teamData={teamData} />
+          </h3>
           <ul>
             {teamData.members.map((member: any) => {
               return <li key={member.email}>{member.email}</li>
