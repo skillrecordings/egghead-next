904737b20bf6b04b5f715c05b19d2b65cbdf3004
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-03-30 16:20:53 +0100

    added styles on individual pages

--- src/pages/great-developer-portfolios/[slug].tsx
+++ src/pages/great-developer-portfolios/[slug].tsx
@@ -49,14 +49,45 @@ const Portfolio = (props: any) => {
       />
       <article className="mx-auto max-w-screen-lg lg:mt-14 md:mt-8 mt-3 mb-16">
         <header>
-          <p className="mb-10 text-gray-400">
+          <div className="flex mb-12 text-gray-400 rounded-full border border-gray-300 inline-flex px-3 py-1 transition duration-200 hover:border-blue-400 hover:text-blue-600 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 mr-1"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
             <Link href="/great-developer-portfolios">
               Back to all portfolios
             </Link>
-          </p>
-          <h1 className="max-w-screen-md lg:text-6xl md:text-5xl sm:text-4xl text-3xl w-full font-bold mb-8 lg:mb-10 leading-tighter">
-            {title}
-          </h1>
+          </div>
+          <div className="flex flex-col md:flex-row items-baseline justify-between mb-7">
+            <h1 className="max-w-screen-md lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-bold leading-tighter">
+              {title}
+            </h1>
+            <div className="inline-flex items-center text-gray-400 transition duration-200  hover:text-blue-600 md:pt-0 pt-2 sm:text-base text-sm">
+              <svg
+                className="h-4 w-4 mr-1"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+                />
+              </svg>
+              <Link href={url}>{url}</Link>
+            </div>
+          </div>
           {image && (
             <div className="mt-4">
               <Image
@@ -70,7 +101,7 @@ const Portfolio = (props: any) => {
             </div>
           )}
         </header>
-        <main className="prose dark:prose-dark sm:prose-lg lg:prose-xl mt-5 max-w-none">
+        <main className="prose dark:prose-dark sm:prose-lg lg:prose-xl mt-10 max-w-3xl mx-auto">
           {description}
         </main>
       </article>
@@ -81,7 +112,8 @@ const Portfolio = (props: any) => {
 const query = groq`*[_type == "resource" && slug.current == $slug][0]{
   image,
   title,
-  description
+  description,
+  url
 }`
 
 export async function getStaticProps(context: any) {
@@ -132,7 +164,6 @@ export async function getStaticPaths() {
       },
     }
   })
-  console.log(allPortfolios)
 
   return {
     paths,
