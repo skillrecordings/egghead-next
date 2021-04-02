4f5f2e84876d790c54d9b78171df3b68f217c028
Lauro <hey@laurosilva.com>
2021-04-02 15:28:30 -0700

    Updating ogimage to reflect egghead

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const IndexPage: FunctionComponent = () => {
           images: [
             {
               url:
-                'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1615844448/egghead-next-pages/build-modern-layouts-with-css-grid/og-image.png',
+                'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1617402374/egghead-next-pages/build-modern-layouts-with-css-grid/egghead.io-build-modern-layouts-with-css-grid_2x.png',
             },
           ],
         }}
