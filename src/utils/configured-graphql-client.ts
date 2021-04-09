592539ef56730eb58aa523c04ceca0529fe0dabd
ijones16 <jones58ian@gmail.com>
2021-04-09 13:15:40 -0400

    make token optional

--- src/utils/configured-graphql-client.ts
+++ src/utils/configured-graphql-client.ts
@@ -2,7 +2,7 @@ import {GraphQLClient} from 'graphql-request'
 import config from '../lib/config'
 import getAccessTokenFromCookie from './get-access-token-from-cookie'
 
-export const getGraphQLClient = (token: string) => {
+export const getGraphQLClient = (token?: string) => {
   const graphQLClient = new GraphQLClient(config.graphQLEndpoint, {
     headers: config.headers,
   })
