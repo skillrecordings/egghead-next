320397ebbca0f7389d789134649c0b5ab61ea495
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-04-16 11:56:22 +0100

    mocked up whats new layouts

--- src/components/pages/home/featured/whats-new.tsx
+++ src/components/pages/home/featured/whats-new.tsx
@@ -1,12 +1,7 @@
 import React, {FunctionComponent} from 'react'
 import {CardResource} from 'components/pages/home/card'
 import Jumbotron from 'components/pages/home/jumbotron'
-import Markdown from 'react-markdown'
-import Link from 'next/link'
-import Image from 'next/image'
-import {get} from 'lodash'
-import {bpMinMD} from 'utils/breakpoints'
-import {track} from 'utils/analytics'
+import CardHorizontal from 'components/pages/home/card/card-horizontal'
 
 type WhatsNewProps = {
   jumbotron: CardResource
@@ -23,13 +18,27 @@ const WhatsNew: FunctionComponent<WhatsNewProps> = ({
         What's New
       </h2>
       <Jumbotron resource={jumbotron} />
-      <div className="grid grid-cols-12 grid-rows-3 gap-4">
-        <div className="bg-gray-200 w-full h-auto row-span-2 col-span-7" />
-        {/* <CardHorizontal resource={featuredResources[0]} /> */}
-        <div className="bg-gray-200 w-full h-auto row-span-2 col-span-5" />
-        <div className="bg-gray-200 w-full h-40 row-span-1 col-span-4" />
-        <div className="bg-gray-200 w-full h-40 row-span-1 col-span-4" />
-        <div className="bg-gray-200 w-full h-40 row-span-1 col-span-4" />
+      <div className="grid grid-cols-12 grid-rows-2 gap-4">
+        <CardHorizontal
+          className="h-auto row-span-2 w-full col-span-7"
+          resource={featuredResources[0]}
+        />
+        <CardHorizontal
+          className="w-full h-auto col-span-5"
+          resource={featuredResources[1]}
+        />
+        <CardHorizontal
+          className="w-full row-span-1 col-span-5"
+          resource={featuredResources[0]}
+        />
+        <CardHorizontal
+          className="w-full row-span-1 col-span-6"
+          resource={featuredResources[1]}
+        />
+        <CardHorizontal
+          className="w-full row-span-1 col-span-6"
+          resource={featuredResources[1]}
+        />
       </div>
     </section>
   )
