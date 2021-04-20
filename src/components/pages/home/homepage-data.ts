b5a77a3baaefdd1091195118dfcebb302776f42d
Zac Jones <zacjones93@gmail.com>
2021-04-20 13:53:58 -0400

    Update homepage-data.ts

--- src/components/pages/home/homepage-data.ts
+++ src/components/pages/home/homepage-data.ts
@@ -8,8 +8,8 @@ const homepageData = {
       'https://d2eip9sf3oo6c2.cloudfront.net/playlists/square_covers/000/437/276/full/EGH_ghost-aws_1000.png',
     background:
       'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1618861248/egghead-next-pages/deploy-ghost-to-aws-using-rds-and-ec2/background-deploy-ghost-to-aws-using-rds-and-ec2_2x.png',
-    path: '/courses/creating-a-digital-garden-cli-with-rust-34b8',
-    slug: 'creating-a-digital-garden-cli-with-rust-34b8',
+    path: '/courses/deploy-ghost-to-aws-using-rds-and-ec2-a3487caa',
+    slug: 'deploy-ghost-to-aws-using-rds-and-ec2-a3487caa',
     instructor: {
       name: 'Sam Julien',
       slug: 'sam-julien',
