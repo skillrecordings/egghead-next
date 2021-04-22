026c3e1fe2d8d10fc9517e99fba7ee7c1991b49e
Lauro <hey@laurosilva.com>
2021-04-21 21:43:14 -0700

    sanity queries

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import groq from 'groq'
 import {sanityClient} from 'utils/sanity-client'
 import staticHomePageData from 'components/pages/home/homepage-data'
 import {digitalGardeningQuery} from './learn/digital-gardening'
+import {developerPortfolioQuery} from './learn/developer-portfolio'
 
 const IndexPage: FunctionComponent = ({homePageData}: any) => {
   return (
@@ -34,6 +35,7 @@ export default IndexPage
 const featureQuery = groq`
 {
   'featureDigitalGardening': ${digitalGardeningQuery},
+  'featureDeveloperPortfolio': ${developerPortfolioQuery}
 }
 `
 
---
+++ src/pages/learn/developer-portfolio/index.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import groq from 'groq'
+import {sanityClient} from 'utils/sanity-client'
+
+const developerPortfolio: React.FC<any> = ({data}) => {
+  return <h1>{data.title}</h1>
+}
+
+export default developerPortfolio
+
+export const developerPortfolioQuery = groq`*[_type == 'resource' && slug.current == "build-business-oriented-portfolio"][0]{
+  title,
+  description,
+ 	"slug": slug.current,
+  "clubs": resources[0].resources[]{
+      title,
+      subTitle,
+      "slug": slug.current,
+      meta,
+      image
+	}
+}`
+
+export const reduxClubQuery = groq`*[_type == 'resource' && slug.current == "build-business-oriented-portfolio"][0]{
+  "redux": resources[0].resources[slug.current == "redux"][0]{
+      title,
+      subTitle,
+      "slug": slug.current,
+      meta,
+      image
+    }
+}`
+
+export const xStateClubQuery = groq`*[_type == 'resource' && slug.current == "build-business-oriented-portfolio"][0]{
+  "xstate": resources[0].resources[slug.current == "xstate"][0]{
+      title,
+      subTitle,
+      "slug": slug.current,
+      meta,
+      image
+    }
+}`
+
+export async function getStaticProps() {
+  const data = await sanityClient.fetch(developerPortfolioQuery)
+
+  return {
+    props: {
+      data,
+    },
+  }
+}
