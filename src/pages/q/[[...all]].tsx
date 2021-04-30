8d16d89bc7961614b99b6f928a8331ec7c475851
Zac Jones <zacjones93@gmail.com>
2021-04-30 14:26:08 -0400

    add typescript course CTA

--- src/pages/q/[[...all]].tsx
+++ src/pages/q/[[...all]].tsx
@@ -196,9 +196,13 @@ export const getServerSideProps: GetServerSideProps = async function ({
 
   if (selectedTopics?.length === 1 && !selectedTopics.includes('undefined')) {
     const topic = first<string>(selectedTopics)
+
+    console.log({selectedTopics})
     try {
       if (topic) {
         initialTopic = await loadTag(topic)
+
+        console.log({topic})
       }
     } catch (error) {
       console.error(error)
