2c8bfa1981fd3d8aefe909b34a8b5771214fef1d
Lauro <hey@laurosilva.com>
2021-05-17 12:14:41 -0700

    update padding

--- src/components/pages/home/jumbotron/index.tsx
+++ src/components/pages/home/jumbotron/index.tsx
@@ -30,7 +30,7 @@ const Jumbotron: FunctionComponent<JumbotronProps> = ({
     >
       {/* <div className="absolute top-0 left-0 bg-gradient-to-r from-orange-500 to-purple-500 w-full h-2 z-20" /> */}
       <div className="relative z-10 px-5 sm:py-16 py-10 sm:text-left text-center">
-        <div className="space-y-5 mx-auto flex items-center justify-center max-w-screen-xl lg:px-8 w-full sm:mb-0 mb-12">
+        <div className="space-y-5 mx-auto flex items-center justify-center max-w-screen-xl lg:px-8 w-full sm:mb-4 md:my-12 lg:m-0 mt-0 mb-15">
           <div className="flex lg:flex-row flex-col items-center justify-center sm:space-x-10 sm:space-y-0 space-y-5 0 w-full xl:pr-16">
             <div className="flex-shrink-0">
               <Link href={path}>
