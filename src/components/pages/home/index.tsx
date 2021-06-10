a0ea77ab3e91e660d6a992f144203db07e3010f4
Zac Jones <zacjones93@gmail.com>
2021-06-10 10:16:40 -0400

    center images and adjust padding

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -137,7 +137,7 @@ const Home: FunctionComponent<any> = ({homePageData}) => {
           </h2>
           <div className="grid lg:grid-cols-3 grid-cols-1 space-y-3 lg:space-y-0 gap-4">
             <VerticalResourceCollectionCard
-              className="sm:py-3 py-2"
+              className="sm:py-8 py-6"
               resource={getStarted}
               location={location}
             />
@@ -185,6 +185,7 @@ const Home: FunctionComponent<any> = ({homePageData}) => {
                     key={resource.path}
                     resource={resource}
                     location={location}
+                    className="text-center"
                   />
                 )
               })}
@@ -217,7 +218,7 @@ const Home: FunctionComponent<any> = ({homePageData}) => {
             <div className="px-5 sm:py-16 py-10 sm:text-left text-center">
               <div className="space-y-5 mx-auto flex items-center justify-center lg:px-8 w-full">
                 <div className="flex lg:flex-row flex-col sm:space-x-12 sm:space-y-0 space-y-5 0 w-full xl:pr-16">
-                  <div className="flex-shrink-0">
+                  <div className="flex-shrink-0 text-center mb-4">
                     <Link href={featureDigitalGardening.path}>
                       <a
                         tabIndex={-1}
