438214f5aa0afa22e93daf850b406482635abc60
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-06-11 21:18:35 +0300

    styles for full screen

--- src/pages/video-test.tsx
+++ src/pages/video-test.tsx
@@ -61,6 +61,7 @@ const EggheadPlayer: React.FC<{videoResource: VideoResource}> = ({
               crossOrigin="anonymous"
               className="font-sans"
               poster={videoResource.poster}
+              // className="cueplayer-react-fullscreen"
             >
               <BigPlayButton position="center" />
               <HLSSource isVideoChild src={videoResource.hls_url} />
