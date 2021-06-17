dae8079b29632f5117a5eb35e6bbb7b9440ab7c6
Joel Hooks <joelhooks@gmail.com>
2021-06-17 14:18:43 -0700

    small visual cueplayer tweaks with state

--- src/pages/video-test.tsx
+++ src/pages/video-test.tsx
@@ -149,7 +149,8 @@ const EggheadPlayer: React.FC<{
 }
 
 const NotesTabContent: React.FC<{cues: VTTCue[]}> = ({cues}) => {
-  const {player} = usePlayer()
+  const {player, manager} = usePlayer()
+  const actions = manager.getActions()
   const disabled: boolean = isEmpty(cues)
   const scrollableNodeRef: any = React.createRef()
 
@@ -184,7 +185,12 @@ const NotesTabContent: React.FC<{cues: VTTCue[]}> = ({cues}) => {
                 </ReactMarkdown>
               )}
               {cue.startTime && (
-                <div className="w-full flex items-baseline justify-end pt-3 text-gray-900 dark:text-white">
+                <div
+                  onClick={() => {
+                    actions.seek(cue.startTime)
+                  }}
+                  className="w-full cursor-pointer underline flex items-baseline justify-end pt-3 text-gray-900 dark:text-white"
+                >
                   <time className="text-xs opacity-60 font-medium">
                     {convertTime(cue.startTime)}
                   </time>
