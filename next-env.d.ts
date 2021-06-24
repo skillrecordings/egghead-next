5eeeb3e60596fdcc23374c802977f52717816b19
Joel Hooks <joelhooks@gmail.com>
2021-06-24 16:10:55 -0700

    emotion type stuff

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
-/// <reference types="@emotion/react/types/css-prop" />
