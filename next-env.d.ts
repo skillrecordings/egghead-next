e7b53674c30d4a16cb807d30da0ecd4e8dc981c5
Joel Hooks <joelhooks@gmail.com>
2021-06-24 15:52:02 -0700

    update with css prop thing

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
+/// <reference types="@emotion/react/types/css-prop" />
