155375395745783465d6b22e2ea0ac3d08e322d6
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-07-01 19:53:24 +0300

    refactor autoplay

--- src/components/player/autoplay-control.tsx
+++ src/components/player/autoplay-control.tsx
@@ -18,67 +18,70 @@ const AutoplayControl: FunctionComponent<AutoplayControlProps> = ({
   const {autoplay, setPlayerPrefs} = useEggheadPlayerPrefs()
 
   return (
-    <div className="flex items-center space-x-1 px-3">
-      <div
-        className={`${
-          onDark ? 'text-gray-300' : ''
-        } text-xxs transition-colors ease-in-out duration-200 leading-normal ${
+    <div className="flex px-3">
+      <label
+        htmlFor="autoplay"
+        className={`text-white text-xxs transition-colors ease-in-out duration-200 leading-normal space-x-1 flex items-center flex-nowrap cursor-pointer ${
           !enabled && 'opacity-30'
         }`}
       >
-        Autoplay
-      </div>
-      <button
-        onClick={() => {
-          if (enabled) {
-            track(`clicked toggle autoplay`, {
-              state: !autoplay ? 'off' : 'on',
-            })
-            setPlayerPrefs({autoplay: !autoplay})
-            if (!autoplay) {
-              if (player) {
-                player.play()
-              }
-            } else {
-              if (player) {
-                player.pause()
+        <span>Autoplay</span>
+        <button
+          onClick={() => {
+            if (enabled) {
+              track(`clicked toggle autoplay`, {
+                state: !autoplay ? 'off' : 'on',
+              })
+              setPlayerPrefs({autoplay: !autoplay})
+              if (!autoplay) {
+                if (player) {
+                  player.play()
+                }
+              } else {
+                if (player) {
+                  player.pause()
+                }
               }
             }
-          }
-        }}
-        type="button"
-        name="autoplay"
-        id="autoplay"
-        aria-pressed="false"
-        className="flex items-center group space-x-1"
-      >
-        <div
-          className={`${
-            enabled && autoplay
-              ? 'bg-blue-600 group-hover:bg-blue-500'
-              : `${
-                  onDark
-                    ? 'bg-gray-700 group-hover:bg-gray-600'
-                    : 'bg-gray-200 group-hover:bg-gray-300'
-                }`
-          } relative inline-flex flex-shrink-0 h-4 w-8 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
+          }}
+          type="button"
+          name="autoplay"
+          id="autoplay"
+          aria-pressed="false"
+          data-action="aria-switch"
+          aria-label="Toggle Autoplay"
+          aria-checked="false"
+          role="switch"
+          className="flex items-center group space-x-1"
         >
-          {enabled && (
-            <span className="sr-only">
-              {autoplay ? 'Turn autoplay off' : 'Turn autoplay on'}
-            </span>
-          )}
-
-          <span
-            aria-hidden="true"
+          <div
             className={`${
-              enabled && autoplay ? 'translate-x-4' : 'translate-x-0'
-            } inline-block h-3 w-3 rounded-full ${
-              onDark ? 'bg-gray-200' : 'bg-white'
-            } shadow transform ring-0 transition ease-in-out duration-200`}
-          />
-        </div>
-      </button>
+              enabled && autoplay
+                ? 'bg-blue-600 group-hover:bg-blue-500'
+                : `${
+                    onDark
+                      ? 'bg-gray-700 group-hover:bg-gray-600'
+                      : 'bg-gray-200 group-hover:bg-gray-300'
+                  }`
+            } relative inline-flex flex-shrink-0 h-4 w-8 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
+          >
+            {enabled && (
+              <span className="sr-only">
+                {autoplay ? 'Turn autoplay off' : 'Turn autoplay on'}
+              </span>
+            )}
+
+            <span
+              aria-hidden="true"
+              className={`${
+                enabled && autoplay ? 'translate-x-4' : 'translate-x-0'
+              } inline-block h-3 w-3 rounded-full ${
+                onDark ? 'bg-gray-200' : 'bg-white'
+              } shadow transform ring-0 transition ease-in-out duration-200`}
+            />
+          </div>
+        </button>
+      </label>
     </div>
   )
 }
