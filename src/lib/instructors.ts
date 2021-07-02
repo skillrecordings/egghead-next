1a5e59a98f5a94be900a8af10216514f174b12aa
Lucas Minter <lsminteris@gmail.com>
2021-07-02 12:47:37 -0700

    Curated Hiroko's instructor page (#707)
    * curated hiroko instructor page
    
    * fixed and updated with Zacs suggestions
    
    * fix CTA text color in light mode
    
    Co-authored-by: Zac Jones <zacjones93@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- src/lib/instructors.ts
+++ src/lib/instructors.ts
@@ -5,6 +5,7 @@ import {stephanieEcklesQuery} from 'components/search/instructors/stephanie-eckl
 import {kamranAhmedQuery} from 'components/search/instructors/kamran-ahmed'
 import {alexReardonQuery} from 'components/search/instructors/alex-reardon'
 import {kevinCunninghamQuery} from 'components/search/instructors/kevin-cunningham'
+import {hirokoNishimuraQuery} from 'components/search/instructors/hiroko-nishimura'
 import {kristianFreemanQuery} from 'components/search/instructors/kristian-freeman'
 import {christianNwambaQuery} from 'components/search/instructors/christian-nwamba'
 
@@ -56,6 +57,7 @@ const sanityInstructorHash = {
   'kamran-ahmed': kamranAhmedQuery,
   'alex-reardon': alexReardonQuery,
   'kevin-cunningham': kevinCunninghamQuery,
+  'hiroko-nishimura': hirokoNishimuraQuery,
   'kristian-freeman': kristianFreemanQuery,
   'christian-nwamba': christianNwambaQuery,
 }
