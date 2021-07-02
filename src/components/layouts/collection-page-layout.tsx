f19d7f79104e7370537628d0376d2acf81d3276e
Vojta Holik <vojta@egghead.io>
2021-07-02 14:42:18 +0200

    mobile tweaks

--- src/components/layouts/collection-page-layout.tsx
+++ src/components/layouts/collection-page-layout.tsx
@@ -357,6 +357,34 @@ const CollectionPageLayout: React.FunctionComponent<CoursePageLayoutProps> = ({
     )
   }
 
+  const EpicReactBanner = ({
+    image = 'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1625226638/epic-react/summer-sale-2021/banner-course-page_2x.jpg',
+    width = 1416,
+    height = 508,
+  }) => {
+    return get(course, 'owner.id') === 15369 ? (
+      <ExternalTrackedLink
+        eventName="clicked epic react banner"
+        params={{location: course.path}}
+        href="https://epicreact.dev"
+        target="_blank"
+        rel="noopener"
+        className="block"
+      >
+        <div className="overflow-hidden flex items-center justify-center rounded-lg">
+          <Image
+            src={image}
+            alt="Get Really Good at React on EpicReact.dev by Kent C. Dodds"
+            width={width}
+            height={height}
+            quality={100}
+            className="hover:scale-[102%] transform ease-in-out duration-500"
+          />
+        </div>
+      </ExternalTrackedLink>
+    ) : null
+  }
+
   return (
     <>
       <NextSeo
@@ -639,27 +667,7 @@ const CollectionPageLayout: React.FunctionComponent<CoursePageLayoutProps> = ({
                   <h2 className="text-lg font-semibold mb-3">
                     You might also like these resources:
                   </h2>
-                  {get(course, 'owner.id') === 15369 && (
-                    <ExternalTrackedLink
-                      eventName="clicked epic react banner"
-                      params={{location: course.path}}
-                      href="https://epicreact.dev"
-                      target="_blank"
-                      rel="noopener"
-                      className="block"
-                    >
-                      <div className="overflow-hidden flex items-center justify-center rounded-lg">
-                        <Image
-                          src="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1625226638/epic-react/summer-sale-2021/banner-course-page_2x.jpg"
-                          alt="Get Really Good at React on EpicReact.dev by Kent C. Dodds"
-                          width={1416}
-                          height={508}
-                          quality={100}
-                          className="hover:scale-[102%] transform ease-in-out duration-500"
-                        />
-                      </div>
-                    </ExternalTrackedLink>
-                  )}
+                  <EpicReactBanner />
                   {relatedResources.map((resource: any) => {
                     return (
                       <div key={resource.slug}>
@@ -935,6 +943,11 @@ const CollectionPageLayout: React.FunctionComponent<CoursePageLayoutProps> = ({
                 <h2 className="text-lg font-semibold mb-3">
                   You might also like these resources:
                 </h2>
+                <EpicReactBanner
+                  image="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1625226676/epic-react/summer-sale-2021/banner-react-page_2x.jpg"
+                  width={916 / 2}
+                  height={1024 / 2}
+                />
                 {pairWithResources.map((resource: any) => {
                   return (
                     <div key={resource.slug}>
