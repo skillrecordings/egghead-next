70daf7dec826d77131d7e708b15ddc8cf7af726a
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-07-13 11:02:32 +0300

    wip

--- src/pages/lessons/[slug].tsx
+++ src/pages/lessons/[slug].tsx
@@ -4,6 +4,7 @@ import {useRouter} from 'next/router'
 import {isEmpty, get, first, isFunction, filter} from 'lodash'
 import {useMachine} from '@xstate/react'
 import {Tabs, TabList, Tab, TabPanels, TabPanel} from '@reach/tabs'
+import VisuallyHidden from '@reach/visually-hidden'
 import {playerMachine} from 'machines/lesson-player-machine'
 import {useEggheadPlayer} from 'components/EggheadPlayer'
 import {useEggheadPlayerPrefs} from 'components/EggheadPlayer/use-egghead-player'
@@ -184,7 +185,7 @@ const Lesson: FunctionComponent<LessonProps> = ({initialLesson}) => {
         'loaded',
         'viewing',
         'completed',
-        'addingNote',
+        // 'addingNote',
       ].includes(currentPlayerState),
     )
   }, [currentPlayerState])
@@ -563,35 +564,40 @@ const Lesson: FunctionComponent<LessonProps> = ({initialLesson}) => {
                 </OverlayWrapper>
               )}
               {playerState.matches('addingNote') && (
-                <div className="absolute inset-0 z-50 bg-black bg-opacity-70 p-4 flex justify-center items-center text-black">
-                  <div className="w-[34rem] h-[20rem] rounded-md bg-white p-4 flex flex-col">
-                    <div className="flex-shrink-0 flex justify-end">
-                      <button
-                        type="button"
-                        onClick={() => send('VIEW')}
-                        className="text-gray-400 hover:text-gray-500"
-                      >
-                        <IconX />
-                      </button>
-                    </div>
-                    <div className="flex-grow my-2 overflow-hidden">
-                      <textarea
-                        className="w-full h-full resize-none border-none p-0 placeholder-gray-400"
-                        placeholder={`Write a note...\n\nYou can use markdown to [add links](https://stackoverflow.com), **bold text** or \`write syntax\``}
-                      />
-                    </div>
-                    <div className="flex-shrink-0 flex justify-between items-end">
-                      <span>Publicly visible</span>
-                      <button
-                        type="button"
-                        onClick={() => console.log('add note')}
-                        className="inline-flex justify-center items-center px-4 py-2 rounded-md bg-blue-600 text-white transition-all hover:bg-blue-700 ease-in-out duration-200"
-                      >
-                        Add to notes
-                      </button>
+                <OverlayWrapper>
+                  <div className="absolute inset-0 z-50 bg-black bg-opacity-70 flex justify-center items-center text-black">
+                    <div className="w-[34rem] h-[20rem] rounded-md bg-white p-4 flex flex-col">
+                      <div className="flex-shrink-0 flex justify-end">
+                        <button
+                          type="button"
+                          onClick={() => send('VIEW')}
+                          className="text-gray-400 hover:text-gray-500"
+                        >
+                          <VisuallyHidden>Close</VisuallyHidden>
+                          <span aria-hidden="true">
+                            <IconX />
+                          </span>
+                        </button>
+                      </div>
+                      <div className="flex-grow my-2 overflow-hidden">
+                        <textarea
+                          className="w-full h-full resize-none border-none p-0 placeholder-gray-400"
+                          placeholder={`Write a note...\n\nYou can use markdown to [add links](https://stackoverflow.com), **bold text** or \`write syntax\``}
+                        />
+                      </div>
+                      <div className="flex-shrink-0 flex justify-between items-end">
+                        <span>Publicly visible</span>
+                        <button
+                          type="button"
+                          onClick={() => console.log('add note')}
+                          className="inline-flex justify-center items-center px-4 py-2 rounded-md bg-blue-600 text-white transition-all hover:bg-blue-700 ease-in-out duration-200"
+                        >
+                          Add to notes
+                        </button>
+                      </div>
                     </div>
                   </div>
-                </div>
+                </OverlayWrapper>
               )}
             </div>
             <div className="lg:col-span-3 side-bar">
