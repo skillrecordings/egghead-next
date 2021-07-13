022525c230af96444daa0fcf6258293f83dcbc61
Parker Landon <parkerjlandon@gmail.com>
2021-07-13 13:54:36 -0700

    Toggle button overhaul!

--- src/components/app/footer.tsx
+++ src/components/app/footer.tsx
@@ -144,52 +144,60 @@ const DarkModeToggle = () => {
   const {subscriber, cioIdentify} = useCio()
   const {theme, setTheme} = useTheme()
   React.useEffect(() => setMounted(true), [])
-
+  const handleClick = () => {
+    const nextTheme = theme === 'dark' ? 'light' : 'dark'
+    setTheme(nextTheme)
+    track(`toggled dark mode`, {
+      mode: nextTheme,
+    })
+    if (subscriber) {
+      cioIdentify(subscriber.id, {
+        theme_preference: nextTheme,
+      })
+    }
+  }
   return (
-    <button
-      aria-label="Toggle Dark Mode"
-      type="button"
-      className="rounded p-3 h-10 w-10"
-      onClick={() => {
-        const nextTheme = theme === 'dark' ? 'light' : 'dark'
-        setTheme(nextTheme)
-        track(`toggled dark mode`, {
-          mode: nextTheme,
-        })
-        if (subscriber) {
-          cioIdentify(subscriber.id, {
-            theme_preference: nextTheme,
-          })
-        }
-      }}
-    >
-      {mounted && (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          stroke="currentColor"
-          className="h-4 w-4 text-gray-800 dark:text-gray-200"
+    <div className="flex justify-between items-center">
+      <h2 className="mr-3">{theme === 'dark' ? 'Dark' : 'Light'} Mode</h2>
+      <div
+        className="w-16 h-10 bg-gray-300 dark:bg-gray-1000 rounded-full flex-shrink-0 p-1"
+        onClick={handleClick}
+        aria-label="Toggle Dark Mode"
+        role="button"
+      >
+        <div
+          className={`bg-white w-8 h-8 rounded-full shadow-md duration-300 ease-in-out flex items-center justify-center dark:bg-gray-800 ${
+            theme === 'dark' ? 'transform translate-x-6' : ''
+          }`}
         >
-          {theme === 'dark' ? (
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-            />
-          ) : (
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-            />
+          {mounted && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              stroke="currentColor"
+              className="h-4 w-4 text-gray-400 dark:text-gray-200"
+            >
+              {theme === 'dark' ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              )}
+            </svg>
           )}
-        </svg>
-      )}
-    </button>
+        </div>
+      </div>
+    </div>
   )
 }
-
 export default Footer
