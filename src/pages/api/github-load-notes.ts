312b69fb3e7124aeb40aa5df88e999ce1bb148eb
Vojta Holik <vojta@egghead.io>
2021-07-31 17:41:13 +0200

    ch -> node

--- src/pages/api/github-load-notes.ts
+++ src/pages/api/github-load-notes.ts
@@ -64,7 +64,9 @@ export async function loadNotesFromUrl(url: string) {
         }
       }, {})
       note.type = 'root'
-      const contents = note.children.map((ch: any) => toMarkdown(ch)).join(' ')
+      const contents = note.children
+        .map((node: any) => toMarkdown(node))
+        .join(' ')
       return {
         text: contents.trim(),
         ...attributes,
