a151250c25b0338f86ca0da0210ebc1230d6b392
Lauro <hey@laurosilva.com>
2021-08-03 11:59:55 -0700

    delete location

--- src/pages/learn/state-management/index.tsx
+++ src/pages/learn/state-management/index.tsx
@@ -9,14 +9,17 @@ import {track} from 'utils/analytics'
 import {CardResource} from 'types'
 import {bpMinMD} from 'utils/breakpoints'
 import {get} from 'lodash'
+import VideoCard from 'components/pages/home/video-card'
+import {VerticalResourceCard} from 'components/card/verticle-resource-card'
 
 const StateManagement: React.FC<any> = ({data}) => {
   return (
     <>
       <div className="sm:-my-5 -my-3 -mx-5 p-5 dark:bg-gray-900 bg-gray-50">
         <div className="mx-auto max-w-screen-xl grid gap-10">
-          <Jumbotron resource={data.jumbotron} textColor="text-green-400" />
-          <SecondarySection resource={data.startHere} />
+          <Jumbotron resource={data.jumbotron} />
+          <BigIdeasSection resource={data.theBigIdeas} />
+          <RecoilSection resource={data.recoilSection} />
         </div>
       </div>
     </>
@@ -27,22 +30,31 @@ export default StateManagement
 
 export const StateManagementQuery = groq`*[_type == 'resource' && slug.current == "state-management"][0]{
   title,
+  description,
   path,
   "jumbotron": {
     "background": images[0] {
     	url
   	},
-  	"name": subTitle,
     image,
     title,
-    description,
+  	description,
+  	"name": subTitle
   },
-	'startHere': {
-    'text1': content[type == 'start-here-text-1'][0]{
-      "description": text,
-      "title": label,
-    },
+  'theBigIdeas': resources[slug.current == 'the-big-ideas'][0]{
+    description,
+    title,
+    resources[] {
+    "name": subTitle,
+      ...
+    }
+ },
+ "recoilSection": resources[slug.current == 'state-management-with-recoil'][0]{
+  ...,
+  resources[]-> {
+    ...
   }
+ },
 }`
 
 export async function getStaticProps() {
@@ -55,25 +67,65 @@ export async function getStaticProps() {
   }
 }
 
-const SecondarySection = ({resource}: any) => {
-  const {text1, text2} = resource
+const RecoilSection = ({resource}: any) => {
+  const {title, description, resources, path} = resource
+
+  return (
+    <div className="mt-5 md:bg-gray-100 dark:bg-gray-700 rounded-lg py-10 p-5">
+      <Link href={path}>
+        <a
+          className="font-bold hover:text-blue-600 dark:hover:text-blue-300 transition ease-in-out"
+          onClick={() => {
+            track('clicked resource', {
+              resource: path,
+            })
+          }}
+        >
+          <h1 className="sm:text-2xl md:text-4xl text-xl font-extrabold leading-tighter text-center mb-4">
+            {title}
+          </h1>
+        </a>
+      </Link>
+
+      <Markdown
+        source={description}
+        allowDangerousHtml={true}
+        className="prose dark:prose-dark md:prose-lg md:dark:prose-lg-dark text-gray-900 dark:text-gray-100  mt-4 max-w-screen-md mx-auto text-center"
+      />
+      <div className="grid lg:grid-cols-3 grid-cols-1 gap-4 mt-10">
+        {resources.map((resource: any) => {
+          return (
+            <VerticalResourceCard
+              resource={resource}
+              className="mb-4 text-center"
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+const BigIdeasSection = ({resource}: any) => {
+  const {title, description, resources} = resource
+
   return (
     <div className="">
-      <div className="grid grid-cols-2 gap-10">
-        <div className="bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-50 rounded-lg shadow-sm px-5 sm:py-16 py-10">
+      <div className="grid lg:grid-cols-5 grid-cols-1 gap-10 bg-gray-100 dark:bg-gray-700 text-gray-700 dark:text-gray-50 rounded-lg shadow-sm px-5 sm:py-16 py-10 ">
+        <div className="col-span-2">
           <h1 className="sm:text-2xl md:text-4xl text-xl font-extrabold leading-tighter text-center mb-4">
-            {text1.title}
+            {title}
           </h1>
           <Markdown
-            source={text1.description}
+            source={description}
             allowDangerousHtml={true}
             className="prose dark:prose-dark md:prose-lg md:dark:prose-lg-dark text-gray-900 dark:text-gray-100  mt-4 max-w-screen-md"
           />
         </div>
-        <div className="bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-50 rounded-lg shadow-sm px-5 sm:py-16 py-10">
-          <h1 className="sm:text-2xl md:text-4xl text-xl font-extrabold leading-tighter text-center mb-4">
-            Big Ideas of State Management
-          </h1>
+        <div className="col-span-3">
+          {resources.map((resource: any) => {
+            return <VideoCard resource={resource} className="mb-4" />
+          })}
         </div>
       </div>
     </div>
@@ -82,7 +134,6 @@ const SecondarySection = ({resource}: any) => {
 
 type JumbotronProps = {
   resource: CardResource
-  textColor?: String
 }
 
 const Jumbotron: FunctionComponent<JumbotronProps> = ({resource}) => {
@@ -90,14 +141,13 @@ const Jumbotron: FunctionComponent<JumbotronProps> = ({resource}) => {
 
   return (
     <div
-      className="relative flex items-center justify-center bg-white text-white overflow-hidden rounded-lg shadow-sm bg-gradient-to-t dark:bg-gradient-to-t dark:from-gray-800 dark:to-gray-600"
+      className="relative flex items-center justify-center bg-white text-white overflow-hidden rounded-lg shadow-sm dark:bg-gray-800 "
       css={{
         [bpMinMD]: {
           minHeight: 477,
         },
       }}
     >
-      {/* <div className="absolute top-0 left-0 bg-gradient-to-r from-purple-500 to-indigo-600 w-full h-2 z-20" /> */}
       <div className="relative z-10 px-5 sm:py-16 py-10 sm:text-left text-center">
         <div className="space-y-5 mx-auto flex items-center justify-center max-w-screen-xl lg:px-8 w-full sm:mb-4 md:my-12 lg:m-0 mt-0 mb-15">
           <div className="flex lg:flex-row flex-col items-center justify-center sm:space-x-10 sm:space-y-0 space-y-5 0 w-full xl:pr-16">
@@ -112,16 +162,16 @@ const Jumbotron: FunctionComponent<JumbotronProps> = ({resource}) => {
               />
             </div>
             <div className="flex flex-col sm:items-start items-center w-full">
-              <h2 className="text-xs text-yellow-400 dark:text-yellow-300 uppercase font-semibold mb-2">
+              <h2 className="text-xs text-yellow-600 dark:text-yellow-300 uppercase font-semibold mb-2">
                 {name}
               </h2>
-              <h1 className="sm:text-2xl md:text-4xl text-xl max-w-screen-lg font-extrabold leading-tighter text-gray-50">
+              <h1 className="sm:text-2xl md:text-4xl text-xl max-w-screen-lg font-extrabold leading-tighter text-gray-900 dark:text-gray-50">
                 {title}
               </h1>
               <Markdown
                 source={description}
                 allowDangerousHtml={true}
-                className="mt-4 text-gray-200 text-base max-w-screen-sm opacity-80"
+                className="mt-4 dark:text-gray-200 text-gray-700 text-base max-w-screen-sm opacity-80"
               />
             </div>
           </div>
