fa0e51812c6c87ac63e374aefd0659cfbb31ff2c
Joel Hooks <joelhooks@gmail.com>
2021-08-04 10:35:52 -0700

    fix ts error

--- src/pages/api/users/current.ts
+++ src/pages/api/users/current.ts
@@ -1,12 +1,15 @@
 import axios from 'axios'
 import {NextApiRequest, NextApiResponse} from 'next'
 import {getTokenFromCookieHeaders, AUTH_DOMAIN} from 'utils/auth'
+import fetchEggheadUser from '../../../api/egghead/users/from-token'
 
 const current = async (req: NextApiRequest, res: NextApiResponse) => {
+  const {eggheadToken} = getTokenFromCookieHeaders(req.headers.cookie as string)
+
   if (req.method === 'GET' && eggheadToken) {
-    const {eggheadToken} = getTokenFromCookieHeaders(
-      req.headers.cookie as string,
-    )
+    const eggheadUser = fetchEggheadUser(eggheadToken)
+
+    res.status(200).json(eggheadUser)
   } else {
     res.status(200).end()
   }
