cc63506685cedc3c41e0a9c7a3fc22c1e3b838b4
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-09-08 14:42:29 +0100

    added rss and download dialogs

--- src/components/layouts/collection-page-layout.tsx
+++ src/components/layouts/collection-page-layout.tsx
@@ -31,7 +31,7 @@ import ClosedCaptionIcon from '../icons/closed-captioning'
 import {HorizontalResourceCard} from '../card/horizontal-resource-card'
 import ExternalTrackedLink from 'components/external-tracked-link'
 import {DialogOverlay, DialogContent} from '@reach/dialog'
-import {motion} from 'framer-motion'
+import slugify from 'slugify'
 
 type CoursePageLayoutProps = {
   lessons: any
@@ -642,16 +642,20 @@ const CollectionPageLayout: React.FunctionComponent<CoursePageLayoutProps> = ({
                       dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-4 py-2 border transition-colors text-sm xs:text-base ease-in-out opacity-90 shadow-sm"
                   >
                     <div className="flex flex-col space-y-4">
-                      <p className="max-w-10 text-center text-gray-700 mb-2">
+                      <p className="max-w-10 text-center text-gray-700">
                         As a Pro member you can download any of our courses and
-                        watch them offline.
+                        watch them offline. As a member you'll also get:
                       </p>
-                      <button className="font-semibold w-full mt-2 inline-flex justify-center items-center px-4 py-3 rounded-md bg-blue-600 text-white transition-all hover:bg-blue-700 ease-in-out duration-200">
+                      <PlanFeatures />
+
+                      <button className="font-semibold w-full mt-6 inline-flex justify-center items-center px-4 py-3 rounded-md bg-blue-600 text-white transition-all hover:bg-blue-700 ease-in-out duration-200">
                         Become a Member
                       </button>
-                      <a className="text-sm w-full inline-flex justify-center items-center pb-2 rounded-md text-blue-600 underline transition-all hover:text-blue-800 ease-in-out duration-200">
-                        Learn more about membership
-                      </a>
+                      <Link href="/pricing">
+                        <a className="text-sm w-full inline-flex justify-center items-center pb-2 rounded-md text-blue-600 underline transition-all hover:text-blue-800 ease-in-out duration-200">
+                          Learn more about membership
+                        </a>
+                      </Link>
                     </div>
                   </DialogButton>
                 )}
@@ -690,10 +694,21 @@ const CollectionPageLayout: React.FunctionComponent<CoursePageLayoutProps> = ({
                       dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-4 py-2 border transition-colors text-sm xs:text-base ease-in-out opacity-90 shadow-sm"
                     >
                       <div className="flex flex-col space-y-4">
-                        <p className="max-w-10 text-center text-gray-700 mb-2">
+                        <p className="max-w-10 text-center text-gray-700">
                           As a Pro member you can subscribe to any of our
-                          courses using an RSS feed.
+                          courses using an RSS feed. As a member you'll also
+                          get:
                         </p>
+                        <PlanFeatures />
+
+                        <button className="font-semibold w-full mt-6 inline-flex justify-center items-center px-4 py-3 rounded-md bg-blue-600 text-white transition-all hover:bg-blue-700 ease-in-out duration-200">
+                          Become a Member
+                        </button>
+                        <Link href="/pricing">
+                          <a className="text-sm w-full inline-flex justify-center items-center pb-2 rounded-md text-blue-600 underline transition-all hover:text-blue-800 ease-in-out duration-200">
+                            Learn more about membership
+                          </a>
+                        </Link>
                       </div>
                     </DialogButton>
                   </a>
@@ -1274,7 +1289,7 @@ const DialogButton = ({title, buttonStyles, buttonText, children}: any) => {
       <DialogOverlay
         isOpen={showDialog}
         onDismiss={closeDialog}
-        className="bg-black/50 backdrop-blur-sm flex justify-center items-center p-4 xs:p-6 z-50"
+        className="bg-black/50 backdrop-blur-sm flex justify-center items-center p-4 xs:px-6 xs:py-8 z-50"
       >
         <DialogContent
           aria-label="sign in to bookmark"
@@ -1282,7 +1297,7 @@ const DialogButton = ({title, buttonStyles, buttonText, children}: any) => {
         >
           <div className="w-full flex flex-col">
             {title && (
-              <h4 className="text-lg sm:text-xl mb-4 font-semibold text-center px-4">
+              <h4 className="text-lg sm:text-xl mb-4 font-semibold text-center px-4 leading-tight">
                 {title}
               </h4>
             )}
@@ -1303,6 +1318,47 @@ const DialogButton = ({title, buttonStyles, buttonText, children}: any) => {
         </DialogContent>
       </DialogOverlay>
     </>
+  )
+}
+
+const DEFAULT_FEATURES = [
+  'Full access to all premium courses and lessons',
+  'RSS course feeds for your favorite podcast app',
+  'Offline viewing',
+  'Commenting and support',
+  'Enhanced Transcripts',
+  'Closed captions for every video',
+]
+
+const PlanFeatures: React.FunctionComponent<{
+  planFeatures: string[]
+}> = ({planFeatures = DEFAULT_FEATURES}) => {
+  const CheckIcon = () => (
+    <svg
+      className="text-blue-500 inline-block flex-shrink-0 mt-1"
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+    >
+      <path
+        fill="currentColor"
+        d="M6.00266104,15 C5.73789196,15 5.48398777,14.8946854 5.29679603,14.707378 L0.304822855,9.71382936 C0.0452835953,9.46307884 -0.0588050485,9.09175514 0.0325634765,8.74257683 C0.123932001,8.39339851 0.396538625,8.12070585 0.745606774,8.02930849 C1.09467492,7.93791112 1.46588147,8.04203262 1.71655287,8.30165379 L5.86288579,12.4482966 L14.1675324,0.449797837 C14.3666635,0.147033347 14.7141342,-0.0240608575 15.0754425,0.00274388845 C15.4367507,0.0295486344 15.7551884,0.250045268 15.9074918,0.578881992 C16.0597953,0.907718715 16.0220601,1.29328389 15.8088932,1.58632952 L6.82334143,14.5695561 C6.65578773,14.8145513 6.38796837,14.9722925 6.09251656,15 C6.06256472,15 6.03261288,15 6.00266104,15 Z"
+      />
+    </svg>
+  )
+
+  return (
+    <ul className="px-4">
+      {planFeatures.map((feature: string) => {
+        return (
+          <li className="py-2 font-medium flex" key={slugify(feature)}>
+            <CheckIcon />
+            <span className="ml-3 leading-tight">{feature}</span>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
