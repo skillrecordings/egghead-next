2c9486dd7bbfbed4dbc6ff707ab8dab0a1d61c03
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-09-13 15:45:08 +0100

    added technologies row

--- src/pages/new-home/index.tsx
+++ src/pages/new-home/index.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react'
 import {motion} from 'framer-motion'
 
-const NewHome: React.FunctionComponent<LoginRequiredParams> = ({
-  loginRequired,
-}) => {
+const NewHome: React.FunctionComponent = () => {
   return (
-    <div className="mt-4 sm:mt-8 mb-28 max-w-screen-xl w-full mx-auto">
-      <section className="my-8 sm:my-16 flex flex-row justify-between items-center">
+    <div className="mt-4 sm:mt-8 mb-28 w-full">
+      <section className="my-8 sm:my-16 flex flex-row justify-between items-center max-w-screen-xl mx-auto">
         <div className="w-3/5">
           <h1 className="text-4xl sm:text-6xl font-bold mb-8 leading-tighter sm:leading-tighter  text-gray-700">
             Learn the best JavaScript tools and frameworks from industry pros
@@ -18,12 +16,6 @@ const NewHome: React.FunctionComponent<LoginRequiredParams> = ({
 
           {/* email input form */}
           <form className="mt-8">
-            <label
-              htmlFor="email"
-              className="block leading-5 text-sm text-gray-800 dark:text-gray-200"
-            >
-              Your email address
-            </label>
             <div className="flex flex-row items-center">
               <div className="mt-1 relative rounded-md shadow-sm w-80">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
@@ -54,12 +46,16 @@ const NewHome: React.FunctionComponent<LoginRequiredParams> = ({
               </button>
             </div>
           </form>
+          <p className="mt-2 text-gray-400 text-sm">
+            We guarantee we're 100% spam-free. Unsubscribe at any time.
+          </p>
         </div>
         <img
           alt="egghead course illustration"
           src="https://via.placeholder.com/400x400"
         />
       </section>
+      <TechnologyRow />
       <section className="my-12 sm:my-32">
         <h3>
           Obviously, you can pick up a new framework or a new language or a new
@@ -71,3 +67,53 @@ const NewHome: React.FunctionComponent<LoginRequiredParams> = ({
 }
 
 export default NewHome
+
+function TechnologyRow() {
+  return (
+    <section className="my-12 sm:my-32">
+      <div className="flex flex-row items-center">
+        {[
+          {
+            label: 'JavaScript',
+            image: 'https://via.placeholder.com/250x250',
+          },
+          {
+            label: 'React',
+            image: 'https://via.placeholder.com/250x250',
+          },
+          {
+            label: 'Angular',
+            image: 'https://via.placeholder.com/250x250',
+          },
+          {
+            label: 'Vue',
+            image: 'https://via.placeholder.com/250x250',
+          },
+          {
+            label: 'Node',
+            image: 'https://via.placeholder.com/250x250',
+          },
+          {
+            label: 'Gatsby',
+            image: 'https://via.placeholder.com/250x250',
+          },
+          {
+            label: 'Next.js',
+            image: 'https://via.placeholder.com/250x250',
+          },
+          {
+            label: 'Redux',
+            image: 'https://via.placeholder.com/250x250',
+          },
+        ].map((tech, i) => {
+          return (
+            <div className="flex flex-col items-center mx-6">
+              <img src={tech.image} alt={tech.label} className="" />
+              <p>{tech.label}</p>
+            </div>
+          )
+        })}
+      </div>
+    </section>
+  )
+}
