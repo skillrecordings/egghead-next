96a11391e69fdd63ff8f153089bf00eb394d6032
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-09-13 13:05:49 +0100

    styling updates

--- src/pages/new-home/index.tsx
+++ src/pages/new-home/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import {motion} from 'framer-motion'
 
 const NewHome: React.FunctionComponent<LoginRequiredParams> = ({
   loginRequired,
@@ -14,22 +15,44 @@ const NewHome: React.FunctionComponent<LoginRequiredParams> = ({
             egghead creates high-quality video tutorials and learning resources
             for badass web developers
           </h2>
+
           {/* email input form */}
-          <form className="mt-10 flex flex-row items-center">
-            <div className="flex flex-col">
-              <label className="text-sm font-bold" htmlFor="email">
-                Email address
-              </label>
-              <input
-                type="email"
-                className="w-60"
-                placeholder="yourname@email.com"
-                onChange={(e) => {}}
-              />
+          <form className="mt-8">
+            <label
+              htmlFor="email"
+              className="block leading-5 text-sm text-gray-800 dark:text-gray-200"
+            >
+              Your email address
+            </label>
+            <div className="flex flex-row items-center">
+              <div className="mt-1 relative rounded-md shadow-sm w-80">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <svg
+                    className="h-5 w-5 text-gray-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                  </svg>
+                </div>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="you@company.com"
+                  className="text-black autofill:text-fill-black py-3 placeholder-gray-400 focus:ring-indigo-500 focus:border-blue-500 block w-full pl-10 border-gray-300 rounded-md"
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="transition-all duration-150 mt-1 ml-2 ease-in-out bg-blue-600 hover:bg-blue-700 active:bg-blue-800 hover:shadow-sm text-white font-semibold py-3 px-5 rounded-md"
+              >
+                Get started for free
+              </button>
             </div>
-            <button className="w-30 px-6 mt-4" type="submit">
-              Get Started for Free
-            </button>
           </form>
         </div>
         <img
