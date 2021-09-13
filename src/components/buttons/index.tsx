c420e52e25b862a582a3bf241a8980ed95c76182
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-09-13 11:32:40 +0100

    replaced header cta with primary button component

--- src/components/buttons/index.tsx
+++ src/components/buttons/index.tsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import * as React from 'react'
+import Link from 'next/link'
+import noop from 'utils/noop'
 
-export function PrimaryButton({className = '', children}) {
+type ButtonProps = {
+  url: string
+  label: string
+  className?: string
+  onClick?: () => void
+}
+
+export const PrimaryButton: React.FC<ButtonProps> = ({
+  url,
+  className,
+  label,
+  onClick = noop,
+}) => {
   return (
-    <button
-      className={`${className} inline-flex justify-center items-center px-4 py-2 rounded-md bg-blue-600 text-white font-medium transition-all hover:bg-blue-800 ease-in-out duration-200`}
-    >
-      {children}
-    </button>
+    <Link href={url}>
+      <a
+        onClick={onClick}
+        className={`${className} inline-flex justify-center items-center px-4 py-2 rounded-md bg-blue-600 text-white font-medium transition-all hover:bg-blue-800 ease-in-out duration-200`}
+      >
+        {label}
+      </a>
+    </Link>
   )
 }
 
-export function SecondaryButton({className = '', children}) {
+export const Secondary: React.FC<ButtonProps> = ({
+  url,
+  className,
+  label,
+  onClick = noop,
+}) => {
   return (
-    <button
-      className={`${className} inline-flex justify-center items-center px-4 py-2 rounded-md bg-blue-600 text-white font-medium transition-all hover:bg-blue-800 ease-in-out duration-200`}
-    >
-      {children}
-    </button>
+    <Link href={url}>
+      <a
+        onClick={onClick}
+        className={`${className} inline-flex justify-center items-center px-4 py-2 rounded-md bg-blue-600 text-white font-medium transition-all hover:bg-blue-800 ease-in-out duration-200`}
+      >
+        {label}
+      </a>
+    </Link>
   )
 }
