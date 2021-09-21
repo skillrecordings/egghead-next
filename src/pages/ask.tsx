19ac16d060f467644343639bc6778f9f0fd45cc4
Joel Hooks <joelhooks@gmail.com>
2021-09-20 20:41:44 -0700

    test survey

--- src/pages/ask.tsx
+++ src/pages/ask.tsx
@@ -1,11 +1,20 @@
 import Survey from 'components/survey/survey'
 import * as React from 'react'
-import {sortingHatInitialState} from '../components/survey/survey-reducer'
+import {SurveyState} from '../components/survey/survey-reducer'
+import {testSurvey} from '../data/test-survey'
+
+export const testSurveyInitialState: SurveyState = {
+  currentQuestionKey: 'first_question',
+  answers: {},
+  closed: true,
+  data: testSurvey,
+  surveyTitle: 'test survey',
+}
 
 const Ask: React.FunctionComponent = () => {
   return (
     <div>
-      <Survey initialSurveyState={sortingHatInitialState} />
+      <Survey initialSurveyState={testSurveyInitialState} />
     </div>
   )
 }
