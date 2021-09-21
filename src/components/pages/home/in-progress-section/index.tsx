3520551413ab33328a19b8ece2a3d44c7bd2a0c8
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-09-21 15:28:41 +0100

    added empty state icon and survey checkboxes

--- src/components/pages/home/in-progress-section/index.tsx
+++ src/components/pages/home/in-progress-section/index.tsx
@@ -3,6 +3,7 @@ import {FunctionComponent} from 'react'
 import InProgressResource from 'components/pages/users/dashboard/activity/in-progress-resource'
 import {isEmpty} from 'lodash'
 import Spinner from 'components/spinner'
+import EmptyCourseIcon from 'components/icons/empty-course-icon'
 
 type InProgressSectionProps = {
   viewer: any
@@ -11,7 +12,9 @@ type InProgressSectionProps = {
   coursesInProgress: any
 }
 
-const showPlaceholder = true
+const showPlaceholder = false
+
+const showLoader = false
 
 const InProgressSection: FunctionComponent<InProgressSectionProps> = ({
   viewer,
@@ -60,15 +63,15 @@ const InProgressSection: FunctionComponent<InProgressSectionProps> = ({
           </div>
         </>
       )}
-      {isEmpty(progress) && (
+      {showLoader && (
         <div className="flex items-center justify-center w-full sm:py-16 py-10 sm:h-80 h-96 bg-gray-100 rounded-lg">
           <Spinner color="gray-500" />
         </div>
       )}
       {showPlaceholder && (
-        <div className="w-full sm:h-80 h-96">
+        <div className="w-full">
           <div className="flex justify-between align-text-top">
-            <h2 className="md:text-xl text-lg mb-4 text-left">
+            <h2 className="md:text-xl text-lg mb-3 text-left font-semibold">
               {isEmpty(viewer.name) ? (
                 `Welcome back!`
               ) : (
@@ -78,16 +81,14 @@ const InProgressSection: FunctionComponent<InProgressSectionProps> = ({
               )}
             </h2>
           </div>
-          <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
-            <div className="h-64 flex flex-col items-center justify-center w-full rounded-lg border-2 border-gray-200 border-dashed text-center">
-              <svg height="80" width="80">
-                <rect width="80" height="80" fill="#888" />
-              </svg>
-              <h3 className="text-lg mb-1 mt-3 font-semibold">
+          <div className="grid md:grid-cols-2 grid-cols-1 gap-4 md:gap-10">
+            <div className="h-64 flex flex-col items-center justify-center w-full rounded-lg border bg-white border-gray-300 border-dashed text-center px-6">
+              <EmptyCourseIcon />
+              <h3 className="text-lg mb-1 mt-4 font-medium text-gray-600">
                 No courses in progress
               </h3>
-              <div className="text-base leading-tight text-gray-600">
-                You haven't started watching any courses yet.
+              <div className="text-sm leading-tight text-gray-500">
+                Once you start watching courses, your progress will appear here
               </div>
             </div>
             <OnboardingSurvey />
@@ -100,36 +101,47 @@ const InProgressSection: FunctionComponent<InProgressSectionProps> = ({
 
 const surveyTopics = [
   'React',
-  'Javascript',
-  'Typescript',
   'CSS',
+  'Javascript',
   'Node.js',
   'AWS',
   'Angular',
   'Next.js',
+
+  'Typescript',
 ]
 
 const OnboardingSurvey = () => {
   return (
-    <div className="p-2">
-      <h3 className="text-lg font-medium text-grey-600 mb-1">
-        What topics are you interested in learning?
-      </h3>
-      <div className="text-sm text-gray-600 mb-6">
-        Select as many as you like
+    <div className="flex flex-col py-2">
+      <div>
+        <h3 className="text-lg font-medium text-grey-600 mb-1">
+          What topics are you interested in learning?
+        </h3>
+        <div className="text-sm text-gray-600 mb-4">
+          Select as many as you want. We'll use these to recommend learning
+          material for you.
+        </div>
       </div>
-      <div className="flex flex-row flex-wrap">
+      <div className="grid grid-cols-3 lg:grid-cols-4">
         {surveyTopics.map((topic: string) => {
           return (
-            <div className="flex items-center border border-gray-300 rounded px-4 py-2 mr-2 mb-2">
-              <div className="text-base text-gray-700 font-medium">{topic}</div>
+            <div className="pr-4 py-2">
+              <label for={topic}>
+                <input
+                  className="w-6 h-6 rounded mr-2 border-gray-300"
+                  id={topic}
+                  type="checkbox"
+                />
+                {topic}
+              </label>
             </div>
           )
         })}
       </div>
-      <div className="w-full flex justify-items-end justify-end">
-        <button className="bg-blue-600 px-4 py-2 text-base text-white font-medium rounded">
-          Submit
+      <div className="w-full flex justify-items-end justify-end mt-5">
+        <button className="bg-blue-600 hover:bg-blue-700 relative right-2 lg:right-6 transition-colors duration-300 px-4 py-2 text-base text-white font-medium rounded">
+          Submit topics
         </button>
       </div>
     </div>
