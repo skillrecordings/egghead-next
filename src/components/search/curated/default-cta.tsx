1a6543710541e9e7fbaa070e84d48a9b12ce005f
Nicoll Guarnizo <43156646+NicollGuarnizo@users.noreply.github.com>
2021-09-27 16:25:48 -0500

    Revert "small fix to the default cta to fill the height of the component (#894)"
    This reverts commit 8e6d5fff4514fe63b8c5a0c5c715b504726adab4.

--- src/components/search/curated/default-cta.tsx
+++ src/components/search/curated/default-cta.tsx
@@ -15,7 +15,8 @@ const DefaultCTA: React.FC<{location: string}> = ({location}) => {
       <Image
         priority
         quality={100}
-        layout="fill"
+        width={417}
+        height={463}
         className="rounded-lg"
         alt="Get Really Good at React on EpicReact.dev by Kent C. Dodds"
         src="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1626109728/epic-react/default-banners/banner-react-page_2x.jpg"
