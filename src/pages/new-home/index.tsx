75351487a29097b814823ad89406a634b37787a0
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-09-28 10:35:18 +0100

    stripped pricing out of homepage

--- src/pages/new-home/index.tsx
+++ src/pages/new-home/index.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react'
-import {get, filter, keys} from 'lodash'
-import slugify from 'slugify'
 import {motion} from 'framer-motion'
 import {BadgeCheckIcon, CheckIcon} from '@heroicons/react/solid'
 import {
@@ -288,65 +286,19 @@ const NewHome: React.FunctionComponent = () => {
             <h2 className="text-white text-5xl sm:text-7xl font-bold text-center mb-8 max-w-screen-md mx-auto leading-tighter">
               Level Up Your Skills and Get a Better Job as a Web Developer
             </h2>
-            <p className="text-blue-100 text-lg text-center mx-auto mb-20 px-96 leading-looser">
-              Get the skills you need to advance your career and build
-              real-world, professional projects. Level up with egghead.
+            <p className="text-blue-100 text-lg text-center mx-auto mb-20 px-80 leading-looser">
+              An egghead membership will give you the skills you need to advance
+              your career and build real-world, professional projects.
             </p>
             <div className="grid grid-cols-2 max-w-screen-lg mx-auto gap-4 mb-20">
               <div className="bg-white shadow-lg w-full rounded-md py-8 px-0">
-                <h3 className="text-2xl font-semibold text-center">
-                  Become a member
-                </h3>
-                <PricingSection />
+                <MemberPricingSection />
               </div>
               <div className="w-full">
                 <p className="text-blue-200 font-medium my-4 text-center">
                   Want to try us out first?
                 </p>
-                <div className="border border-blue-500 shadow-sm rounded-md p-8">
-                  <h3 className="text-xl text-white font-semibold text-center mb-6">
-                    Create a free account
-                  </h3>
-                  <ul>
-                    <li className="py-2 flex text-blue-100">
-                      <CheckIcon className="inline-block flex-shrink-0 w-6 h-6" />
-                      <span className="ml-4 leading-tight text-lg text-blue-50">
-                        Access a limited selection of lessons and courses
-                      </span>
-                    </li>
-                  </ul>
-                  <form>
-                    <div className="flex flex-row flex-wrap items-center mt-8">
-                      <div className="rounded-md relative shadow-sm w-full">
-                        <div className="absolute inset-y-0 left-1 pl-3 flex items-center pointer-events-none">
-                          <svg
-                            className="h-5 w-5 text-blue-800"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            aria-hidden="true"
-                          >
-                            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                          </svg>
-                        </div>
-                        <input
-                          id="email"
-                          type="email"
-                          placeholder="Your email address"
-                          className="text-black autofill:text-fill-black bg-blue-500 placeholder-blue-800 focus:ring-indigo-500 focus:border-blue-200 block w-full pl-12 border-blue-400 rounded-md py-4"
-                          required
-                        />
-                      </div>
-                      <button
-                        className="mt-2 px-5 py-4 text-center bg-blue-50 text-blue-800 font-medium rounded-md w-full hover:bg-blue-200 transition-all duration-300 ease-in-out hover:shadow-md text-lg"
-                        type="button"
-                      >
-                        Create a free account
-                      </button>
-                    </div>
-                  </form>
-                </div>
+                <FreeAccountSection />
               </div>
             </div>
           </div>
@@ -476,242 +428,34 @@ function TechnologyRow() {
   )
 }
 
-// Pricing Section
-
-const PlanTitle: React.FunctionComponent = ({children}) => (
-  <h2 className="text-lg dark:text-white text-gray-900">{children}</h2>
-)
-
-const PlanPrice: React.FunctionComponent<{
-  plan: any
-  pricesLoading: boolean
-}> = ({plan, pricesLoading}) => {
-  const {price, interval, interval_count} = plan
-  const intervalLabel = interval_count > 1 ? 'quarter' : interval
+function MemberPricingSection() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="py-6 flex items-end leading-none">
-        <span className="mt-1 self-start">USD</span>
-        <span className="text-4xl font-light">$</span>
-        <span className="text-4xl font-extrabold">
-          {pricesLoading ? (
-            <div className="px-2 w-full h-full bg-gradient-to-t from-transparent dark:to-gray-700 to-gray-300 animate-pulse rounded-md">
-              <span className="opacity-0">––</span>
-            </div>
-          ) : (
-            '250'
-          )}
-        </span>
-        <span className="text-lg font-light mb-1">/{intervalLabel}</span>
-      </div>
+    <div>
+      <h3 className="text-2xl font-semibold text-center">Become a member</h3>
     </div>
   )
 }
 
-const PlanQuantitySelect: React.FunctionComponent<{
-  quantity: number
-  onQuantityChanged: any
-  plan: any
-  pricesLoading: boolean
-}> = ({quantity, onQuantityChanged, plan, pricesLoading}) => {
-  const {price} = plan
+function FreeAccountSection() {
   return (
-    <div className="flex flex-col items-center space-y-2">
-      <label>
-        <span className="pr-2 text-sm">Seats</span>
-        <input
-          className="form-input dark:bg-gray-800 bg-gray-100 border-none"
-          type="number"
-          value={quantity}
-          max={1000}
-          min={1}
-          onChange={(e) => onQuantityChanged(Number(e.currentTarget.value))}
-        />
-      </label>
-      {quantity > 1 && (
-        <div className="py-2">
-          ${!pricesLoading ? price / quantity : '---'}/seat
-        </div>
-      )}
-    </div>
-  )
-}
-
-const PlanIntervalsSwitch: React.FunctionComponent<{
-  planTypes: any[]
-  disabled: boolean
-  currentPlan: any
-  setCurrentPlan: (plan: any) => void
-}> = ({planTypes, currentPlan, setCurrentPlan, disabled}) => {
-  const plansToRender = disabled ? [currentPlan] : planTypes
-  return (
-    <ul className="flex ">
-      {plansToRender.map((plan: any, i: number) => {
-        const {interval, interval_count} = plan
-        const checked: boolean = plan === currentPlan
-        const intervalLabel = interval_count > 1 ? 'quarter' : interval
-        return (
-          <li key={interval}>
-            <button
-              className={`${
-                checked
-                  ? 'dark:bg-white bg-gray-900 dark:text-gray-900 text-white dark:hover:bg-gray-200 hover:bg-gray-800'
-                  : 'dark:bg-gray-800 bg-gray-100 dark:hover:bg-gray-700 hover:bg-gray-200'
-              } ${i === 0 && 'rounded-l-md'} ${i === 2 && 'rounded-r-md'} ${
-                plansToRender.length === 2 && i === 1 && 'rounded-r-md'
-              } ${
-                plansToRender.length === 1 && 'rounded-md'
-              } capitalize block px-3 py-2 cursor-pointer text-sm font-medium transition-all ease-in-out duration-300`}
-              onClick={() => setCurrentPlan(plan)}
-              tabIndex={0}
-              role="radio"
-              aria-active={checked}
-            >
-              {intervalLabel}
-            </button>
-          </li>
-        )
-      })}
-    </ul>
-  )
-}
-
-const DEFAULT_FEATURES = [
-  'Full access to all the premium courses',
-  'Download courses for offline viewing',
-  'Closed captions for every video',
-  'Commenting and support',
-  'Enhanced Transcripts',
-  'RSS course feeds',
-]
-
-const PlanFeatures: React.FunctionComponent<{
-  planFeatures: string[]
-}> = ({planFeatures = DEFAULT_FEATURES}) => {
-  return (
-    <ul>
-      {planFeatures.map((feature: string) => {
-        return (
-          <li className="py-2 font-medium flex" key={slugify(feature)}>
-            <CheckIcon className="text-blue-500 inline-block flex-shrink-0 w-6 h-6" />
-            <span className="ml-4 leading-tight text-lg">{feature}</span>
-          </li>
-        )
-      })}
-    </ul>
-  )
-}
-
-const GetAccessButton: React.FunctionComponent<{
-  label: string
-  handleClick: (event: any) => Promise<void>
-}> = ({label, handleClick}) => {
-  return (
-    <button
-      className="mt-2 px-5 py-4 text-center bg-blue-600 text-white font-medium rounded-md w-full hover:bg-blue-700 transition-all duration-300 ease-in-out hover:shadow-md text-lg"
-      onClick={handleClick}
-      type="button"
-    >
-      {label}
-    </button>
-  )
-}
-
-type SelectPlanProps = {
-  prices: any
-  pricesLoading: boolean
-  defaultInterval?: string
-  defaultQuantity?: number
-  handleClickGetAccess: (event: any) => any
-  quantityAvailable: boolean
-  onQuantityChanged: (quantity: number) => void
-  onPriceChanged: (priceId: string) => void
-}
-
-const PricingSection: React.FunctionComponent<SelectPlanProps> = ({
-  quantityAvailable = true,
-  handleClickGetAccess,
-  defaultInterval = 'annual',
-  defaultQuantity = 1,
-  pricesLoading,
-  prices,
-  onQuantityChanged,
-  onPriceChanged,
-}) => {
-  const individualPlans = filter(prices, (plan: any) => true)
-
-  const annualPlan = get(prices, 'annualPrice', {
-    name: 'Annual Plan',
-    interval: 'year',
-  })
-  const monthlyPlan = get(prices, 'monthlyPrice')
-  const quarterlyPlan = get(prices, 'quarterlyPrice')
-
-  const pricesForInterval = (interval: any) => {
-    switch (interval) {
-      case 'year':
-        return annualPlan
-      case 'month':
-        return monthlyPlan
-      case 'quarter':
-        return quarterlyPlan
-      default:
-        return annualPlan
-    }
-  }
-
-  const [currentInterval] = React.useState<string>(defaultInterval)
-  const [currentQuantity, setCurrentQuantity] =
-    React.useState<number>(defaultQuantity)
-
-  const [currentPlan, setCurrentPlan] = React.useState<any>(
-    pricesForInterval(currentInterval),
-  )
-
-  const forTeams: boolean = currentQuantity > 1
-  const buttonLabel: string = forTeams ? 'Level Up My Team' : 'Become a Member'
-
-  React.useEffect(() => {
-    setCurrentPlan(annualPlan)
-  }, [annualPlan])
-  return (
-    <div className="flex flex-col items-center relative z-10 mx-auto mt-6">
-      <PlanTitle>{currentPlan?.name}</PlanTitle>
-      <PlanPrice pricesLoading={pricesLoading} plan={currentPlan} />
-      {keys(prices).length > 1 && (
-        <div className={quantityAvailable ? '' : 'pb-4'}>
-          <PlanIntervalsSwitch
-            disabled={false}
-            currentPlan={currentPlan}
-            setCurrentPlan={(newPlan: any) => {
-              setCurrentPlan(newPlan)
-              onPriceChanged(newPlan.stripe_price_id)
-            }}
-            planTypes={individualPlans}
-          />
-        </div>
-      )}
-      {quantityAvailable && (
-        <div className="py-4">
-          <PlanQuantitySelect
-            quantity={currentQuantity}
-            plan={currentPlan}
-            pricesLoading={pricesLoading}
-            onQuantityChanged={(quantity: number) => {
-              setCurrentQuantity(quantity)
-              onQuantityChanged(quantity)
-            }}
-          />
-        </div>
-      )}
-
-      <PlanFeatures planFeatures={currentPlan?.features} />
+    <div className="border border-blue-500 shadow-sm rounded-md p-8">
+      <h3 className="text-2xl text-white font-semibold text-center mb-6">
+        Create a free account
+      </h3>
+      <ul>
+        <li className="py-2 flex text-blue-100">
+          <CheckIcon className="inline-block flex-shrink-0 w-6 h-6" />
+          <span className="ml-4 leading-tight text-lg text-blue-100">
+            Access a limited selection of lessons and courses
+          </span>
+        </li>
+      </ul>
       <form>
         <div className="flex flex-row flex-wrap items-center mt-8">
           <div className="rounded-md relative shadow-sm w-full">
             <div className="absolute inset-y-0 left-1 pl-3 flex items-center pointer-events-none">
               <svg
-                className="h-5 w-5 text-gray-400"
+                className="h-5 w-5 text-blue-100"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
                 fill="currentColor"
@@ -725,14 +469,16 @@ const PricingSection: React.FunctionComponent<SelectPlanProps> = ({
               id="email"
               type="email"
               placeholder="Your email address"
-              className="text-black autofill:text-fill-black placeholder-gray-400 focus:ring-indigo-500 focus:border-blue-500 block w-full pl-12 border-gray-300 rounded-md py-4"
+              className="text-white autofill:text-fill-white bg-blue-600 placeholder-blue-100 focus:ring-indigo-500 focus:border-blue-200 block w-full pl-12 border-blue-400 rounded-md py-4"
               required
             />
           </div>
-          <GetAccessButton
-            label={buttonLabel}
-            handleClick={handleClickGetAccess}
-          />
+          <button
+            className="mt-2 px-5 py-4 text-center bg-blue-50 text-blue-700 font-medium rounded-md w-full hover:bg-blue-200 transition-all duration-300 ease-in-out hover:shadow-md text-lg"
+            type="button"
+          >
+            Create a free account
+          </button>
         </div>
       </form>
     </div>
