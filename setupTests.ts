536ae4dae8b66a6a5c8ea512a90b421a05747da9
jbranchaud <jbranchaud@gmail.com>
2021-10-14 12:12:52 -0500

    Add a custom Jest matcher for XState assertions

---
+++ setupTests.ts
@@ -0,0 +1,34 @@
+// optional: configure or set up a testing framework before each test
+// if you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+
+// used for __tests__/testing-library.js
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect'
+
+// source: https://discord.com/channels/795785288994652170/809564635614150686/897559009077362738
+import {State} from 'xstate'
+
+declare global {
+  namespace jest {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface Matchers<R> {
+      /**
+       *  Match wheter the XState State matches the provided value.
+       */
+      toMatchState(state: string): CustomMatcherResult
+    }
+  }
+}
+
+expect.extend({
+  toMatchState(state: State<unknown>, value: string) {
+    return {
+      pass: state.matches(value),
+      message: () =>
+        `Expected
+  "${JSON.stringify(state.value)}"
+state to match
+  "${JSON.stringify(value)}"`,
+    }
+  },
+})
