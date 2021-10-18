c1aa38f9b9876f75cd1a2ec26849456aed8b1ea5
Lauro <hey@laurosilva.com>
2021-10-18 16:43:59 -0700

    adding index

--- src/pages/case-studies/index.tsx
+++ src/pages/case-studies/index.tsx
@@ -6,50 +6,57 @@ import Image from 'next/image'
 
 const CaseStudies: React.FC = (allCaseStudies: any) => {
   return (
-    <div className="mx-auto max-w-screen-lg lg:py-16 py-10">
+    <div className="mx-auto max-w-screen-xl lg:py-16 py-10">
       <h1 className="md:text-4xl text-2xl text-center font-bold pb-16">
-        Case Studies
+        egghead Case Studies
       </h1>
 
-      {allCaseStudies.allCaseStudies.map((caseStudy: any) => {
-        const fullSlug = `/case-studies/${caseStudy.slug.current}`
-        return (
-          <div key={fullSlug} className="flex flex-col">
-            {caseStudy.coverImage?.url ? (
-              <div className="md:mb-4 mb-2">
-                <Link href={fullSlug}>
-                  <a>
-                    <Image
-                      src={caseStudy.coverImage.url}
-                      alt={caseStudy.coverImage.alt || caseStudy.title}
-                      width={320}
-                      height={320}
-                      quality={100}
-                      className="rounded-lg"
-                    />
-                  </a>
-                </Link>
-              </div>
-            ) : (
-              <div className="aspect-w-16 aspect-h-9 md:mb-4 mb-2">lol</div>
-            )}
+      <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+        {allCaseStudies.allCaseStudies.map((caseStudy: any) => {
+          const fullSlug = `/case-studies/${caseStudy.slug.current}`
 
-            <Link href={fullSlug}>
-              <a>
-                <h2 className="md:text-2xl text-xl font-bold leading-tighter">
-                  {caseStudy.title}
-                </h2>
-              </a>
-            </Link>
+          return (
+            <div
+              key={fullSlug}
+              className="flex flex-col rounded-lg shadow-lg overflow-hidden bg-gray-800 hover:bg-gray-700 transition duration-500 ease-in-out hover:-translate-y-1"
+            >
+              <Link href={fullSlug}>
+                <a className="">
+                  {caseStudy.coverImage?.url && (
+                    <div className="flex-shrink-0">
+                      <Image
+                        src={caseStudy.coverImage.url}
+                        alt={caseStudy.coverImage.alt || caseStudy.title}
+                        width={100}
+                        height={100}
+                        quality={100}
+                        layout="responsive"
+                        className="h-88 w-full object-cover"
+                      />
+                    </div>
+                  )}
 
-            {caseStudy.description && (
-              <div className="opacity-70 text-sm leading-snug  pl-2">
-                {caseStudy.description}
-              </div>
-            )}
-          </div>
-        )
-      })}
+                  <div className="flex-1 p-6 flex flex-col justify-between">
+                    <div className="flex-1">
+                      {caseStudy.title && (
+                        <h2 className="text-xl font-bold leading-tighter hover:text-blue-600 dark:hover:text-blue-300">
+                          {caseStudy.title}
+                        </h2>
+                      )}
+
+                      {caseStudy.description && (
+                        <div className="opacity-70 text-sm leading-snug pt-4">
+                          {caseStudy.description}
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
