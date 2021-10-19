ad12986f4b2c99bd6b913621b98576c9e8d5a722
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-10-19 20:18:44 +0300

    initial commit

---
+++ src/pages/new-home/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+const NewHome: React.FunctionComponent = () => {
+  return (
+    <div className="container">
+      <div>new home page</div>
+    </div>
+  )
+}
+export default NewHome
