95f3d5fad02462299132134ba2924a6b7523c8a8
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-10-20 16:58:46 +0300

    TechIcons component

--- src/pages/new-home/index.tsx
+++ src/pages/new-home/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
 
 import Hero from './components/hero'
+import TechIcons from './components/tech-icons'
 
 const NewHome: React.FunctionComponent = () => {
   return (
     <div className="pt-4 md:pt-8 lg:pt-16">
       <Hero />
+      <section className="mt-16 md:mt-28">
+        <TechIcons />
+      </section>
     </div>
   )
 }
