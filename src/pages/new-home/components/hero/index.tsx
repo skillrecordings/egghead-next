514f6333992ef747f06624f81cf562e456f48530
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-10-20 17:27:55 +0300

    reversed hero in mobile

--- src/pages/new-home/components/hero/index.tsx
+++ src/pages/new-home/components/hero/index.tsx
@@ -6,7 +6,7 @@ import CreateAccount from '../create-account'
 const Hero: React.FunctionComponent = () => {
   return (
     <div className="container">
-      <div className="flex flex-col items-center gap-8 lg:gap-10 xl:gap-20 md:flex-row">
+      <div className="flex flex-col-reverse items-center gap-8 lg:gap-10 xl:gap-20 md:flex-row">
         <div className="space-y-6 lg:space-y-8 xl:space-y-10 md:pt-6 lg:pt-10 md:w-7/12">
           <h1 className="text-xl font-extrabold leading-tight lg:leading-tighter sm:text-2xl md:text-2xl lg:text-3xl xl:text-4xl">
             Learn the best Javascript tools and frameworks from industry pros
