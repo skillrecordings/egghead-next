2df00cac87eb778fc639d513ea0d6360ed5e12d9
Vojta Holik <vojta@egghead.io>
2021-10-29 13:23:06 +0200

    navigate to /confirm after submitting the form

--- src/components/pages/landing/create-account/index.tsx
+++ src/components/pages/landing/create-account/index.tsx
@@ -3,6 +3,8 @@ import {FormikProps, useFormik} from 'formik'
 import * as Yup from 'yup'
 import {useViewer} from 'context/viewer-context'
 import {track} from 'utils/analytics'
+import Spinner from 'components/spinner'
+import {useRouter} from 'next/router'
 
 type FormikValues = {
   email: string
@@ -22,7 +24,7 @@ const CreateAccount: React.FC<{actionLabel?: string; location: string}> = ({
   location,
 }) => {
   const {requestSignInEmail} = useViewer()
-
+  const router = useRouter()
   const formik: FormikProps<FormikValues> = useFormik<FormikValues>({
     initialValues: {
       email: '',
@@ -31,45 +33,69 @@ const CreateAccount: React.FC<{actionLabel?: string; location: string}> = ({
       email: Yup.string().email('Invalid email').required('Required'),
     }),
     onSubmit: async ({email}) => {
-      requestSignInEmail(email).then(() => {
-        track(`clicked ${actionLabel}`, {location})
-      })
-      //   .catch(() => {
-      //     setIsSubmitted(false)
-      //     setIsError(true)
-      //   })
+      formik.setStatus('loading')
+      requestSignInEmail(email)
+        .then(() => {
+          track(`clicked ${actionLabel}`, {location})
+        })
+        .then(() => {
+          formik.setStatus('submitted')
+          router.push({
+            pathname: '/confirm',
+            query: {
+              email: email,
+            },
+          })
+        })
+        .catch(() => {
+          formik.setStatus('error')
+        })
     },
   })
   return (
-    <form onSubmit={formik.handleSubmit} className="flex max-w-md w-full">
-      <div className="relative flex items-center w-full text-gray-400 lg:w-80 dark:text-white">
-        <svg
-          className="absolute w-5 h-5 left-3"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          aria-hidden="true"
+    <form onSubmit={formik.handleSubmit} className="max-w-md w-full">
+      <div className="flex">
+        <div className="relative flex items-center w-full text-gray-400 lg:w-80 dark:text-white">
+          <svg
+            className="absolute w-5 h-5 left-3"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+          </svg>
+          <input
+            id={location ? `email_${location}` : 'email'}
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            placeholder="you@company.com"
+            className="dark:autofill:text-fill-white block w-full py-3 pl-10 text-black placeholder-gray-400 bg-transparent border-r-0 border-gray-300 dark:border-gray-700 rounded-l-md shadow-sm dark:text-white autofill:text-fill-black focus:outline-none outline-none focus:ring-0 dark:focus:border-blue-500 focus:border-blue-500"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-600 dark:hover:bg-blue-500 hover:bg-blue-500 rounded-r-md px-4 text-white font-medium text-sm flex-shrink-0 flex items-center justify-center transition-all ease-in-out duration-200 group focus:outline-none outline-none focus:ring-2 focus:ring-blue-700 focus:bg-blue-500 dark:focus:ring-blue-300 relative z-10"
         >
-          <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-          <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-        </svg>
-        <input
-          id={location ? `email_${location}` : 'email'}
-          name="email"
-          type="email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          placeholder="you@company.com"
-          className="block w-full py-3 pl-10 text-black placeholder-gray-400 bg-transparent border-r-0 border-gray-300 dark:border-gray-700 rounded-l-md shadow-sm dark:text-white autofill:text-fill-black focus:outline-none outline-none focus:ring-0 dark:focus:border-blue-500 focus:border-blue-500"
-          required
-        />
+          {formik.status === 'loading' ? <Spinner /> : actionLabel}
+        </button>
       </div>
-      <button
-        type="submit"
-        className="bg-blue-600 dark:hover:bg-blue-500 hover:bg-blue-500 rounded-r-md px-4 text-white font-medium text-sm flex-shrink-0 flex items-center justify-center transition-all ease-in-out duration-200 group focus:outline-none outline-none focus:ring-2 focus:ring-blue-700 focus:bg-blue-500 dark:focus:ring-blue-300 relative z-10"
-      >
-        {actionLabel}
-      </button>
+      {formik.status === 'error' && (
+        <p className="text-center pt-4">
+          Uh-oh. Something went wrong. Please try again or email{' '}
+          <a
+            className="dark:text-blue-400 text-blue-500 underline"
+            href="mailto:support@egghead.io"
+          >
+            support@egghead.io
+          </a>{' '}
+          if the issue persists.
+        </p>
+      )}
     </form>
   )
 }
--- src/pages/confirm.tsx
+++ src/pages/confirm.tsx
@@ -1,15 +1,67 @@
 import * as React from 'react'
-import EmailConfirmation from 'components/pages/email-confirmation'
+import {useRouter} from 'next/router'
 
 const Confirm: React.FunctionComponent = () => {
+  const router = useRouter()
+  const email = router.query.email
+
   return (
-    <EmailConfirmation>
-      <h1>Please check your inbox to confirm your email.</h1>
-      <p>
-        If you have any trouble, you can email <code>support@egghead.io</code>{' '}
-        for help at any time.
-      </p>
-    </EmailConfirmation>
+    <div className="flex-grow h-full w-full flex flex-col items-center justify-center text-center sm:py-48 py-24">
+      <div className="max-w-2xl flex flex-col items-center">
+        <NotificationIcon />
+        <h1 className="text-2xl font-semibold py-4">Email sent</h1>
+        <h2 className="pb-2 text-lg font-semibold">
+          Please check your inbox {email && `(${email})`} for your sign in link.
+        </h2>
+        <p className="py-4 max-w-md dark:text-gray-300 text-gray-700 text-sm">
+          Sometimes this can land in SPAM! While we hope that isn't the case if
+          it doesn't arrive in a minute or three, please check.
+        </p>
+        <p className="dark:text-gray-300 text-gray-700 text-sm">
+          If you have any trouble, you can email{' '}
+          <a
+            href="mailto:support@egghead.io"
+            className="dark:text-blue-400 text-blue-500 underline"
+          >
+            support@egghead.io
+          </a>{' '}
+          for help at any time.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+const NotificationIcon = () => {
+  return (
+    <svg
+      width="80"
+      height="80"
+      viewBox="0 0 180 180"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="dark:text-white text-gray-800"
+    >
+      <rect
+        width="180"
+        height="180"
+        rx="90"
+        className="fill-current"
+        fill-opacity="0.1"
+      />
+      <path
+        d="M48 74.1429L85.1985 98.9419C88.3656 101.053 92.4915 101.053 95.6586 98.9419L132.857 74.1429M57.4286 126H123.429C128.636 126 132.857 121.779 132.857 116.571V69.4286C132.857 64.2213 128.636 60 123.429 60H57.4286C52.2213 60 48 64.2213 48 69.4286V116.571C48 121.779 52.2213 126 57.4286 126Z"
+        className="stroke-current"
+        stroke-width="5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <circle opacity="0.98" cx="128" cy="65" r="25" fill="#EF4748" />
+      <path
+        d="M127.344 74.75H130.803V55.0215H127.316L122.162 58.6445V61.8848L127.262 58.3848H127.344V74.75Z"
+        fill="white"
+      />
+    </svg>
   )
 }
 
