dc14f7319c4687213c3845d61a2be5f8c4b76a59
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-11-01 18:43:37 +0200

    Footer for non-pro users only

--- src/pages/new-home/index.tsx
+++ src/pages/new-home/index.tsx
@@ -1,24 +1,26 @@
 import * as React from 'react'
+import {useViewer} from 'context/viewer-context'
 import Header from 'components/pages/landing/header'
 import Article from 'components/pages/landing/article/index.mdx'
 import MembershipBenefits from 'components/pages/landing/membership-benefits'
 import Footer from 'components/pages/landing/footer'
 
 const NewHome = () => {
+  const {viewer} = useViewer()
   return (
     <>
       <Header />
       <main className="pt-16">
         <Article />
         <section className="-mx-5">
-          <div className="w-full bg-gradient-to-t dark:from-gray-1000 dark:via-gray-1000 dark:to-gray-900 from-gray-50 via-gray-50 to-white sm:pt-24 sm:pb-24 pt-16 pb-5">
-            <h2 className="text-xl leading-tight font-semibold text-center pb-14 mx-auto">
+          <div className="w-full pt-16 pb-5 bg-gradient-to-t dark:from-gray-1000 dark:via-gray-1000 dark:to-gray-900 from-gray-50 via-gray-50 to-white sm:pt-24 sm:pb-24">
+            <h2 className="mx-auto text-xl font-semibold leading-tight text-center pb-14">
               What you’ll get as an egghead member
             </h2>
             <MembershipBenefits />
           </div>
         </section>
-        <Footer />
+        {!viewer?.is_pro && <Footer />}
       </main>
     </>
   )
