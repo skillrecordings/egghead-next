1c1847273eaaafb292f42288283f5408430e40ee
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-11-03 18:25:03 +0200

    clean up

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -1,20 +1,9 @@
 import * as React from 'react'
 import {FunctionComponent} from 'react'
-import {useViewer} from 'context/viewer-context'
-// import stripeCheckoutRedirect from 'api/stripe/stripe-checkout-redirect'
-// import emailIsValid from 'utils/email-is-valid'
 import {track} from 'utils/analytics'
-import {useCommerceMachine} from 'hooks/use-commerce-machine'
-// import {first, get} from 'lodash'
-// import {StripeAccount} from 'types'
 import {useRouter} from 'next/router'
-// import SelectPlanNew from 'components/pricing/select-plan-new'
-// import PoweredByStripe from 'components/pricing/powered-by-stripe'
 import Testimonials from 'components/pricing/testimonials'
 import testimonialsData from 'components/pricing/testimonials/data'
-// import ParityCouponMessage from 'components/pricing/parity-coupon-message'
-import isEmpty from 'lodash/isEmpty'
-// import {Coupon} from 'machines/commerce-machine'
 import PricingWidget from 'components/pricing/pricing-widget'
 
 type PricingProps = {
@@ -29,7 +18,6 @@ type PricingProps = {
 }
 
 const Pricing: FunctionComponent<PricingProps> & {getLayout: any} = () => {
-  const {viewer, authToken} = useViewer()
   const router = useRouter()
 
   React.useEffect(() => {
@@ -38,78 +26,6 @@ const Pricing: FunctionComponent<PricingProps> & {getLayout: any} = () => {
       track('checkout: cancelled from stripe')
     }
   }, [])
-
-  const {
-    state,
-    send,
-    priceId,
-    quantity,
-    // prices,
-    availableCoupons,
-    // currentPlan,
-  } = useCommerceMachine()
-
-  // machine-derived states
-  // const pricesLoading = !state.matches('pricesLoaded')
-  // const pppCouponIsApplied =
-  //   state.matches('pricesLoaded.withPPPCoupon') ||
-  //   (pricesLoading && state.context?.couponToApply?.couponType === 'ppp')
-
-  // machine-derived data
-  // const parityCoupon = availableCoupons?.['ppp']
-
-  // const countryCode = get(parityCoupon, 'coupon_region_restricted_to')
-  // const countryName = get(parityCoupon, 'coupon_region_restricted_to_name')
-
-  // const pppCouponAvailable =
-  //   !isEmpty(countryName) && !isEmpty(countryCode) && !isEmpty(parityCoupon)
-  // const pppCouponEligible = quantity === 1
-
-  // handlers
-  // const onApplyParityCoupon = () => {
-  //   send('APPLY_PPP_COUPON')
-  // }
-
-  // const onDismissParityCoupon = () => {
-  //   send('REMOVE_PPP_COUPON')
-  // }
-
-  // const onClickCheckout = async () => {
-  //   if (!priceId) return
-  //   const account = first<StripeAccount>(get(viewer, 'accounts'))
-  //   await track('checkout: selected plan', {
-  //     priceId: priceId,
-  //   })
-
-  //   if (emailIsValid(viewer?.email)) {
-  //     await track('checkout: valid email present', {
-  //       priceId: priceId,
-  //     })
-  //     await track('checkout: redirect to stripe', {
-  //       priceId,
-  //     })
-  //     stripeCheckoutRedirect({
-  //       priceId,
-  //       email: viewer.email,
-  //       stripeCustomerId: account?.stripe_customer_id,
-  //       authToken,
-  //       quantity,
-  //       coupon: state.context.couponToApply?.couponCode,
-  //     })
-  //   } else {
-  //     await track('checkout: get email', {
-  //       priceId: priceId,
-  //     })
-  //     router.push({
-  //       pathname: '/pricing/email',
-  //       query: {
-  //         priceId,
-  //         quantity,
-  //         coupon: state.context.couponToApply?.couponCode,
-  //       },
-  //     })
-  //   }
-  // }
 
   return (
     <>
@@ -130,39 +46,6 @@ const Pricing: FunctionComponent<PricingProps> & {getLayout: any} = () => {
         </header>
         <main className="flex flex-col items-center">
           <PricingWidget />
-          {/* <div className="relative p-2 bg-gray-100 rounded-md shadow-lg dark:bg-gray-800 dark:shadow-none">
-            <SelectPlanNew
-              prices={prices}
-              pricesLoading={pricesLoading}
-              handleClickGetAccess={() => {
-                send({type: 'CONFIRM_PRICE', onClickCheckout})
-              }}
-              quantityAvailable={true}
-              onQuantityChanged={(quantity: number) => {
-                send({type: 'CHANGE_QUANTITY', quantity})
-              }}
-              onPriceChanged={(priceId: string) => {
-                send({type: 'SWITCH_PRICE', priceId})
-              }}
-              currentPlan={currentPlan}
-              currentQuantity={quantity}
-            />
-          </div>
-          {pppCouponAvailable && pppCouponEligible && (
-            <div className="max-w-screen-md pb-5 mx-auto mt-4">
-              <ParityCouponMessage
-                coupon={parityCoupon as Coupon}
-                countryName={countryName as string}
-                onApply={onApplyParityCoupon}
-                onDismiss={onDismissParityCoupon}
-                isPPP={pppCouponIsApplied}
-              />
-            </div>
-          )}
-          <div className="flex items-center py-24 space-x-5">
-            <PoweredByStripe />
-            <div className="text-sm">30 day money back guarantee</div>
-          </div> */}
           <Testimonials testimonials={testimonialsData} />
         </main>
       </div>
