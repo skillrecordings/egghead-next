4628ef286958c968258c66a48bf62c59fad98279
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-11-09 19:30:43 +0200

    container width: fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,20 @@ const {spacing, fontFamily} = require('tailwindcss/defaultTheme')
 const containerStylesPlugin = ({addComponents}) =>
   addComponents({
     '.container': {
-      maxWidth: '100%',
-      '@screen sm': {
-        maxWidth: '640px',
-      },
-      '@screen md': {
-        maxWidth: '768px',
-      },
-      '@screen lg': {
-        maxWidth: '1024px',
-      },
-      '@screen xl': {
-        maxWidth: '1280px',
-      },
+      maxWidth: '1280px',
+      // maxWidth: '100%',
+      // '@screen sm': {
+      //   maxWidth: '640px',
+      // },
+      // '@screen md': {
+      //   maxWidth: '768px',
+      // },
+      // '@screen lg': {
+      //   maxWidth: '1024px',
+      // },
+      // '@screen xl': {
+      //   maxWidth: '1280px',
+      // },
     },
   })
 
