4fc6896004ac5f6c28a22a0d6243f55214f7437c
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-11-09 17:26:57 +0200

    cleanup

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,5 +1,4 @@
 import React, {FunctionComponent} from 'react'
-import {Card} from 'components/card'
 import Link from 'next/link'
 import Image from 'next/image'
 import {map, get, find} from 'lodash'
