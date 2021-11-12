7bbeafd30fba9b3136de88bbfe1f8ffefb6f0975
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-11-12 17:57:58 +0200

    rework confirm page

--- src/pages/confirm.tsx
+++ src/pages/confirm.tsx
@@ -6,29 +6,32 @@ const Confirm: React.FunctionComponent = () => {
   const email = router.query.email
 
   return (
-    <main className="flex-grow h-full w-full flex flex-col items-center justify-center text-center md:py-48 sm:py-32 py-10">
-      <div className="lg:max-w-screen-md max-w-md flex flex-col items-center">
-        <NotificationIcon />
-        <h1 className="sm:text-sm text-xs uppercase font-semibold dark:text-gray-300 text-gray-700 pt-6 pb-2 tracking-wide">
-          Email sent
-        </h1>
-        <h2 className="pb-2 sm:text-xl text-lg font-semibold leading-tight">
-          Please check your inbox {email && `(${email})`} for your sign in link.
-        </h2>
-        <p className="py-4 max-w-md dark:text-gray-300 text-gray-700 text-sm">
-          Sometimes this can land in SPAM! While we hope that isn't the case if
-          it doesn't arrive in a minute or three, please check.
-        </p>
-        <p className="dark:text-gray-300 text-gray-700 text-sm">
-          If you have any trouble, you can email{' '}
-          <a
-            href="mailto:support@egghead.io"
-            className="dark:text-blue-400 text-blue-600 underline"
-          >
-            support@egghead.io
-          </a>{' '}
-          for help at any time.
-        </p>
+    <main className="flex-grow w-full h-full py-10 text-center md:py-48 sm:py-32">
+      <div className="container">
+        <div className="flex flex-col items-center max-w-md mx-auto lg:max-w-screen-md">
+          <NotificationIcon />
+          <h1 className="pt-6 pb-2 text-xs font-semibold tracking-wide text-gray-700 uppercase sm:text-sm dark:text-gray-300">
+            Email sent
+          </h1>
+          <h2 className="pb-2 text-lg font-semibold leading-tight sm:text-xl">
+            Please check your inbox {email && `(${email})`} for your sign in
+            link.
+          </h2>
+          <p className="max-w-md py-4 text-sm text-gray-700 dark:text-gray-300">
+            Sometimes this can land in SPAM! While we hope that isn't the case
+            if it doesn't arrive in a minute or three, please check.
+          </p>
+          <p className="text-sm text-gray-700 dark:text-gray-300">
+            If you have any trouble, you can email{' '}
+            <a
+              href="mailto:support@egghead.io"
+              className="text-blue-600 underline dark:text-blue-400"
+            >
+              support@egghead.io
+            </a>{' '}
+            for help at any time.
+          </p>
+        </div>
       </div>
     </main>
   )
@@ -45,14 +48,14 @@ const NotificationIcon = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <rect
-        className="fill-current dark:text-white text-gray-400"
+        className="text-gray-400 fill-current dark:text-white"
         width="180"
         height="180"
         rx="90"
         fillOpacity="0.1"
       />
       <path
-        className="stroke-current dark:text-white text-gray-800"
+        className="text-gray-800 stroke-current dark:text-white"
         d="M48 74.1429L85.1985 98.9419C88.3656 101.053 92.4915 101.053 95.6586 98.9419L132.857 74.1429M57.4286 126H123.429C128.636 126 132.857 121.779 132.857 116.571V69.4286C132.857 64.2213 128.636 60 123.429 60H57.4286C52.2213 60 48 64.2213 48 69.4286V116.571C48 121.779 52.2213 126 57.4286 126Z"
         strokeWidth="5"
         strokeLinecap="round"
