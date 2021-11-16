9004fe92513a1d71088b90f1276f5f31c9917aa4
Vojta Holik <vojta@egghead.io>
2021-11-16 18:32:50 +0100

    remove negative margin

--- src/pages/20-days.tsx
+++ src/pages/20-days.tsx
@@ -17,7 +17,7 @@ const EOYSale2021Page: React.FC<EOYSale2021PageProps> & {getLayout?: any} = ({
   return (
     <>
       <NextSeo noindex={true} title={'20 days of egghead'} />
-      <div className="dark:bg-gray-900 bg-gray-50 -m-5">
+      <div className="dark:bg-gray-900 bg-gray-50">
         <div className="flex flex-col items-center justify-center w-full relative py-40">
           <h3 className="md:text-4xl sm:text-3xl text-2xl font-bold tracking-tight text-center relative z-10 leading-none pb-2">
             New Course a Day
