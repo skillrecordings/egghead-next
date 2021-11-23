16f192a4819ce20ab22180a8c50bb2717e5bf1e1
Joel Hooks <joelhooks@gmail.com>
2021-11-23 11:38:52 -0800

    optimize the hooks

--- src/hooks/use-course-presence.tsx
+++ src/hooks/use-course-presence.tsx
@@ -1,29 +1,51 @@
 import * as React from 'react'
 import {useViewer} from '../context/viewer-context'
-import {useEffect} from 'react'
 import Pusher from 'pusher-js'
 import axios from 'axios'
-import {keys} from 'lodash'
+
+const usePusher = () => {
+  // memoize the pusher instance so we aren't making them willy nilly
+  return React.useMemo(
+    () =>
+      new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY || '', {
+        cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+        authEndpoint: '/api/pusher/auth',
+      }),
+    [],
+  )
+}
 
 export const useCoursePresence = (slug: string) => {
   const [learners, setLearners] = React.useState<any[]>([])
   const {viewer} = useViewer()
 
+  const pusher = usePusher()
+
   const contactId = viewer?.contact_id
 
-  useEffect(() => {
-    if (!slug) return
-
-    const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY || '', {
-      cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
-      authEndpoint: '/api/pusher/auth',
-    })
+  React.useEffect(() => {
+    //we only want to subscribe if there is a slug and a logged in user
+    if (!slug || !contactId) return
 
     const channelName = `private-${slug}${contactId ? `@@${contactId}` : ''}`
 
-    if (contactId) {
-      pusher.subscribe(channelName)
+    pusher.subscribe(channelName)
+
+    return () => {
+      if (channelName) pusher.unsubscribe(channelName)
     }
+  }, [slug, contactId])
+
+  React.useEffect(() => {
+    /*
+      here we only care if ia course slug is present because this will fetch the
+      list of currently active learners (channels) for display on a 1 minute
+      interval after loading initially
+
+      utilizes a serverless API function so doesn't need a pusher instance
+    */
+
+    if (!slug) return
 
     async function checkChannels() {
       const channels = await axios.get(`/api/pusher/channels/${slug}`)
@@ -35,12 +57,9 @@ export const useCoursePresence = (slug: string) => {
     checkChannels()
 
     return () => {
-      if (contactId) {
-        pusher.unsubscribe(channelName)
-      }
       clearInterval(intervalId)
     }
-  }, [contactId, slug])
+  }, [slug])
 
   return learners
 }
