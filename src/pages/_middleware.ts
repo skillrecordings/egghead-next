3e6c47ca50de436f3b78ae2fc18deb75f463ba7c
Joel Hooks <joelhooks@gmail.com>
2021-11-24 15:07:28 -0800

    more debugging

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -8,17 +8,19 @@ const CIO_BASE_URL = `https://beta-api.customer.io/v1/api/`
 
 const loadCio = async (cioId: string) => {
   try {
-    console.log(
-      'LOADING CIO ',
-      cioId,
-      process.env.CUSTOMER_IO_APPLICATION_API_KEY,
-    )
+    console.log('LOADING CIO ', cioId)
     const cioApiPath = `/customers/${cioId}/attributes`
     const headers = new Headers({
       Authorization: `Bearer ${process.env.CUSTOMER_IO_APPLICATION_API_KEY}`,
     })
     const request = new Request(`${CIO_BASE_URL}${cioApiPath}`, {headers})
-    const {customer} = await fetch(request).then((response) => {
+
+    console.log('HEADERS', headers)
+    console.log('REQUEST', request)
+
+    const {customer} = await fetch(`${CIO_BASE_URL}${cioApiPath}`, {
+      headers,
+    }).then((response) => {
       console.log('CUSTOMERIO RESPONSE', response)
       return response.json()
     })
