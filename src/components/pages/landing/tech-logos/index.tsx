40e8e1f504ff7a3c36062b74cb6b02d4d9d0b8ac
Joel Hooks <joelhooks@gmail.com>
2021-11-24 17:12:41 -0800

    logos clickable if customer also tracking

--- src/components/pages/landing/tech-logos/index.tsx
+++ src/components/pages/landing/tech-logos/index.tsx
@@ -3,9 +3,23 @@ import Image from 'next/image'
 import Link from 'next/link'
 import {useTheme} from 'next-themes'
 import {useViewer} from 'context/viewer-context'
+import {track} from '../../../../utils/analytics'
+import axios from '../../../../utils/configured-axios'
+import cookies from '../../../../utils/cookies'
+
+const useCustomer = () => {
+  const [customer, setCustomer] = React.useState()
+
+  React.useEffect(() => {
+    setCustomer(cookies.get('customer'))
+  }, [])
+
+  return customer
+}
 
 const TechLogos = () => {
   const {viewer} = useViewer()
+  const customer = useCustomer()
   const {resolvedTheme} = useTheme()
   const logos = [
     'react',
@@ -26,9 +40,21 @@ const TechLogos = () => {
   return (
     <div className="flex flex-wrap items-center justify-center md:max-w-none sm:max-w-lg max-w-[450px]">
       {logos.map((tech) => {
-        return viewer ? (
+        return viewer || customer ? (
           <Link key={tech} href={`/q/${tech}`}>
-            <a className="px-2 py-1 scale-75 lg:px-4 md:px-2 sm:scale-90">
+            <a
+              className="px-2 py-1 scale-75 lg:px-4 md:px-2 sm:scale-90"
+              onClick={() => {
+                track('clicked topic', {
+                  topic: tech,
+                  location: 'signup page',
+                })
+                axios.post(`/api/topic`, {
+                  topic: tech,
+                  amount: 1,
+                })
+              }}
+            >
               <Image
                 src={require(`./${
                   resolvedTheme === 'dark' ? 'dark' : 'light'
@@ -41,6 +67,12 @@ const TechLogos = () => {
           <div
             key={tech}
             className="px-2 py-1 scale-75 lg:px-4 md:px-2 sm:scale-90"
+            onClick={() => {
+              track('clicked topic', {
+                topic: tech,
+                location: 'signup page (no link)',
+              })
+            }}
           >
             <Image
               src={require(`./${
