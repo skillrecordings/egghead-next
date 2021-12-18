462cf72a6592f4114648362b7aae7850dfb46897
Joel Hooks <joelhooks@gmail.com>
2021-12-18 15:51:11 -0800

    customer prop was nested

--- src/lib/customer.ts
+++ src/lib/customer.ts
@@ -25,10 +25,12 @@ async function fetchCustomer(cioId: string) {
       headers,
     })
       .then((response) => {
-        if(!timedOut) resolve(response.json())
+        response.json().then(({customer}) => {
+          if (!timedOut) resolve(customer)
+        })
       })
       .catch((error) => {
-        if(!timedOut) reject(error)
+        if (!timedOut) reject(error)
       })
       .finally(() => {
         clearTimeout(id)
@@ -39,19 +41,19 @@ async function fetchCustomer(cioId: string) {
 /**
  * Loads customer data from customer.io first checking if a customer is supplied
  * (via cookie) and then loading the customer async via the CIO api if not.
- * 
- * Since edge functions on Vercel **require** a response within 1.5s we have a 
+ *
+ * Since edge functions on Vercel **require** a response within 1.5s we have a
  * safety net fallback that times out the request as some users globally have a slow
  * response here.
- * 
+ *
  * Those users will have the `customer` cookie filled in later in the client side environment
  * where hard limits on response times aren't imposed.
- * 
+ *
  * @see {@link https://vercel.com/docs/concepts/functions/edge-functions#maximum-execution-duration}
- * 
+ *
  * @param cioId the customer.io id to load
  * @param customer optional customer object likely loaded from cookies
- * @returns customer 
+ * @returns customer
  */
 export const loadCio = async (cioId: string, customer?: any) => {
   try {
@@ -66,7 +68,9 @@ export const loadCio = async (cioId: string, customer?: any) => {
   }
 
   try {
-    return await fetchCustomer(cioId)
+    const customer: any = await fetchCustomer(cioId)
+    console.log(customer)
+    return customer?.customer ? customer.customer : customer
   } catch (error) {
     console.error(error)
   }
