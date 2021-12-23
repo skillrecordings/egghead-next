98500f8188b1b89b625ce30fc9dda58151aea333
Joel Hooks <joelhooks@gmail.com>
2021-12-23 11:56:15 -0800

    add middleware to redirect pro members to user page from pricing

---
+++ src/pages/pricing/_middleware.ts
@@ -0,0 +1,53 @@
+import {NextRequest, NextResponse} from 'next/server'
+import {ACCESS_TOKEN_KEY} from '../../config'
+import {loadCio} from '../../lib/customer'
+
+const CIO_COOKIE_KEY = 'cio_id'
+
+export async function middleware(req: NextRequest) {
+  let response = NextResponse.next()
+
+  const cioId =
+    req.cookies[CIO_COOKIE_KEY] || req.nextUrl.searchParams.get(CIO_COOKIE_KEY)
+
+  // if there's a cookie or a token they are logged in
+  let status = cioId || req.cookies[ACCESS_TOKEN_KEY] ? 'identified' : 'anon'
+
+  switch (status) {
+    case 'anon':
+      response = NextResponse.next()
+      break
+    case 'identified':
+      if (cioId) {
+        try {
+          const customer = await loadCio(cioId, req.cookies['customer'])
+
+          const isMember = [
+            customer?.attributes?.pro,
+            customer?.attributes?.instructor,
+          ].includes('true')
+
+          switch (true) {
+            case isMember:
+              response = NextResponse.rewrite('user')
+              break
+            default:
+              response = NextResponse.next()
+          }
+
+          if (customer) {
+            response.cookie('customer', JSON.stringify(customer))
+          }
+
+          response.cookie(CIO_COOKIE_KEY, cioId)
+        } catch (_e) {
+          response = NextResponse.next()
+        }
+      }
+      break
+    default:
+      response = NextResponse.next()
+  }
+
+  return response
+}
