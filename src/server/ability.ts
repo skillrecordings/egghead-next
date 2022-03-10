e27d7d249a4a8fd3262b87e57fad38e29d30889f
Joel Hooks <joelhooks@gmail.com>
2022-03-09 21:24:53 -0800

    feat(abilities): clean it up and contain the user loading for ability

--- src/server/ability.ts
+++ src/server/ability.ts
@@ -1,17 +1,23 @@
 import {AbilityBuilder, Ability} from '@casl/ability'
+import {loadCurrentUser} from 'lib/users'
 
 type Actions = 'manage' | 'upload'
 type Subjects = 'Video' | 'all'
 type AppAbility = Ability<[Actions, Subjects]>
 
-export default function defineAbilityFor(user: any) {
+export async function getAbilityFromToken(token: string) {
+  const user = await loadCurrentUser(token)
+  return defineAbilityFor(user)
+}
+
+function defineAbilityFor(user: any) {
   const {can, build} = new AbilityBuilder<AppAbility>(Ability)
 
   if (user.roles?.includes('admin')) {
     can('manage', 'all') // read-write access to everything
   }
 
-  if (user.roles?.includes('admin', 'editor', 'publisher')) {
+  if (user.roles?.includes('editor', 'publisher')) {
     can('upload', 'Video')
   }
 
