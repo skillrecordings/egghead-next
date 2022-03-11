737179d17ded66e281f97af9e9a4489f90804011
jbranchaud <jbranchaud@gmail.com>
2022-03-10 17:17:46 -0700

    Fix role checking logic to compare multiple roles
    JavaScript's `includes` cannot compare multiple values on the right side
    in the way we were trying to do.
    
    ```javascript
    ['user', 'publisher'].includes('editor', 'publisher')
    //=> false
    ```
    
    Instead, I've replaced that with a small utility function `hasRoles`
    that handles the logic of comparing a single role or multiple roles to
    the user's list of roles.

--- src/server/ability.ts
+++ src/server/ability.ts
@@ -1,5 +1,6 @@
 import {AbilityBuilder, Ability} from '@casl/ability'
 import {loadCurrentUser} from 'lib/users'
+import {intersection, isString} from 'lodash'
 
 type Actions = 'manage' | 'upload'
 type Subjects = 'Video' | 'all'
@@ -14,13 +15,28 @@ function defineAbilityFor(user: any) {
   // AbilityBuilder https://casl.js.org/v5/en/guide/define-rules#ability-builder-class
   const {can, build} = new AbilityBuilder<AppAbility>(Ability)
 
-  if (user.roles?.includes('admin')) {
+  if (hasRoles(user, 'admin')) {
     can('manage', 'all') // read-write access to everything
   }
 
-  if (user.roles?.includes('editor', 'publisher')) {
+  if (hasRoles(user, ['editor', 'publisher'])) {
     can('upload', 'Video')
   }
 
   return build()
+}
+
+function hasRoles(user: any, rolesToCheck: string | string[]) {
+  // ensure rolesToCheck is an array
+  let rolesToCheckArray: string[]
+  if (isString(rolesToCheck)) {
+    rolesToCheckArray = [rolesToCheck]
+  } else {
+    rolesToCheckArray = rolesToCheck
+  }
+
+  const userRoles = user?.roles || []
+
+  // check if at least one role overlaps
+  return intersection(userRoles, rolesToCheckArray).length > 0
 }
