725577042678e4da2b34fbe38d051f76f5c98967
Lucas Minter <lsminteris@gmail.com>
2022-03-18 13:05:34 -0700

    updated the styling on the sync your discord to egghead page

--- src/pages/discord/index.tsx
+++ src/pages/discord/index.tsx
@@ -7,22 +7,25 @@ import LoginRequired, {LoginRequiredParams} from 'components/login-required'
 const Discord: FunctionComponent<LoginRequiredParams> = ({loginRequired}) => {
   return (
     <LoginRequired loginRequired={loginRequired}>
-      {process.env.NEXT_PUBLIC_DISCORD_AUTHORIZE && (
-        <div className="w-1/2 flex space-y-6   flex-col">
-          <p className="prose lg:prose-xl">
-            This will authorize egghead to see your Discord identity and email.
-            If you are currently logged in and a PRO member of egghead you will
-            be invited added to the egghead Discord server as a member. If you
-            aren't a PRO member, you will still be added to the server.
-          </p>
+      <div>
+        {process.env.NEXT_PUBLIC_DISCORD_AUTHORIZE && (
+          <div className="grid place-items-center h-96">
+            <p className="prose lg:prose-xl text-white text-center">
+              This will authorize egghead to see your Discord identity and
+              email. If you are currently logged in and a PRO member of egghead
+              you will be invited added to the egghead Discord server as a
+              member. If you aren't a PRO member, you will still be added to the
+              server.
+            </p>
 
-          <Link href={process.env.NEXT_PUBLIC_DISCORD_AUTHORIZE}>
-            <a className="transition text-center duration-150 ease-in-out bg-blue-500 hover:bg-blue-700 text-white font-semibold py-3 px-5 rounded">
-              Sync your Discord to egghead
-            </a>
-          </Link>
-        </div>
-      )}
+            <Link href={process.env.NEXT_PUBLIC_DISCORD_AUTHORIZE}>
+              <a className="transition text-center duration-150 ease-in-out bg-blue-500 hover:bg-blue-700 text-white font-semibold py-3 px-5 rounded">
+                Sync your Discord to egghead
+              </a>
+            </Link>
+          </div>
+        )}
+      </div>
     </LoginRequired>
   )
 }
