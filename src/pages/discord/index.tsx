ad07a7a3259dcb1183f2b0ee35e1fca14e847bb7
Lucas Minter <lsminteris@gmail.com>
2022-03-22 10:43:12 -0700

    added dark mode text color instead of just hard coding

--- src/pages/discord/index.tsx
+++ src/pages/discord/index.tsx
@@ -9,7 +9,7 @@ const Discord: FunctionComponent<LoginRequiredParams> = ({loginRequired}) => {
     <LoginRequired loginRequired={loginRequired}>
       {process.env.NEXT_PUBLIC_DISCORD_AUTHORIZE && (
         <div className="grid place-items-center h-96">
-          <p className="prose lg:prose-xl text-white text-center">
+          <p className="prose lg:prose-xl dark:text-white text-center">
             This will authorize egghead to see your Discord identity and email.
             If you are currently logged in and a PRO member of egghead you will
             be invited added to the egghead Discord server as a member. If you
