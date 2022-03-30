811463533041742d8589a4af847b955d12a2c2ff
jbranchaud <jbranchaud@gmail.com>
2022-03-30 11:39:55 -0500

    Make inputs fully rounded

--- src/pages/upload/index.tsx
+++ src/pages/upload/index.tsx
@@ -266,7 +266,7 @@ const Upload: React.FC<FormikProps<FormProps> & {instructors: Instructor[]}> = (
                 Title
               </label>
               <Field
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 name={`lessons.${i}.title`}
               />
               <p className="mt-2 text-center text-sm text-gray-600">
