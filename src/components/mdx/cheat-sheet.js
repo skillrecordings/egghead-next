4f7303e925598a1e40a197eb15eda0b1d67af21c
Zac Jones <zacjones93@gmail.com>
2022-05-12 10:22:20 -0400

    add dark mode font colors for article

--- src/components/mdx/cheat-sheet.js
+++ src/components/mdx/cheat-sheet.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function CheatSheet({title, children}) {
   return (
-    <div className="bg-white shadow-md p-6 dark:border-gray-700 sm:px-8 border border-gray-100 rounded-md m-6 dark:bg-gray-800">
+    <div className=" bg-white shadow-md p-6 dark:border-gray-700 sm:px-8 border border-gray-100 rounded-md m-6 dark:bg-gray-800">
       <details>
-        <summary className="font-medium text-xl text-extrabold text-gray-800">
+        <summary className="cursor-pointer prose dark:prose-dark font-medium text-xl text-extrabold text-gray-800 ">
           {title}
         </summary>
         <p className="prose dark:prose-dark text-lg text-gray-800 dark:prose-a:text-blue-300 prose-a:text-blue-500 mt-0 p-4 leading-relaxed">
