b949c263fb2bcbf40b8319bd0c0e00e920243de0
jbranchaud <jbranchaud@gmail.com>
2022-05-18 12:16:35 -0500

    Move Course component into its own file

--- src/pages/lessons/[slug].tsx
+++ src/pages/lessons/[slug].tsx
@@ -7,6 +7,7 @@ import {useMachine} from '@xstate/react'
 import {Tab, TabList, TabPanel, TabPanels, Tabs} from '@reach/tabs'
 import {lessonMachine} from 'machines/lesson-machine'
 import {useEggheadPlayer} from 'components/EggheadPlayer'
+import Course from 'components/pages/lessons/course'
 import Tags from 'components/pages/lessons/tags'
 import Transcript from 'components/pages/lessons/transcript'
 import {loadBasicLesson, loadLesson} from 'lib/lessons'
@@ -958,48 +959,3 @@ const LessonPage: React.FC<{initialLesson: VideoResource}> = ({
 }
 
 export default LessonPage
-
-const Course: React.FC<{
-  course: {
-    title: string
-    square_cover_480_url: string
-    slug: string
-    path: string
-  }
-  currentLessonSlug: string
-}> = ({course, currentLessonSlug}) => {
-  return course ? (
-    <div>
-      <div className="flex items-center">
-        <Link href={course.path}>
-          <a className="relative flex-shrink-0 block w-12 h-12 lg:w-20 lg:h-20">
-            <Image
-              src={course.square_cover_480_url}
-              alt={`illustration for ${course.title}`}
-              layout="fill"
-            />
-          </a>
-        </Link>
-        <div className="ml-2 lg:ml-4">
-          <h4 className="mb-px text-xs font-semibold text-gray-700 uppercase dark:text-gray-100">
-            Course
-          </h4>
-          <Link href={course.path}>
-            <a
-              onClick={() => {
-                track(`clicked open course`, {
-                  lesson: currentLessonSlug,
-                })
-              }}
-              className="hover:underline"
-            >
-              <h3 className="font-bold leading-tighter 2xl:text-lg">
-                {course.title}
-              </h3>
-            </a>
-          </Link>
-        </div>
-      </div>
-    </div>
-  ) : null
-}
