ad0eaebbb0edc33b6776c5eaea376147c2b8e532
jbranchaud <jbranchaud@gmail.com>
2022-05-19 11:59:37 -0500

    Include instructor check to fully replace API call

--- src/pages/api/users/check-pro-status.ts
+++ src/pages/api/users/check-pro-status.ts
@@ -1,5 +1,3 @@
-import axios from 'axios'
-
 import {NextApiRequest, NextApiResponse} from 'next'
 import emailIsValid from 'utils/email-is-valid'
 import {isEmpty} from 'lodash'
@@ -15,16 +13,6 @@ const checkProStatus = async (req: NextApiRequest, res: NextApiResponse) => {
     if (!emailIsValid(email)) {
       res.status(400).end()
     } else {
-      // const userUrl = `${process.env.NEXT_PUBLIC_AUTH_DOMAIN}/api/v1/users/${email}?by_email=true`
-
-      // const eggheadUser = await axios
-      //   .get(userUrl, {
-      //     headers: {
-      //       Authorization: `Bearer ${process.env.EGGHEAD_SUPPORT_BOT_TOKEN}`,
-      //     },
-      //   })
-      //   .then(({data}) => data)
-
       const prisma = new PrismaClient()
 
       // Unfortunately, cannot do this at the moment until we have un-ignored
@@ -63,17 +51,14 @@ const checkProStatus = async (req: NextApiRequest, res: NextApiResponse) => {
         join roles
           on roles.id = users_roles.role_id
         where users.email = ${email}
-          and roles.name = 'pro'
+          and (roles.name = 'pro' or
+               roles.name = 'instructor')
         `,
       )
 
-      console.log({result})
-
-      const hasProAccess = result.length >= 1
-
-      // const hasProAccess =
-      //   !isEmpty(eggheadUser) &&
-      //   (eggheadUser.is_pro || eggheadUser.is_instructor)
+      // if a user has at least one of the roles: `pro` or `instructor`, then
+      // they are considered to have pro access.
+      const hasProAccess = !isEmpty(result)
 
       res.status(200).json({hasProAccess})
     }
