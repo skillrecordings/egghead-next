a69b815c02a3ce642af2941a0a23b8af7f610794
Zac Jones <zacjones93@gmail.com>
2022-05-25 17:26:43 -0600

    add courses to remix topic page

--- src/components/search/curated/remix/index.tsx
+++ src/components/search/curated/remix/index.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import {NextSeo} from 'next-seo'
 import Image from 'next/image'
 import groq from 'groq'
+import {HorizontalResourceCard} from 'components/card/topic-page-horizontal-resource-card'
 
 const SearchRemix = ({topic}: any) => {
   const location = 'Remix Topic Page'
   const description = `Build your Developer Portfolio and climb the engineering career ladder with in-depth Remix resources.`
   const title = `In-Depth Remix Resources for ${new Date().getFullYear()}`
 
-  console.log({topic})
+  console.log(topic.courses)
 
   return (
     <div>
@@ -31,11 +32,19 @@ const SearchRemix = ({topic}: any) => {
           ],
         }}
       />
-      <div className="space-y-5 md:space-y-0 -mx-5">
-        <div className="mt-10 mb-20 mx-auto w-fit">
+      <div className="flex flex-col max-w-3xl mx-auto space-y-5 md:space-y-0 mb-4">
+        <div className="my-10">
           <Image width={850} height={290} src={topic.image} />
         </div>
-        <h2 className="text-3xl">Get Started</h2>
+        <h2 className="text-3xl col-span-1">Get Started</h2>
+        <p className="mt-2 pb-12">{topic.description}</p>
+        <div className="rounded dark:bg-gray-800 bg-white dark:bg-opacity-60 shadow-smooth">
+          <HorizontalResourceCard resource={topic.courses.primary} />
+          <HorizontalResourceCard
+            resource={topic.courses.second}
+            left={false}
+          />
+        </div>
       </div>
     </div>
   )
@@ -53,7 +62,9 @@ export const remixPageQuery = groq`
       'cta': content[0].text,
       meta,
       'title': resources[0]->title,
-      'image': resources[0]->image
+      'image': resources[0]->image,
+      'path': resources[0]->path
+      
   },
     "second": resources[slug.current == "second-course"][0]{
       byline,
@@ -61,7 +72,8 @@ export const remixPageQuery = groq`
       'cta': content[0].text,
       meta,
       'title': resources[0]->title,
-      'image': resources[0]->image
+      'image': resources[0]->image,
+      'path': resources[0]->path
     }
   }
 }`
