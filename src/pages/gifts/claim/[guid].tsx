1206b8adeb139810cb8b59b218295b6814ac6a0e
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-06-29 13:36:02 -0700

    tweak UI

--- src/pages/gifts/claim/[guid].tsx
+++ src/pages/gifts/claim/[guid].tsx
@@ -3,12 +3,11 @@ import {GetServerSideProps} from 'next'
 import {getTokenFromCookieHeaders} from '../../../utils/auth'
 import {getGift} from '../../../lib/gifts'
 import Link from 'next/link'
+import Image from 'next/image'
 import axios from 'utils/configured-axios'
 import {useRouter} from 'next/router'
 import {useViewer} from '../../../context/viewer-context'
 import {PrimaryButton} from '../../../components/buttons'
-
-// tachyons to tailwind converter https://gist.github.com/knoopx/240cbe3b16a7a2f9e4101238533f87c8
 
 export const getServerSideProps: GetServerSideProps = async function ({
   req,
@@ -61,45 +60,61 @@ const GiftClaim: React.FC<GiftClaimProps> = ({error, gift}) => {
   }
 
   return (
-    <div className="p-5 prose dark:prose-dark prose-sm ">
-      {error ? (
-        <>
-          <h1>{error}</h1>
-          <p>
-            <Link href="/login">
-              <a>Click here</a>
-            </Link>{' '}
-            to sign in. Once you are signed in you will need to click your gift
-            claim link again.
-          </p>
-        </>
-      ) : (
-        <>
-          <h1>Claim your membership.</h1>
-          {viewer?.is_pro ? (
+    <div className="container flex justify-center">
+      <div className="prose dark:prose-dark prose-sm mt-8 md:mt-20 pb-10 text-center">
+        {error ? (
+          <>
+            <h1>{error}</h1>
             <p>
-              A gift membership can only be claimed one time. As an existing
-              egghead member this gift membership will be applied{' '}
-              <em>on top of your existing membership</em>. They stack. You can
-              claim 10 gift memberships and have PRO access for 10 years. When
-              the gift expires, your existing membership will resume. You will
-              not lose any membership time/money.
+              <Link href="/login">
+                <a>Click here</a>
+              </Link>{' '}
+              to sign in. Once you are signed in you will need to click your
+              gift claim link again.
             </p>
-          ) : (
-            <p>
-              A gift membership can only be claimed one time. Once you've
-              claimed this gift nobody else will be able to you. It can't be
-              undone! Make sure that <em>{viewer?.email}</em> is the account you
-              want the gift applied to.
-            </p>
-          )}
-          <PrimaryButton
-            url={'#'}
-            label={`Claim Your Membership for ${viewer?.email}`}
-            onClick={handleClaimGift}
-          />
-        </>
-      )}
+          </>
+        ) : (
+          <>
+            <div className="w-28 md:w-40 mx-auto">
+              <Image
+                src="/images/eggo-membership.png"
+                width={480}
+                height={560}
+              />
+            </div>
+            <h1 className="mt-6 md:mt-12">Claim your membership.</h1>
+            {viewer?.is_pro ? (
+              <p>
+                A gift membership can only be claimed one time. As an existing
+                egghead member this gift membership will be applied{' '}
+                <em>
+                  <b>on top of your existing membership</b>
+                </em>
+                . They stack. You can claim 10 gift memberships and have PRO
+                access for 10 years. When the gift expires, your existing
+                membership will resume. You will not lose any membership
+                time/money.
+              </p>
+            ) : (
+              <p>
+                A gift membership can only be claimed one time. Once you've
+                claimed this gift nobody else will be able to you. It can't be
+                undone! Make sure that{' '}
+                <em>
+                  <b>{viewer?.email}</b>
+                </em>{' '}
+                is the account you want the gift applied to.
+              </p>
+            )}
+            <PrimaryButton
+              url={'#'}
+              label={`Claim Your Membership for ${viewer?.email}`}
+              onClick={handleClaimGift}
+              className="no-underline mt-6"
+            />
+          </>
+        )}
+      </div>
     </div>
   )
 }
