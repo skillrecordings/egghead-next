7a46e8d077b9d0d3ea9686b3a0c9fd3f1e309d61
Zac Jones <zacjones93@gmail.com>
2022-07-06 11:46:00 -0400

    force a refresh if message type is claimed

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import {useRouter} from 'next/router'
 import {HorizontalResourceCard} from 'components/card/new-horizontal-resource-card'
 import {VerticalResourceCard} from 'components/card/new-vertical-resource-card'
 import Search from 'components/pages/home/search'
@@ -11,15 +12,35 @@ import Link from 'next/link'
 import {track} from 'utils/analytics'
 import Jumbotron from 'components/pages/home/jumbotron'
 import {Jumbotron as HolidayReleaseJumbotron} from 'components/pages/20-days-of-egghead/course-grid'
+import toast, {Toaster} from 'react-hot-toast'
 
 const Home: React.FC<any> = ({data, jumbotron, location}) => {
+  const router = useRouter()
+
+  React.useEffect(() => {
+    const {query} = router
+    if (query.message) {
+      toast(query.message as string, {
+        icon: '✅',
+        duration: 5000,
+      })
+    }
+  }, [router])
+
+  React.useEffect(() => {
+    const {query} = router
+    if (query.type === 'claimed') {
+      router.reload()
+    }
+  }, [])
+
   return (
     <>
       <div className="md:container">
         <Jumbotron data={jumbotron} />
       </div>
       <div className="container">
-        <main className="sm:pt-16 pt-8">
+        <main className="pt-8 sm:pt-16">
           {data.sections
             .filter((s: any) => s.slug !== 'jumbotron')
             .map((section: any, i: number) => {
@@ -29,14 +50,14 @@ const Home: React.FC<any> = ({data, jumbotron, location}) => {
                 <section className="pb-16" key={section.id}>
                   {!section.image && !section.description ? (
                     // simple section
-                    <div className="flex w-full pb-6 items-center justify-between">
-                      <h2 className="lg:text-2xl sm:text-xl text-lg dark:text-white font-semibold leading-tight">
+                    <div className="flex items-center justify-between w-full pb-6">
+                      <h2 className="text-lg font-semibold leading-tight lg:text-2xl sm:text-xl dark:text-white">
                         {section.title}
                       </h2>
                     </div>
                   ) : (
                     // section with image and description
-                    <div className="flex md:flex-row flex-col md:items-start items-center justify-center w-full mb-5 pb-8">
+                    <div className="flex flex-col items-center justify-center w-full pb-8 mb-5 md:flex-row md:items-start">
                       {section.image && (
                         <div className="flex-shrink-0 md:max-w-none max-w-[200px]">
                           <Image
@@ -50,11 +71,11 @@ const Home: React.FC<any> = ({data, jumbotron, location}) => {
                         </div>
                       )}
                       <div>
-                        <h2 className="w-full lg:text-2xl sm:text-xl text-lg dark:text-white font-semibold leading-tight pb-4">
+                        <h2 className="w-full pb-4 text-lg font-semibold leading-tight lg:text-2xl sm:text-xl dark:text-white">
                           {section.title}
                         </h2>
                         {section.description && (
-                          <ReactMarkdown className="prose sm:prose prose-sm dark:prose-dark dark:text-gray-300 text-gray-700 dark:prose-a:text-blue-300 prose-a:text-blue-500">
+                          <ReactMarkdown className="prose-sm prose text-gray-700 sm:prose dark:prose-dark dark:text-gray-300 dark:prose-a:text-blue-300 prose-a:text-blue-500">
                             {section.description}
                           </ReactMarkdown>
                         )}
@@ -131,11 +152,11 @@ const Home: React.FC<any> = ({data, jumbotron, location}) => {
                               section: section.title,
                             })
                           }}
-                          className="flex items-center text-sm px-4 py-3 border-b-2 dark:border-gray-800 bg-transparent  border-gray-200 border-opacity-70 dark:hover:bg-gray-800 dark:hover:bg-opacity-50 opacity-80 hover:opacity-100 transition-all ease-in-out duration-200 group"
+                          className="flex items-center px-4 py-3 text-sm transition-all duration-200 ease-in-out bg-transparent border-b-2 border-gray-200 dark:border-gray-800 border-opacity-70 dark:hover:bg-gray-800 dark:hover:bg-opacity-50 opacity-80 hover:opacity-100 group"
                         >
                           Browse all{' '}
                           <span
-                            className="pl-1 group-hover:translate-x-1 transition-all ease-in-out duration-200"
+                            className="pl-1 transition-all duration-200 ease-in-out group-hover:translate-x-1"
                             aria-hidden
                           >
                             →
@@ -147,7 +168,7 @@ const Home: React.FC<any> = ({data, jumbotron, location}) => {
                 </section>
               )
             })}
-          <div className="md:container mb-16 rounded-lg dark:bg-gray-800 bg-white dark:bg-opacity-60 bg-opacity-100">
+          <div className="mb-16 bg-white bg-opacity-100 rounded-lg md:container dark:bg-gray-800 dark:bg-opacity-60">
             <HolidayReleaseJumbotron />
           </div>
           <Search />
