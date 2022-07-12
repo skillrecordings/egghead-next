f45bfdb8d9f8e956397a250fa5647ad3fc5968fb
Josh Branchaud <jbranchaud@gmail.com>
2022-07-12 10:21:04 -0500

    Comment the 2 day maxAge in english

--- src/server/customer-io-cookies.ts
+++ src/server/customer-io-cookies.ts
@@ -25,7 +25,7 @@ export function setCustomerCookie(res: NextResponse, customer: any) {
   if (customer) {
     const {id, attributes} = customer
     res.cookies.set(CIO_CUSTOMER_OBJECT_KEY, JSON.stringify({id, attributes}), {
-      maxAge: 1000 * 60 * 60 * 24 * 2,
+      maxAge: 1000 * 60 * 60 * 24 * 2, // 2 days in milliseconds
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
       domain: process.env.NEXT_PUBLIC_AUTH_COOKIE_DOMAIN,
