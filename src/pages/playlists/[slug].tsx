885f1ecb38c859b389d4b81fabf1f94e6a4915ab
Joel Hooks <joelhooks@gmail.com>
2022-07-13 22:18:37 -0700

    fix: course redirects properly from playlist route

--- src/pages/playlists/[slug].tsx
+++ src/pages/playlists/[slug].tsx
@@ -8,8 +8,10 @@ export default Playlist
 
 export const getServerSideProps: GetServerSideProps = async ({res, params}) => {
   const slug = params && (params.slug as string)
-  res.setHeader('Location', `/courses/${slug}`)
-  res.statusCode = 302
-  res.end()
-  return {props: {}}
+  return {
+    redirect: {
+      destination: `/courses/${slug}`,
+      permanent: true,
+    },
+  }
 }
