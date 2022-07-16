bfbe5fe1eb986368e79a0fef34e7fd19d2c109e3
jbranchaud <jbranchaud@gmail.com>
2022-07-16 11:00:52 -0500

    refactor: move production process state to object
    Use an exportable schema object to define the Production Process states
    in a single place.
    
    Also, use the name of 'Production Process State' instead of just 'State'
    which is ambiguous.
    
    More details: https://roamresearch.com/#/app/egghead/page/RgkwDbkAz

--- studio/schemas/documents/course.js
+++ studio/schemas/documents/course.js
@@ -22,37 +22,9 @@ export default {
       },
     },
     {
-      name: 'state',
-      title: 'State',
-      type: 'string',
-      options: {
-        list: [
-          {
-            title: 'new',
-            value: 'new',
-          },
-          {
-            title: 'drafting',
-            value: 'drafting',
-          },
-          {
-            title: 'published',
-            value: 'published',
-          },
-          {
-            title: 'content review',
-            value: 'contentReview',
-          },
-          {
-            title: 'pre release',
-            value: 'preRelease',
-          },
-          {
-            title: 'retired',
-            value: 'retired',
-          },
-        ],
-      },
+      name: 'productionProcessState',
+      title: 'Production Process State',
+      type: 'productionProcessState',
     },
     {
       name: 'description',
