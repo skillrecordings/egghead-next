85c7ea74a34f9d9fe9bf2642366bd9abe7abf043
Zac Jones <zacjones93@gmail.com>
2022-07-19 15:05:45 -0400

    fix: add consistent style to course callout on blog pages

--- src/components/mdx/course.tsx
+++ src/components/mdx/course.tsx
@@ -12,11 +12,13 @@ type CourseWidgetProps = {
 const CourseWidget: FunctionComponent<CourseWidgetProps> = ({slug}) => {
   const {data} = useSWR(slug, loadCourse)
 
+  console.log(data)
+
   return data?.path ? (
-    <section>
+    <section className="bg-gray-100 dark:bg-gray-800 bg-opacity-60 rounded p-8 mt-4">
       <Link href={data.path}>
         <a
-          className="flex items-center space-x-3"
+          className="flex sm:flex-row flex-col items-center justify-center sm:space-x-3"
           onClick={() => {
             track(`clicked course widget`, {
               slug: data.slug,
@@ -25,15 +27,26 @@ const CourseWidget: FunctionComponent<CourseWidgetProps> = ({slug}) => {
         >
           <img
             alt="illustration"
-            className="w-32"
+            className="w-40 sm:mr-4"
             src={data.square_cover_480_url}
           />
-          <div>
-            <p className="text-lg">{data.title}</p>
+          <div className="flex flex-col mt-4 items-center text-center sm:items-start sm:justify-start sm:text-left">
+            <span className="uppercase font-medium sm:text-[0.65rem] text-[0.55rem] pb-1 text-gray-700 dark:text-indigo-100 opacity-60">
+              Course
+            </span>
+            <p className="text-lg font-bold">{data.title}</p>
+            <p className="mt-4">{data.summary}</p>
+            <div className="flex flex-row gap-2 my-4">
+              <img
+                className="w-7 h-7 overflow-hidden flex-shrink-0 rounded-full lg:w-7 lg:h-7"
+                alt={`${data.instructor.full_name}  profile picture`}
+                src={data.instructor.avatar_url}
+              />
+              <p>{data.instructor.full_name}</p>
+            </div>
           </div>
         </a>
       </Link>{' '}
-      by {data.instructor.full_name}
     </section>
   ) : null
 }
