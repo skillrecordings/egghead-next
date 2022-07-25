056139397e51bcf1edc641fbbdd73f629cd26fb9
jbranchaud <jbranchaud@gmail.com>
2022-07-25 12:34:00 -0500

    refactor: extract graphql query to const
    This makes it easier to open the file and immediately read the core
    logic which is the loadLesson function.

--- src/lib/lessons.ts
+++ src/lib/lessons.ts
@@ -3,106 +3,6 @@ import {getGraphQLClient} from '../utils/configured-graphql-client'
 import getAccessTokenFromCookie from '../utils/get-access-token-from-cookie'
 
 export async function loadLesson(slug: string, token?: string) {
-  const query = /* GraphQL */ `
-    query getLesson($slug: String!) {
-      lesson(slug: $slug) {
-        slug
-        title
-        description
-        duration
-        next_up_url
-        free_forever
-        path
-        transcript
-        transcript_url
-        subtitles_url
-        hls_url
-        dash_url
-        http_url
-        media_url
-        lesson_view_url
-        thumb_url
-        icon_url
-        download_url
-        staff_notes_url
-        repo_url
-        code_url
-        created_at
-        updated_at
-        published_at
-        collection {
-          ... on Playlist {
-            title
-            slug
-            type
-            square_cover_480_url
-            path
-            lessons {
-              slug
-              type
-              path
-              title
-              completed
-              duration
-              thumb_url
-              media_url
-            }
-          }
-          ... on Course {
-            title
-            slug
-            type
-            square_cover_480_url
-            path
-            lessons {
-              slug
-              type
-              path
-              title
-              completed
-              duration
-              thumb_url
-              media_url
-            }
-          }
-        }
-        course {
-          title
-          square_cover_480_url
-          slug
-        }
-        tags {
-          name
-          label
-          http_url
-          image_url
-        }
-        instructor {
-          full_name
-          avatar_64_url
-          slug
-          twitter
-        }
-        comments {
-          comment
-          commentable_id
-          commentable_type
-          created_at
-          id
-          is_commentable_owner
-          state
-          user {
-            avatar_url
-            full_name
-            instructor {
-              first_name
-            }
-          }
-        }
-      }
-    }
-  `
-
   token = token || getAccessTokenFromCookie()
   const graphQLClient = getGraphQLClient(token)
 
@@ -110,7 +10,110 @@ export async function loadLesson(slug: string, token?: string) {
     slug: slug,
   }
 
-  const {lesson} = await graphQLClient.request(query, variables)
+  const {lesson} = await graphQLClient.request(
+    loadLessonGraphQLQuery,
+    variables,
+  )
 
   return lesson as LessonResource
 }
+
+const loadLessonGraphQLQuery = /* GraphQL */ `
+  query getLesson($slug: String!) {
+    lesson(slug: $slug) {
+      slug
+      title
+      description
+      duration
+      next_up_url
+      free_forever
+      path
+      transcript
+      transcript_url
+      subtitles_url
+      hls_url
+      dash_url
+      http_url
+      media_url
+      lesson_view_url
+      thumb_url
+      icon_url
+      download_url
+      staff_notes_url
+      repo_url
+      code_url
+      created_at
+      updated_at
+      published_at
+      collection {
+        ... on Playlist {
+          title
+          slug
+          type
+          square_cover_480_url
+          path
+          lessons {
+            slug
+            type
+            path
+            title
+            completed
+            duration
+            thumb_url
+            media_url
+          }
+        }
+        ... on Course {
+          title
+          slug
+          type
+          square_cover_480_url
+          path
+          lessons {
+            slug
+            type
+            path
+            title
+            completed
+            duration
+            thumb_url
+            media_url
+          }
+        }
+      }
+      course {
+        title
+        square_cover_480_url
+        slug
+      }
+      tags {
+        name
+        label
+        http_url
+        image_url
+      }
+      instructor {
+        full_name
+        avatar_64_url
+        slug
+        twitter
+      }
+      comments {
+        comment
+        commentable_id
+        commentable_type
+        created_at
+        id
+        is_commentable_owner
+        state
+        user {
+          avatar_url
+          full_name
+          instructor {
+            first_name
+          }
+        }
+      }
+    }
+  }
+`
