bd7c6471823de6846aa60afbba130ffb09cc09e6
Creeland <cree@provinsal.com>
2022-08-16 14:13:58 -0500

    feat: update jumbotron styles for bold course logo and background

--- src/components/pages/home/jumbotron.tsx
+++ src/components/pages/home/jumbotron.tsx
@@ -9,10 +9,31 @@ const Jumbotron: React.FC<any> = ({data}) => {
     <ResourceLink location="jumbotron" path={resource.path} className="group">
       <header className="md:aspect-w-16 md:aspect-h-6 relative h-full rounded-b-lg text-white ">
         <div className="flex items-center justify-center relative z-10 md:pb-16 pb-32 md:px-0 px-5 md:pt-0 pt-10">
-          <div className="w-full max-w-screen-md flex md:flex-row flex-col items-center justify-center md:text-left text-center ">
+          <div className="w-full lg:max-w-screen-lg md:max-w-screen-sm flex md:flex-row flex-col items-center justify-center md:text-left text-center lg:pt-0 md:pt-10">
             <div
               aria-hidden
-              className="flex-shrink-0 relative flex items-center justify-center lg:max-w-none md:max-w-[220px] max-w-[180px] p-5"
+              className="hidden flex-shrink-0 relative lg:flex items-center justify-center p-5"
+            >
+              <Image
+                src={resource.image}
+                alt={resource.title}
+                width={360}
+                height={360}
+                quality={100}
+                loading="eager"
+                priority
+                className="group-hover:scale-95 group-hover:opacity-90 transition-all ease-in-out duration-300"
+              />
+              <div
+                aria-hidden
+                className="absolute flex items-center justify-center group-hover:opacity-100 opacity-0 group-hover:scale-100 scale-0 transition-all ease-in-out duration-300 w-10 h-10 rounded-full bg-white bg-opacity-80 shadow-smooth"
+              >
+                <PlayIcon className="w-4 text-black" />
+              </div>
+            </div>
+            <div
+              aria-hidden
+              className="lg:hidden flex flex-shrink-0 relative items-center justify-center p-5"
             >
               <Image
                 src={resource.image}
@@ -31,7 +52,7 @@ const Jumbotron: React.FC<any> = ({data}) => {
                 <PlayIcon className="w-4 text-black" />
               </div>
             </div>
-            <div className="md:pl-10 text-black">
+            <div className="md:pt-10 text-black">
               <p className="uppercase font-mono text-xs pb-1 opacity-80">
                 Fresh Course
               </p>
