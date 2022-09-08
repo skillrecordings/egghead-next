23a9ce235cf853e4d0a1c6e763f439c098e59749
Zac Jones <zacjones93@gmail.com>
2022-09-08 16:40:59 -0400

    feat: add learner stats to profile page

--- src/utils/time-utils.ts
+++ src/utils/time-utils.ts
@@ -5,6 +5,11 @@ function pad(s: string | any[]) {
   return s.length > 10 ? s : s.length == 0 ? '00' : s
 }
 
+export function convertMintoHours(minutes: number) {
+  const mins = ~~(minutes / 60)
+  return `${pad(mins.toString())}h`
+}
+
 export function convertTimeToMins(seconds: number) {
   const mins = ~~(seconds / 60)
   return `${pad(mins.toString())}m`
