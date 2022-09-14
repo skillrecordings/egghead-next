6fc722e562110479579061959e816abdacbf033b
Zac Jones <zacjones93@gmail.com>
2022-09-14 18:11:24 -0400

    fix type error and add check for courses

--- src/pages/user/components/completed-courses.tsx
+++ src/pages/user/components/completed-courses.tsx
@@ -28,6 +28,20 @@ const CompletedCourses: React.FC<{completeCourseData: CourseData[]}> = ({
   completeCourseData,
 }) => {
   const [filter, setFilter] = React.useState<string>('byDate')
+
+  if (completeCourseData.length === 0) {
+    return (
+      <div>
+        <div className="flex justify-between items-center pb-1 text-xl border-b border-gray-200 dark:border-gray-800">
+          <h2>Completed Courses</h2>
+        </div>
+        <p className="text-center mt-6">
+          You haven't finished any courses yet, pick up where you left off 👇
+        </p>
+      </div>
+    )
+  }
+
   return (
     <div>
       <div className="flex justify-between items-center pb-1 text-xl border-b border-gray-200 dark:border-gray-800">
@@ -80,7 +94,7 @@ const CompletedCourses: React.FC<{completeCourseData: CourseData[]}> = ({
                   <Link href={collection.path}>
                     <a className="blok shrink-0 w-8 h-8 relative">
                       <Image
-                        src={collection.image}
+                        src={collection.image as string}
                         alt=""
                         objectFit="contain"
                         layout="fill"
