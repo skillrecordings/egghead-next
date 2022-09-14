34fd18d6b12f818ee90b7a90095bb7ba3946734b
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-09-14 14:32:26 -0700

    tweaks

--- src/pages/user/components/completed-courses.tsx
+++ src/pages/user/components/completed-courses.tsx
@@ -17,12 +17,17 @@ const CompletedCourses: React.FC<any> = ({completeCourseData}) => {
       <div className="flex justify-between items-center pb-1 text-xl border-b border-gray-200 dark:border-gray-800">
         <h2>Completed Courses</h2>
         <div className="flex space-x-2 text-sm items-center">
-          <span className="font-bold">Sort by:</span>
+          <span className="font-bold hidden dark:text-white md:block">
+            Sort by:
+          </span>
+          <span className="font-bold sm:hidden dark:text-white">By:</span>
           <button
             onClick={() => setFilter('byDate')}
             className={cx('rounded-md text-sm border  py-1 px-2 leading-none', {
-              'border-blue-600 bg-blue-600 text-white': filter === 'byDate',
-              'border-gray-200 bg-gray-50/50': filter === 'byTitle',
+              'border-blue-600 bg-blue-600 text-white cursor-default':
+                filter === 'byDate',
+              'border-gray-200 dark:border-gray-100 bg-gray-100 dark:text-black':
+                filter === 'byTitle',
             })}
           >
             date
@@ -30,15 +35,17 @@ const CompletedCourses: React.FC<any> = ({completeCourseData}) => {
           <button
             onClick={() => setFilter('byTitle')}
             className={cx('rounded-md text-sm border py-1 px-2 leading-none', {
-              'border-gray-200 bg-gray-50/50': filter === 'byDate',
-              'border-blue-600 bg-blue-600 text-white': filter === 'byTitle',
+              'border-gray-200 dark:border-gray-100 bg-gray-100 dark:text-black':
+                filter === 'byDate',
+              'border-blue-600 bg-blue-600 text-white cursor-default':
+                filter === 'byTitle',
             })}
           >
             title
           </button>
         </div>
       </div>
-      <div className="mt-2 max-h-[570px] overscroll-contain overflow-y-auto">
+      <div className="mt-3 max-h-[400px] md:max-h-[570px] overscroll-contain overflow-y-auto">
         {[...completeCourseData]
           .sort(filter === 'byDate' ? byDate : byTitle)
           .map(
@@ -52,7 +59,7 @@ const CompletedCourses: React.FC<any> = ({completeCourseData}) => {
               return (
                 <div
                   key={collection.slug}
-                  className="flex border-b border-gray-200 py-3 items-center space-x-2"
+                  className="flex border-b border-gray-200 dark:border-gray-800 py-3 items-center space-x-2"
                 >
                   <Link href={collection.path}>
                     <a className="blok shrink-0 w-8 h-8 relative">
@@ -67,14 +74,15 @@ const CompletedCourses: React.FC<any> = ({completeCourseData}) => {
                   <div className="grow">
                     <Link href={collection.path}>
                       <a className="blok shrink-0 w-8 h-8 relative dark:hover:text-blue-300 hover:text-blue-700">
-                        <h3 className="text-base font-bold leading-tighter">
+                        <h3 className="text-base font-bold leading-snug md:leading-tighter">
                           {collection.title}
                         </h3>
                       </a>
                     </Link>
                   </div>
-                  <div className="my-auto text-xs text-gray-600 font-bold">
-                    Completed on {format(new Date(completed_at), 'yyyy/MM/dd')}
+                  <div className="text-xs text-gray-600 font-medium md:font-bold self-start md:self-center dark:text-white/80">
+                    <span className="hidden sm:inline">Completed on</span>{' '}
+                    {format(new Date(completed_at), 'yyyy/MM/dd')}
                   </div>
                 </div>
               )
@@ -86,41 +94,3 @@ const CompletedCourses: React.FC<any> = ({completeCourseData}) => {
 }
 
 export default CompletedCourses
-
-{
-  /* <div>
-        <h2 className="pb-1 text-xl border-b border-gray-200 dark:border-gray-800">
-          Completed Courses
-        </h2>
-        <div className="flex flex-wrap gap-4 justify-evenly mt-4">
-          {completeCourseData.map(
-            ({
-              collection,
-              completed_at,
-            }: {
-              collection: CardResource
-              completed_at: string
-            }) => {
-              return (
-                <div className="flex flex-col justify-between">
-                  <VerticalResourceCard
-                    resource={collection}
-                    location="user profile"
-                    className="text-center w-44 flex flex-col items-center justify-center self-center"
-                  />
-                  <span className="z-10 flex flex-row">
-                    <span>
-                      <BadgeCheckIcon color="green" width="1.5em" />
-                    </span>
-                    <span className="ml-1 h-fit my-auto text-xs text-gray-600 italic font-bold">
-                      Completed on{' '}
-                      {format(new Date(completed_at), 'yyyy/MM/dd')}
-                    </span>
-                  </span>
-                </div>
-              )
-            },
-          )}
-        </div>
-      </div> */
-}
