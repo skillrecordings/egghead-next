0cfdc31b2a2a7ca84abe2d5926888f765376a11e
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-09-13 20:04:55 -0700

    wip

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 import {loadAccount} from 'lib/accounts'
 import LoginRequired, {LoginRequiredParams} from 'components/login-required'
 import {useViewer} from 'context/viewer-context'
@@ -71,6 +73,93 @@ function useUserCompletedCourses(viewerId: number) {
   })
 }
 
+const CompletedCourses: React.FC<any> = ({completeCourseData}) => {
+  return (
+    <div>
+      {/* <div>
+        <h2 className="pb-1 text-xl border-b border-gray-200 dark:border-gray-800">
+          Completed Courses
+        </h2>
+        <div className="flex flex-wrap gap-4 justify-evenly mt-4">
+          {completeCourseData.map(
+            ({
+              collection,
+              completed_at,
+            }: {
+              collection: CardResource
+              completed_at: string
+            }) => {
+              return (
+                <div className="flex flex-col justify-between">
+                  <VerticalResourceCard
+                    resource={collection}
+                    location="user profile"
+                    className="text-center w-44 flex flex-col items-center justify-center self-center"
+                  />
+                  <span className="z-10 flex flex-row">
+                    <span>
+                      <BadgeCheckIcon color="green" width="1.5em" />
+                    </span>
+                    <span className="ml-1 h-fit my-auto text-xs text-gray-600 italic font-bold">
+                      Completed on{' '}
+                      {format(new Date(completed_at), 'yyyy/MM/dd')}
+                    </span>
+                  </span>
+                </div>
+              )
+            },
+          )}
+        </div>
+      </div> */}
+      <h2 className="pb-1 text-xl border-b border-gray-200 dark:border-gray-800">
+        Completed Courses
+      </h2>
+      <div className="mt-2">
+        {completeCourseData.map(
+          ({
+            collection,
+            completed_at,
+          }: {
+            collection: CardResource
+            completed_at: string
+          }) => {
+            console.log('collection:', collection)
+            return (
+              <div
+                key={collection.slug}
+                className="flex border-b border-gray-200 py-3 items-center space-x-2"
+              >
+                <Link href={collection.path}>
+                  <a className="blok shrink-0 w-8 h-8 relative">
+                    <Image
+                      src={collection.image}
+                      alt=""
+                      objectFit="contain"
+                      layout="fill"
+                    />
+                  </a>
+                </Link>
+                <div className="grow">
+                  <Link href={collection.path}>
+                    <a className="blok shrink-0 w-8 h-8 relative dark:hover:text-blue-300 hover:text-blue-700">
+                      <h3 className="text-base font-bold leading-tighter">
+                        {collection.title}
+                      </h3>
+                    </a>
+                  </Link>
+                </div>
+                <div className="my-auto text-xs text-gray-600 font-bold">
+                  Completed on {format(new Date(completed_at), 'yyyy/MM/dd')}
+                </div>
+              </div>
+            )
+          },
+        )}
+      </div>
+    </div>
+  )
+}
+
 const User: React.FunctionComponent<
   LoginRequiredParams & {account: ViewerAccount}
 > = () => {
@@ -125,6 +214,34 @@ const User: React.FunctionComponent<
               </div>
             </div>
           </div>
+          {completeCourseStatus === 'loading' ? (
+            'Loading...'
+          ) : completeCourseStatus === 'error' ? (
+            <span>There was an error fetching completed courses.</span>
+          ) : (
+            <CompletedCourses completeCourseData={completeCourseData} />
+          )}
+          {progressStatus === 'loading' ? (
+            'Loading...'
+          ) : progressStatus === 'error' ? (
+            <span>There was an error fetching courses in progress.</span>
+          ) : (
+            <>
+              <div className="flex flex-col space-y-2">
+                <h2 className="pb-1 text-xl border-b border-gray-200 dark:border-gray-800">
+                  Continue learning
+                </h2>
+                {progressData?.progress.map((item: any) => {
+                  return (
+                    <InProgressResource
+                      key={item.collection.title}
+                      resource={item.collection}
+                    />
+                  )
+                })}
+              </div>
+            </>
+          )}
           {/* Connect to GitHub */}
           {isConnectedToGithub ? (
             <div className="sm:px-6 lg:px-0 lg:col-span-9">
@@ -150,75 +267,11 @@ const User: React.FunctionComponent<
               </div>
             </div>
           )}
-          {completeCourseStatus === 'loading' ? (
-            'Loading...'
-          ) : completeCourseStatus === 'error' ? (
-            <span>There was an error fetching completed courses.</span>
-          ) : (
-            <>
-              <div>
-                <h2 className="pb-1 text-xl border-b border-gray-200 dark:border-gray-800">
-                  Completed Courses
-                </h2>
-                <div className="flex flex-wrap gap-4 justify-evenly mt-4">
-                  {completeCourseData.map(
-                    ({
-                      collection,
-                      completed_at,
-                    }: {
-                      collection: CardResource
-                      completed_at: string
-                    }) => {
-                      return (
-                        <div className="flex flex-col justify-between">
-                          <VerticalResourceCard
-                            resource={collection}
-                            location="user profile"
-                            className="text-center w-44 flex flex-col items-center justify-center self-center"
-                          />
-                          <span className="z-10 flex flex-row">
-                            <span>
-                              <BadgeCheckIcon color="green" width="1.5em" />
-                            </span>
-                            <span className="ml-1 h-fit my-auto text-xs text-gray-600 italic font-bold">
-                              Completed on{' '}
-                              {format(new Date(completed_at), 'yyyy/MM/dd')}
-                            </span>
-                          </span>
-                        </div>
-                      )
-                    },
-                  )}
-                </div>
-              </div>
-            </>
-          )}
           {account && (
             <SubscriptionDetails
               stripeCustomerId={account.stripe_customer_id}
               slug={slug}
             />
-          )}
-          {progressStatus === 'loading' ? (
-            'Loading...'
-          ) : progressStatus === 'error' ? (
-            <span>There was an error fetching courses in progress.</span>
-          ) : (
-            <>
-              <div className="flex flex-col space-y-2">
-                <h2 className="pb-1 text-xl border-b border-gray-200 dark:border-gray-800">
-                  Continue learning
-                </h2>
-                {progressData?.progress.map((item: any) => {
-                  return (
-                    <InProgressResource
-                      key={item.slug}
-                      resource={item.collection}
-                    />
-                  )
-                })}
-              </div>
-            </>
           )}
         </div>
       </main>
