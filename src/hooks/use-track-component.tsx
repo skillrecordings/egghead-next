49427c52e7662e198918597c37ee18eed0c0efda
Nicoll <nicoll.g4@gmail.com>
2022-09-19 12:10:00 -0500

    import track directly

--- src/hooks/use-track-component.tsx
+++ src/hooks/use-track-component.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {track} from 'utils/analytics'
+import {track} from 'utils/analytics/track'
 
 export const useTrackComponent = (event: string, properties: any = {}) => {
   React.useEffect(() => {
