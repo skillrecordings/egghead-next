43479c3ccb296fa416a1eb8595c1437bd9fc6a4e
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-09-21 15:30:43 -0700

    Learner Stats widget + additional markup

---
+++ src/components/pages/user/components/index.tsx
@@ -0,0 +1,5 @@
+import CompletedCourses from './completed-courses'
+import ContinueLearning from './continue-learning'
+import LearnerStats from './learner-stats'
+
+export {CompletedCourses, ContinueLearning, LearnerStats}
---
+++ src/components/pages/user/tabs-content/account-info-tab-content.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const AccountInfoTabContent: React.FC<any> = () => {
+  return (
+    <div>
+      <div>Account Info</div>
+    </div>
+  )
+}
+
+export default AccountInfoTabContent
