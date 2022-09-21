f2e538d4847915ee70461f660e837cbe585421b3
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-09-20 17:31:45 -0700

    markup tabs

--- src/pages/user-new/index.tsx
+++ src/pages/user-new/index.tsx
@@ -12,8 +12,6 @@ type ViewerAccount = {
 }
 
 function getAccountWithSubscription(accounts: ViewerAccount[]) {
-  // prefer the account with a subscription, otherwise grab the first, or just
-  // an empty object (which would be an error state, but possible)
   return (
     find<ViewerAccount>(
       accounts,
@@ -42,13 +40,116 @@ const User: React.FunctionComponent<
     loadAccountForSlug(slug)
   }, [slug, authToken])
 
+  function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+  const [currentTab, setCurrentTab] = React.useState<string>('Activity')
+
   return (
     <LoginRequired>
-      <main className="container py-5 mb-16">
-        <div>it's the new user page</div>
+      <main className="container">
+        <main className="flex-1">
+          <div className="relative mx-auto max-w-4xl md:px-8 xl:px-0">
+            <div className="pt-10 pb-16">
+              <div className="px-4 sm:px-6 md:px-0">
+                <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+                  Hello, {viewer?.username}!
+                </h1>
+              </div>
+              <div className="px-4 sm:px-6 md:px-0">
+                <div className="py-6">
+                  <div className="lg:hidden">
+                    <label htmlFor="selected-tab" className="sr-only">
+                      Select a tab
+                    </label>
+                    <select
+                      id="selected-tab"
+                      name="selected-tab"
+                      className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-purple-500 focus:outline-none focus:ring-purple-500 sm:text-sm"
+                      defaultValue={
+                        tabs.find((tab) => tab.label === currentTab)?.label
+                      }
+                      onChange={(e) => {
+                        setCurrentTab(e.target.value)
+                      }}
+                    >
+                      {tabs.map((tab) => (
+                        <option key={tab.label}>{tab.label}</option>
+                      ))}
+                    </select>
+                  </div>
+                  <div className="hidden lg:block">
+                    <div className="border-b border-gray-200">
+                      <nav className="-mb-px flex space-x-8">
+                        {tabs.map((tab) => (
+                          <button
+                            key={tab.label}
+                            className={classNames(
+                              tab.label === currentTab
+                                ? 'border-purple-500 text-purple-600'
+                                : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                              'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm',
+                            )}
+                            onClick={() => setCurrentTab(tab.label)}
+                          >
+                            {tab.label}
+                          </button>
+                        ))}
+                      </nav>
+                    </div>
+                  </div>
+
+                  <div className="mt-10">
+                    {tabs.find((tab) => tab.label === currentTab)?.component}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
       </main>
     </LoginRequired>
   )
 }
 
 export default User
+
+const Component1 = () => {
+  return (
+    <div>
+      <div>Component 1</div>
+    </div>
+  )
+}
+
+const Component2 = () => {
+  return (
+    <div>
+      <div>Component 2</div>
+    </div>
+  )
+}
+
+const Component3 = () => {
+  return (
+    <div>
+      <div>Component 3</div>
+    </div>
+  )
+}
+
+const Component4 = () => {
+  return (
+    <div>
+      <div>Component 4</div>
+    </div>
+  )
+}
+
+const tabs = [
+  {label: 'Activity', component: <Component1 />},
+  {label: 'Membership', component: <Component2 />},
+  {label: 'Account Info', component: <Component3 />},
+  {label: 'Payment', component: <Component4 />},
+]
