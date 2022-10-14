dbedc70eba00478800835e21e15818cf60e5c2ab
Joel Hooks <joelhooks@gmail.com>
2022-10-14 11:57:14 -0700

    hotfix: remove CIO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,21 +33,7 @@ export default class MyDocument extends Document {
                         'link_attribution': true
                       });
 
-                      var _cio = _cio || [];
-                      try {
-                      (function() {
-                        var a,b,c;a=function(f){return function(){_cio.push([f].
-                        concat(Array.prototype.slice.call(arguments,0)))}};b=["load","identify",
-                        "sidentify","track","page"];for(c=0;c<b.length;c++){_cio[b[c]]=a(b[c])};
-                        var t = document.createElement('script'),
-                            s = document.getElementsByTagName('script')[0];
-                        t.async = true;
-                        t.id    = 'cio-tracker';
-                        t.setAttribute('data-site-id', '${process.env.NEXT_PUBLIC_CUSTOMER_IO_SITE_ID}');
-                        t.src = 'https://assets.customer.io/assets/track.js';
-                        s.parentNode.insertBefore(t, s);
-                      })();
-                      } catch(e) { console.error('customer.io error', e); }
+
                       
                         `,
             }}
