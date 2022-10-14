f6ab8e813ecc1ee4cf67d474552d51d9c49c2ce9
Joel Hooks <joelhooks@gmail.com>
2022-10-14 10:57:48 -0700

    hotfix: try to remove effect calls

--- src/pages/redirect.tsx
+++ src/pages/redirect.tsx
@@ -6,15 +6,19 @@ import cookieUtil from 'utils/cookies'
 
 const Redirect = () => {
   const router = useRouter()
+  const [wasCalled, setWasCalled] = React.useState(false)
 
   React.useEffect(() => {
+    if (wasCalled) return
     const lastResource = cookieUtil.get(LAST_RESOURCE_COOKIE_NAME)
     if (!isEmpty(lastResource)) {
+      setWasCalled(true)
       router.replace(lastResource.path)
     } else {
+      setWasCalled(true)
       router.replace('/')
     }
-  }, [])
+  }, [router, wasCalled])
   return null
 }
 
