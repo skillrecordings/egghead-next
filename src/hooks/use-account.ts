00904f85580d0f57024f3e01c6fb634c7e143fcd
Joel Hooks <joelhooks@gmail.com>
2023-01-19 16:14:08 -0800

    make a hook

---
+++ src/hooks/use-account.ts
@@ -0,0 +1,52 @@
+import {useViewer} from '../context/viewer-context'
+import {trpc} from '../trpc/trpc.client'
+
+export const useAccount = () => {
+  const {viewer} = useViewer()
+  const {data: userAccounts, status: accountLoadingStatus} =
+    trpc.user.accountsForCurrent.useQuery()
+
+  const isActiveAccountMember = userAccounts?.some(
+    (account: {members: {id: number}[]}) => {
+      return account.members?.find((member: {id: number}) => {
+        return member.id === viewer.id
+      })
+    },
+  )
+  const isAccountOwner = userAccounts?.some(
+    (account: {owner: {id: number}}) => {
+      return account.owner?.id === viewer.id
+    },
+  )
+
+  const isTeamAccountOwner = userAccounts?.some(
+    (account: {owner: {id: number}; capacity: number}) => {
+      return account.owner?.id === viewer.id && account.capacity > 1
+    },
+  )
+
+  const account =
+    isAccountOwner &&
+    userAccounts?.find((account: {owner: {id: number}}) => {
+      return account.owner?.id === viewer.id
+    })
+
+  const isGiftMembership = account?.subscriptions?.[0]?.type === 'gift'
+  const giftExpiration = account?.subscriptions?.[0]?.current_period_end
+
+  const isTeamMember = isActiveAccountMember && !isAccountOwner
+  const hasStripeAccount = Boolean(account?.stripe_customer_id)
+
+  return {
+    account,
+    isActiveAccountMember,
+    isTeamAccountOwner,
+    isAccountOwner,
+    isGiftMembership,
+    giftExpiration,
+    isTeamMember,
+    hasStripeAccount,
+    accountLoading: accountLoadingStatus === 'loading',
+    accountOwner: userAccounts?.find((account: any) => account.owner).owner,
+  }
+}
