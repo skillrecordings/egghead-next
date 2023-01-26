71f14a5fa3d089304c96cd197fe52d59b2938fb9
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-01-26 11:26:04 -0800

    fix text copy and styles

--- src/pages/user/membership.tsx
+++ src/pages/user/membership.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {format} from 'date-fns'
 
+import {useAccount} from 'hooks/use-account'
 import SubscriptionDetails from 'components/pages/user/components/subscription-details'
 import {ItemWrapper} from 'components/pages/user/components/widget-wrapper'
 import AppLayout from 'components/app/layout'
@@ -8,7 +9,6 @@ import UserLayout from 'components/pages/user/components/user-layout'
 import PricingWidget from 'components/pricing/pricing-widget'
 import Invoices from 'components/invoices'
 import Spinner from 'components/spinner'
-import {useAccount} from 'hooks/use-account'
 
 const Membership = () => {
   const {
@@ -24,26 +24,30 @@ const Membership = () => {
   switch (true) {
     case accountLoading:
       return (
-        <div className="relative flex justify-center">
+        <div className="relative flex justify-center w-full">
           <Spinner className="w-6 h-6 text-gray-600" />
         </div>
       )
     case isGiftMembership:
       return (
-        <div className="flex flex-col justify-center">
-          <h2 className="pb-3 md:pb-4 text-lg font-medium md:font-normal md:text-xl leading-none w-fit mx-auto">
+        <div className="flex flex-col justify-center w-full leading-relaxed">
+          <h2 className="pb-3 md:pb-4 text-lg font-medium md:font-normal md:text-xl">
             You have a pre-paid egghead membership.
           </h2>
-          <p className="w-fit mx-auto">
-            Your membership expires on:{' '}
+          <p>
+            You currently have <strong>PRO</strong>
+            <sup>⭐️</sup> access through a <strong>Gift Subscription</strong>{' '}
+            that ends on{' '}
             <strong>{format(new Date(giftExpiration), 'yyyy/MM/dd')}</strong>.
+            After that, you would need to subscribe to a <strong>Pro</strong>{' '}
+            plan to access our <strong>Pro</strong> materials.
           </p>
         </div>
       )
     case hasStripeAccount:
       return (
-        <div>
-          <ItemWrapper title="Subscription">
+        <div className="w-full">
+          <ItemWrapper title="Membership">
             <SubscriptionDetails
               stripeCustomerId={account.stripe_customer_id}
               slug={account.slug}
@@ -54,12 +58,22 @@ const Membership = () => {
       )
     case isTeamMember:
       return (
-        <div className="text-center">
+        <div className="text-center w-full leading-relaxed">
           <h2 className="mb-4 md:mb-5 text-lg font-medium md:font-normal md:text-xl leading-none">
             You are a member of a team account.
           </h2>
-          <p className="mb-6">
-            You have an access to all of our <strong>PRO</strong> resources.
+          <p className="mb-3 md:mb-4">
+            You have <strong>PRO</strong>
+            <sup>⭐️</sup> access through a team subscription managed by{' '}
+            <strong>
+              <a
+                href={`mailto:${accountOwner.email}`}
+                className="text-white hover:underline"
+              >
+                {accountOwner.email}
+              </a>
+            </strong>
+            .
           </p>
           <p>
             If this is incorrect, please reach out to{' '}
@@ -79,7 +93,7 @@ const Membership = () => {
                 href={`mailto:${accountOwner.email}`}
                 className="hover:underline duration-100"
               >
-                team member
+                team manager
               </a>
             </strong>
             .
@@ -89,11 +103,16 @@ const Membership = () => {
   }
 
   return (
-    <div className="w-full">
-      <h2 className="pb-3 md:pb-4 text-lg font-medium md:font-normal md:text-xl leading-none w-fit mx-auto">
-        No Subscription Found
+    <div className="w-full leading-relaxed">
+      <h2 className="mb-3 md:mb-4 text-lg font-medium md:font-normal md:text-xl leading-none w-fit mx-auto">
+        No Membership Found
       </h2>
-      <p className="w-fit mx-auto mb-12">
+      <p className="mb-3 md:mb-4">
+        You have access to all of our <strong>Free</strong> videos. You can
+        subscribe for full access to all of our Pro<sup>⭐️</sup> lessons any
+        time.
+      </p>
+      <p className="mb-12">
         If this is incorrect, please reach out to{' '}
         <strong>
           <a
