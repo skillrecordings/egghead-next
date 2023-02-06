4dd8a604bd778e0957e08985da59c8c6f75f3968
Joel Hooks <joelhooks@gmail.com>
2023-02-06 13:59:02 -0800

    remove specific discounts

--- src/components/pricing/select-plan-new/index.tsx
+++ src/components/pricing/select-plan-new/index.tsx
@@ -236,7 +236,7 @@ const SelectPlanNew: React.FunctionComponent<SelectPlanProps> = ({
         <PlanTitle>{currentPlan?.name}</PlanTitle>
         {!isPPP && appliedCoupon?.coupon_expires_at && !pricesLoading && (
           <Countdown
-            label="Save 40% on Yearly Memberships Price goes up in:"
+            label="Save on Yearly Memberships Price goes up in:"
             date={fromUnixTime(appliedCoupon.coupon_expires_at)}
           />
         )}
