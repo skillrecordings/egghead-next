bc5b800099a773ce1d1716289864aa9e853c4b9b
Joel Hooks <joelhooks@gmail.com>
2023-02-24 15:35:27 -0800

    adds a playlist progress endpoint to trpc

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,12 +5,14 @@ import {baseProcedure, router} from '../trpc.server'
 import {userRouter} from './user'
 import {subscriptionDetailsRouter} from './subscription-detail'
 import {stripeRouter} from './stripe'
+import {progressRouter} from './progress'
 
 export const appRouter = router({
   healthcheck: baseProcedure.query(() => 'yay!'),
   user: userRouter,
   subscriptionDetails: subscriptionDetailsRouter,
   stripe: stripeRouter,
+  progress: progressRouter,
 })
 
 export type AppRouter = typeof appRouter
