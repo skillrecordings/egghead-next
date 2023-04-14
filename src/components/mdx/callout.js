49e642d8b4c5160a181f8755dcaa5b67a448ac98
Creeland <cree@provinsal.com>
2023-04-14 13:08:36 -0500

    feat: touch up callout component

--- src/components/mdx/callout.js
+++ src/components/mdx/callout.js
@@ -1,13 +1,9 @@
-import React from 'react'
-
-const container = ''
-
-export default function Callout({children, className}) {
+export default function Callout({children, text, className}) {
   return (
     <div
-      className={`mx-auto my-3 w-full dark:bg-gray-800 bg-blue-100 border-l-4 overflow-hidden border-blue-900 dark:text-blue-200 text-blue-600 sm:px-8 px-4 py-3 rounded-sm`}
+      className={`leading-relaxed mx-auto my-3 w-full dark:bg-gray-800 bg-blue-100 border-l-4 border-blue-900  sm:px-8 px-4 py-6 rounded-sm font-small sm:text-lg text-base tracking-normal shadow-md ${className}`}
     >
-      {children}
+      {text ? text : children}
     </div>
   )
 }
