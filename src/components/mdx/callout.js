f350053a7af40c8558f940e4e698503763e366ea
Creeland <cree@provinsal.com>
2023-04-14 13:58:11 -0500

    fix: make callout more consistent with horizontal card

--- src/components/mdx/callout.js
+++ src/components/mdx/callout.js
@@ -1,7 +1,7 @@
 export default function Callout({children, text, className}) {
   return (
     <div
-      className={`leading-relaxed mx-auto my-3 w-full dark:bg-gray-800 bg-blue-100 border-l-4 border-blue-900  sm:px-8 px-4 py-6 rounded-sm font-small sm:text-lg text-base tracking-normal shadow-md ${className}`}
+      className={`p-5 leading-relaxed mx-auto my-3 w-full dark:bg-gray-800 bg-blue-100 border-l-4 border-blue-900 sm:space-x-5 space-x-0 sm:space-y-0 space-y-5 items-center text-left shadow-sm rounded-lg overflow-hidden lg:text-lg sm:text-md ${className}`}
     >
       {text ? text : children}
     </div>
