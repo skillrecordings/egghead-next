1ff6b8ae22d3496198499b49c94334ccdf211c90
Nicoll <nicoll.g4@gmail.com>
2023-05-05 14:01:31 -0500

    create button component

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -166,24 +166,30 @@ const Search: FunctionComponent<SearchProps> = ({
             <RefinementList limit={6} attribute="instructor_name" />
             <RefinementList attribute="access_state" />
             <RefinementList attribute="type" />
-            <section className="flex flex-col items-center sm:pb-32 pt-3 pb-16 w-full h-full">
-              <Link
-                passHref
-                onClick={() => {
-                  track('clicked egghead for teams', {
-                    location: 'egghead-for-teams',
-                  })
-                }}
-                href="/pricing"
-              >
-                <a className="bg-gradient-to-b from-amber-300 to-amber-400 text-black rounded-md px-4 py-2 font-medium text-sm dark:border border-amber-500/50 hover:brightness-110 transition shadow-2xl shadow-amber-300/20">
-                  Level Up Your Team
-                </a>
-              </Link>
-            </section>
+            <TeamButton />
           </div>
         </SimpleBar>
       </aside>
+    )
+  }
+
+  const TeamButton = () => {
+    return (
+      <section className="flex flex-col items-center sm:pb-32 pt-3 pb-16 w-full h-full">
+        <Link
+          passHref
+          onClick={() => {
+            track('clicked egghead for teams', {
+              location: 'egghead-for-teams',
+            })
+          }}
+          href="/pricing"
+        >
+          <a className="bg-gradient-to-b from-amber-300 to-amber-400 text-black rounded-md px-4 py-2 font-medium text-sm dark:border border-amber-500/50 hover:brightness-110 transition shadow-2xl shadow-amber-300/20">
+            Level Up Your Team
+          </a>
+        </Link>
+      </section>
     )
   }
 
@@ -219,21 +225,9 @@ const Search: FunctionComponent<SearchProps> = ({
               <RefinementList attribute="type" />
             </div>
           </div>
-          <section className="flex flex-col items-center justify-center sm:pb-32 pt-6 pb-16 w-full h-full">
-            <Link
-              passHref
-              onClick={() => {
-                track('clicked level-up-team', {
-                  location: 'egghead-for-teams',
-                })
-              }}
-              href="/pricing"
-            >
-              <a className="bg-gradient-to-b from-amber-300 to-amber-400 text-black rounded-md px-4 py-2 align-middle font-medium text-sm dark:border border-amber-500/50 hover:brightness-110 transition shadow-2xl shadow-amber-300/20">
-                Level Up Your Team
-              </a>
-            </Link>
-          </section>
+          <div className="justify-center mt-4">
+            <TeamButton />
+          </div>
         </SimpleBar>
       </div>
     )
