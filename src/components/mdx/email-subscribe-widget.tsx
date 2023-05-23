2d23216e0b9ae3bd42a1ef09619b13ef5e2885ce
Creeland <cree@provinsal.com>
2023-05-23 12:42:53 -0500

    feat: add formik to email widget

--- src/components/mdx/email-subscribe-widget.tsx
+++ src/components/mdx/email-subscribe-widget.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import {Formik, Field, Form} from 'formik'
 
 const EmailSubscribeWidget = (props: any) => {
   return (
@@ -10,28 +11,49 @@ const EmailSubscribeWidget = (props: any) => {
         <p className="py-2">
           sign up and receive regular updates on our latest articles and courses
         </p>
-        <form className="flex flex-col">
-          <input type="email"></input>
-          <p className="py-2">What are you interested in hearing about?</p>
-          <label className="pb-1">
-            <input type="checkbox" />
-            <span className="pl-2">Articles</span>
-          </label>
-          <label className="pb-1">
-            <input type="checkbox" />
-            <span className="pl-2">Courses</span>
-          </label>
-          <label className="pb-4">
-            <input type="checkbox" />
-            <span className="pl-2">Learning Paths</span>
-          </label>
-          <button
-            className="bg-blue-600 rounded-md font-semibold p-1"
-            type="submit"
-          >
-            SIGN UP
-          </button>
-        </form>
+        <Formik
+          initialValues={{
+            email: '',
+            articles: true,
+            courses: false,
+            learningPaths: false,
+          }}
+          onSubmit={async (values) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2))
+            }, 500)
+          }}
+        >
+          {({values}) => (
+            <Form className="flex flex-col">
+              <Field
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="text-black"
+              />
+              <p className="py-2">What are you interested in hearing about?</p>
+              <label className="pb-1">
+                <Field type="checkbox" name="articles" />
+                <span className="pl-2">Articles</span>
+              </label>
+              <label className="pb-1">
+                <Field type="checkbox" name="courses" />
+                <span className="pl-2">Courses</span>
+              </label>
+              <label className="pb-4">
+                <Field type="checkbox" name="learningPaths" />
+                <span className="pl-2">Learning Paths</span>
+              </label>
+              <button
+                className="bg-blue-600 rounded-md font-semibold p-1"
+                type="submit"
+              >
+                SIGN UP
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
       <div className="hidden sm:flex sm:flex-col p-6 rounded-r-md">
         <h2 className="text-3xl leading-tight font-bold">Your time matters.</h2>
