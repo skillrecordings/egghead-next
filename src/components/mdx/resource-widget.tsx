38228dbff54a6b06d153dc865d3c7719b7fa34b9
Creeland <cree@provinsal.com>
2023-05-24 16:25:22 -0500

    feat: adjust article resources to proper grid

--- src/components/mdx/resource-widget.tsx
+++ src/components/mdx/resource-widget.tsx
@@ -81,7 +81,7 @@ const ResourceWidget: React.FC<{
           )
         })}
       {articles && (
-        <div className="bg-gray-100 p-2 xl:p-4">
+        <div className="bg-gray-100 dark:bg-gray-800 p-8">
           <h3 className="prose dark:prose-dark sm:prose-xl lg:prose-2xl max-w-none dark:prose-a:text-blue-300 prose-a:text-blue-500 font-bold mb-4">
             {resource.title}
           </h3>
@@ -89,51 +89,74 @@ const ResourceWidget: React.FC<{
             {' '}
             {resource.description}{' '}
           </p>
-          <Grid className="hidden sm:grid sm:grid-cols-2 gap-2 md:grid-cols-4 xl:gap-4">
+          <Grid>
             {articles?.map((article: any, i: number) => {
               switch (articles.length) {
                 case 1: {
                   return (
                     <HorizontalResourceCard
                       location={location}
-                      className="col-span-2 md:col-span-4"
+                      className="col-span-2 md:col-span-4 dark:bg-gray-600"
                       key={article.slug}
                       resource={article}
                     />
                   )
                 }
+                case 3:
+                  return i === 0 ? (
+                    <HorizontalResourceCard
+                      location={location}
+                      className="col-span-2 dark:bg-gray-600"
+                      key={article.slug}
+                      resource={article}
+                    />
+                  ) : (
+                    <VerticalResourceCard
+                      location={location}
+                      key={article.slug}
+                      resource={article}
+                      className="dark:bg-gray-600"
+                    />
+                  )
+                case 6:
+                  return i === 0 || i === 1 ? (
+                    <HorizontalResourceCard
+                      location={location}
+                      className="col-span-2 dark:bg-gray-600"
+                      key={article.slug}
+                      resource={article}
+                    />
+                  ) : (
+                    <VerticalResourceCard
+                      location={location}
+                      key={article.slug}
+                      resource={article}
+                      className="dark:bg-gray-600"
+                    />
+                  )
+                case 7:
+                  return i === 0 ? (
+                    <HorizontalResourceCard
+                      location={location}
+                      className="col-span-2 dark:bg-gray-600"
+                      key={article.slug}
+                      resource={article}
+                    />
+                  ) : (
+                    <VerticalResourceCard
+                      location={location}
+                      key={article.slug}
+                      resource={article}
+                      className="dark:bg-gray-600"
+                    />
+                  )
                 default:
                   return (
                     <VerticalResourceCard
-                      className="md:col-span-2"
                       location={location}
                       key={article.slug}
                       resource={article}
-                    />
-                  )
-              }
-            })}
-          </Grid>
-          <Grid className="grid sm:hidden sm:grid-cols-2 gap-2 md:grid-cols-4 xl:gap-4">
-            {articles?.map((article: any, i: number) => {
-              switch (articles.length) {
-                case 1: {
-                  return (
-                    <HorizontalResourceCard
-                      location={location}
-                      className="col-span-2 md:col-span-4"
-                      key={article.slug}
-                      resource={article}
-                    />
-                  )
-                }
-                default:
-                  return (
-                    <HorizontalResourceCard
-                      location={location}
-                      className=""
-                      key={article.slug}
-                      resource={article}
+                      className="dark:bg-gray-600 text-xs"
                     />
                   )
               }
@@ -142,7 +165,7 @@ const ResourceWidget: React.FC<{
         </div>
       )}
       {(podcasts || talks) && (
-        <Grid className="grid grid-cols-2 gap-2 md:grid-cols-4 xl:gap-5 sm:gap-3">
+        <Grid className="grid grid-cols-2 gap-2 md:grid-cols-4 xl:gap-5 sm:gap-3 ">
           {talks.map((talk: any) => {
             return (
               <VerticalResourceCard
