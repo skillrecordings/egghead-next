a8082b37d30b4a203f9218f127827ae0842f1c60
Creeland <cree@provinsal.com>
2023-05-25 17:21:43 -0500

    feat: adjust selected interests to be more explicit in cio

--- src/trpc/routers/customer-io.ts
+++ src/trpc/routers/customer-io.ts
@@ -15,9 +15,9 @@ export const customerIORouter = router({
         email: z.string().optional(),
         id: z.string().optional(),
         selectedInterests: z.object({
-          portfolio: z.number().optional(),
-          fullStack2023: z.number().optional(),
-          typescript: z.number().optional(),
+          article_cta_portfolio: z.number().optional(),
+          article_cta_fullStack2023: z.number().optional(),
+          article_cta_typescript: z.number().optional(),
         }),
       }),
     )
@@ -33,7 +33,9 @@ export const customerIORouter = router({
           ...selectedInterests,
           pro: false,
           created_at: Math.floor(Date.now() * 0.001), // Customer.io uses seconds with their UNIX epoch timestamps
-          ...(selectedInterests.typescript && {typescript_score: 10}),
+          ...(selectedInterests.article_cta_typescript && {
+            typescript_score: 10,
+          }),
         })
       } else {
         await cio.identify(id, {
