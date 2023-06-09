343f7c79311ccb645614fed61183b8557baa87b6
Creeland <cree@provinsal.com>
2023-06-09 16:27:00 -0500

    feat: fix overlap and card content all sizes

--- src/components/pages/lessons/overlay/offer-search-cta-overlay.tsx
+++ src/components/pages/lessons/overlay/offer-search-cta-overlay.tsx
@@ -51,108 +51,109 @@ const ContinueCourseCard: React.FC<any> = ({
   resource,
   location,
   className = 'border-none',
+  onClickRewatch,
+  router,
+  lesson,
+  nextLesson,
   ...props
 }) => {
   return (
     <Card
       {...props}
       className={twMerge(
-        'bg-white dark:bg-gray-800 dark:bg-opacity-60 dark:text-gray-200 shadow-sm rounded-lg overflow-hidden p-5 flex flex-col sm:space-y-5 space-y-0 sm:space-x-0 space-x-5 items-center justify-center sm:text-left text-center',
+        'bg-white dark:bg-gray-800 dark:bg-opacity-60 dark:text-gray-200 shadow-sm rounded-lg flex flex-col items-center justify-center sm:text-left text-center',
         className,
       )}
     >
-      {resource.image && (
-        <Link href={resource.path}>
-          <a
-            onClick={() => {
-              track('clicked resource', {
-                resource: resource.path,
-                linkType: 'image',
-                location,
-              })
-            }}
-            className="block flex-shrink-0 sm:w-auto m:w-24 w-36"
-            tabIndex={-1}
-          >
-            <CardPreview>
-              <Image
-                src={get(resource.image, 'src', resource.image)}
-                width={160}
-                height={160}
-                layout="fixed"
-                className="transition ease-in-out hover:scale-110 duration-100 object-cover rounded-md"
-                alt={`illustration for ${resource.title}`}
-              />
-            </CardPreview>
-          </a>
-        </Link>
-      )}
       <CardContent className="flex flex-col justify-center items-center">
-        <CardHeader className="mx-16">
-          <Link href={resource.path}>
-            <a
+        <div className="hidden md:flex md:flex-col justify-center items-center">
+          {resource.image && (
+            <Link href={resource.path}>
+              <a
+                onClick={() => {
+                  track('clicked resource', {
+                    resource: resource.path,
+                    linkType: 'image',
+                    location,
+                  })
+                }}
+                className="block flex-shrink-0 sm:w-auto m:w-24 w-36"
+                tabIndex={-1}
+              >
+                <CardPreview>
+                  <Image
+                    src={get(resource.image, 'src', resource.image)}
+                    width={180}
+                    height={180}
+                    layout="fixed"
+                    className="transition ease-in-out hover:scale-110 duration-100 object-cover rounded-md"
+                    alt={`illustration for ${resource.title}`}
+                  />
+                </CardPreview>
+              </a>
+            </Link>
+          )}
+          <CardHeader className="mx-16">
+            <Link href={resource.path}>
+              <a
+                onClick={() => {
+                  track('clicked resource', {
+                    resource: resource.path,
+                    linkType: 'text',
+                    location,
+                  })
+                }}
+                className="inline-block hover:text-blue-600 dark:hover:text-blue-300 w-fit"
+              >
+                <h3 className="text-lg lg:text-xl font-bold leading-tighter dark:text-white dark:hover:text-blue-300 text-center">
+                  {resource.title}
+                </h3>
+              </a>
+            </Link>
+          </CardHeader>
+          <CardMeta className="text-xs justify-center text-gray-600 dark:text-gray-300 pb-2 pt-1 text-center">
+            {resource.byline}
+          </CardMeta>
+        </div>
+        <div className="flex flex-col items-center gap-y-2">
+          {/* bg-blue-600 rounded px-4 py-2 flex sm:w-full w-48 items-center justify-center hover:bg-blue-500 transition-colors duration-200 ease-in-out text-xs md:text-base */}
+          <button
+            className="bg-blue-600 rounded py-2 flex w-full items-center justify-center hover:bg-blue-500 transition-colors duration-200 ease-in-out text-xs md:text-base whitespace-nowrap"
+            onClick={() => {
+              analytics.events.engagementClickedWatchedLessonAgain(lesson.slug)
+              onClickRewatch()
+            }}
+          >
+            <AcademicCapIcon className="w-6 mr-2" />
+            Watch Full Course
+          </button>
+          <div className="flex flex-row space-x-2">
+            <button
               onClick={() => {
-                track('clicked resource', {
-                  resource: resource.path,
-                  linkType: 'text',
-                  location,
+                router.push(nextLesson.path || '#')
+                track('clicked play next', {
+                  lesson: lesson.slug,
+                  location: 'lesson overlay',
                 })
               }}
-              className="inline-block hover:text-blue-600 dark:hover:text-blue-300 w-fit"
+              className="border border-blue-600 rounded px-4 py-2 flex w-full sm:w-1/2 items-center justify-center hover:bg-gray-900 transition-colors duration-200 ease-in-out text-xs md:text-base whitespace-nowrap"
             >
-              <h3 className="text-lg lg:text-xl font-bold leading-tighter dark:text-white dark:hover:text-blue-300 text-center">
-                {resource.title}
-              </h3>
-            </a>
-          </Link>
-        </CardHeader>
-        <CardMeta className="text-xs justify-center text-gray-600 dark:text-gray-300 pb-2 pt-1 text-center">
-          {resource.byline}
-        </CardMeta>
-        <CardBody className="flex flex-col prose dark:prose-dark dark:prose-dark-sm dark:prose-a:text-blue-300 prose-a:text-blue-500 prose-sm max-w-none justify-center">
-          <Markdown>{resource.description}</Markdown>
-          <div className="grid grid-rows-2 items-center gap-y-2">
-            {/* bg-blue-600 rounded px-4 py-2 flex sm:w-full w-48 items-center justify-center hover:bg-blue-500 transition-colors duration-200 ease-in-out text-xs md:text-base */}
-            <button
-              className="bg-blue-600 rounded py-2 flex sm:w-full items-center justify-center hover:bg-blue-500 transition-colors duration-200 ease-in-out text-xs md:text-base whitespace-nowrap"
-              onClick={() => {
-                analytics.events.engagementClickedWatchedLessonAgain(
-                  lesson.slug,
-                )
-                onClickRewatch()
-              }}
-            >
-              <AcademicCapIcon className="w-6 mr-2" />
-              Watch Full Course
+              <PlayIcon className="w-6 mr-2" /> Play Next
             </button>
-            <div className="grid grid-cols-2 gap-2">
-              <button
-                onClick={() => {
-                  router.push(nextLesson.path || '#')
-                  track('clicked play next', {
-                    lesson: lesson.slug,
-                    location: 'lesson overlay',
-                  })
-                }}
-                className="border border-blue-600 rounded px-4 py-2 flex sm:w-full w-48 items-center justify-center hover:bg-gray-900 transition-colors duration-200 ease-in-out text-xs md:text-base whitespace-nowrap"
-              >
-                <PlayIcon className="w-6 mr-2" /> Play Next
-              </button>
-              <button
-                onClick={() => {
-                  router.push(nextLesson.path || '#')
-                  track('clicked play next', {
-                    lesson: lesson.slug,
-                    location: 'lesson overlay',
-                  })
-                }}
-                className="border border-blue-600 rounded px-4 py-2 flex sm:w-full w-48 items-center justify-center hover:bg-gray-900 transition-colors duration-200 ease-in-out text-xs md:text-base whitespace-nowrap"
-              >
-                <IconRefresh className="w-6 mr-2" /> Watch Again
-              </button>
-            </div>
+            <button
+              onClick={() => {
+                router.push(nextLesson.path || '#')
+                track('clicked play next', {
+                  lesson: lesson.slug,
+                  location: 'lesson overlay',
+                })
+              }}
+              className="border border-blue-600 rounded px-4 py-2 flex sm:w-1/2 w-full items-center justify-center hover:bg-gray-900 transition-colors duration-200 ease-in-out text-xs md:text-base whitespace-nowrap"
+            >
+              <IconRefresh className="w-6 mr-2" /> Watch Again
+            </button>
           </div>
-        </CardBody>
+        </div>
       </CardContent>
     </Card>
   )
@@ -168,14 +169,14 @@ const SearchCard = ({
   return (
     <Card
       className={twMerge(
-        `bg-white dark:bg-gray-800 dark:bg-opacity-60 dark:text-gray-200 shadow-sm rounded-lg overflow-hidden p-5 flex sm:flex-row flex-col sm:space-x-5 space-x-0 sm:space-y-0 space-y-5 items-center sm:text-left text-center max-h-1/3`,
+        `bg-gray-800 bg-opacity-60 text-gray-200 rounded-lg overflow-hidden`,
         className,
       )}
     >
-      <CardContent className="grid grid-rows-2 w-full justify-center items-center">
-        <CardHeader>
+      <CardContent className="flex flex-col w-full h-full justify-center items-center">
+        <CardHeader className="mb-4">
           <p className="tracking-tight text-gray-700 dark:text-white text-center">
-            <h3 className="text-md font-medium leading-tighter">
+            <h3 className="text-lg font-medium leading-tighter">
               Search for more {tagLabel}:
             </h3>
           </p>
@@ -188,67 +189,6 @@ const SearchCard = ({
         </CardBody>
       </CardContent>
     </Card>
-  )
-}
-
-const SearchVideoControls: React.FunctionComponent<{
-  router: any
-  nextLesson: any
-  lesson: any
-  tag: any
-  courseImage: any
-  onClickRewatch: any
-  className?: string
-}> = ({
-  router,
-  nextLesson,
-  lesson,
-  tag,
-  courseImage,
-  onClickRewatch,
-  className = '',
-}) => {
-  return (
-    <div
-      className={twMerge(
-        'flex flex-col items-center justify-center p-4 space-y-6',
-        className,
-      )}
-    >
-      <div className="hidden sm:flex flex-col justify-center items-center space-y-4">
-        <h3 className="text-lg xl:text-xl 2xl:text-2xl font-semibold text-center">
-          Search for more {tag.label}
-        </h3>
-        <SearchBar
-          className="rounded-lg shadow-md transition duration-200 hover:shadow-lg focus-within:shadow-lg bg-gray-800 border-gray-700 focus-within:border-gray-500 hover:bg-gray-700 w-full max-w-md"
-          initialValue={tag.label}
-        />
-      </div>
-      <div className="flex flex-col items-center sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
-        {/* bg-blue-600 rounded px-4 py-2 flex sm:w-full w-48 items-center justify-center hover:bg-blue-500 transition-colors duration-200 ease-in-out text-xs md:text-base */}
-        <button
-          className="border border-blue-600 rounded px-4 py-2 flex sm:w-full w-48 items-center justify-center hover:bg-gray-900 transition-colors duration-200 ease-in-out text-xs md:text-base whitespace-nowrap"
-          onClick={() => {
-            analytics.events.engagementClickedWatchedLessonAgain(lesson.slug)
-            onClickRewatch()
-          }}
-        >
-          <IconRefresh className="w-5 mr-2" /> Watch again
-        </button>
-        <button
-          onClick={() => {
-            router.push(nextLesson.path || '#')
-            track('clicked play next', {
-              lesson: lesson.slug,
-              location: 'lesson overlay',
-            })
-          }}
-          className="bg-blue-600 rounded px-4 py-2 flex sm:w-full w-48 items-center justify-center hover:bg-blue-500 transition-colors duration-200 ease-in-out text-xs md:text-base whitespace-nowrap"
-        >
-          <IconPlay className="w-5 mr-2" /> Play next
-        </button>
-      </div>
-    </div>
   )
 }
 
@@ -271,8 +211,8 @@ const OfferSearchCTAOverlay: React.FunctionComponent<{
 
   return (
     <OverlayWrapper className="absolute top-0 z-10 h-full max-w-full bg-opacity-100 dark max-h-full">
-      <div className="hidden 2xl:grid grid-cols-2 gap-4 m-8 h-full">
-        <div className="flex flex-col">
+      <div className="flex flex-row h-full w-3/4">
+        <div className="flex flex-col max-h-full 2xl:w-1/2 w-full">
           <ContinueCourseCard
             resource={
               {
@@ -284,12 +224,19 @@ const OfferSearchCTAOverlay: React.FunctionComponent<{
                 byline: ``,
               } as any
             }
-            className="row-span-2 mb-4 h-3/4"
+            router={router}
+            onClickRewatch={onClickRewatch}
+            lesson={lesson}
+            nextLesson={nextLesson}
+            className="row-span-2 2xl:mb-4 h-full 2xl:h-3/4"
           />
-          <SearchCard tagLabel={tag.label as string} className="h-1/4" />
+          <SearchCard
+            tagLabel={tag.label as string}
+            className="hidden 2xl:block 2xl:h-1/4"
+          />
         </div>
         {hits.length > 0 ? (
-          <div className="flex flex-col">
+          <div className="hidden 2xl:flex 2xl:flex-col h-full w-1/2 ml-4">
             <HorizontalResourceCard
               resource={
                 {
@@ -301,9 +248,9 @@ const OfferSearchCTAOverlay: React.FunctionComponent<{
                   byline: ``,
                 } as any
               }
-              className="mb-4"
+              className="mb-4 h-1/2"
             />
-            <div className="grid grid-cols-2 gap-x-4">
+            <div className="grid grid-cols-2 gap-x-4 h-1/2 overflow-hidden">
               <VerticalResourceCard resource={hits[0] as any} />
               <VerticalResourceCard resource={hits[1] as any} />
             </div>
@@ -323,30 +270,6 @@ const OfferSearchCTAOverlay: React.FunctionComponent<{
             className="p-8 min-w-0 3xl:w-[40rem] lg:w-[30rem] xl:ml-16 3xl:p-4"
           />
         )}
-      </div>
-      <div className="grid sm:grid-cols-2 2xl:hidden w-full mx-8 justify-center">
-        <SearchVideoControls
-          router={router}
-          nextLesson={nextLesson}
-          courseImage={courseImage}
-          lesson={lesson}
-          onClickRewatch={onClickRewatch}
-          className="p-16"
-          tag={tag}
-        />
-        <VerticalResourceCard
-          resource={
-            {
-              title: `More Expert Curated ${tag.label} Courses`,
-              slug: tag.name,
-              image: tag.image_url,
-              path: `/q/${tag.name}?access_state=free&type=playlist`,
-              description: `A hand-curated collection of the best free ${tag.label} courses on egghead.io.`,
-              byline: ``,
-            } as any
-          }
-          className="hidden sm:block p-16"
-        />
       </div>
     </OverlayWrapper>
   )
