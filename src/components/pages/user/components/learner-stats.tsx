8ceada81cb1c72be9f1f4fafa2814d218741e1c6
Zac Jones <zacjones93@gmail.com>
2023-06-16 10:00:44 -0400

    fix: update completeCourseCount Variable name

--- src/components/pages/user/components/learner-stats.tsx
+++ src/components/pages/user/components/learner-stats.tsx
@@ -38,7 +38,7 @@ const LearnerStats: React.FC<{
                   Courses completed
                 </dt>
                 <dd className="order-1 text-4xl lg:text-5xl font-bold tracking-tight text-blue-600 dark:text-white leading-none">
-                  {learnerStatsData.completedCourseCount}
+                  {learnerStatsData.completeCourseCount}
                 </dd>
               </div>
               <div className="flex flex-col text-center pt-5 sm:py-4">
