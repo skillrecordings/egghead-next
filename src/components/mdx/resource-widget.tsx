4cc4291e47b17c94cb39d06f48551806ee8d92e8
Zac Jones <zacjones93@gmail.com>
2023-07-13 16:28:38 -0600

    Zj/update square card (#1342)
    * clean up titles and use vertical for 2 resources
    
    * update cards to be square
    
    * remove duplicate import

--- src/components/mdx/resource-widget.tsx
+++ src/components/mdx/resource-widget.tsx
@@ -1,7 +1,210 @@
 import Grid from 'components/grid'
-import {HorizontalResourceCard} from 'components/card/new-horizontal-resource-card'
 import {VerticalResourceCard} from 'components/card/new-vertical-resource-card'
+import {HorizontalResourceCard} from 'components/card/new-horizontal-resource-card'
+import cx from 'classnames'
+import Image from 'next/image'
+import {
+  Card,
+  CardPreview,
+  CardHeader,
+  CardContent,
+  CardBody,
+  CardAuthor,
+  CardFooter,
+} from 'components/card'
+import {CardResource} from 'types'
+import {get, isEmpty, truncate} from 'lodash'
+import {CheckIcon} from '@heroicons/react/solid'
+import {Textfit} from 'react-textfit'
+import ReactMarkdown from 'react-markdown'
+import Link from 'next/link'
+import analytics from 'utils/analytics'
 import {twMerge} from 'tailwind-merge'
+
+const ResourceLink: React.FC<{
+  path: string
+  resource_type: string
+  location: string
+  tag?: any
+  instructor?: string
+  className?: string
+  linkType?: string
+  feature?: string
+}> = ({
+  children,
+  path,
+  tag,
+  resource_type,
+  instructor,
+  location,
+  linkType = 'text',
+  feature,
+  ...props
+}) => (
+  <Link href={path}>
+    <a
+      onClick={() => {
+        if (feature) {
+          analytics.events.activityInternalLinkClick(
+            resource_type,
+            location,
+            tag,
+            path,
+            instructor,
+            feature,
+          )
+        } else {
+          analytics.events.activityInternalLinkClick(
+            resource_type,
+            location,
+            tag,
+            path,
+            instructor,
+          )
+        }
+      }}
+      {...props}
+    >
+      {children}
+    </a>
+  </Link>
+)
+
+export const PreviewImage: React.FC<{title: string; image: any; name: string}> =
+  ({image, name}) => {
+    if (!image) return null
+
+    const getSize = (name: string) => {
+      switch (name) {
+        case 'lesson':
+          return 40
+        case 'talk':
+          return 80
+        case 'article':
+          return 500
+        default:
+          return 200
+      }
+    }
+
+    return (
+      <CardPreview
+        className={`relative flex items-center justify-center w-full top-28 ${cx(
+          {
+            'max-w-[40px]': name === 'lesson',
+            'max-w-[80px]': name === 'talk',
+            'xl:max-w-[200px] sm:max-w-[150px] max-w-[100px]':
+              name === 'course',
+          },
+        )}`}
+      >
+        <Image
+          aria-hidden
+          src={get(image, 'src', image)}
+          width={getSize(name)}
+          height={getSize(name)}
+          objectFit={name === 'article' ? 'cover' : 'contain'}
+          quality={100}
+          alt=""
+        />
+      </CardPreview>
+    )
+  }
+
+const SquareResourceCard: React.FC<{
+  resource: CardResource
+  location?: string
+  describe?: boolean
+  className?: string
+  completedCoursesIds?: number[]
+  feature?: string
+}> = ({
+  children,
+  resource,
+  location,
+  className = '',
+  describe = true,
+  completedCoursesIds,
+  feature,
+  ...props
+}) => {
+  if (isEmpty(resource)) return null
+  const {externalId} = resource
+  const isCourseCompleted =
+    !isEmpty(completedCoursesIds) &&
+    externalId &&
+    completedCoursesIds?.some((courseId: number) => courseId === externalId)
+
+  const resourceType =
+    resource.name === 'landing-page' ? 'guide' : resource.name
+
+  const defaultClassName =
+    'rounded-md aspect-w-4 aspect-h-4 w-full h-full transition-all ease-in-out duration-200 relative overflow-hidden group dark:bg-gray-800 bg-white dark:bg-opacity-60 shadow-smooth dark:hover:bg-gray-700 dark:hover:bg-opacity-50 max-w-[344px]'
+  return (
+    <ResourceLink
+      path={(resource.path || resource.url) as string}
+      location={location as string}
+      className={className}
+      resource_type={resourceType || ''}
+      instructor={resource.instructor?.name}
+      tag={resource.tag?.name}
+      feature={feature}
+    >
+      <Card {...props} resource={resource} className={defaultClassName}>
+        <CardContent className="grid grid-rows-9 gap-2 items-center px-5 py-2">
+          <CardHeader
+            className={`row-span-3 flex items-center justify-center overflow-hidden ${cx(
+              {
+                '-mt-56 -mx-5': resource.name === 'article',
+              },
+            )}`}
+          >
+            <PreviewImage
+              name={resourceType || ''}
+              image={resource.image}
+              title={resource.title || ''}
+            />
+          </CardHeader>
+          <CardBody className="row-span-4">
+            {resource.name && (
+              <div className="flex items-center gap-1 mb-1">
+                {isCourseCompleted && (
+                  <span title="Course completed" className=" text-green-500">
+                    <CheckIcon />
+                  </span>
+                )}
+                <p
+                  aria-hidden
+                  className="uppercase font-medium lg:text-[0.65rem] text-[0.55rem] text-gray-700 dark:text-indigo-100 opacity-60"
+                >
+                  {resourceType}
+                </p>
+                {resource.byline && (
+                  <span className=" rounded-bl-none rounded-tr-none rounded-md transition-all ease-in-out duration-200 uppercase font-medium lg:text-[0.65rem] text-[0.55rem] text-gray-700 dark:text-indigo-100 opacity-60">
+                    • {resource.byline}
+                  </span>
+                )}
+              </div>
+            )}
+            <Textfit
+              mode="multi"
+              className="lg:h-[65px] md:h-[50px] sm:h-[50px] h-[46px] font-medium leading-tight flex items-center"
+              max={22}
+            >
+              <h3>{resource.title}</h3>
+            </Textfit>
+            {resource.description && describe && (
+              <ReactMarkdown className="py-2 prose dark:prose-dark prose-sm dark:text-gray-300 text-gray-700 dark:prose-a:text-blue-300 prose-a:text-blue-500 sm:block hidden">
+                {truncate(resource.description, {length: 90})}
+              </ReactMarkdown>
+            )}
+          </CardBody>
+        </CardContent>
+        {children}
+      </Card>
+    </ResourceLink>
+  )
+}
 
 const VerticalResourceCardForWidget: React.FC<{
   resource: any
@@ -37,7 +240,7 @@ const HorizontalResourceCardForWidget: React.FC<{
           {resource.byline}
         </span>
       )}
-      <HorizontalResourceCard
+      <SquareResourceCard
         location={location}
         key={resource.slug}
         resource={resource}
@@ -153,14 +356,14 @@ const ResourceWidget: React.FC<{
                 }
                 case 2: {
                   return i === 0 ? (
-                    <VerticalResourceCardForWidget
+                    <SquareResourceCard
                       location={location}
                       className="col-span-2 dark:bg-gray-600 rounded-md"
                       key={article.slug}
                       resource={article}
                     />
                   ) : (
-                    <VerticalResourceCardForWidget
+                    <SquareResourceCard
                       location={location}
                       key={article.slug}
                       resource={article}
