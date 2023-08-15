33fa64f336371b505f3f65f31d8dd62f384a1eec
Zac Jones <zacjones93@gmail.com>
2023-08-15 11:05:33 -0600

    feat: replace tippy with radix tooltip

--- src/components/player/download-control.tsx
+++ src/components/player/download-control.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import {FunctionComponent} from 'react'
 import axios from 'utils/configured-axios'
 import analytics from 'utils/analytics'
-import Tippy from '@tippyjs/react'
+import * as Tooltip from '@radix-ui/react-tooltip'
 import Link from 'next/link'
 
 type DownloadButtonProps = {
@@ -19,35 +19,52 @@ type DownloadControlProps = {
   state?: string
 }
 
-const DownloadButton: FunctionComponent<
+const DownloadButtonWithTooltip: FunctionComponent<
   React.PropsWithChildren<DownloadButtonProps>
 > = ({slug, download_url, state}) => {
-  return download_url ? (
-    <button
-      onClick={(e) => {
-        e.preventDefault()
-        if (download_url) {
-          axios.get(download_url).then(({data}) => {
-            window.location.href = data
-          })
-        }
-        analytics.events.activityCtaClick('lesson download', slug)
-      }}
-      aria-label="download video"
-      className="flex items-center justify-center w-10 h-10 text-white border-none"
-      type="button"
-    >
-      <IconDownload className="w-6" />
-    </button>
-  ) : state === 'RETIRED' ? null : (
-    <Link
-      href="/pricing"
-      passHref
-      aria-label="become a member to download this lesson"
-      className="flex items-center justify-center w-10 h-10 opacity-50"
-    >
-      <IconDownload className="w-6" />
-    </Link>
+  return (
+    <Tooltip.Provider>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          {download_url ? (
+            <button
+              onClick={(e) => {
+                e.preventDefault()
+                if (download_url) {
+                  axios.get(download_url).then(({data}) => {
+                    window.location.href = data
+                  })
+                }
+                analytics.events.activityCtaClick('lesson download', slug)
+              }}
+              aria-label="download video"
+              className="flex items-center justify-center w-10 h-10 text-white border-none"
+              type="button"
+            >
+              <IconDownload className="w-6" />
+            </button>
+          ) : state === 'RETIRED' ? null : (
+            <Link
+              href="/pricing"
+              passHref
+              aria-label="become a member to download this lesson"
+              className="flex items-center justify-center w-10 h-10 opacity-50"
+            >
+              <IconDownload className="w-6" />
+            </Link>
+          )}
+        </Tooltip.Trigger>
+        <Tooltip.Portal>
+          <Tooltip.Content className="z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
+            <div className="block z-10 px-2 py-1 text-sm bg-gray-900 rounded-sm">
+              {download_url
+                ? 'Download video'
+                : 'Become a member to download this lesson'}
+            </div>
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   )
 }
 
@@ -84,24 +101,11 @@ const DownloadControl: FunctionComponent<
 > = ({slug, download_url, state}) => {
   return (
     <DownloadControlErrorBoundary>
-      <Tippy
-        offset={[0, -2]}
-        content={
-          <div className="px-2 py-1 text-sm bg-gray-900 rounded-sm">
-            {download_url
-              ? 'Download video'
-              : 'Become a member to download this lesson'}
-          </div>
-        }
-      >
-        <div>
-          <DownloadButton
-            slug={slug}
-            download_url={download_url}
-            state={state}
-          />
-        </div>
-      </Tippy>
+      <DownloadButtonWithTooltip
+        slug={slug}
+        download_url={download_url}
+        state={state}
+      />
     </DownloadControlErrorBoundary>
   )
 }
