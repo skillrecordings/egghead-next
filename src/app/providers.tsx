cfe8aa0852523850c705903f99ec28fcc4ea2bed
Zac Jones <zacjones93@gmail.com>
2023-09-06 09:15:28 -0600

    wrap app in themeprovider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,14 @@
 
 import {ViewerProvider} from 'context/viewer-context'
 import {CioProvider} from 'hooks/use-cio'
+import {ThemeProvider} from 'next-themes'
 
 export function Providers({children}: {children: React.ReactNode}) {
   return (
-    <ViewerProvider>
-      <CioProvider>{children}</CioProvider>
-    </ViewerProvider>
+    <ThemeProvider attribute="class">
+      <ViewerProvider>
+        <CioProvider>{children}</CioProvider>
+      </ViewerProvider>
+    </ThemeProvider>
   )
 }
