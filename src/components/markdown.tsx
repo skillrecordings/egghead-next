e04f1900b723756212811b1674ddfcdad91b11e6
Zac Jones <zacjones93@gmail.com>
2023-09-06 12:04:12 -0600

    update ReactMarkdown to new api

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import ReactMarkdown from 'react-markdown'
-import gfm from 'remark-gfm'
+import remarkGfm from 'remark-gfm'
 
 const Markdown: React.FunctionComponent<React.PropsWithChildren<any>> = (
   props,
 ) => {
-  return <ReactMarkdown plugins={[gfm]} {...props} />
+  return <ReactMarkdown remarkPlugins={[remarkGfm]} {...props} />
 }
 
 export default Markdown
