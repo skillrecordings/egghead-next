1a27e1d38147058898f270550d9f45334f018442
Creeland <cree@provinsal.com>
2023-10-25 11:42:07 -0500

    feat: add PosthogClient

---
+++ src/lib/posthog-client.ts
@@ -0,0 +1,26 @@
+import posthog from 'posthog-js'
+
+class PosthogClient {
+  static init() {
+    if (typeof window !== 'undefined') {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
+        api_host:
+          process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+        // Enable debug mode in development
+        loaded: (posthog) => {
+          if (process.env.NODE_ENV === 'development') posthog.debug()
+        },
+        capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+        autocapture: false, // Disable automatic default event capture, as we capture manually
+      })
+    }
+
+    return posthog
+  }
+
+  static capture(event: string, params: any) {
+    posthog.capture(event, params)
+  }
+}
+
+export default PosthogClient
