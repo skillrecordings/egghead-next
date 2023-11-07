2c8d3bcdf048da247aab320c09bf693d599b60e3
Creeland <cree@provinsal.com>
2023-11-07 11:46:35 -0600

    feat: add PostHog to app routes

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,13 +4,19 @@ import {ViewerProvider} from 'context/viewer-context'
 import {CioProvider} from 'hooks/use-cio'
 import {ThemeProvider} from 'next-themes'
 import TrpcProvider from 'app/_trpc/Provider'
+import PosthogClient from 'lib/posthog-client'
+import {PostHogProvider} from 'posthog-js/react'
 
 export function Providers({children}: {children: React.ReactNode}) {
+  const posthog = PosthogClient.init()
+
   return (
     <ThemeProvider attribute="class">
       <ViewerProvider>
         <TrpcProvider>
-          <CioProvider>{children}</CioProvider>
+          <CioProvider>
+            <PostHogProvider client={posthog}>{children}</PostHogProvider>
+          </CioProvider>
         </TrpcProvider>
       </ViewerProvider>
     </ThemeProvider>
