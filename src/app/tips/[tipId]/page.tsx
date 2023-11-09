b99390078e4a813655ab6e4e5edce71f48ea5383
Zac Jones <zacjones93@gmail.com>
2023-11-09 09:44:44 -0700

    add metadata to tip pages (#1395)
    * add metadata to tips page
    
    * add ld+json to tip page
    
    * update og image used

--- src/app/tips/[tipId]/page.tsx
+++ src/app/tips/[tipId]/page.tsx
@@ -1,5 +1,49 @@
 import TipTemplate from 'components/tips/tip-template'
 import {serverClient} from 'app/_trpc/serverClient'
+import truncate from 'lodash/truncate'
+import removeMarkdown from 'remove-markdown'
+
+import type {Metadata, ResolvingMetadata} from 'next'
+
+type Props = {
+  params: {tipId: string}
+  searchParams: {[key: string]: string | string[] | undefined}
+}
+
+export async function generateMetadata(
+  {params, searchParams}: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  // read route params
+  const tipId = params?.tipId
+
+  // fetch data
+  const tip = await serverClient.tips.bySlug({slug: params.tipId})
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: truncate(tip.title, {length: 65}),
+    description: truncate(removeMarkdown(tip.description), {
+      length: 155,
+    }),
+    openGraph: {
+      images: [
+        'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1699475351/og-images/card-tips-index_2x.png',
+        ...previousImages,
+      ],
+    },
+    twitter: {
+      title: truncate(tip.title, {length: 65}),
+      images: [
+        'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1699475351/og-images/card-tips-index_2x.png',
+        ...previousImages,
+      ],
+      site: 'eggheadio',
+    },
+  }
+}
 
 export default async function Tip({params}: {params: {tipId: string}}) {
   const tip = await serverClient.tips.bySlug({slug: params.tipId})
@@ -7,13 +51,25 @@ export default async function Tip({params}: {params: {tipId: string}}) {
   const coursesFromTag = await serverClient.tips.relatedContent({
     slug: params.tipId,
   })
-
   const publishedTips =
     allTips.find((tipGroup) => tipGroup.state === 'published')?.tips ?? []
+  const muxPlaybackId = tip?.muxPlaybackId
+  const thumbnail = `https://image.mux.com/${muxPlaybackId}/thumbnail.png?width=720&height=405&fit_mode=preserve`
+
+  const jsonLd = {
+    name: tip.title,
+    image: thumbnail,
+    uploadDate: tip?._createdAt,
+    description: removeMarkdown(tip.description),
+  }
 
   return (
     tip && (
       <>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{__html: JSON.stringify(jsonLd)}}
+        />
         <TipTemplate
           tip={tip}
           tips={publishedTips}
