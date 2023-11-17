4d8fad4e8615a0947cca8102037a3fd9be9abfb7
Zac Jones <zacjones93@gmail.com>
2023-11-17 12:10:20 -0700

    refactor new page to react-hook-form

--- src/components/tips/tip-uploader.tsx
+++ src/components/tips/tip-uploader.tsx
@@ -1,14 +1,29 @@
 'use client'
-import {Dialog, Transition} from '@headlessui/react'
+import {z} from 'zod'
 import React from 'react'
 import cx from 'classnames'
 import toast from 'react-hot-toast'
 import {twMerge} from 'tailwind-merge'
-import {Field, Form, Formik, ErrorMessage} from 'formik'
+import {useRouter} from 'next/navigation'
 import VideoUploader from '@/components/upload/video-uploader'
-import useFileUploadReducer from '@/hooks/use-file-upload-reducer'
+import {useForm} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
 import {trpc} from '@/app/_trpc/client'
 import Spinner from '@/components/spinner'
+import useFileUploadReducer from '@/hooks/use-file-upload-reducer'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/ui/form'
+import {Textarea} from '@/ui/textarea'
+import {Input} from '@/ui/input'
+import {Button} from '@/ui/button'
+import slugify from 'slugify'
 
 function toChicagoTitleCase(slug: string): string {
   const minorWords: Set<string> = new Set([
@@ -55,15 +70,26 @@ const formatString = (str: string) => {
   return toChicagoTitleCase(removeFileExtension(str))
 }
 
+const NewTipFormSchema = z.object({
+  title: z.string().min(30).max(90),
+  description: z.string().min(2).max(240),
+  s3Url: z.string().min(4),
+})
+
 const TipCreationForm: React.FunctionComponent<
   React.PropsWithChildren<any>
 > = () => {
-  const [fileUploadState, dispatch] = useFileUploadReducer([])
+  const [s3Url, sets3Url] = React.useState<string>('')
+  const router = useRouter()
+  const form = useForm<z.infer<typeof NewTipFormSchema>>({
+    resolver: zodResolver(NewTipFormSchema),
+    defaultValues: {
+      title: '',
+      description: '',
+      s3Url: '',
+    },
+  })
   const trpcUtils = trpc.useUtils()
-
-  console.log(fileUploadState)
-
-  const isUploaded = fileUploadState.files[0]?.percent === 100
 
   const {mutate: createTip} = trpc.tips.create.useMutation({
     onSuccess: async () => {
@@ -71,8 +97,12 @@ const TipCreationForm: React.FunctionComponent<
         duration: 3000,
         icon: '✅',
       })
+      form.reset()
+      sets3Url('')
+      form.setValue('s3Url', '')
 
       await trpcUtils.tips.invalidate()
+      // router.refresh()
     },
     onError: (error) => {
       toast.error(
@@ -85,169 +115,186 @@ const TipCreationForm: React.FunctionComponent<
     },
   })
 
-  const uploadingFile = fileUploadState?.files[0]
+  const onSubmit = async (values: z.infer<typeof NewTipFormSchema>) => {
+    createTip({
+      ...values,
+      fileName: slugify(values.title.toLowerCase(), {remove: /[*+~.()'"!:@]/g}),
+    })
+  }
 
   return (
-    <Formik
-      initialValues={{
-        title: undefined,
-        body: undefined,
-      }}
-      onSubmit={async (values, {setErrors, resetForm}) => {
-        const {title, body} = values
-        if (!title || !fileUploadState?.files[0]?.signedUrl) {
-          setErrors({
-            title: 'Title is required',
-          })
-          return
-        }
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel className="text-lg font-bold">Title*</FormLabel>
+              <FormDescription className="mt-2 text-sm">
+                Include any relavent keywords for SEO.
+              </FormDescription>
+              <FormControl>
+                <Textarea placeholder="How do I..." {...field} />
+              </FormControl>
 
-        createTip({
-          title,
-          body,
-          awsFilename: fileUploadState.files[0].signedUrl,
-        })
-        resetForm()
-        // await createTipMutation.mutateAsync({
-        //   description: values.description,
-        //   sanityCourseId,
-        //   title: values.title,
-        //   awsFilename: fileUploadState.files[0].signedUrl as string,
-        // })
-      }}
-    >
-      {(props) => {
-        const {
-          values,
-          isSubmitting,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          setFieldValue,
-        } = props
-        return (
-          <Form className="grow w-fit" onSubmit={handleSubmit}>
-            <div className="w-full p-8 mt-4 space-y-4">
-              <label className="text-base font-semibold">
-                Enter your tip summary here
-                <p className="mt-2 text-sm opacity-50 text-muted-foreground">
-                  Your summary will be used to generate the title and draft body
-                  text based on the transcript of the video.
-                </p>
-                <Field
-                  as="textarea"
-                  rows={5}
-                  id="body"
-                  value={values.body}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  required
-                  disabled={isSubmitting}
-                  className="flex min-h-[60px] w-full rounded-md border border-input bg-transparent hover:border-gray-200 px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel className="text-lg font-bold">Description</FormLabel>
+              <FormDescription className="mt-2 text-sm">
+                This should describe what you teach in the tip and include any
+                relevant codeblocks and links.
+              </FormDescription>
+              <FormControl>
+                <Textarea
+                  placeholder="add as much detail as you'd like :)"
+                  {...field}
                 />
-              </label>
-              <label
-                className={twMerge(
-                  'flex justify-center h-32 px-4 transition-all bg-transparent border border-gray-400 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring',
-                  cx({
-                    hidden: uploadingFile,
-                  }),
-                )}
-              >
-                <span className="flex items-center space-x-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 dark:text-gray-200"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                    />
-                  </svg>
-                  <span className="font-medium dark:text-gray-200">
-                    Drop video files, or{' '}
-                    <span className="text-blue-600 underline">browse</span>
-                  </span>
-                </span>
-                <VideoUploader dispatch={dispatch} />
-              </label>
-              <label
-                className={twMerge(
-                  'flex justify-center h-32 px-4 transition-all bg-transparent border border-gray-400 rounded-md appearance-none hover:border-gray-200 focus:outline-none relative',
-                  cx({
-                    hidden: !uploadingFile,
-                  }),
-                )}
-              >
-                <div
-                  className="absolute top-0 h-full bg-blue-500 -left-0 -z-10 opacity-10"
-                  style={{
-                    width: `${fileUploadState.files[0]?.percent ?? 0}%`,
-                    transition: 'width 0.3s ease',
-                  }}
-                />
-                <span className="flex items-center space-x-2">
-                  {uploadingFile?.percent === 100 ? (
-                    <>
-                      ✅
-                      <span className="ml-2 text-base font-medium">
-                        {uploadingFile?.file?.name} uploaded
-                      </span>
-                      <button
-                        className="px-4 py-2 text-red-500 transition-all border border-transparent rounded hover:border hover:border-red-500"
-                        onClick={() => dispatch({type: 'cancel'})}
-                      >
-                        Cancel
-                      </button>
-                    </>
-                  ) : (
-                    <>
-                      <Spinner className="text-black dark:text-white" />
-                      <span className="text-base font-medium ">
-                        {uploadingFile?.file?.name} uploading...
-                      </span>
-                    </>
-                  )}
-                </span>
-              </label>
-              {uploadingFile?.percent === 100 && (
-                <>
-                  <label className="text-base font-semibold">
-                    Tip Title
-                    <Field
-                      type="input"
-                      id="title"
-                      placeholder={formatString(uploadingFile?.file?.name)}
-                      value={values.title}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      required
-                      disabled={isSubmitting}
-                      className="block w-full px-4 py-2 mb-4 leading-normal prose text-gray-900 bg-transparent border border-gray-400 rounded-md appearance-none resize-y hover:border-gray-200 focus:outline-none focus:shadow-outline dark:border-gray-700 dark:prose-dark md:prose-lg md:dark:prose-lg-dark dark:text-gray-100 dark:prose-a:text-blue-300 dark:hover:prose-a:text-blue-200 prose-a:text-blue-500 hover:prose-a-:text-blue-600"
-                    />
-                  </label>
-                  <ErrorMessage name="title" />
-                </>
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {s3Url.length === 0 ? (
+          <TipUploader
+            sets3Url={(s3Url: string) => {
+              form.setValue('s3Url', s3Url)
+              sets3Url(s3Url)
+            }}
+          />
+        ) : (
+          <div>
+            {s3Url}
+            <Button
+              onClick={() => {
+                sets3Url('')
+                form.setValue('s3Url', '')
+              }}
+            >
+              clear
+            </Button>
+          </div>
+        )}
+        <FormField
+          control={form.control}
+          name="s3Url"
+          render={({field}) => (
+            <FormItem>
+              <FormControl>
+                <Input type="hidden" {...field} />
+              </FormControl>
+              {form.formState.errors.s3Url && (
+                <p>{form.formState.errors.s3Url.message}</p>
               )}
-              <div className="mt-4 space-x-4">
-                <button
-                  className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 disabled:bg-transparent disabled:text-gray-400 disabled:hover:cursor-not-allowed"
-                  type="submit"
-                  disabled={!isUploaded}
-                >
-                  Submit
-                </button>
-              </div>
-            </div>
-          </Form>
-        )
-      }}
-    </Formik>
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="mt-2"
+          variant="default"
+          onClick={() => {
+            if (!s3Url) {
+              form.setError('s3Url', {
+                type: 'manual',
+                message: 'Please upload a video',
+              })
+            }
+          }}
+        >
+          Submit Tip
+        </Button>
+      </form>
+    </Form>
+  )
+}
+
+function TipUploader({sets3Url}: {sets3Url: (value: string) => void}) {
+  const [fileUploadState, dispatch] = useFileUploadReducer([])
+  const uploadingFile = fileUploadState?.files[0]
+  const isUploaded = fileUploadState.files[0]?.percent === 100
+
+  React.useEffect(() => {
+    if (fileUploadState.files[0]?.signedUrl) {
+      sets3Url(fileUploadState.files[0]?.signedUrl)
+    }
+  }, [fileUploadState.files[0]?.signedUrl, sets3Url])
+
+  return (
+    <>
+      <label
+        className={twMerge(
+          'flex justify-center h-32 px-4 transition-all bg-transparent border border-gray-400 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring',
+          cx({
+            hidden: uploadingFile,
+          }),
+        )}
+      >
+        <span className="flex items-center space-x-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6 dark:text-gray-200"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+            />
+          </svg>
+          <span className="font-medium dark:text-gray-200">
+            Drop video files, or{' '}
+            <span className="text-blue-600 underline">browse</span>
+          </span>
+        </span>
+        <VideoUploader dispatch={dispatch} />
+      </label>
+      <label
+        className={twMerge(
+          'flex justify-center h-32 px-4 transition-all bg-transparent border border-gray-400 rounded-md appearance-none hover:border-gray-200 focus:outline-none relative',
+          cx({
+            hidden: !uploadingFile,
+          }),
+        )}
+      >
+        <div
+          className="absolute top-0 h-full bg-blue-500 -left-0 -z-10 opacity-10"
+          style={{
+            width: `${fileUploadState.files[0]?.percent ?? 0}%`,
+            transition: 'width 0.3s ease',
+          }}
+        />
+        <span className="flex items-center space-x-2">
+          {isUploaded ? (
+            <>
+              ✅
+              <span className="ml-2 text-base font-medium">
+                {uploadingFile?.file?.name} uploaded
+              </span>
+            </>
+          ) : (
+            <>
+              <Spinner className="text-black dark:text-white" />
+              <span className="text-base font-medium ">
+                {uploadingFile?.file?.name} uploading...
+              </span>
+            </>
+          )}
+        </span>
+      </label>
+    </>
   )
 }
 
