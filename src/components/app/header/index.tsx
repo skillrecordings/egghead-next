c69a859d1ac05b1ca56a20f3ed8d1098d1635aa3
Zac Jones <zacjones93@gmail.com>
2024-02-26 11:19:58 -0700

    add mobile styles

--- src/components/app/header/index.tsx
+++ src/components/app/header/index.tsx
@@ -23,19 +23,72 @@ import {
   DocumentTextIcon,
   MenuIcon,
   XIcon,
+  PlayIcon,
   MapIcon,
+  ChevronUpIcon,
 } from '@heroicons/react/solid'
 import {
   BookOpenIcon,
-  PlayIcon,
+  PlayIcon as PlayIconOutline,
   MapIcon as MapIconOutline,
   DocumentTextIcon as DocumentTextIconOutline,
   CodeIcon,
 } from '@heroicons/react/outline'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@radix-ui/react-accordion'
 import SaleHeaderBanner from '@/components/cta/sale/header-banner'
 import {MazePattern} from './images'
 import {isMember} from '@/utils/is-member'
 import analytics from '@/utils/analytics'
+
+const browse = [
+  {
+    name: 'React',
+    href: '/q/react',
+    image:
+      'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/square_64/react.png',
+  },
+  {
+    name: 'Angular',
+    href: '/q/angular',
+    image:
+      'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1699480502/tags/angular.png',
+  },
+  {
+    name: 'Vue.js',
+    href: '/q/vue',
+    image:
+      'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/036/square_64/vue.png',
+  },
+  {
+    name: 'Next.js',
+    href: '/q/next',
+    image:
+      'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/074/thumb/Group_1%281%29.png',
+  },
+  {
+    name: 'TypeScript',
+    href: '/q/typescript',
+    image:
+      'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/377/square_64/typescriptlang.png',
+  },
+  {
+    name: 'JavaScript',
+    href: '/q/javascript',
+    image:
+      'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/205/square_64/javascriptlang.png',
+  },
+  {
+    name: 'CSS',
+    href: '/q/css',
+    image:
+      'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/175/square_64/csslang.png',
+  },
+]
 
 const Header: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
   const [isMounted, setIsMounted] = React.useState<boolean>(false)
@@ -113,33 +166,6 @@ const Header: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
     )
   }
 
-  const User = () => {
-    return (
-      <Link href={profileLink}>
-        <a
-          onClick={() =>
-            track('clicked account', {
-              location: 'header',
-            })
-          }
-          className="flex items-center h-full px-2 dark:hover:bg-white hover:bg-gray-50 dark:hover:bg-opacity-5"
-        >
-          <img
-            width={32}
-            height={32}
-            alt="avatar"
-            className="rounded-full"
-            src={viewer.avatar_url}
-          />
-          <span className="pl-1">
-            {viewer.name}
-            {viewer.is_pro && <sup>⭐️</sup>}
-          </span>
-        </a>
-      </Link>
-    )
-  }
-
   const Feedback = () => {
     return (
       <FeedbackInput
@@ -188,63 +214,146 @@ const Header: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
     )
   }
 
-  const MobileNavigation = () => {
+  const MobileTopicsList = () => {
+    const [open, setOpen] = React.useState<string>('')
+
     return (
-      <div className="relative z-20 dark:bg-gray-800 bg-gray-50 shadow-smooth h-screen">
+      <Accordion
+        type="single"
+        collapsible
+        value={open}
+        onValueChange={setOpen}
+        className="font-medium border-b border-gray-100 w-full "
+      >
+        <AccordionItem value="item-1">
+          <AccordionTrigger className="flex justify-between items-center px-5 py-4 w-full">
+            <div>Topics</div>
+            {open.includes(`item-1`) ? (
+              <ChevronUpIcon
+                className="relative h-8 w-8 transition group-radix-state-open:rotate-180"
+                aria-hidden="true"
+              />
+            ) : (
+              <ChevronDownIcon
+                className="relative h-8 w-8 transition"
+                aria-hidden="true"
+              />
+            )}
+          </AccordionTrigger>
+          <AccordionContent className="dark:bg-gray-700 bg-gray-100 w-full data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down">
+            <div className="py-2 flex flex-col w-full">
+              {browse.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  onClick={() => {
+                    analytics.events.activityInternalLinkClick(
+                      'curated topic page',
+                      'header browse',
+                      item.name,
+                      item.href,
+                    )
+                  }}
+                  className="flex items-center justify-start px-5 py-3 transition-all duration-150 ease-in-out rounded-sm hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth"
+                >
+                  <div className="flex items-center justify-center flex-shrink-0">
+                    <Image
+                      width={24}
+                      height={24}
+                      src={item.image}
+                      alt={item.name}
+                      quality={100}
+                      priority
+                    />
+                  </div>
+                  <span className="pl-2 font-medium text-gray-700 transition duration-150 ease-in-out dark:text-white hover:text-black">
+                    {item.name}
+                  </span>
+                </a>
+              ))}
+              <Link href="/topics">
+                <a
+                  onClick={() => {
+                    analytics.events.activityInternalLinkClick(
+                      'search all topics',
+                      'header mobile browse',
+                      'all topics',
+                      '/topics',
+                    )
+                  }}
+                  className="flex items-center w-full px-5 py-3 font-medium leading-tight transition duration-150 ease-in-out rounded-sm lg:col-span-2 lg:px-5 sm:px-3 group hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth justify-between border-t border-gray-100"
+                >
+                  Browse all topics{' '}
+                  <span
+                    className="inline-flex pl-1 transition group-hover:translate-x-1"
+                    aria-hidden="true"
+                  >
+                    &rarr;
+                  </span>
+                </a>
+              </Link>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    )
+  }
+
+  const MobileNavigation = ({isOpen}: {isOpen: boolean}) => {
+    React.useEffect(() => {
+      const originalStyle = window.getComputedStyle(document.body).overflow
+      if (isOpen) {
+        document.body.style.overflow = 'hidden'
+      } else {
+        document.body.style.overflow = originalStyle
+      }
+
+      // Cleanup function to reset overflow style
+      return () => {
+        document.body.style.overflow = originalStyle
+      }
+    }, [isOpen])
+
+    return (
+      <div className="relative z-20 dark:bg-gray-800 bg-gray-50 shadow-smooth h-screen w-full">
         <div className="flex w-full justify-center gap-4 py-4">
           <Link href="/q">
             <a className="dark:bg-gray-700 bg-gray-100 rounded p-2 flex flex-col items-center font-semibold min-w-[72px] dark:hover:bg-white hover:bg-gray-50 dark:hover:bg-opacity-5">
-              <CodeIcon
-                className="text-blue-400 self-center mb-1"
-                height={40}
-                width={40}
-              />
+              <CodeIcon className="mb-1" height={40} width={40} />
               Courses
             </a>
           </Link>
 
           <Link href="/tips">
             <a className="dark:bg-gray-700 bg-gray-100 rounded p-2 flex flex-col items-center font-semibold min-w-[72px] dark:hover:bg-white hover:bg-gray-50 dark:hover:bg-opacity-5">
-              <PlayIcon
-                className="text-blue-400 self-center mb-1"
-                height={40}
-                width={40}
-              />
+              <PlayIcon className="mb-1" height={40} width={40} />
               Tips
             </a>
           </Link>
 
           <Link href="/guides">
             <a className="dark:bg-gray-700 bg-gray-100 rounded p-2 flex flex-col items-center font-semibold min-w-[72px] dark:hover:bg-white hover:bg-gray-50 dark:hover:bg-opacity-5">
-              <MapIconOutline
-                className="text-blue-400 self-center mb-1"
-                height={40}
-                width={40}
-              />
+              <MapIcon className="mb-1" height={40} width={40} />
               Guides
             </a>
           </Link>
 
           <Link href="/blog">
             <a className="dark:bg-gray-700 bg-gray-100 rounded p-2 flex flex-col items-center font-semibold min-w-[72px] dark:hover:bg-white hover:bg-gray-50 dark:hover:bg-opacity-5">
-              <DocumentTextIconOutline
-                className="text-blue-400 self-center mb-1"
-                height={40}
-                width={40}
-              />
+              <DocumentTextIcon className="mb-1" height={40} width={40} />
               Articles
             </a>
           </Link>
         </div>
-        <div className="flex flex-col justify-between">
-          <div className="flex flex-col w-full px-3 pb-5 space-y-2 text-lg ">
-            <div className="px-5 py-2 font-medium">Topics</div>
+        <div className="flex flex-col h-full justify-between">
+          <div className="flex flex-col w-full pb-5 text-lg ">
+            <MobileTopicsList />
             {!isEmpty(viewer) && (
               <div className="bg-white dark:bg-gray-800">
-                <div className="flex flex-col w-full">
+                <div className="flex flex-col mt-4 space-y-4 w-full">
                   <FeedbackInput
                     user={viewer}
-                    className="flex items-center justify-start px-5 py-2 font-medium transition-all duration-150 ease-in-out rounded-sm hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth"
+                    className="flex items-center justify-start px-5 py-2 font-medium transition-all duration-150 ease-in-out rounded-sm hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth border-b border-gray-100"
                   >
                     Feedback
                   </FeedbackInput>
@@ -255,7 +364,7 @@ const Header: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
                           location: 'header',
                         })
                       }
-                      className="flex items-center justify-start px-5 py-2 font-medium transition-all duration-150 ease-in-out rounded-sm hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth"
+                      className="flex items-center justify-start px-5 py-2 font-medium transition-all duration-150 ease-in-out rounded-sm hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth border-b border-gray-100"
                     >
                       Membership
                     </a>
@@ -268,7 +377,7 @@ const Header: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
                           location: 'header',
                         })
                       }
-                      className="flex items-center justify-start px-5 py-2 font-medium transition-all duration-150 ease-in-out rounded-sm hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth"
+                      className="flex items-center justify-start px-5 py-2 font-medium transition-all duration-150 ease-in-out rounded-sm hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth border-b border-gray-100"
                     >
                       Profile
                     </a>
@@ -280,7 +389,7 @@ const Header: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
                           location: 'header',
                         })
                       }
-                      className="flex items-center justify-start px-5 py-2 font-medium transition-all duration-150 ease-in-out rounded-sm hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth"
+                      className="flex items-center justify-start px-5 py-2 font-medium transition-all duration-150 ease-in-out rounded-sm hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth border-b border-gray-100"
                     >
                       Activity
                     </a>
@@ -292,17 +401,16 @@ const Header: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
                           location: 'header',
                         })
                       }
-                      className="flex items-center justify-start px-5 py-2 font-medium transition-all duration-150 ease-in-out rounded-sm hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth"
+                      className="flex items-center justify-start px-5 py-2 font-medium transition-all duration-150 ease-in-out rounded-sm hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth border-b border-gray-100"
                     >
                       Bookmarks
                     </a>
                   </Link>
-                  <hr className="opacity-20" />
                 </div>
               </div>
             )}
           </div>
-          <div>
+          <div className="sticky bottom-2">
             <Link href={`/logout`}>
               <a
                 onClick={() =>
@@ -310,7 +418,7 @@ const Header: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
                     location: 'header',
                   })
                 }
-                className="flex items-center justify-start px-5 py-2 font-medium transition-all duration-150 ease-in-out rounded-sm hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth"
+                className="flex text-lg items-center justify-start px-5 py-2 transition-all duration-150 ease-in-out rounded-sm hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth"
               >
                 Log Out
               </a>
@@ -361,7 +469,7 @@ const Header: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
                           )
                         }
                       >
-                        <PlayIcon
+                        <PlayIconOutline
                           className=" text-blue-400 mr-1"
                           height={20}
                           width={20}
@@ -422,7 +530,7 @@ const Header: FunctionComponent<React.PropsWithChildren<unknown>> = () => {
             </div>
           </div>
         )}
-        {isOpen && <MobileNavigation />}
+        {isOpen && <MobileNavigation isOpen={isOpen} />}
       </nav>
     </>
   )
@@ -614,50 +722,6 @@ const ProfileDropdown: React.FC<React.PropsWithChildren<any>> = ({
 
 const Browse: React.FC<React.PropsWithChildren<any>> = ({viewer}) => {
   const {subscriber} = useCio()
-  const browse = [
-    {
-      name: 'React',
-      href: '/q/react',
-      image:
-        'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/square_64/react.png',
-    },
-    {
-      name: 'Angular',
-      href: '/q/angular',
-      image:
-        'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1699480502/tags/angular.png',
-    },
-    {
-      name: 'Vue.js',
-      href: '/q/vue',
-      image:
-        'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/036/square_64/vue.png',
-    },
-    {
-      name: 'Next.js',
-      href: '/q/next',
-      image:
-        'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/074/thumb/Group_1%281%29.png',
-    },
-    {
-      name: 'TypeScript',
-      href: '/q/typescript',
-      image:
-        'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/377/square_64/typescriptlang.png',
-    },
-    {
-      name: 'JavaScript',
-      href: '/q/javascript',
-      image:
-        'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/205/square_64/javascriptlang.png',
-    },
-    {
-      name: 'CSS',
-      href: '/q/css',
-      image:
-        'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/175/square_64/csslang.png',
-    },
-  ]
 
   return (
     <Popover>
@@ -751,26 +815,27 @@ const Browse: React.FC<React.PropsWithChildren<any>> = ({viewer}) => {
                         </a>
                       ))}
                     </div>
-                    <a
-                      href="/topics"
-                      onClick={() => {
-                        analytics.events.activityInternalLinkClick(
-                          'search all topics',
-                          'header browse',
-                          'all topics',
-                          '/topics',
-                        )
-                      }}
-                      className="flex items-center w-full px-5 py-3 font-medium leading-tight transition duration-150 ease-in-out rounded-sm lg:col-span-2 lg:px-5 sm:px-3 group hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth justify-between"
-                    >
-                      Browse all topics{' '}
-                      <span
-                        className="inline-flex pl-1 transition group-hover:translate-x-1 text-blue-300"
-                        aria-hidden="true"
+                    <Link href="/topics">
+                      <a
+                        onClick={() => {
+                          analytics.events.activityInternalLinkClick(
+                            'search all topics',
+                            'header browse',
+                            'all topics',
+                            '/topics',
+                          )
+                        }}
+                        className="flex items-center w-full px-5 py-3 font-medium leading-tight transition duration-150 ease-in-out rounded-sm lg:col-span-2 lg:px-5 sm:px-3 group hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:bg-opacity-40 hover:shadow-smooth justify-between"
                       >
-                        &rarr;
-                      </span>
-                    </a>
+                        Browse all topics{' '}
+                        <span
+                          className="inline-flex pl-1 transition group-hover:translate-x-1 text-blue-300"
+                          aria-hidden="true"
+                        >
+                          &rarr;
+                        </span>
+                      </a>
+                    </Link>
                   </div>
                 </div>
               )}
