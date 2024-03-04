2d43c640eaa3c0395ab50191cf25d4553d2ce075
Zac Jones <zacjones93@gmail.com>
2024-03-04 10:34:22 -0700

    feat: restructure headings on course and lesson to be more consistent

--- src/components/pages/lessons/overlay/email-capture-cta-overlay.tsx
+++ src/components/pages/lessons/overlay/email-capture-cta-overlay.tsx
@@ -34,23 +34,23 @@ const EmailCaptureCtaOverlay: FunctionComponent<
         >
           <div className="text-center">
             {collection ? (
-              <h1 className="text-xl md:text-2xl lg:text-xl xl:text-3xl leading-tighter tracking-tight font-light text-center max-w-xl mx-auto">
+              <h2 className="text-xl md:text-2xl lg:text-xl xl:text-3xl leading-tighter tracking-tight font-light text-center max-w-xl mx-auto">
                 Ready to finish{' '}
                 <strong className="font-bold">{collection.title}</strong>?
-              </h1>
+              </h2>
             ) : (
-              <h1 className="text-xl md:text-2xl lg:text-xl xl:text-3xl leading-tighter tracking-tight font-light text-center max-w-xl mx-auto">
+              <h2 className="text-xl md:text-2xl lg:text-xl xl:text-3xl leading-tighter tracking-tight font-light text-center max-w-xl mx-auto">
                 Unlock this free lesson!
-              </h1>
+              </h2>
             )}
 
-            <h2 className="pt-4 text-lg leading-tighter tracking-tight font-light text-center max-w-xl mx-auto">
+            <h3 className="pt-4 text-lg leading-tighter tracking-tight font-light text-center max-w-xl mx-auto">
               This lesson (and hundreds more!) by awesome instructors like{' '}
               <br />
               {lesson.instructor.full_name} are{' '}
               <strong className="font-bold">free to watch</strong> with your
               egghead account.
-            </h2>
+            </h3>
             <p className="font-normal text-blue-300 text-base sm:text-md lg:text-base xl:text-md mt-4">
               Enter your email to unlock this free lesson.
             </p>
