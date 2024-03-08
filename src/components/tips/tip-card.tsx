3331b0f923af3040e773b3d379eac4ad592e5234
Zac Jones <zacjones93@gmail.com>
2024-03-08 11:37:35 -0700

    update tips index to list

--- src/components/tips/tip-card.tsx
+++ src/components/tips/tip-card.tsx
@@ -1,73 +1,78 @@
 'use client'
 import {Tip} from '@/lib/tips'
 import {useRouter} from 'next/navigation'
-import React from 'react'
+import React, {FunctionComponent} from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import Icon from '@/components/icons/tip-icons'
 import Balancer from 'react-wrap-balancer'
 import {Card, CardContent, CardHeader} from './ui/card'
+import cx from 'classnames'
+import {twMerge} from 'tailwind-merge'
 import {trpc} from '@/app/_trpc/client'
+import isEmpty from 'lodash/isEmpty'
+import {convertTimeWithTitles} from '@/utils/time-utils'
+import ReactMarkdown from 'react-markdown'
+import analytics from '@/utils/analytics'
 
-const TipCard: React.FC<{tip: Tip}> = ({tip}) => {
+const TipCard: React.FC<{tip: Tip; i: number}> = ({tip, i}) => {
   const {title} = tip
   const muxPlaybackId = tip?.muxPlaybackId
   const thumbnail = `https://image.mux.com/${muxPlaybackId}/thumbnail.png?width=720&height=405&fit_mode=preserve`
   const router = useRouter()
-  const data = tip?.eggheadRailsLessonId
+  const {data} = tip?.eggheadRailsLessonId
     ? trpc.tips.loadTipProgress.useQuery({
         id: tip?.eggheadRailsLessonId,
       })
     : {data: {tipCompleted: false}}
-  const tipCompleted = data.data?.tipCompleted
+  const tipCompleted = data?.tipCompleted
+  const tipTopic = tip?.tags ? tip.tags[0]?.name : ''
+
   return (
-    <Card className="relative flex flex-col items-center overflow-hidden rounded-xl border border-transparent bg-white shadow-2xl shadow-gray-500/20 dark:border-gray-800 dark:bg-gray-400/5 dark:shadow-black/50">
-      <CardHeader className="relative flex aspect-video w-full flex-shrink-0 items-center justify-center border-b border-transparent dark:border-gray-800">
-        {/* <button
-          onClick={() => {
-            router
-              .push({
-                pathname: '/tips/[tip]',
-                query: {
-                  tip: tip.slug,
-                },
-              })
-              .then(() => {
-                const videoElement = document.getElementById(
-                  'mux-player',
-                ) as HTMLVideoElement
-                return videoElement?.play()
-              })
-          }}
-          className="group  flex items-center justify-center"
-        > */}
-        <Link
-          href={`/tips/${tip.slug}`}
-          className="group  flex items-center justify-center"
-        >
+    <Link
+      className={twMerge(
+        cx(
+          'group relative flex flex-row items-center overflow-hidden rounded border border-transparent bg-white hover:bg-gray-100 dark:bg-gray-400/5 hover:dark:bg-gray-800 transition duration-300 ease-in-out',
+          {
+            'dark:bg-gray-900 bg-gray-50': i % 2 === 0,
+          },
+        ),
+      )}
+      href={`/tips/${tip.slug}`}
+      onClick={() => {
+        analytics.events.activityInternalLinkClick(
+          'tip',
+          'tips index',
+          tipTopic,
+          tip.slug,
+        )
+      }}
+    >
+      <CardHeader className="relative hidden aspect-video w-full max-w-[100px] items-center justify-center sm:flex sm:max-w-[200px]">
+        <div className=" flex items-center justify-center">
           <span className="sr-only">
             Play {title}{' '}
             {tipCompleted && <span className="sr-only">(completed)</span>}
           </span>
-          <div className="flex w-full items-center overflow-hidden justify-center min-h-[8rem]">
+          <div className="flex w-full items-center justify-center">
             <Image
               src={thumbnail}
               alt=""
-              className="object-cover"
-              fill={true}
+              objectFit="cover"
+              layout="fill"
               aria-hidden="true"
+              className="brightness-90 transition duration-300 group-hover:brightness-75 dark:brightness-50"
             />
           </div>
           <div
-            className="absolute flex items-center justify-center rounded-full text-white opacity-100 drop-shadow-xl duration-500 ease-in-out group-hover:opacity-100 group-hover:scale-150"
+            className="absolute flex items-center justify-center rounded-full text-white opacity-100 drop-shadow-xl duration-500 ease-in-out group-hover:opacity-100"
             aria-hidden="true"
           >
             <Icon className="h-6 w-6" name="Playmark" />
           </div>
-        </Link>
-        {/* </button> */}
+        </div>
       </CardHeader>
-      <CardContent className="flex h-full w-full flex-col items-start p-6">
+      <CardContent className="flex h-full w-full flex-col px-6  py-4">
         <div
           className="absolute right-5 top-5 z-20 flex items-center gap-2"
           aria-hidden="true"
@@ -77,20 +82,45 @@ const TipCard: React.FC<{tip: Tip}> = ({tip}) => {
               Watched
             </div>
           )}
-          <div className="font-heading rounded-full bg-sky-400/20 px-2 py-1 text-xs font-bold uppercase leading-none tracking-wider text-sky-600 dark:text-sky-400">
-            Tip
-          </div>
         </div>
-        <h2 className="text-base font-semibold leading-tight sm:text-xl">
-          <Link
-            href={`/tips/${tip.slug}`}
-            className="inline-flex items-start gap-1 decoration-gray-300 hover:underline dark:decoration-gray-600"
-          >
-            {title} {tipCompleted && <span className="sr-only">(watched)</span>}
-          </Link>
+        <h2 className="text-lg font-semibold leading-tight sm:text-xl inline-flex items-start gap-1 decoration-gray-300 hover:underline dark:decoration-gray-600">
+          {title} {tipCompleted && <span className="sr-only">(watched)</span>}
         </h2>
+        <div className="flex flex-row flex-wrap gap-2 items-center  md:space-y-0 pt-2">
+          <div className="flex gap-2 items-center place-items-center">
+            {tip?.instructor && tip?.instructor.image && (
+              <div className="w-5 h-5 overflow-hidden flex-shrink-0 rounded-full lg:w-7 lg:h-7">
+                <Image
+                  aria-hidden
+                  src={tip.instructor.image}
+                  alt={tip.instructor.name}
+                  width={32}
+                  height={32}
+                  className="rounded-full"
+                />
+              </div>
+            )}
+            <span className="text-left dark:text-indigo-100 text-gray-700 lg:text-sm text-[0.75rem] opacity-80 leading-none">
+              <span className="sr-only">{tip.title} by </span>
+              {tip?.instructor?.name}
+            </span>
+          </div>
+          {tip?.duration && (
+            <span className="text-xs dark:text-indigo-100 text-gray-700">
+              {convertTimeWithTitles(Math.floor(tip.duration), {
+                showSeconds: true,
+              })}
+            </span>
+          )}
+          <TagList tags={tip.tags} />
+        </div>
+        {tip?.description && (
+          <ReactMarkdown className="prose dark:prose-dark dark:prose-dark-sm dark:prose-a:text-blue-300 prose-a:text-blue-500 prose-sm line-clamp-2 max-w-[50ch] pt-2">
+            {tip.description}
+          </ReactMarkdown>
+        )}
       </CardContent>
-    </Card>
+    </Link>
   )
 }
 
@@ -173,6 +203,57 @@ export const TipTeaser: React.FC<{tip: Tip}> = ({tip}) => {
         </Link>
       </h2>
     </article>
+  )
+}
+
+const TagList: FunctionComponent<
+  React.PropsWithChildren<{
+    tags: any
+    className?: string
+  }>
+> = ({
+  tags,
+  className = 'flex justify-center md:justify-start flex-wrap items-center',
+}) => {
+  return (
+    <>
+      {!isEmpty(tags) && (
+        <ul className={className}>
+          {tags.map((tag: any, index: number) => (
+            <li
+              key={index}
+              className="inline-flex items-center mr-4 mt-0 dark:text-indigo-100 text-gray-700 lg:text-sm text-[0.75rem] opacity-80 leading-none"
+            >
+              <Link
+                href={`/q/${tag.name}`}
+                onClick={() => {
+                  // track(`clicked view topic`, {
+                  //   course: courseSlug,
+                  //   topic: tag.name,
+                  // })
+                }}
+                className="inline-flex items-center hover:underline"
+              >
+                <Image
+                  src={tag.image_url}
+                  alt={tag.name}
+                  width={18}
+                  height={18}
+                  className="flex-shrink-0"
+                  quality={100}
+                />
+                <span className="ml-1">{tag.label}</span>
+              </Link>
+              {tag.version && (
+                <div className="ml-2 opacity-70">
+                  <code>{tag.version}</code>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   )
 }
 
