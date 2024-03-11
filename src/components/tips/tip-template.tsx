31c710d3c6a03face0615569e981e02f84f89b65
Zac Jones <zacjones93@gmail.com>
2024-03-11 10:07:09 -0600

    add suspense to tip template

--- src/components/tips/tip-template.tsx
+++ src/components/tips/tip-template.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {use} from 'react'
+import React, {use, Suspense} from 'react'
 import Link from 'next/link'
 import {get} from 'lodash'
 import Image from 'next/image'
@@ -53,136 +53,138 @@ const TipTemplate = ({
   return (
     <LessonProvider lesson={tip} module={module}>
       <VideoResourceProvider videoResourceId={tip?.videoResourceId || ''}>
-        <VideoProvider
-          muxPlayerRef={muxPlayerRef}
-          onEnded={handleVideoEnded}
-          exerciseSlug={tip.slug}
-          path="/tips"
-        >
-          <main className="w-full mx-auto" id="tip">
-            <div className="relative z-10 flex items-center justify-center">
-              <div className="flex flex-col w-full max-w-screen-xl">
-                <div className="flex items-center justify-center overflow-hidden shadow-gray-600/40 sm:shadow-2xl xl:rounded-b-md">
-                  <TipPlayer
-                    tip={tip}
-                    tips={filteredTips}
-                    coursesFromTag={coursesFromTag}
-                    ref={muxPlayerRef}
-                  />
+        <Suspense fallback={<Error />}>
+          <VideoProvider
+            muxPlayerRef={muxPlayerRef}
+            onEnded={handleVideoEnded}
+            exerciseSlug={tip.slug}
+            path="/tips"
+          >
+            <main className="w-full mx-auto" id="tip">
+              <div className="relative z-10 flex items-center justify-center">
+                <div className="flex flex-col w-full max-w-screen-xl">
+                  <div className="flex items-center justify-center overflow-hidden shadow-gray-600/40 sm:shadow-2xl xl:rounded-b-md">
+                    <TipPlayer
+                      tip={tip}
+                      tips={filteredTips}
+                      coursesFromTag={coursesFromTag}
+                      ref={muxPlayerRef}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <article className="relative z-10 px-5 pt-8 pb-16 border-l border-transparent sm:pt-10 xl:border-gray-800 xl:pt-10">
-              <div className="w-full max-w-screen-lg pb-5 mx-auto lg:px-5">
-                <div className="flex flex-col w-full grid-cols-5 gap-0 sm:gap-10 xl:grid">
-                  <div className="col-span-3">
-                    <div className="flex flex-col">
-                      {tip?.eggheadRailsLessonId && (
-                        <TipCompleted
-                          id={tip.eggheadRailsLessonId}
-                          className="block mb-2 lg:hidden"
-                        />
-                      )}
-                      <div className="flex lg:space-x-2 lg:-ml-7">
+              <article className="relative z-10 px-5 pt-8 pb-16 border-l border-transparent sm:pt-10 xl:border-gray-800 xl:pt-10">
+                <div className="w-full max-w-screen-lg pb-5 mx-auto lg:px-5">
+                  <div className="flex flex-col w-full grid-cols-5 gap-0 sm:gap-10 xl:grid">
+                    <div className="col-span-3">
+                      <div className="flex flex-col">
                         {tip?.eggheadRailsLessonId && (
                           <TipCompleted
                             id={tip.eggheadRailsLessonId}
-                            className="hidden lg:block "
+                            className="block mb-2 lg:hidden"
                           />
                         )}
-                        <h1 className="inline-flex items-baseline w-full max-w-2xl text-xl font-black leading-tighter lg:text-3xl">
-                          {tip.title}
-                        </h1>
-                      </div>
-                      <div className="flex items-center justify-between mt-4">
-                        {instructor && (
-                          <div className="flex items-center flex-shrink-0">
-                            <Link
-                              href={`/q/resources-by-${instructor.slug}`}
-                              onClick={() => {
-                                analytics.events.activityInternalLinkClick(
-                                  'instructor',
-                                  tip.slug,
-                                  'instructor',
-                                )
-                              }}
-                              className="flex mr-2 itemes-center"
-                            >
-                              {get(instructor, 'image') ? (
-                                <Image
-                                  width={48}
-                                  height={48}
-                                  src={instructor.image}
-                                  alt={instructor.name}
-                                  className="m-0 rounded-full"
-                                />
-                              ) : (
-                                <Eggo className="w-8 rounded-full" />
-                              )}
-                            </Link>
-                            <div className="flex flex-col">
-                              <span className="text-xs">Instructor</span>
-                              {get(instructor, 'name') && (
-                                <Link
-                                  href={`/q/resources-by-${instructor.slug}`}
-                                  onClick={() => {
-                                    analytics.events.activityInternalLinkClick(
-                                      'instructor',
-                                      tip.slug,
-                                      'instructor',
-                                    )
-                                  }}
-                                  className="font-semibold leading-tighter hover:underline"
-                                >
-                                  {instructor.name}
-                                </Link>
-                              )}
+                        <div className="flex lg:space-x-2 lg:-ml-7">
+                          {tip?.eggheadRailsLessonId && (
+                            <TipCompleted
+                              id={tip.eggheadRailsLessonId}
+                              className="hidden lg:block "
+                            />
+                          )}
+                          <h1 className="inline-flex items-baseline w-full max-w-2xl text-xl font-black leading-tighter lg:text-3xl">
+                            {tip.title}
+                          </h1>
+                        </div>
+                        <div className="flex items-center justify-between mt-4">
+                          {instructor && (
+                            <div className="flex items-center flex-shrink-0">
+                              <Link
+                                href={`/q/resources-by-${instructor.slug}`}
+                                onClick={() => {
+                                  analytics.events.activityInternalLinkClick(
+                                    'instructor',
+                                    tip.slug,
+                                    'instructor',
+                                  )
+                                }}
+                                className="flex mr-2 itemes-center"
+                              >
+                                {get(instructor, 'image') ? (
+                                  <Image
+                                    width={48}
+                                    height={48}
+                                    src={instructor.image}
+                                    alt={instructor.name}
+                                    className="m-0 rounded-full"
+                                  />
+                                ) : (
+                                  <Eggo className="w-8 rounded-full" />
+                                )}
+                              </Link>
+                              <div className="flex flex-col">
+                                <span className="text-xs">Instructor</span>
+                                {get(instructor, 'name') && (
+                                  <Link
+                                    href={`/q/resources-by-${instructor.slug}`}
+                                    onClick={() => {
+                                      analytics.events.activityInternalLinkClick(
+                                        'instructor',
+                                        tip.slug,
+                                        'instructor',
+                                      )
+                                    }}
+                                    className="font-semibold leading-tighter hover:underline"
+                                  >
+                                    {instructor.name}
+                                  </Link>
+                                )}
+                              </div>
                             </div>
-                          </div>
-                        )}
+                          )}
+                          {tags && (
+                            <div className="hidden lg:block">
+                              <Tags tags={tags} lessonSlug={tip.slug} />
+                            </div>
+                          )}
+                        </div>
                         {tags && (
-                          <div className="hidden lg:block">
+                          <div className="block mt-4 lg:hidden">
                             <Tags tags={tags} lessonSlug={tip.slug} />
                           </div>
                         )}
                       </div>
-                      {tags && (
-                        <div className="block mt-4 lg:hidden">
-                          <Tags tags={tags} lessonSlug={tip.slug} />
+                      {tip.body && (
+                        <>
+                          <div className="w-full pt-5 pb-5 prose max-w-none dark:prose-invert lg:prose-lg">
+                            <MarkdownCodeblock tip={tip.body} />
+                          </div>
+                          <hr className="bg-indigo-400" />
+                        </>
+                      )}
+                      {tip.transcript && tip.body && (
+                        <div className="w-full max-w-2xl pt-5">
+                          <VideoTranscript transcript={tip.transcript} />
                         </div>
                       )}
                     </div>
-                    {tip.body && (
-                      <>
-                        <div className="w-full pt-5 pb-5 prose max-w-none dark:prose-invert lg:prose-lg">
-                          <MarkdownCodeblock tip={tip.body} />
-                        </div>
-                        <hr className="bg-indigo-400" />
-                      </>
-                    )}
-                    {tip.transcript && tip.body && (
-                      <div className="w-full max-w-2xl pt-5">
-                        <VideoTranscript transcript={tip.transcript} />
-                      </div>
-                    )}
-                  </div>
-                  <div className="col-span-2">
-                    {/* TODO: might want to add summary? */}
-                    {tip.body && <RelatedTips currentTip={tip} tips={tips} />}
+                    <div className="col-span-2">
+                      {/* TODO: might want to add summary? */}
+                      {tip.body && <RelatedTips currentTip={tip} tips={tips} />}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex flex-col w-full max-w-screen-xl gap-10 mx-auto sm:pt-10 md:flex-row">
-                {tip.transcript && !tip.body && (
-                  <div className="w-full max-w-2xl pt-5">
-                    <VideoTranscript transcript={tip.transcript} />
-                  </div>
-                )}
-                {!tip.body && <RelatedTips currentTip={tip} tips={tips} />}
-              </div>
-            </article>
-          </main>
-        </VideoProvider>
+                <div className="flex flex-col w-full max-w-screen-xl gap-10 mx-auto sm:pt-10 md:flex-row">
+                  {tip.transcript && !tip.body && (
+                    <div className="w-full max-w-2xl pt-5">
+                      <VideoTranscript transcript={tip.transcript} />
+                    </div>
+                  )}
+                  {!tip.body && <RelatedTips currentTip={tip} tips={tips} />}
+                </div>
+              </article>
+            </main>
+          </VideoProvider>
+        </Suspense>
       </VideoResourceProvider>
     </LessonProvider>
   )
@@ -207,6 +209,29 @@ const TipCompleted = ({id, className}: {id: number; className?: string}) => {
           <CheckCircleIconOutline className="w-5 h-5 text-gray-300" />
         </span>
       )}
+    </div>
+  )
+}
+
+const Error = () => {
+  return (
+    <div className="flex items-center justify-center space-x-10 mb-60">
+      <div className="flex items-center">
+        <Image
+          src="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1610722602/next.egghead.io/pages/broken-eggo.png"
+          alt="broken eggo"
+          width={691}
+          height={493}
+        />
+        <div className="flex flex-col max-w-md">
+          <h1 className="font-extrabold text-9xl">Error</h1>
+          <p>
+            An error has occurred and it is entirely our fault. Can you please
+            click the feedback above and give us any details about what you were
+            trying to view. Sorry for the hassle :/
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
