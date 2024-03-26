cfea0cd9e5144bb318e81a25aee90b87f0f27188
Zac Jones <zacjones93@gmail.com>
2024-03-26 16:15:23 -0600

    update feedback form to post thu api route

---
+++ src/app/api/slack/feedback/route.ts
@@ -0,0 +1,49 @@
+import {NextRequest} from 'next/server'
+
+export async function POST(req: NextRequest) {
+  const body = await req.json()
+
+  const {feedback} = body
+
+  let response = await fetch(process.env.SLACK_FEEDBACK_URL ?? '', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      attachments: [
+        {
+          color: '#2EB67D',
+          blocks: [
+            {
+              type: 'divider',
+            },
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: feedback?.url,
+              },
+            },
+
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: `*[${feedback?.site}] ${feedback?.emotion} ${feedback?.category} feedback was sent by ${feedback?.user?.email}* \n\n ${feedback?.comment}`,
+              },
+            },
+          ],
+        },
+      ],
+    }),
+  })
+
+  if (response?.status !== 200) {
+    throw new Error('error occured, feedback not sent')
+  }
+
+  return new Response('feedback sent', {
+    status: 200,
+  })
+}
