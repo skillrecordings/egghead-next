0fcdfdafce8481f818e8d9d366075856ff53878b
Josh Branchaud <jbranchaud@gmail.com>
2024-05-16 12:44:29 -0500

    Use 7-character id from nanoid

--- src/module-builder/get-unique-filename.ts
+++ src/module-builder/get-unique-filename.ts
@@ -5,7 +5,7 @@ export const getUniqueFilename = (fullFilename: string) => {
   // filename with no extension
   const filename = fullFilename.replace(/\.[^/.]+$/, '')
   // remove stuff s3 hates
-  const scrubbed = `${filename}-${nanoid()}`
+  const scrubbed = `${filename}-${nanoid(7)}`
     .replace(/[^\w\d_\-.]+/gi, '')
     .toLowerCase()
   // rebuild it as a fresh new thing
