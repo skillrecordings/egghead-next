311c53290e32c88aad54caada4bc29a72e8615a3
Zac Jones <zacjones93@gmail.com>
2024-05-31 10:44:14 -0600

    fix learner stats (#1453)

--- src/components/pages/user/components/continue-learning.tsx
+++ src/components/pages/user/components/continue-learning.tsx
@@ -19,7 +19,7 @@ const ContinueLearning: React.FC<
       ) : continueLearningStatus === 'error' ? (
         <span>There was an error fetching stats</span>
       ) : isEmpty(continueLearningData) ? (
-        <span>You don't have any progress yet</span>
+        <span>You aren't learning anything right now</span>
       ) : (
         continueLearningData.map((item: any) => {
           return (
