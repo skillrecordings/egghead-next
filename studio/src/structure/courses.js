d9e8001e441ebe98b3625cde073171c2be0f6e44
Creeland <cree@provinsal.com>
2024-06-21 15:03:36 -0500

    feat: change icon to neat little hat

--- studio/src/structure/courses.js
+++ studio/src/structure/courses.js
@@ -1,9 +1,9 @@
-import {GoVideo} from 'react-icons/go'
+import {GoMortarBoard} from 'react-icons/go'
 
 const courses = (S) =>
   S.listItem()
     .title('Courses')
-    .icon(GoVideo)
+    .icon(GoMortarBoard)
     .child(S.documentTypeList('course').title('All Courses'))
 
 export default courses
