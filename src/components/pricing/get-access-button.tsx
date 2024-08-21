b872727fb3d30394fd93714f4ddcf991de46a4d3
Creeland <cree@provinsal.com>
2024-08-21 16:36:58 -0500

    feat: egg-310 add className to button

--- src/components/pricing/get-access-button.tsx
+++ src/components/pricing/get-access-button.tsx
@@ -1,19 +1,27 @@
 import * as React from 'react'
 import {LifetimePriceContext} from './lifetime-price-provider'
 import Spinner from '../spinner'
+import {twMerge} from 'tailwind-merge'
 
-const GetAccessButton: React.FunctionComponent = () => {
+const GetAccessButton: React.FunctionComponent = ({
+  className,
+}: {
+  className?: string
+}) => {
   const {loaderOn, pricesLoading, onClickCheckout} =
     React.useContext(LifetimePriceContext)
 
   return (
     <button
       disabled={pricesLoading}
-      className={`w-full px-5 py-2 h-[60px] flex justify-center items-center mt-8 font-semibold text-center text-white transition-all duration-300 ease-in-out bg-blue-600 rounded-md ${
-        pricesLoading
-          ? 'opacity-60 cursor-default'
-          : 'hover:bg-blue-700 hover:scale-105'
-      }`}
+      className={twMerge(
+        `w-full px-5 py-2 h-[60px] flex justify-center items-center mt-8 font-semibold text-center text-white transition-all duration-300 ease-in-out bg-blue-600 rounded-md ${
+          pricesLoading
+            ? 'opacity-60 cursor-default'
+            : 'hover:bg-blue-700 hover:scale-105'
+        }`,
+        className,
+      )}
       onClick={(event) => {
         event.preventDefault()
         onClickCheckout()
