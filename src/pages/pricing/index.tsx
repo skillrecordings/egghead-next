50402a6a6be2b600de7333f5349601429825bedf
Creeland <cree@provinsal.com>
2024-08-23 15:08:34 -0500

    feat: adjust best value pill size

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -131,7 +131,7 @@ const Pricing: FunctionComponent<React.PropsWithChildren<PricingProps>> & {
                 >
                   <div className="flex flex-col h-full">
                     <div className="flex flex-col items-center pt-12 pb-6">
-                      <div className="bg-gray-100 py-2 px-4 rounded-full font-semibold dark:bg-gray-700">
+                      <div className="bg-gray-100 py-1 px-3 text-xs rounded-full font-medium dark:bg-gray-700 mb-1">
                         BEST VALUE
                       </div>
                       <PlanTitle className="text-2xl">
