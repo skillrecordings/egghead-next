2423de328cc91a019f06d9ff659312f24ede9bf5
Zac Jones <zacjones93@gmail.com>
2024-09-05 15:20:56 -0600

    fix: remove controlbar from embed

--- src/app/(content)/lessons/[slug]/embed/_components/embed-player.tsx
+++ src/app/(content)/lessons/[slug]/embed/_components/embed-player.tsx
@@ -2,7 +2,10 @@
 import React from 'react'
 import {type VideoResource} from '@/types'
 import {HLSSource, Player} from '@skillrecordings/player'
+import {MinimalEmbedPlayer} from './minimal-embed-player'
+
 import './embed-lesson.css'
+import {GenericErrorBoundary} from '@/components/generic-error-boundary'
 
 export default function EmbedPlayer({
   initialLesson,
@@ -10,26 +13,29 @@ export default function EmbedPlayer({
   initialLesson: VideoResource
 }) {
   const fullscreenWrapperRef = React.useRef<HTMLDivElement>(null)
+
   return (
     <div className=" relative flex aspect-video h-full w-full items-center justify-center ">
       <div className="w-full h-full" ref={fullscreenWrapperRef}>
-        <React.Suspense fallback={<div>Loading player...</div>}>
-          <Player className="font-sans">
-            <HLSSource
-              key={initialLesson.hls_url}
-              src={initialLesson.hls_url}
-            />
-            {initialLesson.subtitles_url && (
-              <track
-                key={initialLesson.subtitles_url}
-                src={initialLesson.subtitles_url}
-                kind="subtitles"
-                srcLang="en"
-                label="English"
+        <GenericErrorBoundary>
+          <React.Suspense fallback={<div>Loading minimal player...</div>}>
+            <MinimalEmbedPlayer>
+              <HLSSource
+                key={initialLesson?.hls_url}
+                src={initialLesson?.hls_url}
               />
-            )}
-          </Player>
-        </React.Suspense>
+              {initialLesson?.subtitles_url && (
+                <track
+                  key={initialLesson?.subtitles_url ?? ''}
+                  src={initialLesson?.subtitles_url ?? ''}
+                  kind="subtitles"
+                  srcLang="en"
+                  label="English"
+                />
+              )}
+            </MinimalEmbedPlayer>
+          </React.Suspense>
+        </GenericErrorBoundary>
       </div>
     </div>
   )
