35f919cd8522ce86ea1ac7f6eb77feb3444dae4f
Creeland <cree@provinsal.com>
2024-09-13 14:46:25 -0500

    WIP: server component refactor

--- src/app/user/membership/page.tsx
+++ src/app/user/membership/page.tsx
@@ -1,8 +1,5 @@
-'use client'
 import React, {Suspense} from 'react'
 import {format} from 'date-fns'
-
-import {useAccount} from '@/hooks/use-account'
 import SubscriptionDetails from '@/components/pages/user/components/subscription-details'
 import {ItemWrapper} from '@/components/pages/user/components/widget-wrapper'
 import PricingProvider from '@/components/pricing/pricing-provider'
@@ -10,19 +7,153 @@ import PricingCard from '@/components/pricing/pricing-card'
 import Invoices from '@/components/invoices'
 import Spinner from '@/components/spinner'
 
-const Membership = () => {
-  const {
-    account,
-    accountLoading,
+import fetchEggheadUser from '@/api/egghead/users/from-token'
+import {cookies} from 'next/headers'
+import {ACCESS_TOKEN_KEY} from '@/utils/auth'
+import {isPast} from 'date-fns'
+
+/*
+
+  user: {
+    name: 'David',
+    id: 37992145,
+    lessons_completed: 0,
+    series_completed: 0,
+    email: 'ninjasoards@gmail.com',
+    is_unverified_email: false,
+    full_name: 'David',
+    discord_id: null,
+    avatar_url: '//gravatar.com/avatar/8776cdc5b935953c00c3cb376f4c687b.png?s=128&d=mp',
+    contact_id: 'co_a195823f6b4471643ceb',
+    roles: [ 'user', 'pro', 'account_member', 'account_owner' ],
+    accounts: [ [Object] ],
+    can_comment: true,
+    providers: [],
+    created_at: 1554926781,
+    opted_out: true,
+    stripeKey: 'pk_test_OOTSSmhjHw8HQBJKPe51fst1',
+    is_pro: true,
+    is_instructor: false,
+    is_cancelled: null,
+    is_community_member: false,
+    user_profile_url: 'http://app.egghead.af:5000/users/37992145',
+    username: 'member',
+    user_url: 'http://app.egghead.af:5000/api/v1/users/37992145',
+    watch_later_bookmarks_url: 'http://app.egghead.af:5000/api/v1/playlists/watch-later-a890ee6486178056',
+    lessons_url: 'http://app.egghead.af:5000/api/v1/lessons',
+    series_url: 'http://app.egghead.af:5000/api/v1/series',
+    answer_quiz_url: 'http://app.egghead.af:5000/api/v1/users/37992145/answer_quiz',
+    recommended_lessons_url: 'http://app.egghead.af:5000/api/v1/users/37992145/lessons?recommended=true&skip_series=true',
+    recommended_series_url: 'http://app.egghead.af:5000/api/v1/users/37992145/series?load_lessons=false&recommended=true',
+    in_progress_series_url: 'http://app.egghead.af:5000/api/v1/users/37992145/series?in_progress=true&load_lessons=false',
+    timezone: 'Central Time (US & Canada)',
+    tags: [],
+    subscription: { stripe_customer_id: 'cus_QhkrzPG1RtnY8l', active: true },
+    purchased: [],
+    deals: [],
+    playlists_url: 'http://app.egghead.af:5000/api/v1/users/37992145/playlists',
+    watch_later_url: 'http://app.egghead.af:5000/watch_later',
+    collections_http_url: 'http://app.egghead.af:5000/playlists',
+    edit_user_url: 'http://app.egghead.af:5000/users/edit',
+    edit_user_password_url: 'http://app.egghead.af:5000/users/edit_password',
+    membership_url: 'http://app.egghead.af:5000/membership',
+    favorites: [],
+    favorite_topic: null,
+    managed_subscription_url: 'http://egghead.af:3000/team',
+    api_v1_responses_url: 'http://app.egghead.af:5000/api/v1/responses'
+  }
+*/
+
+function validateTeamSubscription(subscription: any, user: any) {
+  const isActiveAccountMember = user.roles.includes('account_member')
+
+  const isAccountOwner = user.roles.includes('account_owner')
+
+  const isTeamAccountOwner =
+    user.roles.includes('account_owner') && user.accounts.length > 1
+
+  return {
+    isTeamMember: isActiveAccountMember,
+    accountOwner: isAccountOwner,
+    isTeamAccountOwner,
+  }
+}
+function validateGiftSubscription(subscription: any) {
+  const giftExpiration = subscription?.current_period_end
+  const isGiftExpired = isPast(new Date(giftExpiration))
+  const isGiftMembership =
+    subscription?.type === 'gift' &&
+    (subscription?.status === 'active' ||
+      subscription?.status === 'past_due' ||
+      subscription?.status === 'trialing') &&
+    !isGiftExpired
+
+  return {
+    isGiftMembership,
+    giftExpiration,
+  }
+}
+
+async function getUserMembership(accessToken: string) {
+  const user = (await fetchEggheadUser(accessToken ?? '')) as any
+
+  const {isGiftMembership, giftExpiration} = validateGiftSubscription(
+    user.subscription,
+  )
+
+  const {isTeamMember, accountOwner} = validateTeamSubscription(
+    user.subscription,
+    user,
+  )
+
+  // data is from object above
+  return {
+    hasStripeAccount: user.accounts.some(
+      (account: any) => account.stripe_customer_id,
+    ),
+    subscription: user.subscription,
+
+    isLifetimeMember: user?.roles?.includes('lifetime_subscriber'),
+    isInstructor: user.is_instructor,
+    isDisabled: user.is_cancelled,
     isGiftMembership,
     giftExpiration,
     isTeamMember,
-    hasStripeAccount,
     accountOwner,
-    isDisabled,
-    isInstructor,
+  }
+}
+
+async function Membership() {
+  // const {
+  //   account,
+  //   accountLoading,
+  //   isGiftMembership,
+  //   giftExpiration,
+  //   isTeamMember,
+  //   hasStripeAccount,
+  //   accountOwner,
+  //   isDisabled,
+  //   isInstructor,
+  //   isLifetimeMember,
+  // } = useAccount()
+
+  const cookieStore = cookies()
+  const accessToken = cookieStore.get(ACCESS_TOKEN_KEY)?.value
+
+  const {
+    hasStripeAccount,
     isLifetimeMember,
-  } = useAccount()
+    isInstructor,
+    isDisabled,
+    isGiftMembership,
+    giftExpiration,
+    isTeamMember,
+    accountOwner,
+  } = await getUserMembership(accessToken ?? '')
+
+  // subscription={account.subscriptions[0]}
+  // stripeCustomerId={account.stripe_customer_id}
+  // slug={account.slug}
 
   switch (true) {
     case isLifetimeMember:
@@ -90,12 +221,6 @@ const Membership = () => {
             </div>
           </div>
         </>
-      )
-    case accountLoading:
-      return (
-        <div className="relative flex justify-center w-full">
-          <Spinner className="w-6 h-6 text-gray-600" />
-        </div>
       )
     case isGiftMembership:
       return (
