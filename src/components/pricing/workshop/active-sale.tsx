b422332edb5acaa581c4358ca041d76b2cdd3bb6
Zac Jones <zacjones93@gmail.com>
2025-03-03 16:36:21 -0700

    fix: add padding

--- src/components/pricing/workshop/active-sale.tsx
+++ src/components/pricing/workshop/active-sale.tsx
@@ -66,7 +66,7 @@ const ActiveSale = ({
   }
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center py-16">
       <div className="relative p-2 bg-gray-100 rounded-md shadow-lg dark:bg-gray-800 dark:shadow-none">
         <div className="relative z-10 flex flex-col items-center max-w-sm px-5 py-5 text-gray-900 bg-white rounded-sm dark:text-white dark:bg-gray-900 sm:px-8 sm:py-12">
           <PlanTitle className="text-center">{title}</PlanTitle>
