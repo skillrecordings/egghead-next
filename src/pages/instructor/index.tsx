a925a88ecd8fce20bc2b49cc5326a936057efddc
Zac Jones <zacjones93@gmail.com>
2025-04-09 14:48:49 -0600

    feat: display workshop sales banner (#1521)

--- src/pages/instructor/index.tsx
+++ src/pages/instructor/index.tsx
@@ -4,7 +4,7 @@ import {InstructorTabContent} from '@/components/pages/user'
 import {GetServerSideProps} from 'next/types'
 import {getAbilityFromToken} from '@/server/ability'
 import {ACCESS_TOKEN_KEY} from '@/utils/auth'
-import {getDraftFeatureFlag} from '@/lib/feature-flags'
+import {getFeatureFlag} from '@/lib/feature-flags'
 import {loadCurrentViewerRoles} from '@/lib/viewer'
 
 const Instructor = ({canViewDraftCourses}: {canViewDraftCourses: boolean}) => {
@@ -57,7 +57,7 @@ Instructor.getLayout = function getLayout(Page: any, pageProps: any) {
 export const getServerSideProps: GetServerSideProps = async function ({req}) {
   const ability = await getAbilityFromToken(req.cookies[ACCESS_TOKEN_KEY])
   const roles = await loadCurrentViewerRoles(req.cookies[ACCESS_TOKEN_KEY])
-  const draftCourseRole = await getDraftFeatureFlag(
+  const draftCourseRole = await getFeatureFlag(
     'featureFlagDraftCourse',
     'allowedRoles',
   )
