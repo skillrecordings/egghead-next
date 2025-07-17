c0ad2246ccd2a542a9e63a30a526a1a1c86e398a
Zac Jones <zacjones93@gmail.com>
2025-07-17 15:04:02 -0600

    fix: update lightmode hero styles

--- src/components/workshop/claude-code/hero-logo.tsx
+++ src/components/workshop/claude-code/hero-logo.tsx
@@ -1,13 +1,13 @@
 export default function Logo() {
   return (
     <div
-      className="mb-5 sm:mx-0 sm:px-0 px-2 font-mono text-[#2D3748] dark:text-[#ce6246]  leading-tight w-full"
+      className="mb-5 sm:mx-0 sm:px-0 px-2 font-mono text-[#24292E] dark:text-[#ce6246]  leading-tight w-full"
       role="img"
       aria-label="Claude Code Workshop ASCII Art Logo"
       aria-hidden="true"
     >
-      <div className="flex flex-col gap-2 bg-white dark:bg-[#111110] shadow-[0_0_20px_rgba(255,255,255,0.5)] dark:shadow-black rounded-lg justify-center items-center w-full ">
-        <div className="w-full bg-[#4A5568] dark:bg-[#2f2f2c] h-8 flex items-center pl-2 gap-1 rounded-t-lg">
+      <div className="flex flex-col gap-2 border border-gray-900 dark:border-none bg-white dark:bg-[#111110] shadow-[0_0_20px_rgba(255,255,255,0.5)] dark:shadow-black rounded-lg justify-center items-center w-full ">
+        <div className="w-full bg-gray-900 dark:bg-[#2f2f2c] h-8 flex items-center pl-2 gap-1 rounded-t-lg">
           <div className="w-2 h-2 rounded-full bg-red-400" />
           <div className="w-2 h-2 rounded-full bg-yellow-400" />
           <div className="w-2 h-2 rounded-full bg-green-400" />
