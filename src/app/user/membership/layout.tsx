9c9139e62c894d02b0874a15da94115496018fce
Zac Jones <zacjones93@gmail.com>
2025-08-11 12:13:51 -0600

    Display workshop transactions on user membership page (#1532)
    * feat: add workshop transactions to membership page
    
    * fix: remove loading ui for workshop transactions
    
    * refactor: search for user sessions then fallback to most recent

--- src/app/user/membership/layout.tsx
+++ src/app/user/membership/layout.tsx
@@ -1,4 +1,7 @@
+import {Suspense} from 'react'
 import Invoices from '@/components/invoices'
+import WorkshopTransactions from '@/components/workshop-transactions'
+import Spinner from '@/components/spinner'
 
 export default async function MembershipPageLayout({
   children,
@@ -14,6 +17,20 @@ export default async function MembershipPageLayout({
         {children}
       </div>
       <Invoices headingAs="h3" />
+      <Suspense
+        fallback={
+          <div className="mt-16">
+            <h3 className="text-lg font-medium md:font-normal md:text-xl leading-none mb-4">
+              Workshop Transactions
+            </h3>
+            <div className="flex justify-center">
+              <Spinner className="w-6 h-6 text-gray-600" />
+            </div>
+          </div>
+        }
+      >
+        <WorkshopTransactions headingAs="h3" />
+      </Suspense>
     </div>
   )
 }
