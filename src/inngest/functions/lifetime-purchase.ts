99d1041b9a95cca62e948de9d2322174ed8c94cb
Zac Jones <zacjones93@gmail.com>
2025-12-15 10:33:42 -0700

    refactor: restore and clean up customer lookup and subscription handling in lifetime purchase function

--- src/inngest/functions/lifetime-purchase.ts
+++ src/inngest/functions/lifetime-purchase.ts
@@ -30,73 +30,69 @@ export const lifetimePurchase = inngest.createFunction(
   async ({event, step}) => {
     //   console.log(event.data)
 
-    //   // Look up the user
-    //   const customer = await step.run('get stripe customer', async () => {
-    //     const customer = await stripe.customers.retrieve(event.data.customerId)
+    // Look up the user
+    const customer = await step.run('get stripe customer', async () => {
+      const customer = await stripe.customers.retrieve(event.data.customerId)
 
-    //     if (!customer) {
-    //       throw new Error('Customer not found')
-    //     }
+      if (!customer) {
+        throw new Error('Customer not found')
+      }
 
-    //     if ('deleted' in customer) {
-    //       throw new NonRetriableError('Customer is deleted')
-    //     }
+      if ('deleted' in customer) {
+        throw new NonRetriableError('Customer is deleted')
+      }
 
-    //     return customer
-    //   })
+      return customer
+    })
 
-    //   const email = customer.email
+    const email = customer.email
 
-    //   // Find or create the user with Lifetime Subscription
-    //   const {user, active_subscription_stripe_ids, is_new_user} = await step.run(
-    //     'create user with lifetime subscription',
-    //     async () => {
-    //       // is this endpoint idempotent? Because inngest will run it multiple times.
-    //       const response = await eggAxios.post('/api/v1/lifetime_subscriptions', {
-    //         email,
-    //         site: 'egghead.io',
-    //         stripe_charge_id: event.data.stripeChargeIdentifier,
-    //         stripe_customer_id: event.data.customerId,
-    //       })
+    // Find or create the user with Lifetime Subscription
+    const {user, active_subscription_stripe_ids, is_new_user} = await step.run(
+      'create user with lifetime subscription',
+      async () => {
+        // is this endpoint idempotent? Because inngest will run it multiple times.
+        const response = await eggAxios.post('/api/v1/lifetime_subscriptions', {
+          email,
+          site: 'egghead.io',
+          stripe_charge_id: event.data.stripeChargeIdentifier,
+          stripe_customer_id: event.data.customerId,
+        })
 
-    //       const ResponseSchema = z.object({
-    //         user: z.object({
-    //           id: z.number(),
-    //           email: z.string(),
-    //         }),
-    //         active_subscription_stripe_ids: z.array(z.string()),
-    //         is_new_user: z.boolean(),
-    //       })
+        const ResponseSchema = z.object({
+          user: z.object({
+            id: z.number(),
+            email: z.string(),
+          }),
+          active_subscription_stripe_ids: z.array(z.string()),
+          is_new_user: z.boolean(),
+        })
 
-    //       return ResponseSchema.parse(response.data)
-    //     },
-    //   )
+        return ResponseSchema.parse(response.data)
+      },
+    )
 
-    //   // Cancel any active subscriptions for the user
-    //   const cancelledSubscriptions = await step.run(
-    //     'cancel active subscriptions',
-    //     async () => {
-    //       const cancelledSubscriptions = []
+    // Cancel any active subscriptions for the user
+    const cancelledSubscriptions = await step.run(
+      'cancel active subscriptions',
+      async () => {
+        const cancelledSubscriptions = []
 
-    //       for (const subscriptionId of active_subscription_stripe_ids) {
-    //         const subscription = await stripe.subscriptions.cancel(
-    //           subscriptionId,
-    //           {
-    //             prorate: true,
-    //           },
-    //         )
+        for (const subscriptionId of active_subscription_stripe_ids) {
+          const subscription = await stripe.subscriptions.cancel(
+            subscriptionId,
+            {
+              prorate: true,
+            },
+          )
 
-    //         cancelledSubscriptions.push(subscription)
-    //       }
+          cancelledSubscriptions.push(subscription)
+        }
 
-    //       return cancelledSubscriptions
-    //     },
-    //   )
+        return cancelledSubscriptions
+      },
+    )
 
-    let customer = {
-      email: 'zacjones93@gmail.com',
-      name: 'Zac Jones',
-    }
     // Send promo email with bonus workshop recordings
     await step.run('send promo email', async () => {
       if (!customer.email) {
