0bff3fffd8b8a5a2fd71b2a988f0c48c84818cc5
Nicoll Guarnizo <nicoll.g4@gmail.com>
2026-04-01 17:03:20 -0500

    Remove unnecessary check

--- src/server/routers/feature-flag.ts
+++ src/server/routers/feature-flag.ts
@@ -96,10 +96,6 @@ export const featureFlagRouter = router({
     .query(async ({input, ctx}) => {
       const workshop = await getFeatureFlag(input.flag, 'workshop')
 
-      if (!workshop || typeof workshop === 'boolean') {
-        return null
-      }
-
       const parsedWorkshop = LiveWorkshopSchema.parse(workshop)
       return parsedWorkshop
     }),
