ae86cafb8ab99f3dbfc04a793ab151f7c36cb041
Zac Jones <zacjones93@gmail.com>
2026-05-20 08:29:59 -0600

    EGG-433 remove Sanity instructor merge

--- src/components/search/instructors/kadi-kraman/index.tsx
+++ src/components/search/instructors/kadi-kraman/index.tsx
@@ -1,9 +1,8 @@
-import groq from 'groq'
-
 import CtaCard from '@/components/search/components/cta-card'
 import SearchInstructorEssential from '../instructor-essential'
 
 const SearchKadiKraman = ({instructor}: {instructor: any}) => {
+  instructor = {...instructor, ...curatedInstructorData}
   const {courses} = instructor
 
   if (!courses) {
@@ -34,21 +33,25 @@ const SearchKadiKraman = ({instructor}: {instructor: any}) => {
 }
 export default SearchKadiKraman
 
-export const KadiKramanQuery = groq`
-*[_type == 'resource' && slug.current == 'kadi-kraman-landing-page'][0]{
-  title,
-  'courses': resources[slug.current == 'instructor-landing-page-featured-courses'][0]{
-    resources[]->{
-       title,
-       'description': summary,
-       path,
-       byline,
-       image,
-       'background': images[label == 'feature-card-background'][0].url,
-       'instructor': collaborators[@->.role == 'instructor'][0]->{
-         'name': person->.name
-       },
-     }
-    },
- }
-`
+const curatedInstructorData = {
+  courses: {
+    resources: [
+      {
+        background:
+          'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1636483593/next.egghead.io/resources/building-an-customisable-animated-skeleton-loader-in-react-native-51f6231d/feature-card-background--Skeleton-react-native.png',
+        byline: 'Kadi Karmann・29m・Course',
+        description:
+          "In 30 mins you'll get a quick intro to 3 powerful RN libraries linear gradient, masked view, reanimated 2, and then use them to build a customizable skeleton loader for your app!",
+        image:
+          'https://d2eip9sf3oo6c2.cloudfront.net/playlists/square_covers/000/504/652/full/React2_Lv1_Base_424_2x.png',
+        instructor: {
+          name: 'Kadi Kraman',
+        },
+        path: '/courses/building-a-customizable-animated-skeleton-loader-in-react-native-51f6231d',
+        title:
+          'Building a Customizable Animated Skeleton Loader in React Native',
+      },
+    ],
+  },
+  title: 'Kadi Kraman Landing Page',
+} as Record<string, any>
--- src/components/search/instructors/lazar-nikolov/index.tsx
+++ src/components/search/instructors/lazar-nikolov/index.tsx
@@ -1,9 +1,8 @@
-import groq from 'groq'
-
 import CtaCard from '@/components/search/components/cta-card'
 import SearchInstructorEssential from '../instructor-essential'
 
 const SearchLazarNikolov = ({instructor}: {instructor: any}) => {
+  instructor = {...instructor, ...curatedInstructorData}
   const {courses} = instructor
 
   if (!courses) {
@@ -34,21 +33,24 @@ const SearchLazarNikolov = ({instructor}: {instructor: any}) => {
 }
 export default SearchLazarNikolov
 
-export const LazarNikolovQuery = groq`
-*[_type == 'resource' && slug.current == 'lazar-nikolov-landing-page'][0]{
-  title,
-  'courses': resources[slug.current == 'instructor-landing-page-featured-courses'][0]{
-    resources[]->{
-       title,
-       'description': summary,
-       path,
-       byline,
-       image,
-       'background': images[label == 'feature-card-background'][0].url,
-       'instructor': collaborators[@->.role == 'instructor'][0]->{
-         'name': person->.name
-       },
-     }
-    },
- }
-`
+const curatedInstructorData = {
+  courses: {
+    resources: [
+      {
+        background:
+          'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1633112015/next.egghead.io/resources/build-a-modern-user-interface-with-chakra-ui-fac68106/feature-card-background--modern-interface-chakra-ui.png',
+        byline: 'Lazar Nkolov・42m・Course',
+        description:
+          'Lazar will show you how to build well-designed user interfaces without spending a lot of time (if even) styling every component and element with Chakra UI.',
+        image:
+          'https://d2eip9sf3oo6c2.cloudfront.net/playlists/square_covers/000/505/265/full/chakra-lv1.png',
+        instructor: {
+          name: 'Lazar Nikolov',
+        },
+        path: '/courses/build-a-modern-user-interface-with-chakra-ui-fac68106',
+        title: 'Build a Modern User Interface with Chakra UI',
+      },
+    ],
+  },
+  title: 'Lazar Nikolov Landing Page',
+} as Record<string, any>
